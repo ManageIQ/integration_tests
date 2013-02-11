ad03596fdafcb789db5e8f5eae36a5fae1f34554
Brad P. Crochet <brad@redhat.com>
2013-02-11 16:58:38 -0500

    First passing test

---
+++ tests/test_login.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import pytest
+import time
+from unittestzero import Assert
+from pages.login_page import LoginPage
+
+@pytest.mark.nondestructive
+class TestLogin:
+    def test_login(self, mozwebqa):
+        login_pg = LoginPage(mozwebqa)
+        login_pg.go_to_login_page()
+        home_pg = login_pg.login()
+        Assert.true(home_pg.is_logged_in, "Could not determine if logged in")
