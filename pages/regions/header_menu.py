fc31b6590ce0fb27987ba61bc760c603da2e0c64
Brad P. Crochet <brad@redhat.com>
2013-02-28 14:44:34 -0500

    Fix workaround when running other than firefox

--- pages/regions/header_menu.py
+++ pages/regions/header_menu.py
@@ -60,7 +60,7 @@ class HeaderMenu(Page):
         element = self._root_element.find_element(*self._name_locator)
         # Workaround for Firefox
         chain = ActionChains(self.selenium).move_to_element(element)
-        if self.selenium.firefox_profile:
+        if hasattr(self.selenium, "firefox_profile"):
             chain.move_by_offset(0, element.size['height'])
         chain.perform()
 
