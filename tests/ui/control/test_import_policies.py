df9ab555f8fed46398a036ff7e171686f4e376b3
aweiteka <aweiteka@redhat.com>
2013-03-27 11:46:29 -0400

    basic control tests

---
+++ tests/ui/control/test_import_policies.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import time
+from unittestzero import Assert
+
+@pytest.fixture(scope="module",
+                params=["policies.yaml"])
+def import_policy_file(request):
+    return request.param
+
+@pytest.mark.nondestructive
+#@pytest.mark.usefixtures("maximized")
+class TestControl:
+    def test_import_policies(self, mozwebqa, home_page_logged_in, import_policy_file):
+        home_pg = home_page_logged_in
+        ms_pg = home_pg.header.site_navigation_menu("Control").sub_navigation_menu("Import / Export").click()
+        Assert.true(ms_pg.is_the_current_page)
+        ms_pg = msd_pg.import_policies(import_policy_file)
+        Assert.true(ms_pg.flash.message == "Press commit to Import")
+        ms_pg = msd_pg.click_on_commit()
+        Assert.true(ms_pg.flash.message == "Import file was uploaded successfully")
+
