5af5e3e20eb139d77bd737175150cf0f2d48217d
aweiteka <aweiteka@redhat.com>
2013-04-15 15:30:52 -0400

    support for add, edit, delete local groups. included unit test

--- pages/configuration_subpages/access_control.py
+++ pages/configuration_subpages/access_control.py
@@ -6,7 +6,9 @@ from selenium.webdriver.support.select import Select
 class AccessControl(Base):
     _page_title = 'CloudForms Management Engine: Configuration'
     _roles_button = (By.CSS_SELECTOR, "div[title='View Roles']")
+    _groups_button = (By.CSS_SELECTOR, "div[title='View Groups']")
 
+    # ROLES
     def click_on_roles(self):
         self.selenium.find_element(*self._roles_button).click()
         self._wait_for_results_refresh()
@@ -79,3 +81,87 @@ class AccessControl(Base):
         @property
         def role_name(self):
             return self.selenium.find_element(*self._role_name_label).text.strip()
+
+    # GROUPS
+    def click_on_groups(self):
+        self.selenium.find_element(*self._groups_button).click()
+        self._wait_for_results_refresh()
+        return AccessControl.Groups(self.testsetup)
+
+    class Groups(Base):
+        _page_title = 'CloudForms Management Engine: Configuration'
+        _add_group_button = (By.CSS_SELECTOR, "a[title='Add a new Group']")
+
+        def click_on_add_new(self):
+            self.selenium.find_element(*self._add_group_button).click()
+            self._wait_for_results_refresh()
+            return AccessControl.NewGroup(self.testsetup)
+
+        def click_on_group(self, group_name):
+            selector = "td[title='%s']" % group_name
+            self.selenium.find_element_by_css_selector(selector).click()
+            self._wait_for_results_refresh()
+            return AccessControl.ShowGroup(self.testsetup)
+
+    class NewGroup(Base):
+        _submit_group_button = (By.CSS_SELECTOR, "img[title='Add this Group']")
+        _group_description_field = (By.ID, "description")
+        _role_selector= (By.ID, "group_role")
+        _company_tags_tree = (By.CSS_SELECTOR, "#myco_treebox")
+        _hosts_clusters_tree = (By.CSS_SELECTOR, "#hac_treebox")
+        _vms_templates_tree = (By.CSS_SELECTOR, "#vat_treebox")
+
+        @property
+        def company_tags(self):
+            return CheckboxTree(self.testsetup, self.selenium.find_element(*self._company_tags_tree))
+
+        @property
+        def hosts_clusters(self):
+            return CheckboxTree(self.testsetup, self.selenium.find_element(*self._hosts_clusters_tree))
+
+        @property
+        def vms_templates(self):
+            return CheckboxTree(self.testsetup, self.selenium.find_element(*self._vms_templates_tree))
+
+        def fill_info(self, description, role):
+            self.selenium.find_element(*self._group_description_field).send_keys(description)
+            return self.select_dropdown(role, *self._role_selector)
+
+        def save(self):
+            # when editing an existing group, wait until "save" button shows up
+            # after ajax validation
+            self._wait_for_visible_element(*self._submit_group_button)
+            self.selenium.find_element(*self._submit_group_button).click()
+            self._wait_for_results_refresh()
+            return AccessControl.ShowGroup(self.testsetup)
+
+    class EditGroup(NewGroup):
+        _group_description_field = (By.ID, "description")
+        _role_selector= (By.ID, "group_role")
+        _submit_group_button = (By.CSS_SELECTOR, "img[title='Save Changes']")
+
+        def fill_info(self, description, role):
+            field = self.selenium.find_element(*self._group_description_field)
+            field.clear()
+            field.send_keys(description)
+            return self.select_dropdown(role, *self._role_selector)
+
+    class ShowGroup(Base):
+        _edit_group_button = (By.CSS_SELECTOR, "a[title='Edit this Group']")
+        _delete_group_button = (By.CSS_SELECTOR, "a[title='Delete this Group']")
+        _group_name_label = (By.CSS_SELECTOR, ".style1 tr:nth-child(1) td:nth-child(2)")
+
+        def click_on_edit(self):
+            self.selenium.find_element(*self._edit_group_button).click()
+            self._wait_for_results_refresh()
+            return AccessControl.EditGroup(self.testsetup)
+
+        def click_on_delete(self):
+            self.selenium.find_element(*self._delete_group_button).click()
+            self.handle_popup()
+            self._wait_for_results_refresh()
+            return AccessControl.Groups(self.testsetup)
+
+        @property
+        def group_name(self):
+            return self.selenium.find_element(*self._group_name_label).text.strip()
--- tests/test_configuration_access_control.py
+++ tests/test_configuration_access_control.py
@@ -7,7 +7,7 @@ import time
 from unittestzero import Assert
 
 @pytest.mark.nondestructive
-class TestSettings:
+class TestRoles:
     def test_add_new_role(self, mozwebqa, home_page_logged_in):
         home_pg = home_page_logged_in
         config_pg = home_pg.header.site_navigation_menu("Configuration").sub_navigation_menu("Configuration").click()
@@ -34,3 +34,38 @@ class TestSettings:
         show_role_pg = config_pg.click_on_access_control().click_on_roles().click_on_role('testrole2')
         roles_pg = show_role_pg.click_on_delete()
         Assert.true(roles_pg.flash.message.startswith('Role "testrole2": Delete successful'))
+
+
+@pytest.mark.destructive
+@pytest.mark.usefixtures("maximized")
+class TestGroups:
+    _group_description = "test_group"
+    _group_description_edit = "test_group_edit"
+    _group_role = "EvmRole-administrator"
+    _group_role_edit = "EvmRole-user"
+
+    def test_add_new_local_group(self, mozwebqa, home_page_logged_in):
+        home_pg = home_page_logged_in
+        config_pg = home_pg.header.site_navigation_menu("Configuration").sub_navigation_menu("Configuration").click()
+        Assert.true(config_pg.is_the_current_page)
+        new_group_pg = config_pg.click_on_access_control().click_on_groups().click_on_add_new()
+        new_group_pg.fill_info(self._group_description, self._group_role)
+        show_group_pg = new_group_pg.save()
+        Assert.true(show_group_pg.flash.message.startswith('Group "%s" was saved' % self._group_description))
+
+    def test_edit_group(self, mozwebqa, home_page_logged_in):
+        home_pg = home_page_logged_in
+        config_pg = home_pg.header.site_navigation_menu("Configuration").sub_navigation_menu("Configuration").click()
+        Assert.true(config_pg.is_the_current_page)
+        edit_group_pg = config_pg.click_on_access_control().click_on_groups().click_on_group(self._group_description).click_on_edit()
+        edit_group_pg.fill_info(self._group_description_edit, self._group_role_edit)
+        show_group_pg = edit_group_pg.save()
+        Assert.true(show_group_pg.flash.message.startswith('Group "%s" was saved' % self._group_description_edit))
+
+    def test_delete_group(self, mozwebqa, home_page_logged_in):
+        home_pg = home_page_logged_in
+        config_pg = home_pg.header.site_navigation_menu("Configuration").sub_navigation_menu("Configuration").click()
+        Assert.true(config_pg.is_the_current_page)
+        show_group_pg = config_pg.click_on_access_control().click_on_groups().click_on_group(self._group_description_edit)
+        show_group_pg = show_group_pg.click_on_delete()
+        Assert.true(show_group_pg.flash.message.startswith('EVM Group "%s": Delete successful' % self._group_description_edit))
