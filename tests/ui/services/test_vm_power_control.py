6c322e1a7d0de3d8e420fb2c0fc76177ae08290c
Dave Johnson <dajo@redhat.com>
2013-04-22 15:25:01 -0400

    add tests on vm details

--- tests/ui/services/test_vm_power_control.py
+++ tests/ui/services/test_vm_power_control.py
@@ -42,6 +42,7 @@ class TestVmPowerControl:
     def test_vm_power_on(self, vm_system, verify_vm_stopped):
         vm_pg = verify_vm_stopped
         vm_pg.power_on([vm_system],False)
+        Assert.true(vm_pg.flash.message.startswith("Start initiated"))
         vm_pg.wait_for_vm_state_change(vm_system, 'on', 10)
         Assert.true(vm_pg.quadicon_region.get_quadicon_by_title(vm_system).current_state == 'on', "vm not running")
     
@@ -52,10 +53,11 @@ class TestVmPowerControl:
         vm_pg.refresh() 
         vm_pg.find_vm_page(vm_system,None,False)
         Assert.true(vm_pg.quadicon_region.get_quadicon_by_title(vm_system).current_state == 'off', "vm running")
-    
+
     def test_vm_power_off(self, vm_system, verify_vm_running):
         vm_pg = verify_vm_running
         vm_pg.power_off([vm_system],False)
+        Assert.true(vm_pg.flash.message.startswith("Stop initiated"))
         vm_pg.wait_for_vm_state_change(vm_system, 'off', 10)
         Assert.true(vm_pg.quadicon_region.get_quadicon_by_title(vm_system).current_state == 'off', "vm running")
 
@@ -66,3 +68,34 @@ class TestVmPowerControl:
         vm_pg.refresh() 
         vm_pg.find_vm_page(vm_system,None,False)
         Assert.true(vm_pg.quadicon_region.get_quadicon_by_title(vm_system).current_state == 'on', "vm not running")
+
+@pytest.mark.nondestructive  # IGNORE:E1101
+class TestVmDetailsPowerControl:
+
+    def test_vm_power_on(self, vm_system, verify_vm_stopped):
+        vm_pg = verify_vm_stopped
+        vm_details = vm_pg.find_vm_page(vm_system,'off',False,True)
+        Assert.true(vm_details.power_state == 'off', "power state pre-req wrong" )
+        last_boot_time = vm_details.last_boot_time
+        state_chg_time = vm_details.last_pwr_state_change
+        vm_details.power_button.power_on(False)
+        vm_details.wait_for_vm_state_change('on', 10)
+        Assert.true(vm_details.power_state == 'on', "power state incorrect" )
+        Assert.true(vm_details.last_boot_time != last_boot_time, "last boot time failed to update" )
+        Assert.true(vm_details.last_pwr_state_change != state_chg_time, "last state chg time failed to update" )
+
+    def test_vm_power_off(self, vm_system, verify_vm_running):
+        vm_pg = verify_vm_running
+        vm_details = vm_pg.find_vm_page(vm_system,'on',False,True)
+        Assert.true(vm_details.power_state == 'on' , "power state pre-req wrong")
+        last_boot_time = vm_details.last_boot_time
+        state_chg_time = vm_details.last_pwr_state_change
+        vm_details.power_button.power_off(False)
+        vm_details.wait_for_vm_state_change('off', 10)
+        Assert.true(vm_details.power_state == 'off', "power state incorrect" )
+        Assert.true(vm_details.last_boot_time == last_boot_time, "last boot time failed to update" )
+        Assert.true(vm_details.last_pwr_state_change != state_chg_time, "last state chg time failed to update" )
+
+    #def test_vm_power_on_cancel(self,  vm_system, verify_vm_stopped):
+    #def test_vm_power_off_cancel(self, vm_system, verify_vm_running):
+
