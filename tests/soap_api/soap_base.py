e4dfdbe599155871ceb4a46a6fe2139379b9f24f
David Gao <dgao@redhat.com>
2013-05-16 15:05:08 -0400

    add test fixtures add try/except protection to common errors use testsetup to fetch user/pass
    added mgmt_sys initiation and usage
    some pylint cleanup

--- tests/soap_api/soap_base.py
+++ tests/soap_api/soap_base.py
@@ -8,49 +8,68 @@ from suds.client import Client
 import logging
 from suds.transport.https import HttpAuthenticated
 from suds.xsd.doctor import ImportDoctor, Import
-import yaml
 import paramiko
 from unittestzero import Assert
+from common.MgmtSystem import VMWareSystem, RHEVMSystem
 
 class SoapClient:
-	def __init__(self):
-		logging.basicConfig(level=logging.INFO)
-		# TODO: Set these by configuration in the future
-		#logging.getLogger('suds.client').setLevel(logging.DEBUG)
-		#logging.getLogger('suds.transport').setLevel(logging.DEBUG)
-		
-		# change mozwebqa.cfg hard coded path to read off config arg
-		f = open('../../cfme_pages/mozwebqa.cfg', 'r')
-		content = f.read().split('\n')
-		intermediate = [(x.split('=')[0].strip(), x.split('=')[1].strip()) for x in content if len(x.split('=')) == 2]
-		env_config = dict(intermediate)
-		f.close()
+    """ SoapClient to EVM """
+    def __init__(self, testsetup, cfme_data):
+        logging.basicConfig(level=logging.INFO)
+        #logging.getLogger('suds.client').setLevel(logging.DEBUG)
+        #logging.getLogger('suds.transport').setLevel(logging.DEBUG)
 
-		# change credential.cfg hard coded path to read off config arg
-		f = open('../../cfme_pages/credentials.yaml', 'r')
-		user_config = yaml.load(f)
-		f.close()
+        self.username = testsetup.credentials['default']['username']
+        self.password = testsetup.credentials['default']['password']
+        self.evm_server_hostname = testsetup.base_url.strip('https://')
+        url = '%s/vmdbws/wsdl/' % testsetup.base_url
 
-		self.u = user_config['default']['username']
-		self.p = user_config['default']['password']
-		self.evm_server_hostname = env_config['baseurl'].strip('https://')
-		url = '%s/vmdbws/wsdl/' % env_config['baseurl']
+        trans = HttpAuthenticated(username=self.username, 
+                                  password=self.password)
+        imp = Import('http://schemas.xmlsoap.org/soap/encoding/')
+        doc = ImportDoctor(imp)
 
-		t = HttpAuthenticated(username=self.u, password=self.p)
-		imp = Import('http://schemas.xmlsoap.org/soap/encoding/')
-		d = ImportDoctor(imp)
+        self.client = Client(url, transport=trans, doctor=doc)
+        Assert.not_none(self.client)
 
-		self.client = Client(url, transport=t, doctor=d)
-		Assert.not_none(self.client)
+        self.mgmt_system = []
+        for sys_name, mgmt_sys in cfme_data.data['management_systems'].items():
+            cred = mgmt_sys['credentials'].strip()
+            hostname = mgmt_sys['ipaddress']
+            username = testsetup.credentials[cred]['username']
+            password = testsetup.credentials[cred]['password']
+            name = mgmt_sys['name']
+            self.mgmt_system.append((name, hostname, username, password))
 
-	def ssh_client(self, hostname=None, user=None, pwd=None):
-		if hostname is None:
-			hostname=self.evm_server_hostname
-		if user is None:
-			user = self.u
-		if pwd is None:
-			pwd = self.p
-		client = paramiko.SSHClient()
-		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		client.connect(hostname, username=user, password=pwd)
-		return client
+    def ssh_client(self, hostname=None, user=None, pwd=None):
+        """ Create a ssh client """
+        if hostname is None:
+            hostname = self.evm_server_hostname
+        if user is None:
+            user = self.username
+        if pwd is None:
+            pwd = self.password
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, username=user, password=pwd)
+        return client
+
+    def setup_mgmt_clients(self):
+        """ Create management system API clients """
+        mgmt_clients = []
+        for name, host, user, pwd in self.mgmt_system:
+            if 'vsphere' in name.lower():
+                # create pysphere client
+                client = VMWareSystem(hostname=host, 
+                                      username=user, 
+                                      password=pwd)
+                mgmt_clients.append(client)
+            elif 'rhev' in name.lower():
+                # create rhevm client
+                client = RHEVMSystem(hostname=host, 
+                                     username=user, 
+                                     password=pwd)
+                mgmt_clients.append(client)
+            else:
+                logging.info('Can\'t create client for %s, ignoring...' % name)
+        return mgmt_clients
--- tests/soap_api/test_connectivity.py
+++ tests/soap_api/test_connectivity.py
@@ -4,10 +4,27 @@ Created on May 3, 2013
 @author: dgao
 '''
 
-from soap_base import SoapClient
+import pytest
+from soap_api.soap_base import SoapClient
 from unittestzero import Assert
 
-def test_connectivity():
-	s = SoapClient()
-	result = s.client.service.EVMHostList()
-	Assert.greater(len(result), 0, msg='Making sure there are more than one hosts configured.')
+pytestmark = pytestmark = [pytest.mark.skip_selenium,
+                           pytest.mark.nondestructive]
+
+@pytest.fixture(scope='module') # IGNORE:E1101
+def soap_base(mozwebqa, cfme_data):
+    return SoapClient(mozwebqa, cfme_data)
+
+@pytest.fixture(scope='module') # IGNORE:E1101
+def soap_client(soap_base):
+    return soap_base.client
+
+@pytest.fixture(scope='module') # IGNORE:E1101
+def api_clients(soap_base):
+    clients = soap_base.setup_mgmt_clients()
+    return clients
+
+def test_connectivity(mozwebqa, soap_client):
+    result = soap_client.service.EVMHostList()
+    Assert.greater(len(result), 0, 
+                   msg='Making sure there are more than one hosts configured.')
