9203a1453606ba37f93b8c13f1a81c6af9f16e92
Ronelle Landy <rlandy@redhat.com>
2013-05-17 16:59:16 -0400

    Adding _wait_for_results_refresh() to click() method

--- pages/regions/tree.py
+++ pages/regions/tree.py
@@ -18,30 +18,30 @@ class Tree(Page):
     _main_tree_item_locator = (By.CSS_SELECTOR, "table > tbody > tr > td > table > tbody")
     _root_item_locator = (By.XPATH, "tr")
     _sub_item_locator = (By.XPATH, "following-sibling::*")
-    
+
     def __init__(self,setup,root_element,parent = None):
         Page.__init__(self, setup)
         self._root_element = root_element
         self._parent = parent
-        
+
     @property
     def root(self):
         return self._root_element.find_element(*self._main_tree_item_locator).find_element(*self._root_item_locator)
-        
+
     @property
     def children(self):
         return [Tree(self.testsetup, web_element, self)
                 for web_element in self.root.find_elements(*self._sub_item_locator)]
-    
+
     @property
     def name(self):
         return self.root.text.encode('utf-8')
-        
+
     @property
     def twisty(self):
         from pages.regions.twisty import Twisty
         return Twisty(self.testsetup, self.root)
-    
+
     @property
     def parent(self):
         return self._parent
@@ -50,7 +50,9 @@ class Tree(Page):
         return self._root_element.is_displayed()
     
     def click(self):
-        return self.root.click()
+        element = self.root.click()
+        self._wait_for_results_refresh()
+        return element
 
     def find_node_by_regexp(self, regexp_str):
         # finds first node by name in the whole tree, breadth first
