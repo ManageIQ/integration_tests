444678297cd6c7e4cfee801342c9c5fcf5145f5b
Dave Johnson <dajo@redhat.com>
2013-05-24 16:18:06 -0400

    pwr control unit test update

--- tests/test_power_button.py
+++ tests/test_power_button.py
@@ -7,34 +7,23 @@ def pick_random_vm_template(mozwebqa, home_page_logged_in):
     vm_pg.find_vm_page(None,'template',True, False)
     return vm_pg
 
+@pytest.mark.nondestructive
 class TestPowerButton:        
         
-    @pytest.mark.nondestructive
     def test_power_button_shutdown(self, mozwebqa, home_page_logged_in, pick_random_vm_template):
-        pick_random_vm_template.power_button.shutdown()
-        Assert.true(pick_random_vm_template.flash.message.startswith("Shutdown Guest initiated"))
+        pick_random_vm_template.power_button.shutdown_and_cancel()
         
-    @pytest.mark.nondestructive
     def test_power_button_restart(self, mozwebqa, home_page_logged_in, pick_random_vm_template):
-        pick_random_vm_template.power_button.restart()
-        Assert.true(pick_random_vm_template.flash.message.startswith("Restart Guest initiated"))
+        pick_random_vm_template.power_button.restart_and_cancel()
         
-    @pytest.mark.nondestructive
     def test_power_button_power_on(self, mozwebqa, home_page_logged_in, pick_random_vm_template):
-        pick_random_vm_template.power_button.power_on()
-        Assert.true(pick_random_vm_template.flash.message.startswith("Start initiated"))
+        pick_random_vm_template.power_button.power_on_and_cancel()
         
-    @pytest.mark.nondestructive
     def test_power_button_power_off(self, mozwebqa, home_page_logged_in, pick_random_vm_template):
-        pick_random_vm_template.power_button.power_off()
-        Assert.true(pick_random_vm_template.flash.message.startswith("Stop initiated"))
+        pick_random_vm_template.power_button.power_off_and_cancel()
         
-    @pytest.mark.nondestructive
     def test_power_button_suspend(self, mozwebqa, home_page_logged_in, pick_random_vm_template):
-        pick_random_vm_template.power_button.suspend()
-        Assert.true(pick_random_vm_template.flash.message.startswith("Suspend initiated"))
+        pick_random_vm_template.power_button.suspend_and_cancel()
         
-    @pytest.mark.nondestructive
     def test_power_button_reset(self, mozwebqa, home_page_logged_in, pick_random_vm_template):
-        pick_random_vm_template.power_button.reset()
-        Assert.true(pick_random_vm_template.flash.message.startswith("Reset initiated"))
+        pick_random_vm_template.power_button.reset_and_cancel()
