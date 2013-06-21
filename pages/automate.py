eebc157616e9de876e1c19ec99bcd0a9a22ffa26
Ronelle Landy <rlandy@redhat.com>
2013-06-21 16:43:40 -0400

    Adding locators and methods for namespace cleanup

--- pages/automate.py
+++ pages/automate.py
@@ -3,6 +3,9 @@
 from pages.base import Base
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from pages.regions.list import ListRegion, ListItem
+from selenium.webdriver.support.select import Select
+from time import sleep
 
 class Automate(Base):
     @property
@@ -82,6 +85,9 @@ class Automate(Base):
         _ae_tree = (By.ID, "ae_tree")
         _configuration_button = (By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Configuration']")
         _add_namespace_button = (By.CSS_SELECTOR, "table.buttons_cont tr[title='Add a New Namespace']")
+        _remove_namespaces_button = (By.CSS_SELECTOR, "table.buttons_cont tr[title='Remove selected Namespaces']")
+        _namespace_list_locator = (
+            By.CSS_SELECTOR, "div#ns_list_grid_div > div.objbox > table > tbody")
         _name_text_field = (By.ID, "ns_name")
         _description_text_field = (By.ID, "ns_description")
         _add_system_button = (By.CSS_SELECTOR, "ul#form_buttons > li > img[title='Add']")
@@ -116,6 +122,17 @@ class Automate(Base):
             return self.selenium.find_element(*self._add_namespace_button)
 
         @property
+        def remove_namespaces_button(self):
+            return self.selenium.find_element(*self._remove_namespaces_button)
+
+        @ property
+        def namespace_list(self):
+            return ListRegion(
+                self.testsetup,
+                self.get_element(*self._namespace_list_locator),
+                Automate.NamespaceItem)
+
+        @property
         def add_class_button(self):
             return self.selenium.find_element(*self._add_class_button)
 
@@ -136,6 +153,22 @@ class Automate(Base):
             ActionChains(self.selenium).click(self.configuration_button).click(self.add_namespace_button).perform()
             self._wait_for_results_refresh()
             return Automate.Explorer(self.testsetup)
+
+        def click_on_remove_selected_namespaces(self):
+            self._wait_for_results_refresh()
+            ActionChains(self.selenium).click(self.configuration_button).click(self.remove_namespaces_button).perform()
+            self.handle_popup(cancel=False)
+            return Automate.Explorer(self.testsetup)
+
+        def click_on_namespace_item(self, item_name):
+            namespace_items = self.namespace_list.items
+            selected_item = None
+            for i in range(1, len(namespace_items)):
+                if namespace_items[i]._item_data[2].text == item_name:
+                    selected_item = namespace_items[i]
+                    namespace_items[i]._item_data[0].find_element_by_tag_name('img').click()
+            self._wait_for_results_refresh()
+            return Automate.NamespaceItem(selected_item)
 
         def click_on_add_new_class(self):
             self._wait_for_results_refresh()
@@ -188,3 +221,24 @@ class Automate(Base):
             self._wait_for_results_refresh()
             self.selenium.find_element(*self._add_new_field_schema_button).click()
             return Automate.Explorer(self.testsetup)
+
+
+    class NamespaceItem(ListItem):
+        '''Represents a namespace in the list'''
+        _columns = ["checkbox", "folder", "name", "description"]
+
+        @property
+        def checkbox(self):
+            pass
+
+        @property
+        def folder(self):
+            pass
+
+        @property
+        def iname(self):
+            return self._item_data[2].text
+
+        @property
+        def description(self):
+            pass
