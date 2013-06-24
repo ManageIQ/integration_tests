a833984a0dfdff0baa4f69adc5447cd2eb15a615
Brad P. Crochet <brad@redhat.com>
2013-06-24 09:31:23 -0400

    Add waits in the proper locations for adding a mgmtsys

--- pages/infrastructure_subpages/management_system_subpages/add.py
+++ pages/infrastructure_subpages/management_system_subpages/add.py
@@ -7,6 +7,7 @@ from pages.base import Base
 from selenium.webdriver.common.by import By
 
 class ManagementSystemsAdd(Base):
+    '''Management Systems - Add a management system page'''
     _page_title = 'CloudForms Management Engine: Management Systems'
 
     _management_system_add_button_locator = (
@@ -40,68 +41,90 @@ class ManagementSystemsAdd(Base):
 
     @property
     def add_button(self):
+        '''Add button
+
+        Returns a WebElement'''
         return self.get_element(*self._management_system_add_button_locator)
 
     @property
     def verify_button(self):
+        '''Verify button
+
+        Returns a WebElement'''
         return self.get_element(
                 *self._management_system_credentials_verify_button_locator)
 
     @property
     def cancel_button(self):
+        '''Cancel button
+
+        Returns a WebElement'''
         return self.get_element(
                 *self._management_system_cancel_button_locator)
 
     @property
     def name(self):
+        '''Name of the management system'''
         return self.get_element(*self._management_system_name_locator)
 
     @property
     def hostname(self):
+        '''Hostname of the management system'''
         return self.get_element(*self._management_system_hostname_locator)
 
     @property
     def ipaddress(self):
+        '''IP address of the management system'''
         return self.get_element(*self._management_system_ipaddress_locator)
 
     @property
     def default_userid(self):
+        '''Userid on default credentials tab'''
         return self.get_element(*self._management_system_userid_locator)
 
     @property
     def default_password(self):
+        '''Password on default credentials tab'''
         return self.get_element(*self._management_system_password_locator)
 
     @property
     def default_verify(self):
+        '''Verify password on default credentials tab'''
         return self.get_element(
                 *self._management_system_verify_password_locator)
 
     @property
     def metrics_userid(self):
+        '''Userid on C&U credentials tab'''
         return self.get_element(*self._management_system_cu_userid_locator)
 
     @property
     def metrics_password(self):
+        '''Password on C&U credentials tab'''
         return self.get_element(
                 *self._management_system_cu_password_locator)
 
     @property
     def metrics_verify(self):
+        '''Verify password on C&U credentials tab'''
         return self.get_element(*self._management_system_cu_verify_locator)
 
     @property
     def api_port(self):
+        '''API port'''
         return self.get_element(*self._management_system_api_port_locator)
 
     @property
     def server_zone(self):
+        '''Zone'''
         return self.get_element(*self._server_zone_edit_field_locator)
 
     def click_on_default_credentials(self):
+        '''Click on the default credentials tab'''
         self.get_element(*self._default_credentials_button_locator).click()
 
     def click_on_metrics_credentials(self):
+        '''Click on the C&U credentials tab'''
         self.get_element(*self._metrics_credentials_button_locator).click()
 
     def new_management_system_fill_data(
@@ -111,6 +134,7 @@ class ManagementSystemsAdd(Base):
             ip_address="127.0.0.1",
             user_id="test_user",
             password="test_password"):
+        '''Fill a management system with individual args'''
         self.name.send_keys(name)
         self.hostname.send_keys(hostname)
         self.ipaddress.send_keys(ip_address)
@@ -119,6 +143,7 @@ class ManagementSystemsAdd(Base):
         self.default_verify.send_keys(password)
 
     def _fill_management_system(self, management_system):
+        '''Fill a management system given a dictionary'''
         for key, value in management_system.iteritems():
             # Special cases
             if "server_zone" in key:
@@ -150,18 +175,23 @@ class ManagementSystemsAdd(Base):
                     attr = getattr(self, key)
                     attr.clear()
                     attr.send_keys(value)
+        self._wait_for_results_refresh()
 
     def add_vmware_management_system(self, management_system):
+        '''Fill and click on add for a vmware system'''
         self.select_management_system_type("VMware vCenter")
         self._fill_management_system(management_system)
         return self.click_on_add()
 
     def add_rhevm_management_system(self, management_system):
-        self.select_management_system_type("Red Hat Enterprise Virtualization Manager")
+        '''Fill and click on add for a RHEV system'''
+        self.select_management_system_type(
+                "Red Hat Enterprise Virtualization Manager")
         self._fill_management_system(management_system)
         return self.click_on_add()
 
     def add_management_system(self, management_system):
+        '''Generic add. Will determine the correct management system to add'''
         if "virtualcenter" in management_system["type"]:
             return self.add_vmware_management_system(management_system)
         elif "rhevm" in management_system["type"]:
@@ -169,16 +199,22 @@ class ManagementSystemsAdd(Base):
         raise Exception("Unknown management system type")
 
     def add_management_system_with_bad_credentials(self, management_system):
+        '''Add a management system and click verify, expecting bad creds'''
         if "virtualcenter" in management_system["type"]:
             self.select_management_system_type("VMware vCenter")
         elif "rhevm" in management_system["type"]:
-            self.select_management_system_type("Red Hat Enterprise Virtualization Manager")
+            self.select_management_system_type(
+                    "Red Hat Enterprise Virtualization Manager")
         self._fill_management_system(management_system)
-        self._wait_for_visible_element(*self._management_system_credentials_verify_button_locator)
+        self._wait_for_visible_element(
+                *self._management_system_credentials_verify_button_locator)
         self.click_on_credentials_verify()
+        self._wait_for_results_refresh()
         return self
 
     def select_management_system_type(self, management_system_type):
+        '''Select a management system type from the dropdown, and wait for 
+        the page to refresh'''
         self.select_dropdown(
                 management_system_type,
                 *self._management_system_type_locator)
@@ -186,17 +222,24 @@ class ManagementSystemsAdd(Base):
         return ManagementSystemsAdd(self.testsetup)
 
     def click_on_add(self):
+        '''Click on the add button'''
         self.add_button.click()
-        from pages.infrastructure_subpages.management_systems import ManagementSystems
+        from pages.infrastructure_subpages.management_systems \
+                import ManagementSystems
         return ManagementSystems(self.testsetup)
 
     def click_on_credentials_verify(self):
+        '''Click on the verify credentials button and wait for page refresh'''
         self.verify_button.click()
         self._wait_for_results_refresh()
         return ManagementSystemsAdd(self.testsetup)
 
     def click_on_cancel(self):
+        '''Click on cancel
+
+        Returns ManagementSystems page'''
         self.cancel_button.click()
-        from pages.infrastructure_subpages.management_systems import ManagementSystems
+        from pages.infrastructure_subpages.management_systems \
+                import ManagementSystems
         return ManagementSystems(self.testsetup)
 
