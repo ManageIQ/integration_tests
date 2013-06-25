e654e2c423681fccc34181846d70209ccf39f6f8
Wade Adkins <wadkins@redhat.com>
2013-06-25 16:23:45 -0400

    Retire VM's page and test

--- pages/services.py
+++ pages/services.py
@@ -333,6 +333,17 @@ class Services(Base):
 
     class VirtualMachineDetails(VmCommonComponents):
         _details_locator = (By.CSS_SELECTOR, "div#textual_div")
+        _set_retirement_date_button_locator = (By.CSS_SELECTOR, "table.buttons_cont tr[title='Set Retirement Dates for this VM']")
+        _immediately_retire_vm_button_locator = (By.CSS_SELECTOR, "table.buttons_cont tr[title='Immediately Retire this VM']")
+
+
+        @property
+        def set_retirement_date_button(self):
+            return self.selenium.find_element(*self._set_retirement_date_button_locator)
+
+        @property
+        def immediately_retire_vm_button(self):
+            return self.selenium.find_element(*self._immediately_retire_vm_button_locator)
 
         @property
         def power_state(self):
@@ -352,6 +363,17 @@ class Services(Base):
             root_element = self.selenium.find_element(*self._details_locator)
             return Details(self.testsetup, root_element)
 
+        def click_on_set_retirement_date(self):
+            ActionChains(self.selenium).click(self.center_buttons.lifecycle_button).click(self.set_retirement_date_button).perform()
+            self._wait_for_results_refresh()
+            return Services.SetRetirementDate(self.testsetup)
+
+        def click_on_immediately_retire_vm(self, cancel=False):
+            ActionChains(self.selenium).click(self.center_buttons.lifecycle_button).click(self.immediately_retire_vm_button).perform()
+            self.handle_popup(cancel)
+            self._wait_for_results_refresh()
+            return Services.VirtualMachines(self.testsetup)
+
         def wait_for_vm_state_change(self, desired_state, timeout_in_minutes):
             current_state = self.power_state
             print "Desired state: " + desired_state + "    Current state: " + current_state
@@ -367,7 +389,57 @@ class Services(Base):
                 if (minute_count==timeout_in_minutes) and (current_state != desired_state):
                     raise Exception("timeout reached("+str(timeout_in_minutes)+" minutes) before desired state (" +
                                      desired_state+") reached... current state("+current_state+")")
+    class SetRetirementDate(Base):
+        _date_edit_field_locator = (By.CSS_SELECTOR, "input#miq_date_1")
+        _retirement_warning_edit_field_locator = (By.CSS_SELECTOR, "select#retirement_warn")
+        _save_button_locator = (By.CSS_SELECTOR, "ul#form_buttons > li > img[title='Save Changes']")
+        _cancel_button_locator = (By.CSS_SELECTOR, "ul#form_buttons > li > img[title='Cancel']")
+        _remove_retirement_date_button_locator = (By.ID, "remove_button")
 
+        @property
+        def date_field(self):
+            return self.selenium.find_element(*self._date_edit_field_locator) 
+
+        @property
+        def save_button(self):
+            return self.get_element(*self._save_button_locator)
+
+        @property
+        def cancel_button(self):
+            return self.get_element(*self._cancel_button_locator)
+
+        @property
+        def remove_button(self):
+            return self.get_element(*self._remove_retirement_date_button_locator)
+
+        def click_on_cancel(self):
+            self._wait_for_visible_element(*self._cancel_button_locator)
+            self.cancel_button.click()
+            self._wait_for_results_refresh()
+            return Services.VirtualMachineDetails(self.testsetup)
+
+        def click_on_save(self):
+            self._wait_for_visible_element(*self._save_button_locator)
+            self.save_button.click()
+            self._wait_for_results_refresh()
+            return Services.VirtualMachineDetails(self.testsetup)
+
+        def click_on_remove(self):
+            self._wait_for_visible_element(*self._remove_retirement_date_button_locator)
+            self.remove_button.click()
+            self._wait_for_results_refresh()
+            return Services.SetRetirementDate(self.testsetup)
+
+        def fill_data(self, retirement_date, retirement_warning):
+            if(retirement_date):
+                self.date_field._parent.execute_script("$j('#miq_date_1').attr('value', '%s')" % retirement_date)
+                self._wait_for_results_refresh() 
+
+            if(retirement_warning):
+                self.select_dropdown(retirement_warning, *self._retirement_warning_edit_field_locator)
+                self._wait_for_results_refresh()       
+ 
+    #class ImmediatelyRetireVm(Base):
 
     #class EditVm(Base):
     #    _page_title = 'CloudForms Management Engine: Virtual Machines'
--- tests/test_services.py
+++ tests/test_services.py
@@ -12,30 +12,55 @@ def pick_random_vm(mozwebqa, home_page_logged_in):
 
 
 @pytest.mark.nondestructive
+@pytest.mark.usefixtures("maximized") 
 class TestServices:
-    def test_virtual_machines(self, mozwebqa, home_page_logged_in):
+#    def test_virtual_machines(self, mozwebqa, home_page_logged_in):
+#        home_pg = home_page_logged_in
+#        Assert.true(home_pg.is_logged_in, "Could not determine if logged in")
+#        vm_pg = home_pg.header.site_navigation_menu("Services").sub_navigation_menu("Virtual Machines").click()
+#        Assert.true(vm_pg.is_the_current_page)
+#        vm_pg.paginator.click_next_page()
+#        for vm in vm_pg.quadicon_region.quadicons:
+#            print "Snapshots " + vm.snapshots
+#        vm_pg.paginator.click_last_page()
+#        time.sleep(2)
+#        vm_pg.paginator.click_prev_page()
+#        time.sleep(2)
+#        vm_pg.paginator.click_first_page()
+#        time.sleep(2)
+#
+#    @pytest.mark.nondestructive
+#    def test_vm_details(self, pick_random_vm):
+#        vm_details = pick_random_vm
+#        Assert.true(vm_details.details.does_info_section_exist('Properties'))
+#        Assert.true(vm_details.details.get_section('Properties').get_item('Name') != None)
+#
+#    @pytest.mark.nondestructive
+#    def test_vm_smart_state_scan(self, pick_random_vm):
+#        vm_details = pick_random_vm
+#        vm_details.config_button.refresh_relationships()
+#        Assert.true(vm_details.flash.message.startswith("Refresh Ems initiated"))
+
+    def test_set_vm_retirement_date(self, mozwebqa, home_page_logged_in, pick_random_vm):
         home_pg = home_page_logged_in
         Assert.true(home_pg.is_logged_in, "Could not determine if logged in")
         vm_pg = home_pg.header.site_navigation_menu("Services").sub_navigation_menu("Virtual Machines").click()
         Assert.true(vm_pg.is_the_current_page)
-        vm_pg.paginator.click_next_page()
-        for vm in vm_pg.quadicon_region.quadicons:
-            print "Snapshots " + vm.snapshots
-        vm_pg.paginator.click_last_page()
-        time.sleep(2)
-        vm_pg.paginator.click_prev_page()
-        time.sleep(2)
-        vm_pg.paginator.click_first_page()
-        time.sleep(2)
+        vm_pg = pick_random_vm
+        set_retirement_date_pg = vm_pg.click_on_set_retirement_date()
+        retirement_date = "7/30/2013"
+        retirement_warning = "1 Week before retirement"
+        set_retirement_date_pg.fill_data(retirement_date, retirement_warning)
+        Assert.true(set_retirement_date_pg.date_field.get_attribute("value") == retirement_date)
+        vm_pg = set_retirement_date_pg.click_on_cancel()
+        Assert.true(vm_pg.flash.message.startswith('Set/remove retirement date was cancelled by the user'))
 
-    @pytest.mark.nondestructive
-    def test_vm_details(self, pick_random_vm):
-        vm_details = pick_random_vm
-        Assert.true(vm_details.details.does_info_section_exist('Properties'))
-        Assert.true(vm_details.details.get_section('Properties').get_item('Name') != None)
-
-    @pytest.mark.nondestructive
-    def test_vm_smart_state_scan(self, pick_random_vm):
-        vm_details = pick_random_vm
-        vm_details.config_button.refresh_relationships()
-        Assert.true(vm_details.flash.message.startswith("Refresh Ems initiated"))
+    def test_immediately_retire_vm(self, mozwebqa, home_page_logged_in, pick_random_vm):
+        home_pg = home_page_logged_in
+        Assert.true(home_pg.is_logged_in, "Could not determine if logged in")
+        vm_pg = home_pg.header.site_navigation_menu("Services").sub_navigation_menu("Virtual Machines").click()
+        Assert.true(vm_pg.is_the_current_page)
+        vm_pg = pick_random_vm 
+        vm_name = vm_pg.details.get_section("Properties").get_item("Name").value
+        vm_pg.click_on_immediately_retire_vm(cancel=True)
+        Assert.true(vm_pg.details.get_section("Properties").get_item("Name").value == vm_name)        
