0533d8fec9c82afa507a6bfd580e4ce3d6abf785
Sean Myers <sean.myers@redhat.com>
2013-07-19 14:36:08 -0400

    selenium fixture now supports navigation fixtures

--- fixtures/selenium.py
+++ fixtures/selenium.py
@@ -1,6 +1,11 @@
+import types
+
 import pytest
 from pytest_mozwebqa import split_class_and_test_names
 from pytest_mozwebqa.selenium_client import Client
+
+from fixtures import navigation
+from pages.page import Page
 
 def pytest_runtest_setup(item):
     # If we're using the selenium fixture, mark the test as
@@ -17,16 +22,16 @@ def selenium(mozwebqa, request):
     test_id = '.'.join(split_class_and_test_names(request.node.nodeid))
     mozwebqa.selenium_client = Client(test_id, request.session.config.option)
 
-    def cm_wrapper(url, page_obj):
-        return SeleniumContextManager(mozwebqa, url, page_obj)
+    def cm_wrapper(url, page_or_fixture):
+        return SeleniumContextManager(mozwebqa, url, page_or_fixture)
 
     return cm_wrapper
 
 class SeleniumContextManager(object):
-    def __init__(self, testsetup, url, page_obj):
+    def __init__(self, testsetup, url, page_or_fixture):
         self.testsetup = testsetup
         self.url = url
-        self.page_obj = page_obj
+        self.page_or_fixture = page_or_fixture
 
     def __enter__(self):
         # More mozwebqa bootstrapping, start the browser, expose some
@@ -43,8 +48,19 @@ class SeleniumContextManager(object):
             setattr(self.testsetup, attr, getattr(self.testsetup.selenium_client, attr))
 
         self.testsetup.base_url = self.url
+        self.testsetup.selenium.maximize_window()
         self.testsetup.selenium.get(self.url)
-        return self.page_obj(self.testsetup)
+
+        # Is it a page or a fixture?
+        if type(self.page_or_fixture) == types.FunctionType:
+            # Function! It's a fixture and we should use it...
+            home_page_logged_in = navigation.home_page_logged_in(self.testsetup)
+            # If you passed in the home_page_logged_in fixture, this will be funny.
+            return self.page_or_fixture(home_page_logged_in)
+        else:
+            # Not a function! It's probably a Page class that we should set up
+            return self.page_or_fixture(self.testsetup)
+
 
     def __exit__(self, *args, **kwargs):
         self.testsetup.selenium_client.stop()
