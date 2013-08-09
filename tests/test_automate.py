3edd64773210e92303c7ab9a017badb694a784c7
Sean Myers <sean.myers@redhat.com>
2013-08-09 17:59:27 -0400

    test_automate is now actually nondestructive

--- tests/test_automate.py
+++ tests/test_automate.py
@@ -3,14 +3,9 @@
 import pytest
 from unittestzero import Assert
 
-@pytest.fixture
-def namespace(random_string):
-    '''Returns random name for namespace'''
-    return '%s_namespace' % random_string
-
 @pytest.mark.nondestructive
 @pytest.mark.usefixtures("maximized")
-def test_automate_components(automate_explorer_pg, namespace):
+def test_automate_components(automate_explorer_pg):
     '''Tests components for Automate Explorer Tree'''
     ae_pg = automate_explorer_pg
     ae_tree = ae_pg.accordion.current_content
@@ -29,15 +24,5 @@ def test_automate_components(automate_explorer_pg, namespace):
 
     ae_pg.click_on_namespace_access_node("Sample")
     Assert.equal(ae_tree.children[5].children[0].name, "Methods")
-
     ae_pg.accordion.current_content.find_node_by_name("System").click()
-
     ae_pg.accordion.current_content.find_node_by_name("Datastore").click()
-    ae_pg = ae_pg.click_on_add_new_namespace()
-    ae_pg = ae_pg.fill_namespace_info(namespace, namespace)
-
-    # teardown - remove parent namespace
-    ae_namespace_pg = ae_pg.click_on_namespace_access_node("Datastore")
-    ae_namespace_pg.click_on_namespace_item(namespace)
-    ae_namespace_pg.click_on_remove_selected_namespaces()
-
