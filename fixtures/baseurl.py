76751de8c20c871e6a82d73a75a10c1f80790b98
Sean Myers <sean.myers@redhat.com>
2013-08-20 10:35:07 -0400

    Adds the fixtureconf fixture
    - Kept running into request.node._fixtureconf boilerplate, so now that's a fixture

--- fixtures/baseurl.py
+++ fixtures/baseurl.py
@@ -1,10 +1,10 @@
 import pytest
 
 @pytest.fixture
-def baseurl(request, mozwebqa):
+def baseurl(fixtureconf, mozwebqa):
     """Override mozwebqa's baseurl for an individual test
 
     Put this fixture before other fixtures to ensure it takes effect
 
     """
-    mozwebqa.base_url = request.node._fixtureconf['base_url']
+    mozwebqa.base_url = fixtureconf['base_url']
