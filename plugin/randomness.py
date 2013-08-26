56e9bd3a03ed522946cae1900b899c3630d76ade
Brad P. Crochet <brad@redhat.com>
2013-08-26 09:28:30 -0400

    Fix call to uuid_as_str

--- plugin/randomness.py
+++ plugin/randomness.py
@@ -5,7 +5,7 @@ from common import randomness
 @pytest.fixture  # IGNORE:E1101
 def random_uuid_as_string():
     '''Creates a random uuid and returns is as a string'''
-    return randomness.generate_random_uuid_as_string()
+    return randomness.generate_random_uuid_as_str()
 
 @pytest.fixture
 def random_string():
