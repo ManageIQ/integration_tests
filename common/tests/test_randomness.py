e5d859d4aa00ad57781b8522791000b3f1f5d348
Sean Myers <sean.myers@redhat.com>
2013-08-26 11:04:13 -0400

    Coverage for randomness fixtures
    Also converted all tests to use unittestzero Assert

--- common/tests/test_randomness.py
+++ common/tests/test_randomness.py
@@ -1,6 +1,7 @@
 import random
 
 import pytest
+from unittestzero import Assert
 
 from common import randomness
 
@@ -12,28 +13,35 @@ pytestmark = [
 def test_generate_random_string_noargs():
     random_string = randomness.generate_random_string()
     # 8 is the default length
-    assert len(random_string) == 8
+    Assert.equal(len(random_string), 8)
 
 def test_generate_random_string_args():
     length = 16
     random_string = randomness.generate_random_string(length)
-    assert len(random_string) == length
+    Assert.equal(len(random_string), length)
 
 def test_generate_random_int_noargs():
     # maxint is the default max, so no need to check against it
     random_int = randomness.generate_random_int()
-    assert 0 <= random_int
+    Assert.greater(random_int, 0)
 
 def test_generate_random_int_args():
-    max = 1
-    random_int = randomness.generate_random_int(max)
-    assert 0 <= random_int <= max
+    maxvalue = 1
+    random_int = randomness.generate_random_int(maxvalue)
+    Assert.greater_equal(random_int, 0)
+    Assert.less_equal(random_int, maxvalue)
 
 def test_generate_random_uuid():
     # Not sure if there's a better test than a string of normal uuid length (36)
     uuid = randomness.generate_random_uuid_as_str()
-    assert len(uuid) == 36
-    assert isinstance(uuid, basestring)
+    Assert.equal(len(uuid), 36)
+    Assert.true(isinstance(uuid, basestring))
+
+def test_randomness_fixtures(random_uuid_as_string, random_string):
+    # Make sure the fixtures work as intended
+    Assert.equal(len(random_uuid_as_string), 36)
+    Assert.true(isinstance(random_uuid_as_string, basestring))
+    Assert.true(isinstance(random_string, basestring))
 
 @pytest.fixture(scope="class")
 def random_stash(request):
@@ -51,21 +59,19 @@ def random_stash(request):
 class TestRandomizeValues(object):
     def test_randomizevalues(self):
         # These should be different in the two dicts
-        assert self.after['str'] != self.before['str']
-        assert self.after['tuple'] != self.before['tuple']
-        assert self.after['list'] != self.before['list']
-        assert self.after['set'] != self.before['set']
+        Assert.not_equal(self.after['str'], self.before['str'])
+        Assert.not_equal(self.after['tuple'], self.before['tuple'])
+        Assert.not_equal(self.after['list'], self.before['list'])
+        Assert.not_equal(self.after['set'], self.before['set'])
 
     def test_randomizevalues_type(self):
         # Object type should still be dict
-        assert isinstance(self.after, type(self.before))
+        Assert.true(isinstance(self.after, type(self.before)))
 
     def test_randomizevalues_bogus_randomizer(self):
         # Unmatched randomizer shouldn't change
-        assert self.after['notrandom'] == self.before['notrandom']
+        Assert.equal(self.after['notrandom'], self.before['notrandom'])
 
     def test_randomizevalues_again(self):
         # If we generate the dict again, it should be newly randomized
-        again = randomness.RandomizeValues.from_dict(self.before)
-        assert self.after != self.again
-
+        Assert.not_equal(self.after, self.again)
