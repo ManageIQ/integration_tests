70bd3a73893a6aaecf1e819ef0bcc69dcade408b
Sean Myers <sean.myers@redhat.com>
2013-09-23 17:24:09 -0400

    Provider tweaks to support Openstack backend
    - Also more linty clean, and now knows about EC2System

--- utils/providers.py
+++ utils/providers.py
@@ -2,10 +2,23 @@ from common import mgmt_system
 from cfme_data import load_cfme_data
 from credentials import load_credentials
 
-provider_type_map = {
+
+# infra and cloud provider type maps, useful for type checking
+infra_provider_type_map = {
     'virtualcenter': mgmt_system.VMWareSystem,
     'rhevm': mgmt_system.RHEVMSystem,
 }
+
+cloud_provider_type_map = {
+    'ec2': mgmt_system.EC2System,
+    'openstack': mgmt_system.OpenstackEC2System,
+}
+
+# Combined type map, provider_factory doesn't discriminate
+provider_type_map = dict(
+    infra_provider_type_map.items() + cloud_provider_type_map.items()
+)
+
 
 def provider_factory(provider_name, providers=None, credentials=None):
     if providers is None:
@@ -18,10 +31,9 @@ def provider_factory(provider_name, providers=None, credentials=None):
         credentials_dict = load_credentials()
         credentials = credentials_dict[provider['credentials']]
 
-    provider_instance = provider_type_map[provider['type']](
-        provider['ipaddress'],
-        credentials['username'],
-        credentials['password']
-    )
-
+    # Munge together provider dict and creds,
+    # Let the provider do whatever they need with them
+    provider_kwargs = provider.copy()
+    provider_kwargs.update(credentials)
+    provider_instance = provider_type_map[provider['type']](**provider_kwargs)
     return provider_instance
