8738c2c00ce8944c5028a8cb797146222bbb83b7
Wade Adkins <wadkins@redhat.com>
2013-09-27 10:38:09 -0400

    Updated Toggle VM Script

--- scripts/toggle_vm.py
+++ scripts/toggle_vm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Given a provider from cfme_data along with a vm_name, 
+Given a provider from cfme_data along with a vm_name,
 how long you want the vm to run (uptime, in seconds),
 and how long you want the vm to be off (downtime, in seconds)
 this script will toggle the vm on and off.
@@ -17,6 +17,7 @@ import time
 
 from utils.providers import provider_factory
 
+
 def main():
     parser = argparse.ArgumentParser(epilog=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -24,9 +25,9 @@ def main():
         help='provider name in cfme_data')
     parser.add_argument('vm_name', help='the name of the VM on which to act')
     parser.add_argument('uptime', type=int,
-    	help = 'how long do you want the vm to be on (in seconds)')
+        help='how long do you want the vm to be on (in seconds)')
     parser.add_argument('downtime', type=int,
-    	help = 'how long do you want the vm to be off (in seconds)')
+        help='how long do you want the vm to be off (in seconds)')
 
     args = parser.parse_args()
 
@@ -35,12 +36,14 @@ def main():
 
     if provider.is_vm_running(args.vm_name):
         provider.stop_vm(args.vm_name)
-        vm_status = provider.vm_status(args.vm_name)
+        provider.vm_status(args.vm_name)
 
     # Toggle the VM On and Off based on the Uptime and Downtime input arguments
+    # The script diconnects from the provider before each sleep and reconnects after
+    # each sleep to prevent it from timing out
     while True:
         try:
-            # Initialize start_success to False so it enters the first while loop 
+            # Initialize start_success to False so it enters the first while loop
             # and the times_failed_counter to 0
             start_success = False
             times_failed_counter = 0
@@ -51,14 +54,16 @@ def main():
                     provider.start_vm(args.vm_name)
                     provider.vm_status(args.vm_name)
                     start_success = True
+                    provider.disconnect()
                     time.sleep(args.uptime)
+                    provider = provider_factory(args.provider_name)
                 except Exception:
                     time.sleep(60)
                     times_failed_counter += 1
                     if(times_failed_counter == 30):
                         raise
 
-            # Initialize stop_success to False so it enters the first while loop 
+            # Initialize stop_success to False so it enters the first while loop
             # and the times_failed_counter to 0
             stop_success = False
             times_failed_counter = 0
@@ -69,16 +74,17 @@ def main():
                     provider.stop_vm(args.vm_name)
                     provider.vm_status(args.vm_name)
                     stop_success = True
+                    provider.disconnect()
                     time.sleep(args.downtime)
+                    provider = provider_factory(args.provider_name)
                 except Exception:
                     time.sleep(60)
                     times_failed_counter += 1
                     if(times_failed_counter == 30):
-                        raise            
+                        raise
         except(KeyboardInterrupt):
             return 0
 
 
 if __name__ == '__main__':
     sys.exit(main())
-
