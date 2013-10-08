62006031461ebd22b09daca7c67111cd50b8e7ff
Sean Myers <sean.myers@redhat.com>
2013-10-08 18:26:37 -0400

    README updates
    - Also removed the path-munging code to add cfme_pages to the python path
      since the README now leaves it to the individual developer to set use
      the PYTHONPATH env var

--- conftest.py
+++ conftest.py
@@ -8,34 +8,6 @@ from pkgutil import iter_modules
 
 import pytest
 
-def add_cfme_pages_to_path():
-    '''Automatically add the cfme_pages repo to the sys.path'''
-    try:
-        import pages.page
-    except ImportError:
-        import os
-        import sys
-        HOMEDIR = os.environ['HOME']
-        pages_dirs = ["cfme_pages",
-                "../cfme_pages",
-                "%s/workspace/cfme_pages" % (HOMEDIR),
-                "%s/cfme_pages" % (HOMEDIR)]
-        for pages_dir in pages_dirs:
-            oldpath = sys.path
-            try:
-                sys.path.append(pages_dir)
-                import pages.page
-                break
-            except ImportError:
-                sys.path.remove(pages_dir)
-                continue
-        if not 'pages.page' in sys.modules:
-            print '''Could not find cfme_pages. Please clone to one of the
-            standard locations, or set PYTHONPATH'''
-            raise ImportError
-
-add_cfme_pages_to_path()
-
 # From cfme_tests
 import fixtures, markers
 
