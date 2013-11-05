5817879bd9c51a8ce426fcbb321912bf99e2c97d
Antonin Pagac <apagac@redhat.com>
2013-11-05 17:20:39 +0100

    nightly commit

--- tests/scenarios/refarch_2013_rhev/test_update_appliances.py
+++ tests/scenarios/refarch_2013_rhev/test_update_appliances.py
@@ -3,77 +3,107 @@
 import pytest
 from unittestzero import Assert
 import time
+import threading
 
 #After initializing the registration process, there has to be a wait
 SECONDS_TO_WAIT_FOR_REGISTRATION = 100
+#Check registration every REG_REFRESH seconds
+REG_REFRESH = 5
+#Wait for update process to complete
+SECONDS_TO_WAIT_FOR_UPDATE = 600
+#Check update every UP_REFRESH seconds
+UP_REFRESH = 20
 
 @pytest.mark.usefixtures("maximized")
-def test_edit_registration_rhn(cnf_configuration_pg, cfme_data):
+def test_edit_registration(cnf_configuration_pg, cfme_data):
     updates_pg = cnf_configuration_pg.click_on_redhat_updates()
     update_method = cfme_data.data['redhat_updates']['registration_method']
     updates_data = cfme_data.data['redhat_updates'][update_method]
     creds_data = updates_pg.testsetup.credentials[
         updates_data["credentials"]]
-
-    #passw = creds_data["password"]
-    #Assert.true(1==2, passw)
-
     if "organization" in updates_data:
         registered_pg = updates_pg.edit_registration_and_save(
             updates_data["url"], creds_data, update_method,
             organization=updates_data["organization"])
     else:
         registered_pg = updates_pg.edit_registration_and_save(
-            updates_data["url"], creds_data, update_method, default=True)
+            updates_data["url"], creds_data, update_method)
     flash_message = "Customer Information successfully saved"
     Assert.equal(registered_pg.flash.message, flash_message,
         registered_pg.flash.message)
 
+#TODO insert some more waiting for the default check for updates
+#maybe new test? or include the check in this one
 @pytest.mark.usefixtures("maximized")
 def test_register_appliances(cnf_configuration_pg, cfme_data):
     updates_pg = cnf_configuration_pg.click_on_redhat_updates()
     appliances_to_update = cfme_data.data['redhat_updates']['appliances_to_update']
-    #updates_pg.register_appliances(appliances_to_update)
     updates_pg.register_appliances()
     flash_message = "Registration has been initiated for the selected Servers"
     Assert.equal(updates_pg.flash.message, flash_message, updates_pg.flash.message)
-    waiting_period = SECONDS_TO_WAIT_FOR_REGISTRATION/5
-    for count in range(0, waiting_period):
-        time.sleep(5)
-        #if updates_pg.is_registered(appliances_to_update):
-        if updates_pg.is_registered():
+    waiting_interval = SECONDS_TO_WAIT_FOR_REGISTRATION/REG_REFRESH
+    for count in range(0, waiting_interval):
+        time.sleep(REG_REFRESH)
+        if updates_pg.systems_registered():
             break
         updates_pg.refresh_list()
-    if count == (waiting_period - 1):
+    if count == (waiting_interval - 1):
         pytest.fail("Appliance was unable to register properly")
 
-#Skipped. Appliance is not updating itself yet
-@pytest.mark.skipif("True")
 @pytest.mark.usefixtures("maximized")
-def test_compare_versions_before_update(cnf_configuration_pg, cfme_data):
+def test_versions_before_update(cnf_configuration_pg, cfme_data):
     updates_pg = cnf_configuration_pg.click_on_redhat_updates()
     appliance_versions = cfme_data.data["redhat_updates"]["appliances"]
-    Assert.true(updates_pg.are_old_versions_before_update(
-        appliance_versions))
+    Assert.true(updates_pg.check_versions(appliance_versions),
+        "Version check before update failed. Check your cfme_data.yaml")
 
-#After this test we will probably want to insert some waiting
-#Cannot do it now, in the present time the updates are not working
+#TODO experimental helping function
+def login_again(mozwebqa):
+    from pages.login import LoginPage
+    login_pg = LoginPage(mozwebqa)
+    login_pg.go_to_login_page()
+    home_pg = login_pg.login()
+    cnf_configuration_pg = home_pg.header.site_navigation_menu('Configure')\
+        .sub_navigation_menu('Configuration').click()
+    updates_pg = cnf_configuration_pg.click_on_redhat_updates()
+
+#Workflow without updating the main appliance. It has to be current already
+#TODO update main appliance also
+#TODO timeout - we need somehow to detect login screen
+#if not updates_pg.is_the_current_page then login again
 @pytest.mark.usefixtures("maximized")
-def test_initiate_updates(cnf_configuration_pg, cfme_data):
+def test_update_cfme(cnf_configuration_pg, cfme_data):
     updates_pg = cnf_configuration_pg.click_on_redhat_updates()
     appliances_to_update = cfme_data.data['redhat_updates']['appliances_to_update']
+    appliance_current_version = cfme_data.data["redhat_updates"]["current_version"]
     updates_pg.apply_updates(appliances_to_update)
     flash_message = "Update has been initiated for the selected Servers"
     Assert.equal(updates_pg.flash.message, flash_message, updates_pg.flash.message)
+    waiting_interval = SECONDS_TO_WAIT_FOR_UPDATE/UP_REFRESH
+    for count in range(0, waiting_interval):
+        time.sleep(UP_REFRESH)
+        #TODO check if we need this (run watchdog)
+        if not updates_pg.is_the_current_page:
+            login_again(mozwebqa)
+        if updates_pg.check_versions(appliance_current_version):
+            break
+        updates_pg.refresh_list()
+    if count == (waiting_interval - 1):
+        pytest.fail("Timeout while waiting for appliance update")
 
-#Skipped. Appliance is not updating itself yet
-@pytest.mark.skipif("True")
-@pytest.mark.usefixtures("maximized")
-def test_compare_versions_after_update(cnf_configuration_pg, cfme_data):
+def test_platform_updates(cnf_configuration_pg, cfme_data, ssh_client):
     updates_pg = cnf_configuration_pg.click_on_redhat_updates()
-    appliance_current_version = cfme_data.data["redhat_updates"]["current_version"]
-    Assert.true(updates_pg.is_current_version_after_update(
-        appliance_current_version))
+    appliances = cfme_data.data['redhat_updates']['appliances']
+    for appliance in appliances:
+        ssh_client(hostname=appliance['url']).run_command('yum update -y &')
+    waiting_interval = SECONDS_TO_WAIT_FOR_UPDATE/UP_REFRESH
+    for count in range(0, waiting_interval):
+        time.sleep(UP_REFRESH)
+        if not updates_pg.platform_updates_available():
+            break
+        updates_pg.refresh_updates()
+    if count == (waiting_interval - 1):
+        pytest.fail("Timeout while waiting for platform updates")
 
 @pytest.mark.skip_selenium
 def test_certificates_exist(cfme_data, ssh_client):
