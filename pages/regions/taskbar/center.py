e927ab47f41770581d4a3622ee2e3c801ace5337
Pete Savage <psavage@redhat.com>
2013-11-28 17:24:49 +0000

    More flakey goodness

--- pages/regions/taskbar/center.py
+++ pages/regions/taskbar/center.py
@@ -9,31 +9,30 @@ Created on Feb 28, 2013
 from pages.regions.taskbar.button import Button
 from selenium.webdriver.common.by import By
 
+
 class CenterButtons(Button):
     '''
     classdocs
     '''
 
-    _center_buttons_locator = (
-            By.CSS_SELECTOR, "#center_buttons_div > #center_tb")
-    _configuration_button_locator = (
-            By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Configuration'] > div")
-    _policy_button_locator = (
-            By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Policy'] > div")
-    _lifecycle_button_locator = (
-            By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Lifecycle'] > div")
-    _power_button_locator = (
-            By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Power'] > div")
-    _monitoring_button_locator = (
-            By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Monitoring'] > div")
+    _center_buttons_locator = (By.CSS_SELECTOR, "#center_buttons_div > #center_tb")
+    _configuration_button_locator = (By.CSS_SELECTOR,
+        "div.dhx_toolbar_btn[title='Configuration'] > div")
+    _policy_button_locator = (By.CSS_SELECTOR,
+        "div.dhx_toolbar_btn[title='Policy'] > div")
+    _lifecycle_button_locator = (By.CSS_SELECTOR,
+        "div.dhx_toolbar_btn[title='Lifecycle'] > div")
+    _power_button_locator = (By.CSS_SELECTOR,
+        "div.dhx_toolbar_btn[title='Power'] > div")
+    _monitoring_button_locator = (By.CSS_SELECTOR,
+        "div.dhx_toolbar_btn[title='Monitoring'] > div")
 
     def __init__(self, setup):
         Button.__init__(self, setup, *self._center_buttons_locator)
 
     @property
     def configuration_button(self):
-        return self._root_element.find_element(
-                *self._configuration_button_locator)
+        return self._root_element.find_element(*self._configuration_button_locator)
 
     @property
     def lifecycle_button(self):
--- pages/regions/taskbar/view.py
+++ pages/regions/taskbar/view.py
@@ -9,6 +9,7 @@ Created on Feb 28, 2013
 from pages.regions.taskbar.button import Button
 from selenium.webdriver.common.by import By
 
+
 class ViewButtons(Button):
     _view_buttons_locator = (By.CSS_SELECTOR, "#view_buttons_div > #view_tb")
     _grid_view_button_locator = (By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Grid View']")
@@ -16,34 +17,34 @@ class ViewButtons(Button):
     _list_view_button_locator = (By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='List View']")
     _download_button_locator = (By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Download']")
     _download_arrow_button_locator = (By.CSS_SELECTOR, "div.dhx_toolbar_arw[title='Download']")
-    
-    def __init__(self,setup):
+
+    def __init__(self, setup):
         Button.__init__(self, setup, *self._view_buttons_locator)
 
     @property
     def grid_view_button(self):
         return self._root_element.find_element(*self._grid_view_button_locator)
-    
+
     @property
     def tile_view_button(self):
         return self._root_element.find_element(*self._tile_view_button_locator)
-    
+
     @property
     def list_view_button(self):
         return self._root_element.find_element(*self._list_view_button_locator)
-    
+
     @property
     def download_button(self):
         return self._root_element.find_element(*self._download_button_locator)
-    
+
     @property
     def is_grid_view(self):
         return self._is_current_view(self.grid_view_button)
-    
+
     @property
     def is_tile_view(self):
         return self._is_current_view(self.tile_view_button)
-        
+
     @property
     def is_list_view(self):
         return self._is_current_view(self.list_view_button)
@@ -51,18 +52,15 @@ class ViewButtons(Button):
     def _switch_to_view(self, button):
         button.click()
         self._wait_for_results_refresh()
-    
+
     def change_to_grid_view(self):
         self._switch_to_view(self.grid_view_button)
-        
+
     def change_to_tile_view(self):
         self._switch_to_view(self.tile_view_button)
-    
+
     def change_to_list_view(self):
         self._switch_to_view(self.list_view_button)
-        
+
     def _is_current_view(self, button):
         return "dis" in button.get_attribute('class')
-    
-    
-        
