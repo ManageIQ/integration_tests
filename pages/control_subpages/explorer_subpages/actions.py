32e91f4ab5a3d667113e0f4b820502f40681de3a
Milan Falešník <mfalesni@redhat.com>
2013-12-09 13:56:04 +0100

    A bit of flaking

--- pages/control_subpages/explorer_subpages/actions.py
+++ pages/control_subpages/explorer_subpages/actions.py
@@ -9,8 +9,10 @@ class Actions(Explorer):
     _action_row_locator = (By.XPATH, "tr")
     _action_items_locator = (By.XPATH, "td")
 
-    _configuration_button_locator = (By.CSS_SELECTOR, "div.dhx_toolbar_btn[title='Configuration']")
-    _add_action_button_locator = (By.CSS_SELECTOR, "table.buttons_cont tr[title='Add a new Action']")
+    _configuration_button_locator = (By.CSS_SELECTOR,
+                                     "div.dhx_toolbar_btn[title='Configuration']")
+    _add_action_button_locator = (By.CSS_SELECTOR,
+                                  "table.buttons_cont tr[title='Add a new Action']")
 
     @property
     def configuration_button(self):
@@ -21,13 +23,17 @@ class Actions(Explorer):
         return self.selenium.find_element(*self._add_action_button_locator)
 
     def click_on_add_new(self):
-        ActionChains(self.selenium).click(self.configuration_button).click(self.add_action_button).perform()
+        ActionChains(self.selenium)\
+            .click(self.configuration_button)\
+            .click(self.add_action_button)\
+            .perform()
         self._wait_for_results_refresh()
         return self.NewAction(self.testsetup)
 
     @property
     def root(self):
-        return self.selenium.find_element(*self._actions_table).find_elements(*self._action_row_locator)
+        return self.selenium.find_element(*self._actions_table)\
+                            .find_elements(*self._action_row_locator)
 
     @property
     def actions(self):
@@ -35,16 +41,6 @@ class Actions(Explorer):
         return [[img, a_desc.text, a_type.text] for img, a_desc, a_type in elements]
 
     @property
-    def actions_list(self):
-        elements = [element.find_elements(*self._action_items_locator) for element in self.root]
-        return [(action[1], action[2]) for action in self.actions]
-
-    @property
-    def print_actions_list(self):
-        # for generating default actions list
-        for action in self.actions:
-            print "('%s', '%s')," % (action[1], action[2])
-
     def actions_list(self):
         return [(action[1], action[2]) for action in self.actions]
 
@@ -62,7 +58,8 @@ class Actions(Explorer):
         # TODO: resolve wait after select dropdown issue and save valid action
 
         def add_invalid_action(self, description=None):
-            self.selenium.find_element(*self._description_field).send_keys(description or "custom action")
+            self.selenium.find_element(*self._description_field).send_keys(description or
+                                                                           "custom action")
             #self.select_dropdown(_a_type, *self._type)
             #self._wait_for_results_refresh()
             return self.click_on_add()
