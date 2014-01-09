112b5ce713665640f1a3f33b75ebc54bd039b571
Jeff Weiss <jweiss@redhat.com>
2014-01-09 13:23:08 -0500

    Flash improvements (multiple messages, assertions)

--- cfme/web_ui/flash.py
+++ cfme/web_ui/flash.py
@@ -2,24 +2,42 @@ from cfme.web_ui import Region
 from selenium.webdriver.common.by import By
 import cfme.fixtures.pytest_selenium as sel
 
-# TODO - flash apparently can show more than one message, so
-# we should return a list
-
-# TODO - can also dismiss flash messages currently to reveal more
-# this is a design problem IMO.  Maybe see if we can support that
-# but get dev to fix
-
-flash_area = Region(locators=
-                    {'message': (By.XPATH, "//div[@id='flash_text_div' or @id='flash_div']")})
+area = Region(locators=
+              {'message': (By.XPATH, "//div[@id='flash_text_div' or @id='flash_div']//li")})
 
 
-def dismiss_message():
-    if sel.is_displayed(flash_area.message):
-        sel.click(flash_area.message)
+class Message(object):
+    def __init__(self, message=None, level=None):
+        self.message = message
+        self.level = level
+
+    def __str__(self):
+        return "Flash % message '%'" % self.level, self.message
 
 
-def get_message():
-    if sel.is_displayed(flash_area.message):
-        return sel.text(flash_area.message)
+def message(el):
+    return Message(message=sel.text(el),
+                   level=sel.get_attribute(el, 'class'))
+
+
+def get_messages():
+    '''Return a list of flash messages'''
+    return map(message, sel.elements(area.message))
+
+
+def is_error(message):
+    return message.level == 'error'
+
+
+def assert_no_errors():
+    all_messages = get_messages()
+    errors = filter(is_error, all_messages)
+    if errors:
+        raise Exception(errors)
     else:
-        return None
+        return all_messages
+
+
+def assert_message_match(m):
+    if not any([fm.message == m for fm in get_messages()]):
+        raise Exception("No matching flash message for '%'" % m)
