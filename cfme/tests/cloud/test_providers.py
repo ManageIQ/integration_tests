c5378622bfa8ebe112b6e97c0694855df0d5e24a
Jeff Weiss <jweiss@redhat.com>
2014-01-14 16:56:27 -0500

    Clean up the code i messed up

--- cfme/tests/cloud/test_providers.py
+++ cfme/tests/cloud/test_providers.py
@@ -4,43 +4,13 @@
 
 import pytest
 import cfme.web_ui.flash as flash
-from cfme.cloud.provider as provider
+from cfme.cloud.provider import provider
 
 
 @pytest.fixture(params=['ec2east', 'openstack'])
-def (request, cfme_data):
+def provider_data(request, cfme_data):
     '''Returns management system data from cfme_data'''
-    param = request.param
     return provider.get_from_config(request.param)
-
-
-@pytest.fixture
-def my_provider(request, cloud_providers_pg, provider_data):
-    '''Create a management system
-
-    Creates a management system based on the data from cfme_data.
-    Ideally, this fixture would clean up after itself, but currently
-    the only way to do that is via the UI, and we lose the selenium session
-    before the finalizer gets an opportunity to run.
-
-    This fixture will modify the db directly in the near future'''
-    prov_add_pg = cloud_providers_pg.click_on_add_new_provider()
-
-    # Returns prov_pg object
-    prov_add_pg.add_provider(provider_data)
-
-    # TODO: Finalizer doesn't actually work. The selenium session is
-    #       killed prior to its running.
-    # def fin():
-    #     cloud_providers_pg = mgmtsys_page.header.site_navigation_menu(
-    #             "Infrastructure").sub_navigation_menu(
-    #                     "Management Systems").click()
-    #     ms_name = provider_data[name_property]
-    #     cloud_providers_pg.wait_for_provider_or_timeout(ms_name)
-    #     cloud_providers_pg.select_provider(ms_name)
-    #     cloud_providers_pg.click_on_remove_provider()
-    # request.addfinalizer(fin)
-    return provider_data
 
 
 @pytest.fixture
@@ -61,14 +31,14 @@ def test_that_checks_flash_with_empty_discovery_form():
     '''Tests that the flash message is correct when discovery form is
     empty
     '''
-    discover(None)
+    provider.discover(None)
     flash.assert_message_match('User ID is required')
 
 
 def test_that_checks_flash_when_discovery_cancelled():
     '''Tests that the flash message is correct when discovery is cancelled
     '''
-    discover(None, cancel=True)
+    provider.discover(None, cancel=True)
     flash.assert_message_match('Amazon Cloud Providers Discovery was cancelled by the user')
 
 
@@ -80,8 +50,9 @@ def test_provider_edit(myprovider):
     myprovider.create()
     # TODO finish this
 
+
 def test_that_checks_flash_when_add_cancelled():
     '''Tests that the flash message is correct when add is cancelled'''
-    prov = Provider()
+    prov = provider.Provider()
     prov.create(cancel=True)
     flash.assert_message_match('Add of new Cloud Provider was cancelled by the user')
