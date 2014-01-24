f8506ebc35035eb2473831ab0bb0eec44957ff4c
Sean Myers <sean.myers@redhat.com>
2014-01-24 12:41:09 -0500

    Test start/finished log messages, minor log/path tweaks

---
+++ fixtures/log.py
@@ -0,0 +1,28 @@
+import pytest
+
+from utils.log import logger as cfme_logger
+
+
+# Expose the cfme logger as a fixture for convenience
+@pytest.fixture(scope='session')
+def logger():
+    return cfme_logger
+
+
+@pytest.mark.tryfirst
+def pytest_runtest_setup(item):
+    cfme_logger.info('py.test starting %s' % _format_nodeid(item.nodeid))
+
+
+@pytest.mark.trylast
+def pytest_runtest_logreport(report):
+    if report.when == 'teardown':
+        cfme_logger.info('py.test finished %s' % _format_nodeid(report.nodeid))
+
+
+def _format_nodeid(nodeid):
+    # Remove test class instances, replace with a dot to impersonate a method call
+    nodeid = nodeid.replace('::()::', '.')
+    # Trim double-colons to single
+    nodeid = nodeid.replace('::', ':')
+    return nodeid
