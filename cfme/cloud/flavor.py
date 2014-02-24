a3df5b66555e26ffbc5dc0cdc48ba4366b7c6acc
Sean Myers <sean.myers@redhat.com>
2014-02-24 15:34:22 -0500

    Existing Table instances converted to SplitTable where needed

--- cfme/cloud/flavor.py
+++ cfme/cloud/flavor.py
@@ -5,14 +5,14 @@
 :var details_page: A :py:class:`cfme.web_ui.Region` object describing elements on the detail page.
 """
 
-from cfme.web_ui import Region, Table
+from cfme.web_ui import Region, SplitTable
 
 
 # Page specific locators
 list_page = Region(
     locators={
-        'flavor_table': Table(header_data=('//div[@class="xhdr"]/table', 1),
-                            row_data=('//div[@class="objbox"]/table', 1))
+        'flavor_table': SplitTable(header_data=('//div[@class="xhdr"]/table/tbody', 1),
+            body_data=('//div[@class="objbox"]/table/tbody', 1))
     },
     title='CloudForms Management Engine: Flavors')
 
