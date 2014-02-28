57ed0ba9aa644321a4484b5622d1429d0e5d94c9
Dave Johnson <dajo@redhat.com>
2014-02-27 22:48:20 -0500

    a few minor test fixes/cleanup

--- tests/ui/cloud/test_instance_power_control.py
+++ tests/ui/cloud/test_instance_power_control.py
@@ -57,7 +57,7 @@ class TestInstanceDetailsPowerControlPerProvider:
 
         prov_data = cfme_data["management_systems"][provider]
         if prov_data["type"] == 'openstack':
-            mgmt_sys_api_clients[provider].deploy_template(image_name, flavour_name='m1.tiny',
+            mgmt_sys_api_clients[provider].deploy_template(image_name, flavour_name='m1.small',
                 vm_name=random_string)
         else:
             random_string = mgmt_sys_api_clients[provider].deploy_template(
