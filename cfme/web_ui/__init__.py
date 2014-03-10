e33e7b2a44dd89de7ab8ef66c3668798be5cd087
Milan Falešník <mfalesni@redhat.com>
2014-03-10 11:08:12 +0100

    Added MultiBoxSelect

--- cfme/web_ui/__init__.py
+++ cfme/web_ui/__init__.py
@@ -43,6 +43,7 @@ import cfme.fixtures.pytest_selenium as sel
 from cfme import exceptions
 from cfme.fixtures.pytest_selenium import browser
 from cfme.web_ui import tabstrip
+from cfme.web_ui import flash
 
 from utils.log import logger
 
@@ -1577,3 +1578,79 @@ class DHTMLSelect(Select):
                 self._log('value', value)
             index = browser().execute_script('return %s.getIndexByValue("%s")' % (name, value))
             self.select_by_index(index, _cascade=True)
+
+
+class MultiBoxSelect(object):
+    def __init__(self, unselected, selected, to_unselected, to_selected, remove_all=None):
+        self._unselected = Select(unselected, multi=True)
+        self._selected = Select(selected, multi=True)
+        self._to_unselected = to_unselected
+        self._to_selected = to_selected
+        self._remove_all = remove_all
+
+    def _move_to_unselected(self):
+        browser.click(sel.element(self._to_unselected))
+        return not any(map(flash.is_error, flash.get_all_messages()))
+
+    def _move_to_selected(self):
+        browser.click(sel.element(self._to_selected))
+        return not any(map(flash.is_error, flash.get_all_messages()))
+
+    def _select_unselected(self, *items):
+        for item in items:
+            sel.select(self._unselected, item)
+
+    def _select_selected(self, *items):
+        for item in items:
+            sel.select(self._selected, item)
+
+    def _clear_selection(self):
+        self._unselected.deselect_all()
+        self._selected.deselect_all()
+
+    def remove_all(self):
+        """ Flush the list of selected items.
+
+        Returns: :py:class:`bool` with success.
+        """
+        if self._remove_all is None:
+            raise NotImplementedError("'Remove all' button was not specified!")
+        browser.click(sel.element(self._remove_all))
+        return not any(map(flash.is_error, flash.get_all_messages()))
+
+    def add(self, *values, **kwargs):
+        """ Mark items for selection and then clicks the button to select them.
+
+        Args:
+            *values: Values to select
+
+        Keywords:
+            flush: By using `flush` keyword, the selected items list is flushed prior to selecting
+                new ones
+        Retuns: :py:class:`bool` with success.
+        """
+        if kwargs.get("flush", False):
+            self.remove_all()
+        self._clear_selection()
+        self._select_unselected(*values)
+        return self._move_to_selected()
+
+    def remove(self, *values):
+        """ Mark items for deselection and then clicks the button to deselect them.
+
+        Args:
+            *values: Values to deselect
+
+        Returns: :py:class:`bool` with success.
+        """
+        self._clear_selection()
+        self._select_selected(*values)
+        return self._move_to_unselected()
+
+
+@fill.method((MultiBoxSelect, object))
+def _fill_multibox(multi, values):
+    """ Filler function for MultiBoxSelect
+
+    """
+    return multi.add(*values, flush=True)
