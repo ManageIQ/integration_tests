9caa674aac2598e79e557fa6c685481aa29e00cd
Shveta <shveta@localhost.localdomain>
2014-03-13 11:00:38 -0700

    Service Dialog tests added

--- tests/ui/services/test_service_dialog.py
+++ tests/ui/services/test_service_dialog.py
@@ -11,11 +11,62 @@ from unittestzero import Assert
 @pytest.mark.nondestructive
 class TestServiceDialog:
     '''Service Dialog test cases'''
+
     def test_create_service_dialog(
-            self,
-            automate_customization_pg,
-            random_string):
+        self,
+        automate_customization_pg,
+        random_string):
         '''Create service dialog'''
+        new_dialog_pg = automate_customization_pg\
+            .click_on_service_dialog_accordion().add_new_service_dialog()
+        service_dialog_name = "auto_dialog_" + random_string
+        new_dialog_pg.create_service_dialog(random_string,
+        service_dialog_name,
+        "descr",
+        "service_name")
+        Assert.true(new_dialog_pg.flash.message.startswith(
+            'Dialog "%s" was added' % service_dialog_name))
+
+    def test_edit_service_dialog(
+        self,
+        automate_customization_pg,
+        random_string):
+        '''Edit service dialog'''
+        new_dialog_pg = automate_customization_pg\
+            .click_on_service_dialog_accordion().add_new_service_dialog()
+        service_dialog_name = "auto_dialog_" + random_string
+        new_dialog_pg.create_service_dialog(random_string,
+         service_dialog_name,
+         "descr",
+         "service_name")
+        Assert.true(new_dialog_pg.flash.message.startswith(
+            'Dialog "%s" was added' % service_dialog_name))
+        edit_dialog_name = service_dialog_name + "_edited"
+        new_dialog_pg.edit_service_dialog(edit_dialog_name)
+        Assert.true(new_dialog_pg.flash.message.startswith(
+            'Dialog "%s" was saved' % edit_dialog_name))
+
+    def test_delete_service_dialog(
+        self,
+        automate_customization_pg,
+        random_string):
+        '''Delete service dialog'''
+        new_dialog_pg = automate_customization_pg\
+            .click_on_service_dialog_accordion().add_new_service_dialog()
+        service_dialog_name = "auto_dialog_" + random_string
+        new_dialog_pg.create_service_dialog(random_string, service_dialog_name,
+             "descr", "service_name")
+        Assert.true(new_dialog_pg.flash.message.startswith(
+            'Dialog "%s" was added' % service_dialog_name))
+        new_dialog_pg.delete_service_dialog()
+        Assert.true(new_dialog_pg.flash.message.startswith(
+            'Dialog "%s": Delete successful' % service_dialog_name))
+
+    def test_duplicate_service_dialog(
+        self,
+        automate_customization_pg,
+        random_string):
+        '''Duplicate service dialog'''
         new_dialog_pg = automate_customization_pg\
             .click_on_service_dialog_accordion().add_new_service_dialog()
         service_dialog_name = "auto_dialog_" + random_string
@@ -23,3 +74,8 @@ class TestServiceDialog:
             "descr", "service_name")
         Assert.true(new_dialog_pg.flash.message.startswith(
             'Dialog "%s" was added' % service_dialog_name))
+        dup_pg = new_dialog_pg.add_new_service_dialog()
+        dup_pg.create_service_dialog(random_string, service_dialog_name,
+            "descr", "service_name")
+        Assert.true(dup_pg.flash.message.startswith(
+            'Dialog Label has already been taken'))
