6986f91be13cda4cd0a5996b45d0bfced97492b9
Sean Myers <sean.myers@redhat.com>
2014-03-24 14:48:09 -0400

    pytest.fixture was squashing server_roles.__doc__, docstring moved to module

--- fixtures/server_roles.py
+++ fixtures/server_roles.py
@@ -1,3 +1,64 @@
+"""Set server roles based on a list of roles attached to the test using the server_roles fixture
+
+If you want to specify certain roles that have to be set,
+you can use this type of decoration::
+
+    @pytest.mark.fixtureconf(server_roles="+automate")
+    def test_appliance_roles(server_roles, default_roles_list):
+        assert len(server_roles) == len(default_roles_list) + 1
+
+This takes the current list from cfme_data.yaml and modifies
+it by the server_roles keyword. If prefixed with + or nothing, it adds,
+if prefixed with -, it removes the role. It can be combined either
+in string and in list, so these lines are functionally equivalent::
+
+    "+automate -foo bar" # (add automate and bar, remove foo)
+    ["+automate", "-foo", "bar"]
+
+If you specify the keyword ``clear_roles=True``, then all roles
+are flushed and the list contains only user_interface role.
+
+Roles can be pulled from the cfme_data fixture using yaml selectors,
+which will do a 'set' with the list of roles found at the target path::
+
+    @pytest.mark.fixtureconf(server_roles_cfmedata=('level1', 'sublevel2'))
+    def test_appliance_roles(server_roles):
+        assert len(server_roles) == 3
+
+Which corresponds to this yaml layout::
+
+    level1:
+        sublevel2:
+            - database_operations
+            - user_interface
+            - web_services
+
+To ensure the appliance has the default roles::
+
+    @pytest.mark.fixtureconf(set_default_roles=True)
+    def test_appliance_roles(server_roles):
+        do(test)
+
+List of server role names currently exposed in the CFME interface:
+
+    - automate
+    - ems_metrics_coordinator
+    - ems_metrics_collector
+    - ems_metrics_processor
+    - database_operations
+    - database_synchronization
+    - event
+    - ems_inventory
+    - ems_operations
+    - notifier
+    - reporting
+    - scheduler
+    - rhn_mirror
+    - smartproxy
+    - smartstate
+    - user_interface
+    - web_services
+"""
 import pytest
 from pages.configuration_subpages.settings_subpages.server_settings_subpages.server_roles import (
     RoleChangesRequired
@@ -7,71 +68,7 @@ from utils.conf import cfme_data
 
 @pytest.fixture
 def server_roles(fixtureconf, cnf_configuration_pg):
-    """Set the server roles based on a list of roles attached to the test using this fixture
-
-    Usage:
-
-        If you want to specify certain roles that have to be set,
-        you can use this type of decoration:
-
-        @pytest.mark.fixtureconf(server_roles="+automate")
-        def test_appliance_roles(server_roles, default_roles_list):
-            assert len(server_roles) == len(default_roles_list) + 1
-
-        This takes the current list from cfme_data.yaml and modifies
-        it by the server_roles keyword. If prefixed with + or nothing, it adds,
-        if prefixed with -, it removes the role. It can be combined either
-        in string and in list, so these lines are functionally equivalent:
-
-        "+automate -foo bar" # (add automate and bar, remove foo)
-        ["+automate", "-foo", "bar"]
-
-        If you specify the keyword ``clear_roles=True``, then all roles
-        are flushed and the list contains only user_interface role.
-
-        Roles can be pulled from the cfme_data fixture using yaml selectors,
-        which will do a 'set' with the list of roles found at the target path:
-
-        @pytest.mark.fixtureconf(server_roles_cfmedata=('level1', 'sublevel2'))
-        def test_appliance_roles(server_roles):
-            assert len(server_roles) == 3
-
-        Which corresponds to this yaml layout:
-
-        level1:
-            sublevel2:
-                - database_operations
-                - user_interface
-                - web_services
-
-        To ensure the appliance has the default roles:
-
-        @pytest.mark.fixtureconf(set_default_roles=True)
-        def test_appliance_roles(server_roles):
-            do(test)
-
-
-    List of server role names currently exposed in the CFME interface:
-
-        - automate
-        - ems_metrics_coordinator
-        - ems_metrics_collector
-        - ems_metrics_processor
-        - database_operations
-        - database_synchronization
-        - event
-        - ems_inventory
-        - ems_operations
-        - notifier
-        - reporting
-        - scheduler
-        - rhn_mirror
-        - smartproxy
-        - smartstate
-        - user_interface
-        - web_services
-    """
-
+    """The fixture that does the work. See usage in :py:mod:`fixtures.server_roles`"""
     # Nav to the settings tab
     settings_pg = cnf_configuration_pg.click_on_settings()
     # Workaround to rudely bypass a popup that sometimes appears for
