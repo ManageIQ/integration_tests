01e966de79cf5147e1d92558f75f35000ed69d0d
Sean Myers <sean.myers@redhat.com>
2014-04-01 15:27:25 -0400

    I'm not sure where reverse_lookup and visible_pages belong...
    ..but I'm sure that they belong together wherever that may be

--- cfme/tests/integration/test_aws_iam_auth_and_roles.py
+++ cfme/tests/integration/test_aws_iam_auth_and_roles.py
@@ -1,13 +1,14 @@
 import pytest
 
 from cfme.login import force_login_user
+from cfme.web_ui import menu
 from utils.conf import credentials
 from utils.testgen import auth_groups, generate
 
 pytest_generate_tests = generate(auth_groups, auth_mode='aws_iam')
 
 
-def test_group_roles(configure_aws_iam_auth_mode, group_name, group_data, visible_pages):
+def test_group_roles(configure_aws_iam_auth_mode, group_name, group_data):
     """Basic default AWS_IAM group role RBAC test
 
     Validates expected menu and submenu names are present for default
@@ -22,4 +23,4 @@ def test_group_roles(configure_aws_iam_auth_mode, group_name, group_data, visibl
         pytest.fail('No match in credentials file for group "%s"' % iam_group_name)
 
     force_login_user(username, password)
-    assert visible_pages() == group_data
+    assert menu.visible_pages() == group_data
--- cfme/tests/integration/test_ldap_auth_and_roles.py
+++ cfme/tests/integration/test_ldap_auth_and_roles.py
@@ -1,13 +1,14 @@
 import pytest
 
 from cfme.login import force_login_user
+from cfme.web_ui import menu
 from utils.conf import credentials
 from utils.testgen import auth_groups, generate
 
 pytest_generate_tests = generate(auth_groups, auth_mode='ldap')
 
 
-def test_group_roles(configure_ldap_auth_mode, group_name, group_data, visible_pages):
+def test_group_roles(configure_ldap_auth_mode, group_name, group_data):
     """Basic default LDAP group role RBAC test
 
     Validates expected menu and submenu names are present for default
@@ -21,4 +22,4 @@ def test_group_roles(configure_ldap_auth_mode, group_name, group_data, visible_p
         pytest.fail('No match in credentials file for group "%s"' % group_name)
 
     force_login_user(username, password)
-    assert visible_pages() == group_data
+    assert menu.visible_pages() == group_data
