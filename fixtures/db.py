5d4f9c8e4ae2f308a0a2e9bf3cf69abc53050422
Sean Myers <sean.myers@redhat.com>
2014-04-08 11:33:14 -0400

    db module becomes utils.db with fixtures.db counterpart

---
+++ fixtures/db.py
@@ -0,0 +1,34 @@
+import pytest
+
+import utils.db
+
+
+@pytest.fixture(scope='module')
+def db(uses_db):
+    """Fixture providing :py:attr:`utils.db.cfmedb`
+
+    This is an SQLalchemy-based helper class which provides access to common database functions.
+
+    See also:
+
+        http://www.sqlalchemy.org/ session.
+
+    Usage:
+
+        # This example gets vm names and hostnames from the ext_management_systems table.
+        def test_that_tries_for_db(db):
+            ems_table = db['ext_management_systems']
+            for instance in db.session.query(ems_table.order_by(ems_table.id):)
+                assert instance.name, instance.hostname
+
+    This fixture is module scoped to ensure predictable database access
+    at the module level within tests.
+
+    """
+    return utils.db.cfmedb.copy()
+
+
+@pytest.fixture(scope='session')
+def db_yamls(db):
+    """Returns a mapping of database yaml names to the yaml contents serialized in python"""
+    return utils.db.db_yamls(db)
