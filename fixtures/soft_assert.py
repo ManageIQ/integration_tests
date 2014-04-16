c084d8e6470a3c55a1227ca94d3929f532a16965
Sean Myers <sean.myers@redhat.com>
2014-04-16 11:30:01 -0400

    soft_assert helper functions (and docs!)
    Hopefully these make soft_assert more useful.

--- fixtures/soft_assert.py
+++ fixtures/soft_assert.py
@@ -72,39 +72,6 @@ class SoftAssertionError(AssertionError):
         return '\n'.join(failmsgs)
 
 
-@pytest.fixture
-def soft_assert():
-    """soft assert fixture, used to defer AssertionError to the end of a test run
-
-    Usage:
-
-        # contents of test_soft_assert.py, for example
-        def test_uses_soft_assert(soft_assert):
-            soft_assert(True)
-            soft_assert(False, 'failure message')
-            soft_assert(None)
-
-    This test will report two soft assertion failures, with the following message::
-
-        SoftAssertionError:
-        failure message (test_soft_assert.py:3)
-        soft_assert(None) (test_soft_assert.py:4)
-
-    """
-    def _soft_assert_func(expr, fail_message=''):
-        if not expr:
-            # inspect the calling frame to find where the original assertion failed
-            # explicitly requesting one line of code context so we can set fail_message if needed
-            frameinfo = inspect.getframeinfo(inspect.stack(1)[1][0])
-            if not fail_message:
-                fail_message = str(frameinfo.code_context[0]).strip()
-
-            path = '%s:%r' % (get_rel_path(frameinfo.filename), frameinfo.lineno)
-            fail_message = '%s (%s)' % (fail_message, path)
-            _thread_locals.caught_asserts.append(fail_message)
-    return _soft_assert_func
-
-
 @contextmanager
 def _soft_assert_cm():
     """soft assert context manager
@@ -117,3 +84,81 @@ def _soft_assert_cm():
     yield _thread_locals.caught_asserts
     if _thread_locals.caught_asserts:
         raise SoftAssertionError(_thread_locals.caught_asserts)
+
+
+@contextmanager
+def _catch_assert_cm():
+    """assert catching context manager
+
+    * Catches a single AssertionError, and turns it into a soft assert
+
+    """
+    try:
+        yield
+    except AssertionError as ex:
+        caught_assert = _annote_failure(str(ex))
+        _thread_locals.caught_asserts.append(caught_assert)
+
+
+# Some helper functions for creating or interacting with the caught asserts
+def _get_caught_asserts():
+    return _thread_locals.caught_asserts
+
+
+def _clear_caught_asserts():
+    # delete all items of the caught_asserts list
+    del _thread_locals.caught_asserts[:]
+
+
+def _annote_failure(fail_message=''):
+    # Inspect the stack with 1 line of context, looking at the 2nd frame
+    # before this one, assuming the first frame is whatever called this function,
+    # and the second frame is where the assertion failure took place
+    frameinfo = inspect.getframeinfo(inspect.stack(1)[2][0])
+    if not fail_message:
+        fail_message = str(frameinfo.code_context[0]).strip()
+
+    filename = get_rel_path(frameinfo.filename) or frameinfo.filename
+    path = '%s:%r' % (filename, frameinfo.lineno)
+    return '%s (%s)' % (fail_message, path)
+
+
+@pytest.fixture
+def soft_assert():
+    """soft assert fixture, used to defer AssertionError to the end of a test run
+
+    Usage:
+
+        # contents of test_soft_assert.py, for example
+        def test_uses_soft_assert(soft_assert):
+            soft_assert(True)
+            soft_assert(False, 'failure message')
+
+            # soft_assert.catch_assert will intercept AssertionError
+            # and turn it into a soft assert
+            with soft_assert.catch_assert():
+                assert None
+
+            # Soft asserts can be cleared at any point within a test:
+            soft_assert.clear_asserts()
+
+            # If more in-depth interaction is desired with the caught_asserts, the list of failure
+            # messages can be retrieved. This will return the directly mutable caught_asserts list:
+            caught_asserts = soft_assert.caught_asserts()
+
+    The test above will report two soft assertion failures, with the following message::
+
+        SoftAssertionError:
+        failure message (test_soft_assert.py:3)
+        soft_assert(None) (test_soft_assert.py:8)
+
+    """
+    def soft_assert_func(expr, fail_message=''):
+        if not expr:
+            caught_assert = _annote_failure(fail_message)
+            _thread_locals.caught_asserts.append(caught_assert)
+    # stash helper functions on soft_assert for easy access
+    soft_assert_func.catch_assert = _catch_assert_cm
+    soft_assert_func.caught_asserts = _get_caught_asserts
+    soft_assert_func.clear_asserts = _clear_caught_asserts
+    return soft_assert_func
