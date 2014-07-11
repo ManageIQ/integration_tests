b41bed2f2dec7cef24232f924559feb71522ac39
Pete Savage <psavage@redhat.com>
2014-07-11 10:52:04 +0100

    DockerBot now classified

---
+++ scripts/dockerbot/README.md
@@ -0,0 +1,214 @@
+dockerbot
+=========
+
+DockerBot for CFME
+------------------
+
+DockerBot is a completely disposable testing framework for CFME-QE. It uses docker to create
+separate "selenium" and "pytest" containers. The "selenium" container is maintained at the
+docker hub. Currently the "pytest" container is built locally to help keep it up to date
+with pip requirements updates. This will take a few minutes the first time, but then only needs
+to be run again occasionally.
+
+Getting Started
+---------------
+
+### Log depot
+
+To begin with, you will need to define a **log depot**. This is where DockerBot will store all
+of the artifacts from artifactor. To do this, create a folder and then apply the following if
+you are using SELinux.
+
+`sudo chcon -Rt svirt_sandbox_file_t /path/to/log_depot/`
+
+### Building the pytestbase image
+
+We then need to build the pytest docker container. To do this we need to change directory to the
+pytestbase folder which is contained in this repo and run the following.
+
+`docker build -t py_test_base .`
+
+This can be run multiple times if required to update the pip requirements from master.
+You may need to use the `--no-cache=true` to tell the build not to use the cached versions
+of the previous steps.
+
+### Obtaining the sel_ff_chrome image
+
+Next we need to grab the sel_ff_chrome docker image.
+
+```
+$ docker pull cfmeqe/sel_ff_chrome
+Pulling repository cfmeqe/sel_ff_chrome
+4c6781db3920: Pulling dependent layers
+6f18cdca0c75: Pulling dependent layers
+511136ea3c5a: Download complete
+fd241224e9cf: Download complete
+3f2fed40e4b0: Download complete
+94aebbf3b4a5: Download complete
+49417c3e890e: Download complete
+```
+
+Once this has finished, we can use DockerBot to run tests in totally isolated containers.
+
+### Configuration file
+
+Though not necessary, as DockerBot has a complete set of command line options, you should create
+a `docker.yaml` file to hold the most common options. This sits in the usual place for CFME
+testing framework config files.
+
+An example is below, notice that you can specify as many appliances as you want, and these will
+be referred to by name when invoking DockerBot.
+
+```yaml
+appliances:
+    Downstream: https://xx.xx.xx.xx
+    Upstream: https://yy.yy.yy.yy
+cfme_cred_repo: https://credentials/yaml/cfme-qe-yamls.git
+cfme_cred_repo_dir: /cfme-qe-yamls
+cfme_repo: https://github.com/RedHatQE/cfme_tests
+cfme_repo_dir: /cfme_tests_te
+pytest: -k test_bad_password
+branch: origin/master
+browser: firefox
+selff: cfmeqe/sel_ff_chrome
+pytest_con: py_test_base
+artifactor_dir: /log_report
+log_depot: /home/user/log_depot/
+banner: True
+gh_token: my_gh_token_for_api
+gh_owner: RedHatQE
+gh_repo: cfme_tests
+```
+
+Running DockerBot
+-----------------
+
+`$ scripts/dockerbot/docker_bot.py --appliance-name Downstream --pytest '-k test_bad_password' --watch --browser 'firefox'`
+
+This spawns two containers a "selenium" one and a "pytest" one. The `watch` option fires up a
+VNC viewer. This would run the test against the current master branch as defined in the
+docker.yaml. Read on for more advanced options.
+
+### Specific repo/branch testing
+
+Using the commanline options, DockerBot can be told to go to a different CFME repository, such
+as your own fork, and run a branch there, an example of this is below.
+
+`$ scripts/docker_bot.py --watch --output --appliance-name Downstream --cfme-repo https://github.com/psav/cfme_tests --branch disable_tracer --pytest 'py.test -k test_bad_password'`
+
+### PR testing
+
+DockerBot has a **PR** option which allows you to ask it to download a PR instead of a branch.
+This option will overide the `--branch` option and checkout the latest master and merge the PR
+on top. If this fails, the whole container should fail and the test output will indicate as such.
+
+`$ scripts/docker_bot.py --watch --output --appliance-name Downstream --pr 952 --pytest 'py.test -k test_bad_password'`
+
+### Auto Test Generation
+
+DockerBot is able to make educated guesses about which tests need to be run in order to test the PR.
+This will not always work, but can be used as a first case approximation. DockerBot will look for
+any files in the PR which are in the cfme/tests folder and include those in the run. When this option
+is used, the `--pytest` option does not need to be. At the moment, there is no way to pass other options
+to pytest when using the `--auto-gen-test` option.
+
+### Update Pip
+
+Currently DockerBot needs to be told if you want to update the pip. This can be done with the
+`--update-pip` option.
+
+Full DockerBot example
+----------------------
+
+In the example below we make use of the `output` option to get the output of the setup.sh script.
+There is also an `--update-pip` option which will update the requirements.txt prior to running
+the test. This is not performed by default.
+
+```
+$ scripts/dockerbot/dockerbot.py --watch --output --appliance-name Downstream --pytest 'py.test -k test_bad_password'
+==================================================================
+               ____             __             ____        __
+     :        / __ \____  _____/ /_____  _____/ __ )____  / /_
+   [* *]     / / / / __ \/ ___/ //_/ _ \/ ___/ __  / __ \/ __/
+  -[___]-   / /_/ / /_/ / /__/ ,< /  __/ /  / /_/ / /_/ / /_
+           /_____/\____/\___/_/|_|\___/_/  /_____/\____/\__/
+==================================================================
+
+  APPLIANCE: https://xx.xx.xx.xx (Downstream)
+  PYTEST Command: py.test -k test_bad_password
+  VNC_PORT: 46518
+  REPO: https://github.com/RedHatQE/cfme_tests
+  BROWSER: firefox
+  BRANCH: origin/master
+  LOG_ID: WE4zeGbJ
+  JSON: 35758
+  SMTP: 45529
+
+  Waiting for container for 10 seconds...
+  Initiating VNC watching...
+
+  Press Ctrl+C to kill tests + containers
+
+======================== \/ OUTPUT \/ ============================
+
+Cloning into '/cfme-qe-yamls'...
+Cloning into '/cfme_tests_te'...
+base_url: https://xx.xx.xx.xx
+browser:
+    webdriver_options:
+        command_executor: http://zz.zz.zz.zz:4444/wd/hub
+        desired_capabilities:
+            platform: LINUX
+            browserName: 'firefox'
+
+artifactor:
+    log_dir: /log_report
+    per_run: test #test, run, None
+    reuse_dir: True
+    squash_exceptions: True
+    threaded: True
+    server_address: 127.0.0.1
+    server_enabled: True
+    plugins:
+        logger:
+            enabled: True
+            plugin: logger
+            level: DEBUG
+        filedump:
+            enabled: True
+            plugin: filedump
+        reporter:
+            enabled: True
+            plugin: reporter
+            only_failed: False
+
+mail_collector:
+    ports:
+        smtp: 45529
+        json: 35758
+Checking out branch origin/master
+Note: checking out 'origin/master'.
+
+You are in 'detached HEAD' state. You can look around, make experimental
+changes and commit them, and you can discard any commits you make in this
+state without impacting any branches by performing another checkout.
+
+If you want to create a new branch to retain commits you create, you may
+do so (now or later) by using -b with the checkout command again. Example:
+
+  git checkout -b new_branch_name
+
+HEAD is now at a3d93d8... Merge pull request #960 from dajohnso/update_enable_int_db
+py.test py.test -k test_bad_password
+Instance: [merkyl] does not exist, is your configuration correct?
+Instance: [video] does not exist, is your configuration correct?
+============================= test session starts ==============================
+platform linux2 -- Python 2.7.5 -- py-1.4.20 -- pytest-2.5.2
+collected 558 items
+
+cfme/tests/test_login.py .
+
+===== 557 tests deselected by "-ktest_bad_password -m 'not long_running'" ======
+================== 1 passed, 557 deselected in 30.32 seconds ===================
+
+```
---
+++ scripts/dockerbot/dockerbot.py
@@ -0,0 +1,366 @@
+#!/usr/bin/env python2
+from utils.conf import docker as docker_conf
+from utils.randomness import generate_random_string
+from utils.net import random_port, my_ip_address
+import argparse
+import requests
+import os
+import os.path
+import docker
+import subprocess
+import sys
+
+
+def _dgci(d, key):
+    # dgci = dict get case-insensitive
+    keymap = {k.lower(): k for k in d.keys()}
+    return d.get(keymap[key.lower()])
+
+
+def _name(docker_info):
+    return _dgci(docker_info, 'name').strip('/')
+
+dc = docker.Client(base_url='unix://var/run/docker.sock',
+                   version='1.12',
+                   timeout=10)
+
+
+class DockerInstance(object):
+    def process_bindings(self, bindings):
+        self.port_bindings = {}
+        self.ports = []
+        for bind in bindings:
+            self.port_bindings[bindings[bind][0]] = bindings[bind][1]
+            print "  {}: {}".format(bind, bindings[bind][1])
+            self.ports.append(bindings[bind][1])
+
+    def wait(self):
+        dc.wait(self.container_id)
+
+    def stop(self):
+        dc.stop(self.container_id)
+
+    def remove(self):
+        dc.remove_container(self.container_id, v=True)
+
+    def kill(self):
+        dc.kill(self.container_id)
+
+
+class SeleniumDocker(DockerInstance):
+    def __init__(self, bindings, image):
+        sel_name = generate_random_string(size=8)
+        sel_create_info = dc.create_container(image, tty=True, name=sel_name)
+        self.container_id = _dgci(sel_create_info, 'id')
+        sel_container_info = dc.inspect_container(self.container_id)
+        self.sel_name = _name(sel_container_info)
+        self.process_bindings(bindings)
+
+    def run(self):
+        dc.start(self.container_id, privileged=True, port_bindings=self.port_bindings)
+
+
+class PytestDocker(DockerInstance):
+    def __init__(self, name, bindings, env, log_path, links, pytest_con, artifactor_dir):
+        self.links = links
+        self.log_path = log_path
+        self.artifactor_dir = artifactor_dir
+        self.process_bindings(bindings)
+
+        pt_name = name
+        pt_create_info = dc.create_container(pytest_con, tty=True,
+                                             name=pt_name, environment=env,
+                                             command='sh /setup.sh',
+                                             volumes=[artifactor_dir],
+                                             ports=self.ports)
+        self.container_id = _dgci(pt_create_info, 'id')
+        pt_container_info = dc.inspect_container(self.container_id)
+        pt_name = _name(pt_container_info)
+
+    def run(self):
+        dc.start(self.container_id, privileged=True, links=self.links,
+                 binds={self.log_path: {'bind': self.artifactor_dir, 'ro': False}},
+                 port_bindings=self.port_bindings)
+
+
+class DockerBot(object):
+    def __init__(self, **args):
+        self.args = args
+        self.validate_args()
+        self.display_banner()
+        self.process_appliance()
+        self.create_pytest_command()
+        self.sel_vnc_port = random_port()
+        sel = SeleniumDocker(bindings={'VNC_PORT': (5999, self.sel_vnc_port)},
+                             image=self.args['selff'])
+        sel.run()
+        sel_container_name = sel.sel_name
+        self.create_pytest_envvars()
+        self.handle_pr()
+        self.pytest_name = generate_random_string(size=8)
+        self.log_path = self.create_log_path()
+        self.pytest_bindings = self.create_pytest_bindings()
+        pytest = PytestDocker(name=self.pytest_name, bindings=self.pytest_bindings,
+                              env=self.env_details, log_path=self.log_path,
+                              links=[(sel_container_name, 'selff')],
+                              pytest_con=self.args['pytest_con'],
+                              artifactor_dir=self.args['artifactor_dir'])
+        pytest.run()
+        self.handle_watch()
+        try:
+            pytest.wait()
+        except KeyboardInterrupt:
+            print "  TEST INTERRUPTED....KILLING ALL THE THINGS"
+            pass
+        pytest.kill()
+        pytest.remove()
+        sel.kill()
+        sel.remove()
+        self.handle_output()
+
+    def find_files_by_pr(self, pr=None):
+        files = []
+        token = self.args['gh_token']
+        owner = self.args['gh_owner']
+        repo = self.args['gh_repo']
+        if token:
+            headers = {'Authorization': 'token {}'.format(token)}
+            r = requests.get(
+                'https://api.github.com/repos/{}/{}/pulls/{}/files'.format(owner, repo, pr),
+                headers=headers)
+            try:
+                for filen in r.json():
+                    if filen['filename'].startswith('cfme/tests') and filen['status'] != "deleted":
+                        files.append(filen['filename'])
+                return files
+            except:
+                return None
+
+    def validate_args(self):
+        ec = 0
+        self.args['banner'] = self.args.get('banner', True)
+        self.args['watch'] = self.args.get('watch', True)
+        self.args['output'] = self.args.get('output', True)
+
+        self.args['appliance_name'] = self.args.get('appliance_name', None)
+        self.args['appliance'] = self.args.get('appliance', None)
+
+        if not self.args['appliance_name'] != self.args['appliance']:
+            print "You must supply either an appliance OR an appliance name from config"
+            ec += 1
+
+        self.args['branch'] = self.args.get('branch', 'master')
+        self.args['pr'] = self.args.get('pr', None)
+
+        if not self.args.get('cfme_repo', None):
+            print "You must supply a CFME REPO"
+            ec += 1
+        self.args['cfme_repo_dir'] = self.args.get('cfme_repo_dir', '/cfme_tests_te')
+        if not self.args.get('cfme_cred_repo'):
+            print "You must supply a CFME Credentials REPO"
+            ec += 1
+        self.args['cfme_cred_repo_dir'] = self.args.get('cfme_cred_repo_dir', '/cfme-qe-yamls')
+
+        self.args['gh_token'] = self.args.get('gh_token', None)
+        self.args['gh_owner'] = self.args.get('gh_owner', None)
+        self.args['gh_repo'] = self.args.get('gh_repo', None)
+
+        self.args['browser'] = self.args.get('browser', 'firefox')
+        if not self.args.get('pytest', None):
+            print "You must specify a py.test command"
+            ec += 1
+        self.args['update_pip'] = self.args.get('update_pip', False)
+        self.args['auto_gen_test'] = self.args.get('auto_gen_test', False)
+        self.args['artifactor_dir'] = self.args.get('artifactor_dir', '/log_depot')
+        if not self.args.get('log_depot', None):
+            print "You must specify a log_depot"
+            ec += 1
+
+        if self.args['pr'] and self.args['auto_gen_test'] and not \
+           all([self.args['gh_token'], self.args['gh_owner'], self.args['gh_repo']]):
+            print "You chose to use Auto Test Gen, without supplying GitHub details"
+            ec += 1
+
+        if ec:
+            sys.exit(127)
+
+    def display_banner(self):
+        if self.args['banner']:
+            banner = """
+==================================================================
+               ____             __             ____        __
+     :        / __ \____  _____/ /_____  _____/ __ )____  / /_
+   [* *]     / / / / __ \/ ___/ //_/ _ \/ ___/ __  / __ \/ __/
+  -[___]-   / /_/ / /_/ / /__/ ,< /  __/ /  / /_/ / /_/ / /_
+           /_____/\____/\___/_/|_|\___/_/  /_____/\____/\__/
+==================================================================
+            """
+            print banner
+
+    def process_appliance(self):
+        self.appliance = self.args['appliance']
+        self.app_name = self.args.get('appliance_name', "Unnamed")
+        print "  APPLIANCE: {} ({})".format(self.appliance, self.app_name)
+
+    def create_pytest_command(self):
+        if self.args['auto_gen_test'] and self.args['pr']:
+            files = self.find_files_by_pr(self.args['pr'])
+            if files:
+                self.args['pytest'] = "py.test {} --use-provider default". \
+                                      format(" ".join(files))
+            else:
+                print "  Could not autogenerate test, the following files were "
+                print "  modified but included no tests..."
+                print
+                print files
+                print
+                print "Exiting..."
+                sys.exit(127)
+        print "  PYTEST Command: {}".format(self.args['pytest'])
+
+    def create_pytest_envvars(self):
+        self.env_details = {'APPLIANCE': self.appliance,
+                            'BROWSER': self.args['browser'],
+                            'CFME_CRED_REPO': self.args['cfme_cred_repo'],
+                            'CFME_CRED_REPO_DIR': self.args['cfme_cred_repo_dir'],
+                            'CFME_REPO': self.args['cfme_repo'],
+                            'CFME_REPO_DIR': self.args['cfme_repo_dir'],
+                            'CFME_MY_IP_ADDRESS': my_ip_address(),
+                            'PYTEST': self.args['pytest'],
+                            'BRANCH': self.args['branch'],
+                            'ARTIFACTOR_DIR': self.args['artifactor_dir']}
+        print "  REPO: {}".format(self.args['cfme_repo'])
+        print "  BROWSER: {}".format(self.args['browser'])
+        if self.args['update_pip']:
+            print "  PIP: will be updated!"
+            self.env_details['UPDATE_PIP'] = 'True'
+
+    def handle_pr(self):
+        if self.args['pr']:
+            self.env_details['CFME_PR'] = self.args['pr']
+            print "  PR Number: {}".format(self.args['pr'])
+        else:
+            print "  BRANCH: {}".format(self.args['branch'])
+
+    def create_log_path(self):
+        log_path = os.path.join(self.args['log_depot'], self.pytest_name)
+        os.mkdir(log_path)
+        print "  LOG_ID: {}".format(self.pytest_name)
+        return log_path
+
+    def create_pytest_bindings(self):
+        sel_json = random_port()
+        sel_smtp = random_port()
+        bindings = {'JSON': (sel_json, sel_json), 'SMTP': (sel_smtp, sel_smtp)}
+        self.env_details['JSON'] = sel_json
+        self.env_details['SMTP'] = sel_smtp
+        return bindings
+
+    def handle_watch(self):
+        if self.args['watch']:
+            print
+            print "  Waiting for container for 10 seconds..."
+            import time
+            time.sleep(10)
+            print "  Initiating VNC watching..."
+            ipport = "vnc://127.0.0.1:" + str(self.sel_vnc_port)
+            cmd = ['xdg-open', ipport]
+            subprocess.Popen(cmd)
+        print
+        print "  Press Ctrl+C to kill tests + containers"
+
+    def handle_output(self):
+        if self.args['output']:
+            print
+            print "======================== \/ OUTPUT \/ ============================"
+            print
+            f = open(os.path.join(self.log_path, 'setup.txt'))
+            print f.read()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(argument_default=None)
+
+    interaction = parser.add_argument_group('Interaction')
+    interaction.add_argument('--banner', action='store_true',
+                             help='Chooses upstream',
+                             default=docker_conf.get('banner', False))
+    interaction.add_argument('--watch', action='store_true',
+                             help='Watch it via VNC',
+                             default=None)
+    interaction.add_argument('--output', action='store_true',
+                             help="Output the console?",
+                             default=None)
+
+    appliance = parser.add_argument_group('Appliance Options')
+    appliance.add_argument('--appliance-name',
+                           help=('Chooses an appliance from the config by name'
+                                 'or sets a name if used with --appliance'),
+                           default='CLI Speficied')
+    appliance.add_argument('--appliance',
+                           help='Chooses an appliance address',
+                           default=None)
+
+    repo = parser.add_argument_group('Repository Options')
+    repo.add_argument('--branch',
+                      help='The branch name',
+                      default=docker_conf.get('branch', 'origin/master'))
+    repo.add_argument('--pr',
+                      help='A PR Number (overides --branch)',
+                      default=None)
+    repo.add_argument('--cfme-repo',
+                      help='The cfme repo',
+                      default=docker_conf.get('cfme_repo', None))
+    repo.add_argument('--cfme-repo-dir',
+                      help='The cfme repo dir',
+                      default=docker_conf.get('cfme_repo_dir', '/cfme_tests_te'))
+    repo.add_argument('--cfme-cred-repo',
+                      help='The cfme cred repo',
+                      default=docker_conf.get('cfme_cred_repo', None))
+    repo.add_argument('--cfme-cred-repo-dir',
+                      help='The cfme cred repo dir',
+                      default=docker_conf.get('cfme_cred_repo_dir', '/cfme-qe-yamls'))
+
+    gh = parser.add_argument_group('GitHub Options')
+    gh.add_argument('--gh-token',
+                    help="The GitHub Token to use",
+                    default=docker_conf.get('gh_token', None))
+    gh.add_argument('--gh-owner',
+                    help="The GitHub Owner to use",
+                    default=docker_conf.get('gh_owner', None))
+    gh.add_argument('--gh-repo',
+                    help="The GitHub Repo to use",
+                    default=docker_conf.get('gh_repo', None))
+
+    dkr = parser.add_argument_group('Docker Container Options')
+    dkr.add_argument('--selff',
+                     help="The selenium base docker image",
+                     default=docker_conf.get('selff', 'cfme/sel_ff_chrome'))
+    dkr.add_argument('--pytest_con',
+                     help="The pytest container image",
+                     default=docker_conf.get('pytest_con', 'py_test_base'))
+
+    pytest = parser.add_argument_group('PyTest Options')
+    pytest.add_argument('--browser',
+                        help='The browser',
+                        default=docker_conf.get('browser', 'firefox'))
+    pytest.add_argument('--pytest',
+                        help='The pytest command',
+                        default=docker_conf.get('pytest', None))
+    pytest.add_argument('--update-pip', action='store_true',
+                        help='If we should update requirements',
+                        default=None)
+    pytest.add_argument('--auto-gen-test', action='store_true',
+                        help="Attempt to auto generate related tests (requires --pr)",
+                        default=None)
+    pytest.add_argument('--artifactor-dir',
+                        help="The Artifactor dir",
+                        default=docker_conf.get('artifactor_dir', '/log_depot'))
+    pytest.add_argument('--log_depot',
+                        help="The log_depot",
+                        default=docker_conf.get('log_depot', None))
+    args = parser.parse_args()
+
+    if args.appliance_name and not args.appliance:
+        args.appliance = docker_conf['appliances'][args.appliance_name]
+
+    ab = DockerBot(**vars(args))
