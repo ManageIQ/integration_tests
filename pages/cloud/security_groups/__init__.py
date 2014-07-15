a0ef074c07615611d5df16a811df3d2b4c3a98e5
Pete Savage <psavage@redhat.com>
2014-07-16 00:55:41 +0100

    Destroyed ALL THE THINGS

--- pages/cloud/security_groups/__init__.py
+++
@@ -1,5 +0,0 @@
-from pages.base import Base
-
-
-class SecurityGroups(Base):
-    _page_title = 'CloudForms Management Engine: Security Groups'
--- tests/ui/infrastructure/test_paginator.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from unittestzero import Assert
-
-class TestPaginator:
-    def test_paginator(self, setup_infrastructure_providers, infra_vms_pg):
-        Assert.true(infra_vms_pg.is_the_current_page)
-        Assert.true(infra_vms_pg.paginator.selected_per_page, '20')
-        infra_vms_pg.paginator.set_per_page('100')
-        Assert.true(infra_vms_pg.paginator.selected_per_page, '100')
