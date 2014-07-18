c3bfe04ec19e2b862ba72058a9ecbfbadd53083c
Dave Johnson <dajo@redhat.com>
2014-07-18 14:38:09 -0400

    5.3 compile assets script

---
+++ scripts/precompile_assets.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2
+
+"""SSH into a running appliance and compile ui assets.
+"""
+
+import argparse
+import sys
+from utils.conf import credentials
+from utils.ssh import SSHClient
+
+
+def main():
+    parser = argparse.ArgumentParser(epilog=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('address', help='hostname or ip address of target appliance',
+        nargs='?', default=None)
+
+    args = parser.parse_args()
+
+    ssh_kwargs = {
+        'username': credentials['ssh']['username'],
+        'password': credentials['ssh']['password'],
+    }
+    if args.address:
+        ssh_kwargs['hostname'] = args.address
+
+    # Init SSH client
+    ssh_client = SSHClient(**ssh_kwargs)
+
+    # compile assets if required (not required on 5.2)
+    if not ssh_client.get_version().startswith("5.2"):
+        if ssh_client.run_command("ls /var/www/miq/vmdb/public/assets")[0] != 0:
+            ssh_client.run_rake_command("assets:precompile")
+            ssh_client.run_rake_command("evm:restart")
+
+    print "CFME UI worker restarted, UI should be available shortly"
+
+if __name__ == '__main__':
+    sys.exit(main())
