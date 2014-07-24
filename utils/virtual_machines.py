ad37f6eab08ce436f1a0d31eae82b4dbc1c48677
Jan Krocil <jkrocil@redhat.com>
2014-07-24 13:04:05 +0200

    Update utils.mgmt_system:  - EC2 can now set_name (Name tag)  - shifted responsibility that Vm/instance is provisioned to mgmt_system  - implemented some missing methods and updated docs
    Fix utils.virtual_machines to be able to provision cloud instances as well
    
    Update fixtures.virtual_machines to be usable with instances as well

--- utils/virtual_machines.py
+++ utils/virtual_machines.py
@@ -1,46 +1,60 @@
-"""Helper functions related to the creation and destruction of providers
+"""Helper functions related to the creation and destruction of virtual machines and instances
 """
 
+from cfme.cloud.provider import get_from_config as get_cloud_from_config
+from cfme.exceptions import UnknownProviderType
+from cfme.infrastructure.provider import get_from_config as get_infra_from_config
 from utils.log import logger
-from utils.mgmt_system import RHEVMSystem, EC2System, OpenstackSystem
-from utils.wait import wait_for
+from utils.mgmt_system import RHEVMSystem, VMWareSystem, EC2System, OpenstackSystem
 
 
-def deploy_template(provider_crud, vm_name, template_name=None, timeout_in_minutes=15):
+def deploy_template(provider_key, vm_name, template_name=None, timeout_in_minutes=15,
+                    **deploy_args):
+    try:
+        provider_crud = get_infra_from_config(provider_key)
+    except UnknownProviderType:
+        provider_crud = get_cloud_from_config(provider_key)
     mgmt = provider_crud.get_mgmt_system()
     data = provider_crud.get_yaml_data()
-    deploy_args = {}
+
     deploy_args.update(vm_name=vm_name)
     if isinstance(mgmt, RHEVMSystem):
-        deploy_args.update(cluster_name=data['default_cluster'])
+        if 'default_cluster' not in deploy_args:
+            deploy_args.update(cluster_name=data['default_cluster'])
+    elif isinstance(mgmt, VMWareSystem):
+        pass
     elif isinstance(mgmt, EC2System):
-        deploy_args.update(instance_type=data['default_flavor'])
+        pass
     elif isinstance(mgmt, OpenstackSystem):
-        deploy_args.update(flavour_name=data['default_flavor'])
-        deploy_args.update(assign_floating_ip=data['default_ip_pool'])
+        if ('network_name' not in deploy_args) and data.get('network'):
+            deploy_args.update(network_name=data['network'])
+    else:
+        raise Exception("Unsupported provider type: %s" % mgmt.__class__.__name__)
 
     if template_name is None:
         template_name = data['small_template']
 
-    logger.info("Getting ready to deploy VM %s from template %s on provider %s" %
+    logger.info("Getting ready to deploy VM/instance %s from template %s on provider %s" %
         (vm_name, template_name, data['name']))
-
     try:
-        logger.debug("deploy args: " + str(deploy_args))
-        mgmt.deploy_template(template_name, **deploy_args)
-        wait_for(mgmt.does_vm_exist, [vm_name], num_sec=timeout_in_minutes * 60, delay=30)
+        logger.debug("Deploy args: " + str(deploy_args))
+        vm_name = mgmt.deploy_template(template_name, timeout_in_minutes=timeout_in_minutes,
+                                       **deploy_args)
+        logger.info("Provisioned VM/instance %s" % vm_name)  # instance ID in case of EC2
     except Exception as e:
-        logger.error('Could not provisioning VM %s (%s)', vm_name, e.message)
-        logger.info('Attempting cleanup on VM %s', vm_name)
+        logger.error('Could not provisioning VM/instance %s (%s)', vm_name, e)
+        logger.info('Attempting cleanup on VM/instance %s', vm_name)
         try:
             if mgmt.does_vm_exist(vm_name):
                 # Stop the vm first
-                logger.warning('Destroying VM %s', vm_name)
+                logger.warning('Destroying VM/instance %s', vm_name)
                 if mgmt.delete_vm(vm_name):
-                    logger.info('VM %s destroyed', vm_name)
+                    logger.info('VM/instance %s destroyed', vm_name)
                 else:
-                    logger.error('Error destroying VM %s', vm_name)
+                    logger.error('Error destroying VM/instance %s', vm_name)
         except Exception as f:
-            logger.error('Could not destroy VM %s (%s)', vm_name, f.message)
+            logger.error('Could not destroy VM/instance %s (%s)', vm_name, f)
         finally:
             raise e
+
+    return vm_name
