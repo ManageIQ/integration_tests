f981344a20a561527d18e4744d832a032bb9db26
Shveta <shveta@localhost.localdomain>
2014-08-08 18:40:26 -0700

    service Retirement

--- cfme/services/catalogs/service_catalogs.py
+++ cfme/services/catalogs/service_catalogs.py
@@ -2,15 +2,14 @@
 from functools import partial
 
 import cfme.fixtures.pytest_selenium as sel
-from cfme.web_ui import Form, accordion, fill, flash, menu
+from cfme.web_ui import Form, accordion, flash, menu
 from utils.update import Updateable
 from utils.pretty import Pretty
 
 order_button = "//img[@title='Order this Service']"
 
 service_order_form = Form(
-    fields=[('dialog_service_name_field', "//tr/td[@title='ele_desc']/input[@id='service_name']"),
-            ('submit_button', "//img[@title='Submit']")])
+    fields=[('submit_button', "//img[@title='Submit']")])
 
 accordion_tree = partial(accordion.tree, "Service Catalogs")
 
@@ -44,6 +43,5 @@ class ServiceCatalogs(Updateable, Pretty):
     def order(self, catalog, catalog_item):
         sel.force_navigate('order_service_catalog',
             context={'catalog': catalog, 'catalog_item': catalog_item})
-        fill(service_order_form, {'dialog_service_name_field': self.service_name},
-            action=service_order_form.submit_button)
+        sel.click(service_order_form.submit_button)
         flash.assert_success_message("Order Request was Submitted")
