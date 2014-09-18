b3853cedf9e084de828432d56dacad1b6e9218e4
Jeff Weiss <jweiss@redhat.com>
2014-09-18 17:19:33 -0400

    Test utilization, testgen fixture, both cloud and infra
       Turn off collection after each test
    
       Use one provider at a time, removing the others, to minimize resource
       usage

--- cfme/configure/configuration/candu.py
+++ cfme/configure/configuration/candu.py
@@ -10,9 +10,18 @@ form = ui.Form(
     ])
 
 
+def _enable_disable(enable=True):
+    sel.force_navigate("cfg_settings_region_cu_collection")
+    ui.fill(form, {'all_clusters_cb': enable,
+                   'all_datastores_cb': enable},
+            action=form_buttons.save)
+
+
 def enable_all():
     """Enable all C&U metric collection for this region"""
-    sel.force_navigate("cfg_settings_region_cu_collection")
-    ui.fill(form, {'all_clusters_cb': True,
-                   'all_datastores_cb': True},
-            action=form_buttons.save)
+    _enable_disable()
+
+
+def disable_all():
+    """Enable all C&U metric collection for this region"""
+    _enable_disable(False)
