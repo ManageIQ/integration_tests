b8a624c80a4b8f2b866854b67143f4a3da4bc34f
Pete Savage <psavage@redhat.com>
2014-12-03 09:48:59 +0000

    Some settings speed up work

--- utils/tests/test_db_queries.py
+++ utils/tests/test_db_queries.py
@@ -3,29 +3,28 @@
 """Run against fresh instance to ensure that the data checked is correct.
 """
 import pytest
+from fixtures.pytest_store import store
 from utils import db_queries
-from utils.conf import env
-from urlparse import urlparse
 
 
 @pytest.fixture
 def appliance_ip():
-    return urlparse(env["base_url"]).netloc
+    return store.current_appliance.address
 
 
 def test_configuration_details(appliance_ip):
-    result = db_queries.get_configuration_details(appliance_ip)
+    result = db_queries.get_configuration_details(ip_address=appliance_ip)
     assert result is not None, "get_configuration_details returned None"
 
 
 def test_server_name(appliance_ip):
-    assert db_queries.get_server_name(appliance_ip) == "EVM"
+    assert store.current_appliance.server_name() == "EVM"
 
 
 def test_server_region_and_id(appliance_ip):
     """Server ID begins with region number, so check that.
     """
-    region = db_queries.get_server_region(appliance_ip)
+    region = store.current_appliance.server_region()
     if region == 0:
         pytest.skip("Can't check this if the region is 0")
-    assert str(db_queries.get_server_id(appliance_ip)).startswith(str(region))
+    assert str(store.current_appliance.server_id()).startswith(str(region))
