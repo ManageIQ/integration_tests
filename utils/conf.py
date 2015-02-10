86068d5bbb9c2cb64801debc0ba1dd8b4c56246e
Sean Myers <sean.myers@redhat.com>
2015-02-10 15:27:29 -0500

    Convert utils.conf to yaycl

--- utils/conf.py
+++ utils/conf.py
@@ -1,5 +1,17 @@
 import sys
 
-from utils._conf import Config
+from yaycl import Config
 
-sys.modules[__name__] = Config(__file__)
+from utils import path
+
+yaycl_options = {
+    'config_dir': path.conf_path.strpath
+}
+
+# Look for the .yaml_key file, use it if it exists
+crypt_key_file = path.project_path.join('.yaml_key')
+if crypt_key_file.exists():
+    yaycl_options['crypt_key_file'] = crypt_key_file.strpath
+
+# Replace this module with the yaycl conf
+sys.modules[__name__] = Config(**yaycl_options)
