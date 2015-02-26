9df499a7c54d5eff5517eb4c4be2cb5fc11c5ef2
Pete Savage <psavage@redhat.com>
2015-02-26 14:48:11 +0000

    Typo fix

--- cfme/web_ui/__init__.py
+++ cfme/web_ui/__init__.py
@@ -10,7 +10,7 @@
 * **Elemental**
 
   * :py:class:`ButtonGroup`
-  * :py:class:`ColourPicker`
+  * :py:class:`ColorGroup`
   * :py:class:`CheckboxTable`
   * :py:class:`CheckboxSelect`
   * :py:class:`DHTMLSelect`
