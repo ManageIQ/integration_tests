4575c3edf70f2c9dd375db1e4a1bd8ad86bb389b
Pete Savage <psavage@redhat.com>
2015-03-05 18:37:55 +0000

    Alteration to logic

--- scripts/matrix.py
+++ scripts/matrix.py
@@ -113,8 +113,8 @@ for test_data in elem:
                 status = 'Skipped'
             elif element.tag == 'error':
                 status = 'Error'
-            else:
-                status = 'Passed'
+        else:
+            status = 'Passed'
 
         test = suite.cache_tests.get(node_name, None)
         if not test:
