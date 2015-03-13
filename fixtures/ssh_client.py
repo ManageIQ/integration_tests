739e470e404d2038e3e362e3bb65b5068ed86c5f
Sean Myers <sean.myers@redhat.com>
2015-03-13 16:17:17 -0400

    SSH connection reuse and pubkey auth support

--- fixtures/ssh_client.py
+++ fixtures/ssh_client.py
@@ -1,32 +1,28 @@
 import pytest
 
-from utils.ssh import SSHClient
+from fixtures.pytest_store import store
 
 
 @pytest.fixture(scope="function")
 def ssh_client(uses_ssh):
     """SSH Client Fixture
 
-    By default, it will connect to the host named in the mozwebqa baseurl on use,
-    using the 'ssh' credentials in your credentials file. The mozwebqa fixture is used
-    for this, which will open up a selenium browser by default. If this is undesired,
-    remember to use the skip_selenium mark.
-
     Usage:
 
-        @pytest.mark.skip_selenium
         def test_ssh(ssh_client):
             # Run a basic command
-            exit_status, output = ssh_client.run_command('ls -al')
-            # Exit status is the numeric return code from the called command,
+            result = ssh_client.run_command('ls -al')
+            # rc is the numeric return code from the called command,
             # so 0 means everything is OK.
-            assert exit_status == 0
+            assert result.rc == 0
+            # and the output is available, too
+            print result.output
 
             # Run a task using the CFME rails runner CLI
-            exit_status, output = ssh_client.run_rails_command('do stuff')
+            ssh_client.run_rails_command('do stuff')
 
             # More useful: Run a rake task using the correct invokation
-            exit_status, output = ssh_client.run_rake_command('evm:stop')
+            ssh_client.run_rake_command('evm:stop')
 
     Additionally, the ssh_client fixture can be used to create other ssh clients,
     if you need to connect to multiple hosts in a test run::
@@ -45,6 +41,5 @@ def ssh_client(uses_ssh):
             # Hint: **credentials['credentials_key'], e.g.
             ssh_client_4 = ssh_client(hostname='different.host', **credentials['ssh'])
 
-
     """
-    return SSHClient()
+    return store.current_appliance.ssh_client()
