6a933828ab48bd233984d035ce057731d9edf756
Milan Falešník <mfalesni@redhat.com>
2015-04-01 13:56:21 +0200

    Sprout - Disable appliance automatic renaming until resolved

--- sprout/appliances/tasks.py
+++ sprout/appliances/tasks.py
@@ -426,7 +426,8 @@ def apply_lease_times_after_pool_fulfilled(self, appliance_pool_id, time_minutes
     if pool.fulfilled:
         for appliance in pool.appliances:
             apply_lease_times.delay(appliance.id, time_minutes)
-        rename_appliances_for_pool.delay(pool.id)
+        # TODO: Renaming disabled until orphaning and killing resolved
+        # rename_appliances_for_pool.delay(pool.id)
     else:
         self.retry(args=(appliance_pool_id, time_minutes), countdown=30, max_retries=50)
 
