ac6451e7e2a700292d8ea72b361ebb24ff698615
Pete Savage <psavage@redhat.com>
2015-04-17 14:08:20 +0100

    Simple list test utility

---
+++ scripts/list_tests.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2
+
+import os
+import os.path
+import sys
+import re
+
+if len(sys.argv) == 1:
+    print """
+Invoke either by supplying a path or a file and optionally a string to find in a test name
+
+e.g. list_test . provision
+e.g. list_test file.py
+"""
+    sys.exit(1)
+
+
+def parser(filename, exp=None):
+    try:
+        with open(filename, 'r') as f:
+            data = f.read()
+    except IOError:
+        data = ""
+
+    if not exp:
+        exp = ""
+
+    p = re.findall('\s*def\s*[a-zA-Z0-9_]*?(test_.*?{}.*?)\('.format(exp), data)
+    for test in p:
+        print "{} :: {}".format(filename, test)
+
+exp = None
+if len(sys.argv) == 3:
+    exp = sys.argv[2]
+
+if sys.argv[1].endswith('.py'):
+    parser(sys.argv[1], exp)
+else:
+    files = [os.path.join(d, fn) for d, dn, fns in os.walk(sys.argv[1]) for fn in fns]
+
+    for filename in files:
+        if filename.endswith('.py'):
+            parser(filename, exp)
