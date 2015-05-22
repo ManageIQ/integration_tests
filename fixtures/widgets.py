5a8dd9e954b25ecf7a79a80b97ec6fdfc0ecbd35
Milan Falešník <mfalesni@redhat.com>
2015-05-22 10:12:37 +0200

    Widgets - do not hit generate button until the generation has finished

--- fixtures/widgets.py
+++ fixtures/widgets.py
@@ -4,6 +4,10 @@ import pytest
 from cfme.dashboard import Widget
 from cfme.intelligence.reports import widgets
 
+from utils.wait import wait_for
+
+WAIT_STATES = {"Queued", "Running"}
+
 
 @pytest.fixture(scope="session")
 def widgets_generated(any_provider_session):
@@ -13,4 +17,5 @@ def widgets_generated(any_provider_session):
         widget_list.append((widget.name, widget.content_type))
     for w_name, w_type in widget_list:
         w = widgets.Widget.detect(w_type, w_name)
+        wait_for(w.check_status, fail_condition=WAIT_STATES, delay=5, num_sec=180)
         w.generate()
