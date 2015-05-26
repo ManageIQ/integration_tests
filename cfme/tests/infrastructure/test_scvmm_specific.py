c41b5a296430c40f541a4db5372a5744f491bc8b
Milan Falešník <mfalesni@redhat.com>
2015-05-26 15:16:53 +0200

    Missing host_group

--- cfme/tests/infrastructure/test_scvmm_specific.py
+++ cfme/tests/infrastructure/test_scvmm_specific.py
@@ -13,9 +13,13 @@ def pytest_generate_tests(metafunc):
 
 
 @pytest.mark.meta(blockers=[1178961])
-def test_no_dvd_ruins_refresh(provider_mgmt, provider_crud, small_template):
+@pytest.mark.uncollectif(
+    lambda provider_data: "host_group" in provider_data.get("provisioning", {}))
+def test_no_dvd_ruins_refresh(provider_mgmt, provider_crud, provider_data, small_template):
+    host_group = provider_data["provisioning"]["host_group"]
     with provider_mgmt.with_vm(
-            small_template, vm_name="test_no_dvd_{}".format(fauxfactory.gen_alpha())) as vm_name:
+            small_template, vm_name="test_no_dvd_{}".format(fauxfactory.gen_alpha()),
+            host_group=host_group) as vm_name:
         provider_mgmt.disconnect_dvd_drives(vm_name)
         vm = Vm(vm_name, provider_crud)
         provider_crud.refresh_provider_relationships()
