47f74aaa7b9c7b122f1fe3678ab0f15921f45626
Jan Krocil <jkrocil@redhat.com>
2015-05-26 15:06:21 +0200

    Fix docs in 5.4

--- cfme/configure/about.py
+++ cfme/configure/about.py
@@ -3,20 +3,44 @@ from cfme.web_ui import Region
 from utils import version
 
 product_assistance = Region(
-    locators={
-        'quick_start_guide': "//a[normalize-space(.)='Quick Start Guide']",
-        'insight_guide': "//a[normalize-space(.)='Insight Guide']",
-        'control_guide': "//a[normalize-space(.)='Control Guide']",
-        'lifecycle_and_automation_guide':
-        "//a[normalize-space(.)='Lifecycle and Automation Guide']",
-        'integrate_guide': {
-            version.LOWEST: "//a[normalize-space(.)='Integrate Guide']",
-            '5.3': "//a[normalize-space(.)='Integration Services Guide']",
-            '5.4': "//a[normalize-space(.)='REST API Guide']"
+    locators=version.pick({
+        version.LOWEST: {
+            'quick_start_guide': "//a[normalize-space(.)='Quick Start Guide']",
+            'install_guide': "//a[normalize-space(.)='Installation Guide']",
+            'insight_guide': "//a[normalize-space(.)='Insight Guide']",
+            'control_guide': "//a[normalize-space(.)='Control Guide']",
+            'lifecycle_and_automation_guide':
+                "//a[normalize-space(.)='Lifecycle and Automation Guide']",
+            'integrate_guide': "//a[normalize-space(.)='Integrate Guide']",
+            'settings_and_operations_guide':
+                "//a[normalize-space(.)='Settings and Operations Guide']",
+            'red_hat_customer_portal': "//a[normalize-space(.)='Red Hat Customer Portal']"
         },
-        'settings_and_operations_guide': "//a[normalize-space(.)='Settings and Operations Guide']",
-        'red_hat_customer_portal': "//a[normalize-space(.)='Red Hat Customer Portal']"
-    },
+        '5.3': {
+            'quick_start_guide': "//a[normalize-space(.)='Quick Start Guide']",
+            'insight_guide': "//a[normalize-space(.)='Insight Guide']",
+            'control_guide': "//a[normalize-space(.)='Control Guide']",
+            'lifecycle_and_automation_guide':
+                "//a[normalize-space(.)='Lifecycle and Automation Guide']",
+            'integrate_guide': "//a[normalize-space(.)='Integration Services Guide']",
+            'settings_and_operations_guide':
+                "//a[normalize-space(.)='Settings and Operations Guide']",
+            'red_hat_customer_portal': "//a[normalize-space(.)='Red Hat Customer Portal']"
+        },
+        '5.4': {
+            'quick_start_guide': "//a[normalize-space(.)='Quick Start Guide']",
+            'insight_guide': "//a[normalize-space(.)='Insight Guide']",
+            'control_guide': "//a[normalize-space(.)='Control Guide']",
+            'lifecycle_and_automation_guide':
+                "//a[normalize-space(.)='Lifecycle and Automation Guide']",
+            'rest_guide': "//a[normalize-space(.)='REST API Guide']",
+            'soap_guide': "//a[normalize-space(.)='SOAP API Guide']",
+            'user_guide': "//a[normalize-space(.)='User Guide']",
+            'settings_and_operations_guide':
+                "//a[normalize-space(.)='Settings and Operations Guide']",
+            'red_hat_customer_portal': "//a[normalize-space(.)='Red Hat Customer Portal']"
+        }
+    }),
     title='About',
     identifying_loc='quick_start_guide',
     infoblock_type="form"
