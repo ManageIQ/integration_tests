9b6e0f6978e3e4a8c16f77f4d55a8618bc8e53ea
Pete Savage <psavage@redhat.com>
2015-05-26 17:41:32 +0100

    Fixed test collection

--- cfme/tests/infrastructure/test_scvmm_specific.py
+++ cfme/tests/infrastructure/test_scvmm_specific.py
@@ -9,12 +9,13 @@ from utils import testgen
 def pytest_generate_tests(metafunc):
     argnames, argvalues, idlist = testgen.provider_by_type(
         metafunc, ['scvmm'], 'small_template')
-    metafunc.parametrize(argnames, argvalues, ids=idlist, scope='module')
+    testgen.parametrize(metafunc, argnames, argvalues, ids=idlist, scope='module')
 
 
 @pytest.mark.meta(blockers=[1178961])
 @pytest.mark.uncollectif(
-    lambda provider_data: "host_group" in provider_data.get("provisioning", {}))
+    lambda provider_data: "host_group" in provider_data.get("provisioning", {}),
+    reason="No host group")
 def test_no_dvd_ruins_refresh(provider_mgmt, provider_crud, provider_data, small_template):
     host_group = provider_data["provisioning"]["host_group"]
     with provider_mgmt.with_vm(
