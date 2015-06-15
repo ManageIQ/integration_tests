95071fed67877235526b4cf0501b716919d82c15
Jan Kročil <jkrocil@redhat.com>
2015-06-15 14:46:13 +0200

    Filter out None locators in about tests

--- cfme/tests/configure/test_about_links.py
+++ cfme/tests/configure/test_about_links.py
@@ -2,14 +2,21 @@
 
 from cfme.fixtures import pytest_selenium as sel
 from cfme.configure import about
+from utils import version
+from utils.log import logger
 import pytest
 import requests
 
 
 @pytest.mark.sauce
+@pytest.mark.meta(blockers=["GH#ManageIQ/manageiq:2246"])
 def test_about_links():
     sel.force_navigate('about')
     for link_key, link_loc in about.product_assistance.locators.items():
+        # If its a dict to be ver-picked and the resulting loc is None
+        if isinstance(link_loc, dict) and version.pick(link_loc) is None:
+            logger.info("Skipping link '{}'; not present in this version".format(link_key))
+            continue
         href = sel.get_attribute(link_loc, 'href')
         try:
             resp = requests.head(href, verify=False, timeout=20)
