486f348703ff9d25aad1a59f093331f4441950fd
Milan Falešník <mfalesni@redhat.com>
2015-06-30 10:44:54 +0200

    Ensure VM analysis REST client is from the current appliance.

--- cfme/fixtures/rest_api.py
+++ cfme/fixtures/rest_api.py
@@ -5,14 +5,12 @@ See :py:func:`rest_api` and py:func:`rest_api_modscope`
 """
 import pytest
 
-from fixtures.pytest_store import store
-
 
 @pytest.fixture(scope="function")
 def rest_api():
-    return store._current_appliance[-1].rest_api
+    return pytest.store.current_appliance.rest_api
 
 
 @pytest.fixture(scope="module")
 def rest_api_modscope():
-    return store._current_appliance[-1].rest_api
+    return pytest.store.current_appliance.rest_api
