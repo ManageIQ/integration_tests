f6d31c5d7aa2c812f91c0b094b188916a60fbcf2
shveta <sshveta@redhat.com>
2015-07-28 21:46:53 -0700

    Logout locator fix

--- cfme/tests/services/test_catalog.py
+++ cfme/tests/services/test_catalog.py
@@ -30,7 +30,7 @@ def test_catalog_duplicate_name():
 
 
 @pytest.mark.sauce
-def test_permissions_catalog_add():
+def test_permissions_catalog_add(setup_cloud_providers):
     """ Tests that a catalog can be added only with the right permissions"""
     cat = Catalog(name=fauxfactory.gen_alphanumeric(),
                   description="my catalog")
