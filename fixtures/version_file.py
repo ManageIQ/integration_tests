e19f7c29a818bf9718d72380b52eae87564c44e1
Sean Myers <sean.myers@redhat.com>
2015-08-05 15:16:26 -0400

    stash the tested appliance version in the log dir during a test run

---
+++ fixtures/version_file.py
@@ -0,0 +1,9 @@
+import pytest
+
+from utils.path import log_path
+
+
+def pytest_sessionstart():
+    if pytest.store.parallelizer_role != 'slave':
+        with log_path.join('appliance_version').open('w') as appliance_version:
+            appliance_version.write(pytest.store.current_appliance.version.vstring)
