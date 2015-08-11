9890738571fd19fe0abd1f0ee673fc3f8cf89b40
Sean Myers <sean.myers@redhat.com>
2015-08-11 14:20:07 -0400

    sprout considers templates with no version to be "master", not "latest"

--- sprout/appliances/tasks.py
+++ sprout/appliances/tasks.py
@@ -357,7 +357,7 @@ def prepare_template_verify_version(self, template_id):
     except Exception as e:
         template.set_status("Some SSH error happened during appliance version check.")
         self.retry(args=(template_id,), exc=e, countdown=20, max_retries=5)
-    supposed_version = template.version if template.version is not None else "latest"
+    supposed_version = template.version if template.version is not None else "master"
     if true_version != supposed_version:
         # SPAM SPAM SPAM!
         with transaction.atomic():
