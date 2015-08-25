2f620926c624118607cbff052e9d990e0f969f1b
Taras Lehinevych <tlehinevych@gmail.com>
2015-08-25 12:59:20 +0300

    Added the authorization through OAuth 2.0 + implement and test the basic crud operation with vm

--- utils/mgmt_system/google.py
+++ utils/mgmt_system/google.py
@@ -5,27 +5,48 @@ Used to communicate with providers without using CFME facilities
 """
 
 import time
-
-from oauth2client.client import GoogleCredentials
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import run
 from googleapiclient.discovery import build
 from utils.mgmt_system.base import MgmtSystemAPIBase, VMInfo
 from utils.mgmt_system.exceptions import VMInstanceNotFound
 from utils.log import logger
 
+CLIENT_SECRETS = 'client_secrets.json'
+OAUTH2_STORAGE = 'oauth2.dat'
+SCOPE = 'https://www.googleapis.com/auth/compute'
 
-class GoogleSystem (MgmtSystemAPIBase):
+
+class GoogleCloudSystem (MgmtSystemAPIBase):
     """
     Client to Google Cloud Platform API
 
     """
 
+    states = {
+        'running': ('RUNNING',),
+        'stopped': ('TERMINATED',),
+        'starting': ('STAGING'),
+        'stopping': ('STOPPING'),
+    }
+
     def __init__(self):
         self._project = "cfme-1044"
         self._zone = 'us-central1-f'
+
+        # Default name of the instance name from config file
         self._instance_name = 'demo-instance'
-        # Currently using the default credentials from local machine
-        # Will be re-writing to use OAuth 2.0
-        self._credentials = GoogleCredentials.get_application_default()
+
+        # Perform OAuth 2.0 authorization.
+        # based on OAuth 2.0 client IDs credentials from client_secretes file
+        flow = flow_from_clientsecrets(CLIENT_SECRETS, scope=SCOPE)
+        storage = Storage(OAUTH2_STORAGE)
+        self._credentials = storage.get()
+
+        if self._credentials is None or self._credentials.invalid:
+            self._credentials = run(flow, storage)
+
         self._compute = build('compute', 'v1', credentials=self._credentials)
 
     def _get_all_instances(self):
@@ -36,15 +57,16 @@ class GoogleSystem (MgmtSystemAPIBase):
         return result['items']
 
     def _find_instance_by_name(self, instance_name):
-        instance = self._compute.instances().delete(project=self._project,
-                                                    zone=self._zone,
-                                                    instance=instance_name).execute()
+        instance = self._compute.instances().get(project=self._project,
+                                                 zone=self._zone,
+                                                 instance=instance_name).execute()
         if instance:
             return instance
         else:
             raise VMInstanceNotFound(instance_name)
 
-    # make sense to use here utils.wait -> wait_for
+    # didn't use the utils.wait -> wait_for because native way for cheking the status
+    # is more safety and we can get additional information about error
     def wait_vm_running(self, operation):
         logger.info("Waiting for {} operation to finish".format(operation))
         while True:
@@ -55,6 +77,7 @@ class GoogleSystem (MgmtSystemAPIBase):
 
             if result['status'] == 'DONE':
                 logger.info("DONE")
+                return True
                 if 'error' in result:
                     logger.error("Error during {} operation.".format(operation))
                     raise Exception(result['error'])
@@ -131,31 +154,89 @@ class GoogleSystem (MgmtSystemAPIBase):
 
     def delete_vm(self, instance_name):
         logger.info(" Deleting Google Cloud instance %s" % instance_name)
-        self._compute.instances().delete(project=self._project,
+        operation = self._compute.instances().delete(project=self._project,
                                          zone=self._zone, instance=instance_name).execute()
-        # self.wait_vm_running(operation_name)
+        self.wait_vm_running(operation['name'])
         return True
 
     def restart_vm(self, instance_name):
         logger.info(" Restarting Google Cloud instance %s" % instance_name)
-        self._compute.instances().reset(project=self._project,
+        operation = self._compute.instances().reset(project=self._project,
                                        zone=self._zone, instance=instance_name).execute()
-        # self.wait_vm_running(operation_name)
+        self.wait_vm_running(operation['name'])
         return True
 
     def stop_vm(self, instance_name):
         logger.info(" Stoping Google Cloud instance %s" % instance_name)
-        self._compute.instances().start(project=self._project,
+        operation = self._compute.instances().start(project=self._project,
                                         zone=self._zone, instance=instance_name).execute()
 
-        # self.wait_vm_running(operation_name)
+        self.wait_vm_running(operation['name'])
         return True
 
     def start_vm(self, instance_name):
         # This method starts an instance that was stopped using the using the
         # instances().stop method.
         logger.info(" Starting Google Cloud instance %s" % instance_name)
-        self._compute.instances().start(project=self._project,
+        operation = self._compute.instances().start(project=self._project,
                                         zone=self._zone, instance=instance_name).execute()
-        # self.wait_vm_running(operation_name)
+        self.wait_vm_running(operation['name'])
         return True
+
+    def clone_vm(self):
+        raise NotImplementedError('clone_vm not implemented.')
+
+    def current_ip_address(self):
+        raise NotImplementedError('current_ip_address not implemented.')
+
+    def deploy_template(self):
+        raise NotImplementedError('deploy_template not implemented.')
+
+    def disconnect(self):
+        raise NotImplementedError('disconnect not implemented.')
+
+    def does_vm_exist(self):
+        raise NotImplementedError('does_vm_exist not implemented.')
+
+    def get_ip_address(self):
+        raise NotImplementedError('get_ip_address not implemented.')
+
+    def info(self):
+        raise NotImplementedError('info not implemented.')
+
+    def is_vm_running(self, vm_name):
+        return self.vm_status(vm_name) in self.states['running']
+
+    def is_vm_stopped(self, vm_name):
+        return self.vm_status(vm_name) in self.states['stopped']
+
+    def is_vm_suspended(self, vm_name):
+        raise NotImplementedError('is_vm_suspended not implemented.')
+
+    # These methods indicate if the vm is in the process of stopping or starting
+    def is_vm_stopping(self, vm_name):
+        return self.vm_status(vm_name) in self.states['stopping']
+
+    def is_vm_starting(self, vm_name):
+        return self.vm_status(vm_name) in self.states['starting']
+
+    def list_flavor(self):
+        raise NotImplementedError('list_flavor not implemented.')
+
+    def list_template(self):
+        raise NotImplementedError('list_template not implemented.')
+
+    def remove_host_from_cluster(self):
+        raise NotImplementedError('remove_host_from_cluster not implemented.')
+
+    def suspend_vm(self):
+        raise NotImplementedError('suspend_vm not implemented.')
+
+    def vm_status(self, vm_name):
+        return self._find_instance_by_name(vm_name)['status']
+
+    def wait_vm_stopped(self):
+        raise NotImplementedError('wait_vm_stopped not implemented.')
+
+    def wait_vm_suspended(self):
+        raise NotImplementedError('wait_vm_suspended not implemented.')
