862b6cdaec3d029d80b4b5af9068c4248a9ea552
akarol <akarol@redhat.com>
2015-09-04 11:45:55 +0530

    Automate delete stack object

--- cfme/tests/cloud/test_delete_cloud_object.py
+++ cfme/tests/cloud/test_delete_cloud_object.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from cfme.fixtures import pytest_selenium as sel
-from cfme.cloud import instance
+from cfme.cloud import instance, stack
 from cfme.web_ui import Region, toolbar
+from utils.version import current_version
 from utils import testgen
 import pytest
 
@@ -34,8 +35,19 @@ def set_grid():
     toolbar.set_vms_grid_view()
 
 
+@pytest.fixture(scope="module")
+def set_grid_stack():
+    sel.force_navigate("clouds_stacks")
+    toolbar.set_vms_grid_view()
+
+
 def reset():
     sel.force_navigate("clouds_images")
+    toolbar.set_vms_list_view()
+
+
+def reset_grid_stack():
+    sel.force_navigate("clouds_stacks")
     toolbar.set_vms_list_view()
 
 
@@ -66,3 +78,19 @@ def test_delete_image(setup_provider, provider, remove_test, set_grid, request):
     provider.refresh_provider_relationships()
     test_image.wait_for_appear()
     request.addfinalizer(reset)
+
+
+@pytest.mark.uncollectif(lambda: current_version() < "5.4")
+def test_delete_stack(setup_provider, provider, remove_test, set_grid_stack, request):
+    """ Tests delete stack
+
+    Metadata:
+        test_flag: delete_object
+    """
+    stack_name = remove_test['stack']
+    test_stack = stack.Stack(stack_name)
+    test_stack.delete()
+    test_stack.wait_for_delete()
+    provider.refresh_provider_relationships()
+    test_stack.wait_for_appear()
+    request.addfinalizer(reset_grid_stack)
