cd189abcc936ff90095e51a25d6b220b4814ea55
Milan Falešník <mfalesni@redhat.com>
2015-11-11 11:50:01 +0100

    Check the broken angular selects in service ordering and missing Keystone API version.

--- cfme/tests/test_ui_problems.py
+++ cfme/tests/test_ui_problems.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+import fauxfactory
 import pytest
 
+from cfme.services.catalogs import service_catalogs  # NOQA
+from cfme.services.catalogs.catalog import Catalog
+from cfme.services.catalogs.catalog_item import CatalogItem
+from cfme.web_ui import AngularSelect, fill
 from cfme.web_ui.mixins import pull_splitter, left_half_size
 from utils.version import current_version
 
@@ -26,3 +31,41 @@ def test_pull_splitter(location):
     assert (
         original_position - TOLERANCE <= left_half_size() <= original_position + TOLERANCE,
         "Splitter fail!")
+
+
+@pytest.mark.uncollectif(lambda: current_version() < "5.5.0.7")
+@pytest.mark.ignore_stream("upstream")
+@pytest.mark.meta(blockers=[1274665])
+def test_broken_angular_select(request):
+    """Test that checks the fancy selects do not break.
+
+    Prerequisities:
+        * A fresh downstream appliance
+
+    Steps:
+        1) Create a catalog.
+        2) Create a catalog item, can be Generic and assign the catalog and OSE Installer dialog
+            for testing purposes
+        3) Try ordering the service, but instead of confirming the form, try changing some select.
+    """
+    # OSE Installer dialog, one dropdown from it
+    the_select = AngularSelect("ose_size")
+    cat = Catalog("Test_catalog_{}".format(fauxfactory.gen_alpha()))
+    cat.create()
+    request.addfinalizer(cat.delete)
+    item = CatalogItem(
+        item_type="Generic",
+        name="Catitem_{}".format(fauxfactory.gen_alpha()),
+        description=fauxfactory.gen_alpha(),
+        display_in=True,
+        catalog=cat.name,
+        dialog="OSE Installer")
+    item.create()
+    request.addfinalizer(item.delete)
+
+    # The check itself
+    pytest.sel.force_navigate(
+        "order_service_catalog",
+        context={"catalog": cat.name, "catalog_item": item})
+    fill(the_select, "Medium")
+    assert not the_select.is_broken, "The select displayed itself next ot the angular select"
