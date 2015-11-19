e7f05235cf88193d7a4e5de8a81e66d605d91fbb
Milan Falešník <mfalesni@redhat.com>
2015-11-19 17:55:18 +0100

    Make it possible to update appliances from inside py.test run

---
+++ fixtures/appliance_update.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""This module allows you to update an appliance with latest RHEL.
+
+It has two uses:
+1) If only ``--update-appliance`` is specified, it will use the YAML url.
+2) If you also specify one or more ``--update-url``, it will use them instead.
+"""
+import pytest
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('cfme')
+    group.addoption(
+        '--update-appliance',
+        dest='update_appliance', action='store_true', default=False,
+        help="Enable updating an appliance before the first test is run.")
+    group.addoption(
+        '--update-url',
+        dest='update_urls', action='append', default=[],
+        help="URLs to update with. If none are passed, yaml key is used.")
+
+
+def pytest_sessionstart(session):
+    if pytest.store.parallelizer_role not in {'slave', None}:
+        return
+    if not session.config.getoption("update_appliance"):
+        return
+    pytest.store.terminalreporter.write("Initiating appliance update ...\n")
+    urls = session.config.getoption("update_urls")
+    pytest.store.current_appliance.update_rhel(*urls, reboot=True)
+    pytest.store.terminalreporter.write("Appliance update finished, waiting for UI ...\n")
+    pytest.store.current_appliance.wait_for_web_ui()
+    pytest.store.terminalreporter.write("Appliance update finished ...\n")
