3334e53ca03642a0ab2e79aee28a756c67177296
Milan Falešník <mfalesni@redhat.com>
2015-12-07 13:55:16 +0100

    Returned back some branches
    I did not realize that the destinatoins can b especified in YAMLs, so putting them back.
    
    {{pytest: cfme/tests/configure/test_default_views_infra.py -v -k service --long-running}}

--- cfme/services/workloads.py
+++ cfme/services/workloads.py
@@ -10,11 +10,17 @@ templates_images_tree = partial(accordion.tree, "Templates & Images")
 
 @menu.extend_nav
 class services_workloads:
+    def service_vms_instances(_):
+        vm_instances_tree("All VMs & Instances")
+
     def service_vms_instances_filter_folder(ctx):
         vm_instances_tree("All VMs & Instances", ctx["folder_name"])
 
     def service_vms_instances_filter(ctx):
         vm_instances_tree("All VMs & Instances", ctx["folder_name"], ctx["filter_name"])
+
+    def service_templates_images(_):
+        templates_images_tree("All Templates & Images")
 
     def service_templates_images_filter_folder(ctx):
         templates_images_tree("All Templates & Images", ctx["folder_name"])
