19cef4b2f56375114a5341f83a98fd97f7b5ec49
Milan Falešník <mfalesni@redhat.com>
2016-02-01 17:31:42 +0100

    Add seal argument in templatize method, tweak appliance script
    I need to opt out with the sealing when using inside CFME because I do it in a different state machine. This also requires me to add some code that processes some known strings into actual values, like boolean. I changed it to run for booleans and none too. This code should be later extracted out into some tools if we find use for that. Added a small unit test for the argument processor.

---
+++ scripts/tests/test_appliance.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Tests for appliance.py script."""
+import pytest
+
+from scripts import appliance
+
+
+@pytest.mark.parametrize(
+    ('input', 'output'), [
+        ([], []),
+        (['a', 'b', 'c'], ['a', 'b', 'c']),
+        (['1', 'True', 'False', 'None'], [1, True, False, None]),
+    ])
+def test_process_args(input, output):
+    assert appliance.process_args(input) == output
