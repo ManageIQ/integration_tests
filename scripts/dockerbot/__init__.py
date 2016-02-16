e05c26f49ef2bd147dc3ed12386fc12f34b1fda2
Vadim Rutkovsky <vrutkovs@redhat.com>
2016-02-16 12:41:23 +0100

    Guard agains none and show pr_body's type

--- scripts/dockerbot/__init__.py
+++ scripts/dockerbot/__init__.py
@@ -3,6 +3,11 @@ import yaml
 
 
 def parse_pr_metadata(pr_body):
+    if not pr_body:
+        return {}
+    if not isinstance(pr_body, basestring):
+        print "pr_body '%s' is not a string, %s" % (pr_body, type(pr_body))
+        return {}
     metadata = re.findall("{{(.*?)}}", pr_body)
     if not metadata:
         return {}
