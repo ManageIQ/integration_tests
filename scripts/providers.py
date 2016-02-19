19e4f5861b047ecb0d94ca5b285cf8c58c02cd10
Milan Falešník <mfalesni@redhat.com>
2016-02-19 11:57:56 +0100

    Tweak the provider control script.
    Added possibility of passing the kwargs because that is required for the floatin IP operation.

--- scripts/providers.py
+++ scripts/providers.py
@@ -16,16 +16,24 @@ For example, this will work but not do anyhting helpful:
 
     scripts/providers.py providername __init__ username password
 
+
+You can also specify keyword arguments, similarly like the argparse works:
+
+    scripts/providers.py somevsphere do_action 1 2 --foo bar
+
+It expects pairs in format ``--key value``. If you fail to provide such formatted arguments, an
+error will happen.
+
 """
 import argparse
 import os
 import sys
+from utils import iterate_pairs, process_shell_output
+from utils.providers import get_mgmt
 
 # Make sure the parent dir is on the path before importing get_mgmt
 cfme_tests_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, cfme_tests_path)
-
-from utils.providers import get_mgmt
 
 
 def main():
@@ -38,23 +46,23 @@ def main():
     parser.add_argument('action_args', nargs='*',
         help='foo')
 
-    args = parser.parse_args()
+    args, kw_argument_list = parser.parse_known_args()
+    kwargs = {}
+    for key, value in iterate_pairs(kw_argument_list):
+        if not key.startswith('--'):
+            raise Exception('Wrong kwargs specified!')
+        key = key[2:]
+        try:
+            value = int(value)
+        except ValueError:
+            try:
+                value = float(value)
+            except ValueError:
+                pass
+        kwargs[key] = value
     try:
-        result = call_provider(args.provider_name, args.action, *args.action_args)
-        if isinstance(result, list):
-            exit = 0
-            for entry in sorted(result):
-                print entry
-        elif isinstance(result, str):
-            exit = 0
-            print result
-        elif isinstance(result, bool):
-            # 'True' result becomes flipped exit 0, and vice versa for False
-            exit = int(not result)
-        else:
-            # Unknown type, print it
-            exit = 0
-            print str(result)
+        result = call_provider(args.provider_name, args.action, *args.action_args, **kwargs)
+        exit, output = process_shell_output(result)
     except Exception as e:
         exit = 1
         exc_type = type(e).__name__
@@ -62,11 +70,14 @@ def main():
             sys.stderr.write('%s: %s\n' % (exc_type, e.message))
         else:
             sys.stderr.write('%s\n' % exc_type)
+    else:
+        if output is not None:
+            print(output)
 
     return exit
 
 
-def call_provider(provider_name, action, *args):
+def call_provider(provider_name, action, *args, **kwargs):
     # Given a provider class, find the named method and call it with
     # *args. This could possibly be generalized for other CLI tools.
     provider = get_mgmt(provider_name)
@@ -74,8 +85,8 @@ def call_provider(provider_name, action, *args):
     try:
         call = getattr(provider, action)
     except AttributeError:
-        raise Exception('Action "%s" not found' % action)
-    return call(*args)
+        raise Exception('Action {} not found'.format(repr(action)))
+    return call(*args, **kwargs)
 
 if __name__ == '__main__':
     sys.exit(main())
