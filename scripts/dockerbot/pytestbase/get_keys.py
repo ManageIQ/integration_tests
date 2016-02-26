1456ea8699fb197d0a8c03eca657239d985bbfe8
Vadim Rutkovsky <vrutkovs@redhat.com>
2016-02-26 14:14:20 +0100

    gpg: use list comprehension

--- scripts/dockerbot/pytestbase/get_keys.py
+++ scripts/dockerbot/pytestbase/get_keys.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python2
 from utils import conf
-from time import sleep
 import subprocess
+import sys
 
-key_list = []
-for key in conf['gpg']['allowed_keys']:
-    key_list.append(key[-9:].replace(' ', ''))
+key_list = [key[-9:].replace(' ', '') for key in conf['gpg']['allowed_keys']]
 
-for attempt in range(0, 10):
-    proc = subprocess.Popen(['gpg', '--recv-keys'] + key_list)
-    proc.wait()
-    if proc.returncode == 0:
-        break
-    sleep(5)
+proc = subprocess.Popen(['gpg', '--recv-keys'] + key_list)
+proc.wait()
+sys.exit(proc.returncode)
