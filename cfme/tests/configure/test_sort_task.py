4a1ea7dfab2525445d624fa3eeb69cf7e7f3c496
nachandr <nachandr@localhost.localdomain>
2016-03-14 14:06:26 -0500

    Automated test for task sorting

--- cfme/tests/configure/test_sort_task.py
+++ cfme/tests/configure/test_sort_task.py
@@ -1,37 +1,42 @@
 # -*- coding: utf-8 -*-
 import pytest
+import cfme.infrastructure.virtual_machines as vm
 
 from cfme.fixtures import pytest_selenium as sel
 from cfme.configure import tasks
+from utils.providers import setup_a_provider
 
 
-@pytest.mark.sauce
-def test_sort_my_vm_tasks():
+@pytest.fixture(scope="module")
+def provider():
+    return setup_a_provider(prov_class="infra", prov_type="rhevm")
+
+
+def test_sort_my_vm_tasks(provider):
+    vm_list = vm.get_all_vms()
+    vm.perform_smartstate_analysis(vm_list)
     sel.force_navigate('tasks_my_vm')
-    other_tasks = tasks.all_other_tasks()
+    other_tasks = tasks.my_vm_analysis_tasks()
     if len(other_tasks) > 1:
-        tasks.sort_by_header('Started', 'ascending')
+        tasks.sort_by_header('Task Name', 'ascending')
 
 
-@pytest.mark.sauce
 def test_sort_my_other_ui_tasks():
     sel.force_navigate('tasks_my_other_ui')
-    other_tasks = tasks.all_other_tasks()
+    other_tasks = tasks.my_other_ui_tasks()
     if len(other_tasks) > 1:
-        tasks.sort_by_header('Started', 'ascending')
+        tasks.sort_by_header('Task Name', 'ascending')
 
 
-@pytest.mark.sauce
-def test_sort_all_vm_analysis_tasks():
+def test_sort_all_vm_analysis_tasks(provider):
     sel.force_navigate('tasks_all_vm')
-    other_tasks = tasks.all_other_tasks()
+    other_tasks = tasks.all_vm_analysis_tasks()
     if len(other_tasks) > 1:
-        tasks.sort_by_header('Started', 'ascending')
+        tasks.sort_by_header('Task Name', 'ascending')
 
 
-@pytest.mark.sauce
 def test_sort_all_other_tasks():
     sel.force_navigate('tasks_all_other')
     other_tasks = tasks.all_other_tasks()
     if len(other_tasks) > 1:
-        tasks.sort_by_header('Started', 'ascending')
+        tasks.sort_by_header('Task Name', 'ascending')
