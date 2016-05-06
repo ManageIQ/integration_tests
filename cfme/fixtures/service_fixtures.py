0adc06b74d55ad91126ea28a0b82270d98a969d1
Shveta Sachdeva <sshveta@redhat.com>
2016-05-06 13:48:47 -0700

    Vsphere vlan added

--- cfme/fixtures/service_fixtures.py
+++ cfme/fixtures/service_fixtures.py
@@ -6,7 +6,6 @@ import pytest
 from cfme.automate.service_dialogs import ServiceDialog
 from cfme.services.catalogs.catalog import Catalog
 from cfme.services.catalogs.catalog_item import CatalogItem
-from utils import version
 
 
 @pytest.fixture(scope="function")
@@ -42,23 +41,18 @@ def catalog():
 
 @pytest.fixture(scope="function")
 def catalog_item(provider, provisioning, vm_name, dialog, catalog):
-    template, host, datastore, iso_file, catalog_item_type = map(provisioning.get,
-        ('template', 'host', 'datastore', 'iso_file', 'catalog_item_type'))
+    template, host, datastore, iso_file, catalog_item_type, vlan = map(provisioning.get,
+        ('template', 'host', 'datastore', 'iso_file', 'catalog_item_type', 'vlan'))
 
     provisioning_data = {
         'vm_name': vm_name,
         'host_name': {'name': [host]},
-        'datastore_name': {'name': [datastore]}
+        'datastore_name': {'name': [datastore]},
+        'vlan': vlan
     }
 
     if provider.type == 'rhevm':
         provisioning_data['provision_type'] = 'Native Clone'
-        provisioning_data['vlan'] = provisioning['vlan']
-        catalog_item_type = version.pick({
-            version.LATEST: "RHEV",
-            '5.3': "RHEV",
-            '5.2': "Redhat"
-        })
     elif provider.type == 'virtualcenter':
         provisioning_data['provision_type'] = 'VMware'
     item_name = fauxfactory.gen_alphanumeric()
