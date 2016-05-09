f454da59a747c2a4ac5a95e0a887a95df9ff34ef
quarckster <dmisharo@redhat.com>
2016-05-09 16:47:56 +0200

    Added @pytest.mark.tier() for the control basic tests

--- cfme/tests/control/test_basic.py
+++ cfme/tests/control/test_basic.py
@@ -158,6 +158,7 @@ def alert_profile(request):
     alert.delete()
 
 
+@pytest.mark.tier(2)
 def test_vm_condition_crud(soft_assert):
     condition = explorer.VMCondition(
         fauxfactory.gen_alphanumeric(),
@@ -181,6 +182,7 @@ def test_vm_condition_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_host_condition_crud(soft_assert):
     if current_version() >= "5.4":
         expression = "fill_count(Host / Node.Files, >, 150)"
@@ -207,6 +209,7 @@ def test_host_condition_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_action_crud(soft_assert):
     action = explorer.Action(
         fauxfactory.gen_alphanumeric(),
@@ -233,6 +236,7 @@ def test_action_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_vm_control_policy_crud(soft_assert):
     policy = explorer.VMControlPolicy(fauxfactory.gen_alphanumeric())
     # CR
@@ -252,6 +256,7 @@ def test_vm_control_policy_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_vm_compliance_policy_crud(soft_assert):
     policy = explorer.VMCompliancePolicy(fauxfactory.gen_alphanumeric())
     # CR
@@ -271,6 +276,7 @@ def test_vm_compliance_policy_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_host_control_policy_crud(soft_assert):
     policy = explorer.HostControlPolicy(fauxfactory.gen_alphanumeric())
     # CR
@@ -290,6 +296,7 @@ def test_host_control_policy_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_host_compliance_policy_crud(soft_assert):
     policy = explorer.HostCompliancePolicy(fauxfactory.gen_alphanumeric())
     # CR
@@ -309,6 +316,7 @@ def test_host_compliance_policy_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_container_image_control_policy_crud(soft_assert):
     policy = explorer.ContainerImageControlPolicy(fauxfactory.gen_alphanumeric())
     # CR
@@ -331,6 +339,7 @@ def test_container_image_control_policy_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_container_image_compliance_policy_crud(soft_assert):
     policy = explorer.ContainerImageCompliancePolicy(fauxfactory.gen_alphanumeric())
     # CR
@@ -352,24 +361,28 @@ def test_container_image_compliance_policy_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(3)
 def test_policies_copy(random_policy, soft_assert):
     random_policy_copy = random_policy.copy()
     soft_assert(random_policy_copy.exists, "The {} does not exist!".format(random_policy_copy))
     random_policy_copy.delete()
 
 
+@pytest.mark.tier(3)
 def test_assign_events_to_vm_control_policy(random_vm_control_policy, soft_assert):
     random_vm_control_policy.assign_events("VM Retired", "VM Clone Start")
     soft_assert(random_vm_control_policy.is_event_assigned("VM Retired"))
     soft_assert(random_vm_control_policy.is_event_assigned("VM Clone Start"))
 
 
+@pytest.mark.tier(3)
 def test_assign_events_to_host_control_policy(random_host_control_policy, soft_assert):
     random_host_control_policy.assign_events("Host Auth Error", "Host Compliance Passed")
     soft_assert(random_host_control_policy.is_event_assigned("Host Auth Error"))
     soft_assert(random_host_control_policy.is_event_assigned("Host Compliance Passed"))
 
 
+@pytest.mark.tier(3)
 def test_assign_vm_condition_to_vm_policy(
         random_vm_control_policy, random_vm_condition, soft_assert):
     random_vm_control_policy.assign_conditions(random_vm_condition)
@@ -377,6 +390,7 @@ def test_assign_vm_condition_to_vm_policy(
     random_vm_control_policy.assign_conditions()  # unassign
 
 
+@pytest.mark.tier(3)
 def test_assign_host_condition_to_host_policy(
         random_host_control_policy, random_host_condition, soft_assert):
     random_host_control_policy.assign_conditions(random_host_condition)
@@ -384,6 +398,7 @@ def test_assign_host_condition_to_host_policy(
     random_host_control_policy.assign_conditions()  # unassign
 
 
+@pytest.mark.tier(2)
 def test_policy_profile_crud(random_vm_control_policy, random_host_control_policy, soft_assert):
     profile = explorer.PolicyProfile(
         fauxfactory.gen_alphanumeric(),
@@ -399,6 +414,7 @@ def test_policy_profile_crud(random_vm_control_policy, random_host_control_polic
     soft_assert(not profile.exists, "The policy profile {} exists!".format(profile.description))
 
 
+@pytest.mark.tier(3)
 # RUBY expression type is no longer supported.
 @pytest.mark.uncollectif(lambda expression: "RUBY" in expression and current_version() >= "5.5")
 @pytest.mark.parametrize(("expression", "verify"), VM_EXPRESSIONS_TO_TEST)
@@ -414,6 +430,7 @@ def test_modify_vm_condition_expression(
         soft_assert(expression_editor.get_expression_as_text() == verify)
 
 
+@pytest.mark.tier(2)
 def test_alert_crud(soft_assert):
     alert = explorer.Alert(
         fauxfactory.gen_alphanumeric(), timeline_event=True, driving_event="Hourly Timer"
@@ -438,6 +455,7 @@ def test_alert_crud(soft_assert):
     ))
 
 
+@pytest.mark.tier(3)
 @pytest.mark.meta(blockers=[1303645], automates=[1303645])
 def test_control_alert_copy(random_alert, soft_assert):
     alert_copy = random_alert.copy()
@@ -450,6 +468,7 @@ def test_control_alert_copy(random_alert, soft_assert):
     ))
 
 
+@pytest.mark.tier(2)
 def test_alert_profile_crud(alert_profile, soft_assert):
     alert_profile.create()
     soft_assert(alert_profile.exists, "The alert profile {} does not exist!".format(
