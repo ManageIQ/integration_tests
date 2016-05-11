bcc1a1164a001d9cd68c81d1a92ce6cc4b41ed67
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-05-11 11:30:19 +0200

    componentize quickstart script

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -1,22 +1,59 @@
 #!/usr/bin/env bash
 
-echo "setting up the needed system level tools"
-pkcon install -y \
-    python-pip gcc postgresql-devel \
-    libxml2-devel libxslt-devel \
-    zeromq3-devel libcurl-devel \
-    redhat-rpm-config
+# pass the cfme qe yamls repo as first argument
+QE_YAML_REPO=${1:-../cfme-qe-yamls}
 
-echo "user install of the latest pip/virtualenv to avoid old distro packages"
-pip install -U --user pip virtualenv
+setup_redhat_based_system() {
 
-echo "Creating Virtualenv"
-virtualenv .cfme_tests
+  echo "setting up the needed system level tools"
+  pkcon install -y \
+      python-pip gcc postgresql-devel \
+      libxml2-devel libxslt-devel \
+      zeromq3-devel libcurl-devel \
+      redhat-rpm-config
 
-# make our virtualenv setup a bit nicer, bash only
-echo "export PYTHONPATH='`pwd`'" | tee -a ./.cfme_tests/bin/activate
-echo "export PYTHONDONTWRITEBYTECODE=yes" | tee -a ./.cfme_tests/bin/activate
+  echo "user install of the latest pip/virtualenv to avoid old distro packages"
+  pip install -U -q --user pip virtualenv
+}
 
-. ./.cfme_tests/bin/activate
-PYCURL_SSL_LIBRARY=nss pip install -Ur ./requirements.txt
-echo "Run '. ./.cfme_tests/bin/activate' to load the virtualenv"
+
+append() {
+  grep -q "$2" "$1" || (echo "$2" >> $1)
+}
+
+setup_virtualenv() {
+
+  echo "Creating Virtualenv"
+  virtualenv .cfme_tests
+
+  # make our virtualenv setup a bit nicer, bash only
+  echo "Patching ./.cfme_tests/bin/activate"
+  append ./.cfme_tests/bin/activate "export PYTHONPATH='`pwd`'"
+  append ./.cfme_tests/bin/activate "export PYTHONDONTWRITEBYTECODE=yes"
+
+  . ./.cfme_tests/bin/activate
+  PYCURL_SSL_LIBRARY=nss pip install -Uqr ./requirements.txt
+  echo "Run '. ./.cfme_tests/bin/activate' to load the virtualenv"
+}
+
+link_yamls() {
+  ln -s -t "$2" "$1"/*.yaml
+  ln -s -t "$2" "$1"/*.eyaml
+}
+
+setup_yaml_symlinks() {
+  if   [ -d "$QE_YAML_REPO" ]
+  then
+    echo "Linkin qe repo yaml files"
+    link_yamls "$QE_YAML_REPO/complete" conf
+  else
+    echo "No qe yamls found"
+  fi
+}
+
+
+
+
+[ -f /etc/redhat-release ] && setup_redhat_based_system
+setup_virtualenv
+setup_yaml_symlinks
