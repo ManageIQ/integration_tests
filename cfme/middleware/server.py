270d1498bf73d0d72918e703a338a76bb48246c2
Hayk Hovsepyan <hhovsepy@redhat.com>
2016-05-19 15:15:35 +0200

    Added new methods for listing servers on UI, DB and mgmt_systems. Added test case to compare those lists and headers of servers list in UI. Added new functionality to filter servers and deployments by provider, added new test cases.

--- cfme/middleware/server.py
+++ cfme/middleware/server.py
@@ -1,10 +1,26 @@
+import re
 from cfme.common import Taggable
 from cfme.fixtures import pytest_selenium as sel
 from cfme.web_ui import CheckboxTable
 from cfme.web_ui.menu import nav, toolbar as tb
+from utils.db import cfmedb
 from . import LIST_TABLE_LOCATOR, mon_btn, pwr_btn, MiddlewareBase
 
 list_tbl = CheckboxTable(table_locator=LIST_TABLE_LOCATOR)
+
+
+def _db_select_query(name=None, server=None, provider=None):
+    t_ms = cfmedb()['middleware_servers']
+    t_ems = cfmedb()['ext_management_systems']
+    query = cfmedb().session.query(t_ms.name, t_ms.feed,
+                                   t_ms.product, t_ems.name).join(t_ems, t_ms.ems_id == t_ems.id)
+    if name:
+        query = query.filter(t_ms.name == name)
+    if server:
+        query = query.filter(t_ms.nativeid.like('%{}%'.format(server)))
+    if provider:
+        query = query.filter(t_ems.name == provider)
+    return query
 
 nav.add_branch(
     'middleware_servers', {
@@ -46,12 +62,46 @@ class MiddlewareServer(MiddlewareBase, Taggable):
 
     @classmethod
     def servers(cls, provider):
-        sel.force_navigate('middleware_servers')
-        deployments = []
+        servers = []
+        if provider:
+            # if provider instance is provided try to navigate provider's servers page
+            # if no servers are registered in provider, returns empty list
+            if provider.load_all_provider_servers():
+                return servers
+        else:
+            # if provider instance is not provided then navigates  to all servers page
+            sel.force_navigate('middleware_servers')
         for row in list_tbl.rows():
-            deployments.append(MiddlewareServer(
-                provider=provider, name=row.server_name.text, product=row.product.text))
-        return deployments
+            servers.append(MiddlewareServer(
+                name=row.server_name.text, id=row.feed.text,
+                product=row.product.text, provider=row.provider.text))
+        return servers
+
+    @classmethod
+    def headers(cls):
+        sel.force_navigate('middleware_servers')
+        list_tbl._update_cache()
+        headers = [sel.text(hdr).encode("utf-8") for hdr in list_tbl.headers if hdr.text]
+        return headers
+
+    @classmethod
+    def servers_in_db(cls, server=None, provider=None):
+        servers = []
+        rows = _db_select_query(server=server, provider=(provider.name if provider else None)).all()
+        for server in rows:
+            servers.append(MiddlewareServer(name=server[0], id=server[1],
+                                            product=server[2], provider=server[3]))
+        return servers
+
+    @classmethod
+    def servers_in_mgmt(cls, provider):
+        servers = []
+        rows = provider.mgmt.list_server()
+        for server in rows:
+            servers.append(MiddlewareServer(provider=provider.name,
+                    id=server.path.feed, name=re.sub(r'(~~$)', '', server.path.resource),
+                    product=server.data['Product Name']))
+        return servers
 
     def load_details(self, refresh=False):
         if not self._on_detail_page():
