23db43a6384f3aa2f4cb40fe32726bae32be3431
akarol <akarol@redhat.com>
2016-06-06 15:46:01 +0530

    Remove bz blocker

--- cfme/tests/configure/test_default_views_infra.py
+++ cfme/tests/configure/test_default_views_infra.py
@@ -197,7 +197,6 @@ def test_exists_view(request, setup_a_provider, key):
     reset_default_view(button_name, default_view)
 
 
-@pytest.mark.meta(blockers=[1305423])
 def test_hybrid_view(request, setup_a_provider):
     path = ["Configuration Management", "Hosts", "Virtual Infrastructure Platforms"]
     report = CannedSavedReport.new(path)
@@ -206,7 +205,6 @@ def test_hybrid_view(request, setup_a_provider):
     assert tb.is_active('Hybrid View'), "Hybrid view setting failed"
 
 
-@pytest.mark.meta(blockers=[1305423])
 def test_graph_view(request, setup_a_provider):
     path = ["Configuration Management", "Hosts", "Virtual Infrastructure Platforms"]
     report = CannedSavedReport.new(path)
@@ -215,7 +213,6 @@ def test_graph_view(request, setup_a_provider):
     assert tb.is_active('Graph View'), "Graph view setting failed"
 
 
-@pytest.mark.meta(blockers=[1305423])
 def test_tabular_view(request, setup_a_provider):
     path = ["Configuration Management", "Hosts", "Virtual Infrastructure Platforms"]
     report = CannedSavedReport.new(path)
