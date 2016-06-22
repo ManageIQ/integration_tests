4cba5f8a1fbcf0b1bb1d1b6792e425a4f3eecedc
Pavel Zagalsky <zagalsky@gmail.com>
2016-06-22 13:51:04 +0300

    Update test_containers_default_views.py

--- cfme/tests/containers/test_containers_default_views.py
+++ cfme/tests/containers/test_containers_default_views.py
@@ -1,19 +1,31 @@
 import pytest
 from cfme.fixtures import pytest_selenium as sel
 from cfme.web_ui import toolbar as tb, ButtonGroup, form_buttons
+from cfme.configure import settings
 from utils import testgen
 from utils.version import current_version
+
 
 pytestmark = [
     pytest.mark.uncollectif(
         lambda: current_version() < "5.6"),
-    pytest.mark.usefixtures('setup_provider')]
+    pytest.mark.usefixtures('setup_provider'),
+    pytest.mark.tier(3)]
 pytest_generate_tests = testgen.generate(
-    testgen.container_providers, scope="function")
+    testgen.container_providers, scope='function')
 
 
 # CMP-9936
+
+
 def test_containers_providers_view():
+    """
+
+    This test checks successful change of default views settings for Containers --> Providers menu
+    * Goes to Settings --> My Settings --> Default Views menu and change the default view settings of Containers --> Containers Providers to Grid/Tile/List view
+    * Goes to Compute --> Containers --> Providers and verifies the selected view
+
+    """
     # Change the view to Grid View
     sel.force_navigate('my_settings_default_views')
     bg = ButtonGroup('Containers Providers')
@@ -22,7 +34,7 @@ def test_containers_providers_view():
 
     # Go to Container Providers and verify that the View changed to Grid View
     sel.force_navigate('containers_providers')
-    assert tb.is_active('Grid View'), "Providers grid view setting failed"
+    assert tb.is_active('Grid View'), 'Providers grid view setting failed'
 
     # Change the view to Tile View
     sel.force_navigate('my_settings_default_views')
@@ -32,7 +44,7 @@ def test_containers_providers_view():
 
     # Go to Container Providers and verify that the View changed to Tile View
     sel.force_navigate('containers_providers')
-    assert tb.is_active('Tile View'), "Providers tile view setting failed"
+    assert tb.is_active('Tile View'), 'Providers tile view setting failed'
 
     # Change the view to List View
     sel.force_navigate('my_settings_default_views')
@@ -42,12 +54,19 @@ def test_containers_providers_view():
 
     # Go to Container Providers and verify that the View changed to Tile View
     sel.force_navigate('containers_providers')
-    assert tb.is_active('List View'), "Providers list view setting failed"
+    assert tb.is_active('List View'), 'Providers list view setting failed'
 
 # CMP-9937
 
 
 def test_containers_projects_view():
+    """
+
+    This test checks successful change of default views settings for Containers --> Projects menu
+    * Goes to Settings --> My Settings --> Default Views menu and change the default view settings of Containers --> Containers Projects to Grid/Tile/List view
+    * Goes to Compute --> Containers --> Projects and verifies the selected view
+
+    """
     # Change the view to Grid View
     sel.force_navigate('my_settings_default_views')
     bg = ButtonGroup('Projects')
@@ -56,7 +75,7 @@ def test_containers_projects_view():
 
     # Go to Container Projects and verify that the View changed to Grid View
     sel.force_navigate('containers_projects')
-    assert tb.is_active('Grid View'), "Projects grid view setting failed"
+    assert tb.is_active('Grid View'), 'Projects grid view setting failed'
 
     # Change the view to Tile View
     sel.force_navigate('my_settings_default_views')
@@ -66,7 +85,7 @@ def test_containers_projects_view():
 
     # Go to Container Projects and verify that the View changed to Tile View
     sel.force_navigate('containers_projects')
-    assert tb.is_active('Tile View'), "Projects tile view setting failed"
+    assert tb.is_active('Tile View'), 'Projects tile view setting failed'
 
     # Change the view to List View
     sel.force_navigate('my_settings_default_views')
@@ -76,22 +95,28 @@ def test_containers_projects_view():
 
     # Go to Container Projects and verify that the View changed to Tile View
     sel.force_navigate('containers_projects')
-    assert tb.is_active('List View'), "Projects list view setting failed"
+    assert tb.is_active('List View'), 'Projects list view setting failed'
 
 # CMP-9938
 
 
 def test_containers_routes_view():
-    # change the default view to the following: Container Routes tiles to Grid
-    # View
+    """
+
+    This test checks successful change of default views settings for Containers --> Routes menu
+    * Goes to Settings --> My Settings --> Default Views menu and change the default view settings of Containers --> Containers Routes to Grid/Tile/List view
+    * Goes to Compute --> Containers --> Routes and verifies the selected view
+
+    """
+    #change the default view to the following: Container Routes tiles to Grid View
     sel.force_navigate('my_settings_default_views')
-    bg = ButtonGroup('Routes')
-    bg.choose('Grid View')
+    b = ButtonGroup('Routes')
+    b.choose('Grid View')
     sel.click(form_buttons.save)
 
     # Go to Container Routes and verify that the View changed to Grid View
     sel.force_navigate('containers_routes')
-    assert tb.is_active('Grid View'), "Routes grid view setting failed"
+    assert tb.is_active('Grid View'), 'Routes grid view setting failed'
 
     # Change the view to Tile View
     sel.force_navigate('my_settings_default_views')
@@ -101,7 +126,7 @@ def test_containers_routes_view():
 
     # Go to Container Routes and verify that the View changed to Tile View
     sel.force_navigate('containers_routes')
-    assert tb.is_active('Tile View'), "Routes tile view setting failed"
+    assert tb.is_active('Tile View'), 'Routes tile view setting failed'
 
     # Change the view to List View
     sel.force_navigate('my_settings_default_views')
@@ -111,12 +136,19 @@ def test_containers_routes_view():
 
     # Go to Container Routes and verify that the View changed to Tile View
     sel.force_navigate('containers_routes')
-    assert tb.is_active('List View'), "Routes list view setting failed"
+    assert tb.is_active('List View'), 'Routes list view setting failed'
 
 # CMP-10003
 
 
 def test_containers_nodes_views():
+    """
+
+    This test checks successful change of default views settings for Containers --> Nodes menu
+    * Goes to Settings --> My Settings --> Default Views menu and change the default view settings of Containers --> Containers Nodes to Grid/Tile/List view
+    * Goes to Compute --> Containers --> Nodes and verifies the selected view
+
+    """
     # Change the view to Grid View
     sel.force_navigate('my_settings_default_views')
     bg = ButtonGroup('Nodes')
@@ -125,7 +157,7 @@ def test_containers_nodes_views():
 
     # Go to Container Nodes and verify that the View changed to Grid View
     sel.force_navigate('containers_nodes')
-    assert tb.is_active('Grid View'), "Nodes grid view setting failed"
+    assert tb.is_active('Grid View'), 'Nodes grid view setting failed'
 
     # Change the view to Tile View
     sel.force_navigate('my_settings_default_views')
@@ -135,7 +167,7 @@ def test_containers_nodes_views():
 
     # Go to Container Nodes and verify that the View changed to Tile View
     sel.force_navigate('containers_nodes')
-    assert tb.is_active('Tile View'), "Nodes tile view setting failed"
+    assert tb.is_active('Tile View'), 'Nodes tile view setting failed'
 
     # Change the view to List View
     sel.force_navigate('my_settings_default_views')
@@ -145,12 +177,19 @@ def test_containers_nodes_views():
 
     # Go to Container Nodes and verify that the View changed to Tile View
     sel.force_navigate('containers_nodes')
-    assert tb.is_active('List View'), "Nodes list view setting failed"
+    assert tb.is_active('List View'), 'Nodes list view setting failed'
 
 # CMP-10002
 
 
 def test_containers_containers_view():
+    """
+
+    This test checks successful change of default views settings for Containers --> Containers menu
+    * Goes to Settings --> My Settings --> Default Views menu and change the default view settings of Containers --> Containers Containers to Grid/Tile/List view
+    * Goes to Compute --> Containers --> Containers and verifies the selected view
+
+    """
     # Change the view to Grid View
     sel.force_navigate('my_settings_default_views')
     bg = ButtonGroup('Containers')
@@ -159,7 +198,7 @@ def test_containers_containers_view():
 
     # Go to Container Containers and verify that the View changed to Grid View
     sel.force_navigate('containers_containers')
-    assert tb.is_active('Grid View'), "Containers grid view setting failed"
+    assert tb.is_active('Grid View'), 'Containers grid view setting failed'
 
     # Change the view to Tile View
     sel.force_navigate('my_settings_default_views')
@@ -169,7 +208,7 @@ def test_containers_containers_view():
 
     # Go to Container Containers and verify that the View changed to Tile View
     sel.force_navigate('containers_containers')
-    assert tb.is_active('Tile View'), "Containers tile view setting failed"
+    assert tb.is_active('Tile View'), 'Containers tile view setting failed'
 
     # Change the view to List View
     sel.force_navigate('my_settings_default_views')
@@ -179,12 +218,19 @@ def test_containers_containers_view():
 
     # Go to Container Containers and verify that the View changed to Tile View
     sel.force_navigate('containers_containers')
-    assert tb.is_active('List View'), "Containers list view setting failed"
+    assert tb.is_active('List View'), 'Containers list view setting failed'
 
 # CMP-10000
 
 
 def test_containers_replicators_view():
+    """
+
+    This test checks successful change of default views settings for Containers --> Replicators menu
+    * Goes to Settings --> My Settings --> Default Views menu and change the default view settings of Containers --> Containers Replicators to Grid/Tile/List view
+    * Goes to Compute --> Containers --> Replicators and verifies the selected view
+
+    """
     # Change the view to Grid View
     sel.force_navigate('my_settings_default_views')
     bg = ButtonGroup('Replicators')
@@ -193,7 +239,7 @@ def test_containers_replicators_view():
 
     # Go to Container Pods and verify that the View changed to Grid View
     sel.force_navigate('containers_replicators')
-    assert tb.is_active('Grid View'), "Replicators grid view setting failed"
+    assert tb.is_active('Grid View'), 'Replicators grid view setting failed'
 
     # Change the view to Tile View
     sel.force_navigate('my_settings_default_views')
@@ -203,7 +249,7 @@ def test_containers_replicators_view():
 
     # Go to Container Replicators and verify that the View changed to Tile View
     sel.force_navigate('containers_replicators')
-    assert tb.is_active('Tile View'), "Replicators tile view setting failed"
+    assert tb.is_active('Tile View'), 'Replicators tile view setting failed'
 
     # Change the view to List View
     sel.force_navigate('my_settings_default_views')
@@ -213,4 +259,4 @@ def test_containers_replicators_view():
 
     # Go to Container Replicators and verify that the View changed to Tile View
     sel.force_navigate('containers_replicators')
-    assert tb.is_active('List View'), "Replicators list view setting failed"
+    assert tb.is_active('List View'), 'Replicators list view setting failed'
