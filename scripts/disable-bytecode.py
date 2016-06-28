b3276d2be21a15c8dc2c1bc407a802d03d953a71
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-06-28 17:20:01 +0200

    restore conftest and create script that disables bytecode

---
+++ scripts/disable-bytecode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+from os import path
+import distutils
+
+site_packages = distutils.sysconfig_get_python_lib()
+target = path.join(site_packages, 'usercustomize.py')
+
+content = "import sys\nsys.dont_write_bytecode = True\n"
+
+if path.exists(target):
+    with open(target) as fp:
+        assert fp.read() == content
+else:
+    with open(target, 'w') as fp:
+        fp.write(content)
