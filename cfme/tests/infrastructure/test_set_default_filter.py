0245b0dc5fe49a94d040b8164d43bd73e6ec08ed
Martin Kourim <mkourim@redhat.com>
2016-07-11 19:03:50 +0200

    Fixes of test_set_default_filter for 5.6

--- cfme/tests/infrastructure/test_set_default_filter.py
+++ cfme/tests/infrastructure/test_set_default_filter.py
@@ -6,7 +6,8 @@ from cfme.login import login_admin, logout
 from cfme.web_ui.search import search_box
 from cfme.infrastructure import host, datastore
 from utils.providers import setup_a_provider
-from cfme.web_ui import listaccordion as list_acc
+from utils import version
+from cfme.web_ui import accordion, listaccordion as list_acc
 
 
 @pytest.fixture(scope="module")
@@ -47,6 +48,7 @@ def test_clear_host_filter_results(provider):
     assert page_title == 'Hosts', 'Clear filter results failed'
 
 
+@pytest.mark.uncollectif(lambda: version.current_version() >= "5.6")
 def test_set_default_datastore_filter(provider, request):
     """ Test for setting default filter for datastores."""
 
@@ -70,8 +72,16 @@ def test_set_default_datastore_filter(provider, request):
 def test_clear_datastore_filter_results(provider):
     """ Test for clearing filter results for datastores."""
 
+    if version.current_version() >= 5.6:
+        expected_page_title = 'All Datastores'
+        datastore_select = lambda: accordion.tree('Datastores', 'All Datastores', 'Global Filters',
+            'Store Type / VMFS')
+    else:
+        expected_page_title = 'Datastores'
+        datastore_select = lambda: list_acc.select('Filters', 'Store Type / VMFS', by_title=False)
+
     pytest.sel.force_navigate('infrastructure_datastores')
-    list_acc.select('Filters', 'Store Type / VMFS', by_title=False)
+    datastore_select()
     pytest.sel.click(search_box.clear_advanced_search)
     page_title = pytest.sel.text(datastore.page_title_loc)
-    assert page_title == 'Datastores', 'Clear filter results failed'
+    assert page_title == expected_page_title, 'Clear filter results failed'
