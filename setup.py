6ad1b29deb4d8c5078d6face1e584163e2d05a56
Shveta Sachdeva <sshveta@redhat.com>
2016-07-11 10:30:45 -0700

    Vm name change

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 # dummy for editable installs
 import sys
+import os
 from setuptools import setup
-assert 'develop' in sys.argv or 'egg_info' in sys.argv, \
-    'this is a hack, use pip install -e'
+
+# just cleanly exit on readthedocs
+if os.environ.get('READTHEDOCS', None) == 'True':
+    sys.exit()
+elif 'develop' in sys.argv or 'egg_info' in sys.argv:
+    pass
+else:
+    sys.exit('this is a hack, use pip install -e')
 
 setup(
     name='manageiq-integration-tests',
