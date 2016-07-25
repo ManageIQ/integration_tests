02eba3a3133b7760ebde2d0458fa90fc3c60d96e
Pavel Zagalsky <pzagalsk@redhat.com>
2016-07-25 13:54:56 +0300

    Fixed Provider selection

--- cfme/tests/containers/test_containers_default_project_replicators.py
+++ cfme/tests/containers/test_containers_default_project_replicators.py
@@ -9,8 +9,7 @@ from utils.version import current_version
 
 pytestmark = [
     pytest.mark.uncollectif(
-        lambda: current_version() < "5.6"),
-    pytest.mark.uncollectif(lambda provider: provider.version < 3.2),
+        (lambda: current_version() < "5.6") or (lambda provider: provider.version < 3.2)),
     pytest.mark.usefixtures('setup_provider'),
     pytest.mark.tier(1)]
 pytest_generate_tests = testgen.generate(
