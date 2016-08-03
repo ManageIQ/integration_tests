d3a883b24a9e459d40bcb90ac68be7fd7603b2a7
Milan Falešník <mfalesni@redhat.com>
2016-08-03 19:36:24 +0200

    Sprout - Fix deprecation warning

--- sprout/sprout/settings.py
+++ sprout/sprout/settings.py
@@ -45,15 +45,16 @@ INSTALLED_APPS = (
     'django_object_actions',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
+
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
