f3186f19bed455a5f0be27f5ab8d493f12eb0cf3
Pavel Zagalsky <pzagalsk@redhat.com>
2016-08-07 12:09:36 +0300

    replicators test fix

--- cfme/tests/containers/test_containers_default_project_replicators.py
+++ cfme/tests/containers/test_containers_default_project_replicators.py
@@ -8,8 +8,7 @@ from utils.version import current_version
 
 
 pytestmark = [
-    pytest.mark.uncollectif(
-        lambda: current_version() < "5.6"),
+    pytest.mark.uncollectif(lambda provider: current_version() < "5.6" and provider.version > 3.2),
     pytest.mark.usefixtures('setup_provider'),
     pytest.mark.tier(1)]
 pytest_generate_tests = testgen.generate(
@@ -26,37 +25,20 @@ REPLICATORS_RELATIONSHIPS_FIELDS = ['Containers Provider', 'Project', 'Pods', 'N
 # CMP - 9531
 
 
-@pytest.mark.parametrize('replicators_properties_fields', product(REPLICATORS_PROPERTIES_FIELDS))
-def test_containers_default_project_replicators_properties(provider, replicators_properties_fields):
+@pytest.mark.parametrize(('prop', 'rel'), product(REPLICATORS_PROPERTIES_FIELDS,
+                                                  REPLICATORS_RELATIONSHIPS_FIELDS))
+def test_replicators_properties(provider, prop, rel):
     """ Default Project Replicator properties test
         This test checks the properties fields of each Replicator
         Steps:
             * Goes to Containers --> Replicators
-            * Makes sure "router-1" is one of the existing Replicators
              * Goes through each Replicator and checks each Properties fields
         """
     sel.force_navigate('containers_replicators')
     replicator_name = [r.name.text for r in list_tbl.rows()]
-    assert "router-1" in replicator_name
     for name in replicator_name:
         obj = Replicator(name, provider)
-        val = obj.get_detail('Properties', ''.join(replicators_properties_fields))
-        assert val
-
-
-@pytest.mark.parametrize('replicators_relationships_fields',
-                         product(REPLICATORS_RELATIONSHIPS_FIELDS))
-def test_containers_default_project_replicators_relationships(
-        provider, replicators_relationships_fields):
-    """ Default Project Replicator properties test
-        This test checks the properties fields of each Replicator
-        Steps:
-            * Goes to Containers --> Replicators
-             * Goes through each Replicator and checks each Relationships fields
-        """
-    sel.force_navigate('containers_replicators')
-    replicator_name = [r.name.text for r in list_tbl.rows()]
-    for name in replicator_name:
-        obj = Replicator(name, provider)
-        val = obj.get_detail('Relationships', ''.join(replicators_relationships_fields))
-        assert val
+        prop_val = obj.get_detail('Properties', ''.join(prop))
+        rel_val = obj.get_detail('Relationships', ''.join(rel))
+        assert prop_val
+        assert rel_val
