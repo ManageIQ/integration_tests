eefe8ed1822d6029502cbf9317c7d5a0a0c71b99
Ronnie Rasouli <rrasouli@redhat.com>
2016-08-31 11:08:47 +0300

    removed soft_assert

--- cfme/tests/infrastructure/test_relationships.py
+++ cfme/tests/infrastructure/test_relationships.py
@@ -1,31 +1,29 @@
-from utils import testgen
-import pytest
-
 """
 This test can run only after overcloud cloud provider created and linked to
 undercloud infra provider, need to compare the cloud providers with the
 results of the relationships
 """
+from utils import testgen
+import pytest
+
 
 pytestmark = [pytest.mark.meta(server_roles='+smartproxy +smartstate')]
 
 
-def pytest_generate_tests(metafunc):
-    argnames, argvalues, idlist = \
-        testgen.infra_providers(metafunc, required_fields=["ssh_credentials"])
-    testgen.parametrize(metafunc, argnames, argvalues, ids=idlist,
-                        scope="module")
+pytest_generate_tests = testgen.generate(testgen.provider_by_type,
+                                         ['openstack-infra'],
+                                         scope='module')
 
 
 @pytest.mark.usefixtures("setup_provider_modscope")
-def test_assinged_roles(provider, soft_assert):
+def test_assinged_roles(provider):
     provider.load_details()
     result = int(provider.summary.relationships.deployment_roles.value)
 
     assert result > 0
 
 
-def test_assinged_tenants(provider, soft_assert):
+def test_assinged_tenants(provider):
     provider.load_details()
     result = int(provider.summary.relationships.cloud_tenants.value)
     """
@@ -36,7 +34,7 @@ def test_assinged_tenants(provider, soft_assert):
     assert result > 0
 
 
-def test_assinged_zones(provider, soft_assert):
+def test_assinged_zones(provider):
     provider.load_details()
     result = int(provider.summary.relationships.availability_zones.value)
     """
@@ -47,7 +45,7 @@ def test_assinged_zones(provider, soft_assert):
     assert result > 0
 
 
-def test_nodes(provider, soft_assert):
+def test_nodes(provider):
     provider.load_details()
     result = int(provider.summary.relationships.nodes.value)
     """
@@ -58,7 +56,7 @@ def test_nodes(provider, soft_assert):
     assert result > 0
 
 
-def test_templates(provider, soft_assert):
+def test_templates(provider):
     provider.load_details()
     result = int(provider.summary.relationships.templates.value)
     """
@@ -69,7 +67,7 @@ def test_templates(provider, soft_assert):
     assert result == 5
 
 
-def test_stacks(provider, soft_assert):
+def test_stacks(provider):
     provider.load_details()
     result = int(provider.summary.relationships.orchestration_stacks.value)
     """
