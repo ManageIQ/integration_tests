e83447ae0abf80fb535fd582f9dc1f128181ed6b
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-09-05 18:09:57 +0200

    filter deprecation warnings
    this avoids duplicate logging

--- utils/deprecation.py
+++ utils/deprecation.py
@@ -1,5 +1,9 @@
+import warnings
+
 from debtcollector.removals import remove
 
+
+warnings.simplefilter('once', category=DeprecationWarning)
 
 removed_in_fw30 = remove(
     removal_version="framework 3.0",
