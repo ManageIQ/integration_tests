b700d4b428b334d4392bf6fdf38fbbe953d3012a
gshefer <gshefer@redhat.com>
2016-09-06 18:34:05 +0300

    In CFME - Test data integrity for topology

--- cfme/tests/containers/test_data_integrity_for_topology.py
+++ cfme/tests/containers/test_data_integrity_for_topology.py
@@ -1,71 +1,66 @@
 import pytest
 from cfme.fixtures import pytest_selenium as sel
-from cfme.web_ui import StatusBox, Table
+from cfme.web_ui import StatusBox, Quadicon
 from utils import testgen
 import time
-from cfme.containers.node import Node
-from cfme.containers.pod import Pod
-from cfme.containers.container import Container
-from cfme.containers.project import Project
-from cfme.containers.image_registry import ImageRegistry
-from cfme.containers.service import Service
-from cfme.containers.image import Image
-from cfme.containers.route import Route
+from cfme.containers.node import Node, list_tbl as list_tbl_nodes
+from cfme.containers.pod import Pod, list_tbl as list_tbl_pods
+from cfme.containers.container import Container, list_tbl as list_tbl_containers
+from cfme.containers.project import Project, list_tbl as list_tbl_projects
+from cfme.containers.image_registry import ImageRegistry, list_tbl as list_tbl_image_registrys
+from cfme.containers.service import Service, list_tbl as list_tbl_services
+from cfme.containers.image import Image, list_tbl as list_tbl_images
+from cfme.containers.route import Route, list_tbl as list_tbl_routes
 from utils.version import current_version
 from cfme.containers.provider import Provider
 
 pytestmark = [
     pytest.mark.uncollectif(
-        lambda provider: current_version() < "5.6" and provider.version > 3.2),
+        lambda provider: current_version() < "5.6"),
     pytest.mark.usefixtures('setup_provider'),
     pytest.mark.tier(1)]
 pytest_generate_tests = testgen.generate(
     testgen.container_providers, scope="function")
 
-# CONTAINER_CLASSES: Referenced from: Menu.sections()
-CONTAINER_CLASSES = {
-    Node: (
-        'containers_nodes', 'Nodes'), Container: (
-            'containers_containers', 'Containers'), ImageRegistry: (
-                'containers_image_registries', 'Registries'), Project: (
-                    'containers_projects', 'Projects'), Pod: (
-                        'containers_pods', 'Pods'), Service: (
-                            'containers_services', 'Services'), Image: (
-                                'containers_images', 'Images'), Route: (
-                                    'containers_routes', 'Routes'), Provider: (
-                                        'containers_providers', 'Providers')}
+# TEST_DATAS: Referenced from: Menu.sections()
+TEST_DATAS = [
+    (Node, 'containers_nodes', 'Nodes', list_tbl_nodes),
+    (Container, 'containers_containers', 'Containers', list_tbl_containers),
+    (ImageRegistry, 'containers_image_registries', 'Registries', list_tbl_image_registrys),
+    (Project, 'containers_projects', 'Projects', list_tbl_projects),
+    (Pod, 'containers_pods', 'Pods', list_tbl_pods),
+    (Service, 'containers_services', 'Services', list_tbl_services),
+    (Image, 'containers_images', 'Images', list_tbl_images),
+    (Route, 'containers_routes', 'Routes', list_tbl_routes),
+    (Provider, 'containers_providers', 'Providers')
+]
 #   CMP-9521
 
 
-def test_data_integrity_for_topology():
-    '''
-    This test verify that every Status box value under Containers Overview
-    is identical to the number presents on its page.
-    Step:
-        In CFME go to Containers -> Overview
-    Expected result:
-        All cells should contain the correct relevant information
-            # of Providers
-            # of Nodes
-            ...
-    '''
+@pytest.mark.parametrize(('test_data'), TEST_DATAS)
+def test_data_integrity_for_topology(test_data):
+    """ This test verifies that every status box value under Containers Overview is identical to the
+    number present on its page.
+    Steps:
+        * Go to Containers / Overview
+        * All cells should contain the correct relevant information
+            # of nodes
+            # of providers
+            # ...
+    """
     section_values = {}
     sel.force_navigate('container_dashboard')
-    # We should wait a second to let it be loaded (until we find a better
-    # solution)
+    # We should wait ~2 seconds for the StatusBox population
+    # (until we find a better solution)
     time.sleep(2)
-    for cls, properties in CONTAINER_CLASSES.items():
-        status_box = StatusBox(properties[1])
-        section_values[cls] = int(status_box.value())
-    for cls, properties in CONTAINER_CLASSES.items():
-        sel.force_navigate(properties[0])
-        if section_values[cls] > 0:
-            if cls == Provider:
-                icons = sel.elements('//div[@id="quadicon"]')
-                assert len(icons) == section_values[cls]
-            else:
-                table = Table(table_locator='//div[@id="list_grid"]//table')
-                assert len(map(lambda r: r, table.rows())
-                           ) == section_values[cls]
+    status_box = StatusBox(test_data[2])
+    section_values[test_data[0]] = int(status_box.value())
+    sel.force_navigate(test_data[1])
+    if section_values[test_data[0]] > 0:
+        if test_data[0] is Provider:
+            assert len(map(lambda i: i, Quadicon.all())) == section_values[test_data[0]]
         else:
-            assert sel.is_displayed_text('No Records Found.')
+            assert len(map(lambda r: r, test_data[3].rows())
+                       ) == section_values[test_data[0]]
+    else:
+        assert sel.is_displayed_text('No Records Found.')
