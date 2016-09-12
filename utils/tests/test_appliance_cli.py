3455974532aed36fdf778699b9fb8541e6bc0f0a
Luke Couzens <lcouzens@redhat.com>
2016-09-12 16:20:08 +0100

    Changes to test_appliance_cli

--- utils/tests/test_appliance_cli.py
+++ utils/tests/test_appliance_cli.py
@@ -4,14 +4,15 @@ from store import current_appliance
 def test_set_hostname(request):
     current_appliance.ap_cli.set_hostname()
     return_code, output = current_appliance.ssh.run_command(
-    "cat /etc/hosts | grep test.example.com")
+        "cat /etc/hosts | grep test.example.com")
     assert return_code == 0
+
 
 def test_configure_appliance(request):
     current_appliance.ap_cli.configure_appliance(region, internal, dbhostname, username,
     password, dbname, key, fetch_key, sshlogin, sshpass)
     return_code, output = (current_appliance.ssh.run_command(
-    "systemctl status evmserverd | grep running"))
+        "systemctl status evmserverd | grep running"))
     assert return_code == 0
 
 # do I still need two tests for configuring appliance now
@@ -23,16 +24,16 @@ def test_configure_appliance(request):
 # ("systemctl status evmserverd | grep running"))
 #    assert return_code == 0
 
+
 def test_configure_ipa(request):
     current_appliance.ap_cli.configure_ipa(
     hostname="", domain="", realm="", username="", password="")
     return_code, output = (current_appliance.ssh.run_command(
-    "systemctl status sssd | grep running"))
+        "systemctl status sssd | grep running"))
     assert return_code == 0
     return_code, output = (current_appliance.ssh.run_command(
-    "cat /etc/ipa/default.conf | grep enable_ra = True"))
+        "cat /etc/ipa/default.conf | grep enable_ra = True"))
     assert return_code == 0
-
 
 
 def test_uninstall_ipa(request):
