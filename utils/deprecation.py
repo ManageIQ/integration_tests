378deb156369d8b0d5216589b443fb3c55197a9d
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-09-22 18:03:20 +0200

    mark the selenum-fixture classes as deprecated

--- utils/deprecation.py
+++ utils/deprecation.py
@@ -1,10 +1,9 @@
 import warnings
-
-from debtcollector.removals import remove
+from functools import partial
+from debtcollector.removals import remove, removed_class
 
 
 warnings.simplefilter('once', category=DeprecationWarning)
 
-removed_in_fw30 = remove(
-    removal_version="framework 3.0",
-)
+removed_in_fw30 = remove(removal_version="framework 3.0")
+removed_class_in_fw30 = partial(removed_class, removal_version="framework 3.0")
