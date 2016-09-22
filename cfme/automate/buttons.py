0f43c646c0dd8b7144dad9d678c3f3bbbaafce06
Pete Savage <psavage@redhat.com>
2016-09-22 13:01:00 +0100

    Converted Button and ButtonGroup

--- cfme/automate/buttons.py
+++ cfme/automate/buttons.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+from navmazing import NavigateToSibling, NavigateToAttribute
+
 from functools import partial
 from cfme import web_ui as ui
 from cfme.exceptions import CandidateNotFound
 from cfme.fixtures import pytest_selenium as sel
-from cfme.web_ui import Form, accordion, fill, flash, form_buttons, menu, DHTMLSelect, AngularSelect
+from cfme.web_ui import Form, accordion, fill, flash, form_buttons, DHTMLSelect, AngularSelect
 from cfme.web_ui import toolbar as tb
+from utils.appliance import Navigatable
+from utils.appliance.endpoints.ui import navigator, navigate_to, CFMENavigateStep
 from utils.update import Updateable
 from utils import deferred_verpick, version
 
@@ -31,53 +35,7 @@ button_form = Form(
     ])
 
 
-def _new_button_group(_context):
-    cfg_btn('Add a new Button Group')
-    sel.wait_for_element(button_group_form.btn_group_text)
-
-
-def _new_button(_context):
-    cfg_btn('Add a new Button')
-    sel.wait_for_element(button_form.btn_text)
-
-
-menu.nav.add_branch(
-    'automate_customization',
-    {
-        'button_group_category':
-        [
-            lambda ctx: buttons_tree(ctx['buttongroup'].type),
-            {
-                'new_button_group': _new_button_group
-            }
-        ],
-        'button_group':
-        [
-            lambda ctx: buttons_tree(ctx['buttongroup'].type, ctx['buttongroup'].text),
-            {
-                'group_button_edit': menu.nav.partial(cfg_btn, "Edit this Button Group")
-            }
-        ],
-        'button_new':
-        [
-            lambda ctx: buttons_tree(ctx['buttongroup'].type, ctx['buttongroup'].text),
-            {
-                'new_button': _new_button
-            }
-        ],
-        'button':
-        [
-            lambda ctx: buttons_tree(
-                ctx['button'].group.type, ctx['button'].group.text, ctx['button'].text),
-            {
-                'button_edit': menu.nav.partial(cfg_btn, "Edit this Button")
-            }
-        ],
-    }
-)
-
-
-class ButtonGroup(Updateable):
+class ButtonGroup(Updateable, Navigatable):
     """Create,Edit and Delete Button Groups
 
     Args:
@@ -96,13 +54,15 @@ class ButtonGroup(Updateable):
     TEMPLATE = "VM Template and Image"
     VM_INSTANCE = "VM and Instance"
 
-    def __init__(self, text=None, hover=None, type=None):
+    def __init__(self, text=None, hover=None, type=None, appliance=None):
+        Navigatable.__init__(self, appliance=appliance)
         self.text = text
         self.hover = hover
         self.type = type
 
     def create(self):
-        sel.force_navigate('new_button_group', context={"buttongroup": self})
+        navigate_to(self, 'Add')
+
         fill(button_group_form, {'btn_group_text': self.text,
                                  'btn_group_hvr_text': self.hover})
         if version.current_version() < "5.5":
@@ -114,14 +74,14 @@ class ButtonGroup(Updateable):
         flash.assert_success_message('Buttons Group "{}" was added'.format(self.hover))
 
     def update(self, updates):
-        sel.force_navigate('group_button_edit', context={"buttongroup": self})
+        navigate_to(self, 'Edit')
         edited_hvr_text = updates.get('hover', None)
         fill(button_group_form, {'btn_group_hvr_text': edited_hvr_text})
         sel.click(button_group_form.save_button)
         flash.assert_success_message('Buttons Group "{}" was saved'.format(edited_hvr_text))
 
     def delete(self):
-        sel.force_navigate('button_group', context={"buttongroup": self})
+        navigate_to(self, 'Details')
         cfg_btn("Remove this Button Group", invokes_alert=True)
         sel.handle_alert()
         flash.assert_success_message('Buttons Group "{}": Delete successful'.format(self.hover))
@@ -129,7 +89,7 @@ class ButtonGroup(Updateable):
     @property
     def exists(self):
         try:
-            sel.force_navigate('button_group', context={"buttongroup": self})
+            navigate_to(self, 'Details')
             return True
         except CandidateNotFound:
             return False
@@ -139,7 +99,45 @@ class ButtonGroup(Updateable):
             self.delete()
 
 
-class Button(Updateable):
+@navigator.register(ButtonGroup, 'All')
+class ButtonGroupAll(CFMENavigateStep):
+    prerequisite = NavigateToAttribute('appliance', 'LoggedIn')
+
+    def step(self):
+        from cfme.web_ui.menu import nav
+        nav._nav_to_fn('Automate', 'Customization')(None)
+
+    def resetter(self):
+        accordion.tree("Buttons", "Object Types")
+
+
+@navigator.register(ButtonGroup, 'Add')
+class ButtonGroupNew(CFMENavigateStep):
+    prerequisite = NavigateToSibling('All')
+
+    def step(self):
+        accordion.tree("Buttons", "Object Types", self.obj.type)
+        cfg_btn('Add a new Button Group')
+        sel.wait_for_element(button_group_form.btn_group_text)
+
+
+@navigator.register(ButtonGroup, 'Details')
+class ButtonGroupDetails(CFMENavigateStep):
+    prerequisite = NavigateToSibling('All')
+
+    def step(self):
+        accordion.tree("Buttons", "Object Types", self.obj.type, self.obj.text)
+
+
+@navigator.register(ButtonGroup, 'Edit')
+class ButtonGroupEdit(CFMENavigateStep):
+    prerequisite = NavigateToSibling('Details')
+
+    def step(self):
+        cfg_btn("Edit this Button Group")
+
+
+class Button(Updateable, Navigatable):
     """Create,Edit and Delete buttons under a Button
 
     Args:
@@ -152,7 +150,8 @@ class Button(Updateable):
 
     def __init__(self, group=None, text=None,
                  hover=None, dialog=None,
-                 system=None, request=None):
+                 system=None, request=None, appliance=None):
+        Navigatable.__init__(self, appliance=appliance)
         self.group = group
         self.text = text
         self.hover = hover
@@ -161,7 +160,7 @@ class Button(Updateable):
         self.request = request
 
     def create(self):
-        sel.force_navigate('new_button', context={'buttongroup': self.group})
+        navigate_to(self, 'Add')
         fill(button_form, {'btn_text': self.text,
                            'btn_hvr_text': self.hover})
         if version.current_version() < "5.5":
@@ -176,14 +175,14 @@ class Button(Updateable):
         flash.assert_success_message('Button "{}" was added'.format(self.hover))
 
     def update(self, updates):
-        sel.force_navigate('button_edit', context={'button': self})
+        navigate_to(self, 'Edit')
         edited_hover = updates.get('hover', None)
         fill(button_form, {'btn_hvr_text': edited_hover})
         sel.click(button_form.save_button)
         flash.assert_success_message('Button "{}" was saved'.format(edited_hover))
 
     def delete(self):
-        sel.force_navigate('button', context={'button': self})
+        navigate_to(self, 'Details')
         cfg_btn("Remove this Button", invokes_alert=True)
         sel.handle_alert()
         flash.assert_success_message('Button "{}": Delete successful'.format(self.hover))
@@ -191,7 +190,7 @@ class Button(Updateable):
     @property
     def exists(self):
         try:
-            sel.force_navigate('button', context={"button": self})
+            navigate_to(self, 'Details')
             return True
         except CandidateNotFound:
             return False
@@ -199,3 +198,42 @@ class Button(Updateable):
     def delete_if_exists(self):
         if self.exists:
             self.delete()
+
+
+@navigator.register(Button, 'All')
+class ButtonAll(CFMENavigateStep):
+    prerequisite = NavigateToAttribute('appliance', 'LoggedIn')
+
+    def step(self):
+        from cfme.web_ui.menu import nav
+        nav._nav_to_fn('Automate', 'Customization')(None)
+
+    def resetter(self):
+        accordion.tree("Buttons", "Object Types")
+
+
+@navigator.register(Button, 'Add')
+class ButtonNew(CFMENavigateStep):
+    prerequisite = NavigateToSibling('All')
+
+    def step(self):
+        accordion.tree("Buttons", "Object Types", self.obj.group.type, self.obj.group.text)
+        cfg_btn('Add a new Button')
+        sel.wait_for_element(button_form.btn_text)
+
+
+@navigator.register(Button, 'Details')
+class ButtonDetails(CFMENavigateStep):
+    prerequisite = NavigateToSibling('All')
+
+    def step(self):
+        accordion.tree(
+            "Buttons", "Object Types", self.obj.group.type, self.obj.group.text, self.obj.text)
+
+
+@navigator.register(Button, 'Edit')
+class ButtonEdit(CFMENavigateStep):
+    prerequisite = NavigateToSibling('Details')
+
+    def step(self):
+        cfg_btn("Edit this Button")
