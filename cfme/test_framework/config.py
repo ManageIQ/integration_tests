b1ec6afbd165ccbc0dacc89622c93f074ebd9287
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-09-23 15:32:24 +0200

    begin configuration encapsulation
    move the setup code around and keep your standard setup

---
+++ cfme/test_framework/config.py
@@ -0,0 +1,41 @@
+import os
+import warnings
+import yaycl
+
+
+class Configuration(object):
+    def __init__(self):
+        self.yaycl_config = None
+
+    def configure(self, config_dir, crypt_key_file=None):
+        assert self.yaycl_config is None
+        if crypt_key_file and os.path.exists(crypt_key_file):
+            self.yaycl_config = yaycl.Config(
+                config_dir=config_dir,
+                crypt_key_file=crypt_key_file)
+        else:
+            self.yaycl_config = yaycl.Config(config_dir=config_dir)
+
+    def get_config(self, name):
+        if self.yaycl_config is None:
+            raise RuntimeError('cfme configuration was not initialized')
+        return getattr(self.yaycl_config, name)
+
+
+class DeprecatedConfigWrapper(object):
+    def __init__(self, configuration):
+        self.configuration = configuration
+
+    def __getattr__(self, key):
+        return self.configuration.get_config(key)
+
+    def __getitem__(self, key):
+        return self.configuration.get_config(key)
+
+    def __delitem__(self, key):
+        # used in bad logging
+        warnings.warn('clearing configuration is bad', category=Warning)
+
+        del self.configuration.yaycl_config[key]
+
+global_configuration = Configuration()
