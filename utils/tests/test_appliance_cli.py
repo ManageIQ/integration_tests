2fdcb6b694e100cccbbe95ed051f99e8b67bc2bd
Luke Couzens <lcouzens@redhat.com>
2016-09-29 16:04:24 +0100

    New updates to WIP tests

--- utils/tests/test_appliance_cli.py
+++ utils/tests/test_appliance_cli.py
@@ -1,6 +1,7 @@
 from fixtures.pytest_store import store
 from utils import version
 from utils.appliance import provision_appliance
+from utils.conf import cfme_data, credentials
 import pytest
 
 
@@ -11,7 +12,7 @@ def test_set_hostname(request):
     assert return_code == 0
 
 
-@pytest.yield_fixture
+@pytest.yield_fixture(scope="module")
 def provisioned_appliance():
     ver_to_prov = str(version.current_version())
     app = provision_appliance(version=ver_to_prov)
@@ -20,29 +21,51 @@ def provisioned_appliance():
 
     app.destroy()
 
-# Work in progress trying to simplify strings
+
+@pytest.fixture()
+def app_creds():
+    return {
+        'username': credentials['database']['username'],
+        'password': credentials['database']['password'],
+        'sshlogin': credentials['ssh']['username'],
+        'sshpass': credentials['ssh']['password']
+    }
 
 
-def test_configure_appliance_internal_fetch_key(request, provisioned_appliance):
-    app = provisioned_appliance
-    app.ap_cli.configure_appliance_fetch_key(db_configuration)
-    db_configuration = ('-r 0 -i -h localhost -U {} -p {} -d {} -k {} -v -K {} -s {} -a {}')
-    assert app.is_evm_service_running()
-    assert app.is_web_ui_running()
+def test_configure_appliance_internal_fetch_key(request, provisioned_appliance, app_creds):
+        app = provisioned_appliance
+        fetch_key_ip = store.current_appliance.address
+        app.ap_cli.configure_appliance(0, 'localhost', app_creds['username'], app_creds['password'],
+            'vmdb_production', fetch_key_ip, app_creds['sshlogin'], app_creds['sshpass'])
+        assert app.is_evm_service_running()
+        app.ipapp.wait_for_web_ui()
+        assert app.is_web_ui_running()
 
 
-def test_configure_ipa(request):
-    current_appliance.ap_cli.configure_ipa(
-        hostname, domain, realm, username, password)
-    return_code, output = (current_appliance.ssh.run_command(
+@pytest.fixture()
+def ipa_creds():
+    fqdn = cfme_data['ipa']['ipaserver']
+    fqdn.split('.', 1)
+    return{
+        'hostname': fqdn[0]
+        'domain': fqdn[1]
+        'realm': cfme_data['ipa']['realm']
+        'username': credentials['ipa_server']['username']
+        'password': credentials['ipa_server']['password']
+    }
+
+
+def test_configure_ipa(request, ipa_creds):
+    store.current_appliance.ap_cli.configure_ipa(hostname, domain, realm, username, password)
+    return_code, output = (store.current_appliance.ssh_client.run_command(
         "systemctl status sssd | grep running"))
     assert return_code == 0
-    return_code, output = (current_appliance.ssh.run_command(
+    return_code, output = (store.current_appliance.ssh.run_command(
         "cat /etc/ipa/default.conf | grep enable_ra = True"))
     assert return_code == 0
 
 
 def test_uninstall_ipa(request):
-    current_appliance.ap_cli.uninstall_ipa()
-    return_code, output = (current_appliance.ssh.run_command("cat /etc/ipa/default.conf"))
+    store.current_appliance.ap_cli.uninstall_ipa()
+    return_code, output = (store.current_appliance.ssh.run_command("cat /etc/ipa/default.conf"))
     assert return_code == 0
