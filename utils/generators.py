4bb2844b36cf25414cc715e1f1dcf876699a2cd7
Milan Falešník <mfalesni@redhat.com>
2016-10-03 09:45:56 +0200

    random_vm_name: Reduce length

--- utils/generators.py
+++ utils/generators.py
@@ -3,18 +3,23 @@ import fauxfactory
 import re
 
 
-def random_vm_name(context=None):
+def random_vm_name(context=None, max_length=15):
     """Generates a valid VM name that should be valid for any provider we use.
 
     Constraints:
-        * Maximum string length 39 characters
+        * Maximum string length 15 characters (by default)
         * Only [a-z0-9-]
 
     Args:
-        context: If you want to provide some custom string after ``test-`` instead of ``vm``
+        context: If you want to provide some custom string after ``test-`` instead of ``vm``.
+            It is recommended to use a maximum of 5 characters with the default 15 character limit.
+            Longer strings will be truncated
 
     Returns:
         A valid randomized VM name.
     """
-    context = re.sub(r'[^a-z0-9-]', '', (context or 'vm').lower())[:27]
-    return 'test-{}-{}'.format(context, fauxfactory.gen_alphanumeric(length=6).lower())
+    template_str_length = 6
+    random_chars = 4
+    context_length = max_length - random_chars - template_str_length
+    context = re.sub(r'[^a-z0-9-]', '', (context or 'vm').lower())[:context_length]
+    return 'test-{}-{}'.format(context, fauxfactory.gen_alphanumeric(length=random_chars).lower())
