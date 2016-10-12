a4e96af8b209853f5e4f29afd0b1ec53734bdc44
amavinag <amavinag@redhat.com>
2016-10-12 17:26:12 -0400

    CFME Container Image Registry Navmazing conversion.
    {{pytest: cfme/tests/containers/test_views_objects.py::test_imageregistry_views[cm-env1] --use-provider cm-env1}}

--- cfme/containers/image_registry.py
+++ cfme/containers/image_registry.py
@@ -1,42 +1,68 @@
 # -*- coding: utf-8 -*-
+from functools import partial
+
+from navmazing import NavigateToSibling, NavigateToAttribute
+
 from cfme.common import SummaryMixin, Taggable
 from cfme.fixtures import pytest_selenium as sel
-from cfme.web_ui import CheckboxTable, toolbar as tb
-from cfme.web_ui.menu import nav
+from cfme.web_ui import CheckboxTable, toolbar as tb, paginator, match_location
+from utils.appliance import Navigatable
+from utils.appliance.implementations.ui import CFMENavigateStep, navigator, navigate_to
+from . import pol_btn
 
 list_tbl = CheckboxTable(table_locator="//div[@id='list_grid']//table")
 
-nav.add_branch(
-    'containers_image_registries',
-    {
-        'containers_image_registry':
-        lambda ctx: list_tbl.select_row_by_cells(
-            {'Host': ctx['image_registry'].host, 'Provider': ctx['image_registry'].provider.name}),
 
-        'containers_image_registry_detail':
-        lambda ctx: list_tbl.click_row_by_cells(
-            {'Host': ctx['image_registry'].host, 'Provider': ctx['image_registry'].provider.name}),
-    }
-)
+match_page = partial(match_location, controller='container_image_registry',
+                     title='Image Registries')
 
 
-class ImageRegistry(Taggable, SummaryMixin):
+class ImageRegistry(Taggable, SummaryMixin, Navigatable):
 
-    def __init__(self, host, provider):
+    def __init__(self, host, provider, appliance=None):
         self.host = host
         self.provider = provider
+        Navigatable.__init__(self, appliance=appliance)
 
     def load_details(self, refresh=False):
-        if not self._on_detail_page():
-            self.navigate(detail=True)
-        elif refresh:
+        navigate_to(self, 'Details')
+        if refresh:
             tb.refresh()
 
-    def navigate(self, detail=True):
-        if detail is True:
-            if not self._on_detail_page():
-                sel.force_navigate('containers_image_registry_detail',
-                    context={'image_registry': self})
-        else:
-            sel.force_navigate('containers_image_registry',
-                context={'image_registry': self})
+
+@navigator.register(ImageRegistry, 'All')
+class ImageRegistryAll(CFMENavigateStep):
+    prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
+
+    def am_i_here(self):
+        return match_page(summary='Image Registries')
+
+    def step(self):
+        from cfme.web_ui.menu import nav
+        nav._nav_to_fn('Compute', 'Containers', 'Image Registries')(None)
+
+    def resetter(self):
+        tb.select('List View')
+        if paginator.page_controls_exist():
+            sel.check(paginator.check_all())
+            sel.uncheck(paginator.check_all())
+
+
+@navigator.register(ImageRegistry, 'Details')
+class ImageRegistryDetails(CFMENavigateStep):
+    prerequisite = NavigateToSibling('All')
+
+    def am_i_here(self):
+        return match_page(summary='{} (Summary)'.format(self.obj.host))
+
+    def step(self):
+        tb.select('List View')
+        list_tbl.click_row_by_cells({'Host': self.obj.host})
+
+
+@navigator.register(ImageRegistry, 'EditTags')
+class ImageRegistryEditTags(CFMENavigateStep):
+    prerequisite = NavigateToSibling('Details')
+
+    def step(self):
+        pol_btn('Edit Tags')
