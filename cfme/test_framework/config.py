4ae7ae30bb75b142c0d908c6aa31144450786584
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-10-17 09:18:19 +0200

    config objects: add docstrings

--- cfme/test_framework/config.py
+++ cfme/test_framework/config.py
@@ -1,13 +1,31 @@
+"""
+classes to manage the cfme test framework configuration
+"""
+
 import os
 import warnings
 import yaycl
 
 
 class Configuration(object):
+    """
+    holds the current configuration
+    """
     def __init__(self):
         self.yaycl_config = None
 
     def configure(self, config_dir, crypt_key_file=None):
+        """do the defered initial loading of the configuration
+        :param config_dir: path to the folder with configuration files
+        :param crypt_key_file:
+            optional name of a file holding the key
+            for encrypted configurationfiles
+
+        :raises: AssertionError if called more than once
+
+        if the `utils.conf` api is removed, the loading can be transformed to eager loading
+        """
+
         assert self.yaycl_config is None
         if crypt_key_file and os.path.exists(crypt_key_file):
             self.yaycl_config = yaycl.Config(
@@ -23,6 +41,9 @@ class Configuration(object):
 
 
 class DeprecatedConfigWrapper(object):
+    """
+    a wrapper that provides the old :code:``utils.conf`` api
+    """
     def __init__(self, configuration):
         self.configuration = configuration
 
