495a88748c52cfc24aa8d1bd7b63c28bf680aa48
Pete Savage <psav@users.noreply.github.com>
2016-10-31 16:57:27 +0000

    Fix artifactor slave handling (#3717)

--- fixtures/rbac.py
+++ fixtures/rbac.py
@@ -105,6 +105,7 @@ enable_rbac = False
 
 
 def save_traceback_file(node, contents):
+    from fixtures.artifactor_plugin import SLAVEID
     """A convenience function for artifactor file sending
 
     This function simply takes the nodes id and the contents of the file and processes
@@ -117,19 +118,24 @@ def save_traceback_file(node, contents):
     name, location = get_test_idents(node)
     art_client.fire_hook('filedump', test_location=location, test_name=name,
                          description="RBAC Traceback",
-                         contents=contents, file_type="rbac", group_id="RBAC")
+                         contents=contents, file_type="rbac", group_id="RBAC", slaveid=SLAVEID)
 
 
 def save_screenshot(node, ss, sse):
     name, location = get_test_idents(node)
+    from fixtures.artifactor_plugin import SLAVEID
     if ss:
-        art_client.fire_hook('filedump', test_location=location, test_name=name,
+        art_client.fire_hook(
+            'filedump', test_location=location, test_name=name,
             description="RBAC Screenshot", file_type="rbac_screenshot", mode="wb",
-            contents_base64=True, contents=ss, display_glyph="camera", group_id="RBAC")
+            contents_base64=True, contents=ss, display_glyph="camera", group_id="RBAC",
+            slaveid=SLAVEID)
     if sse:
-        art_client.fire_hook('filedump', test_location=location, test_name=name,
+        art_client.fire_hook(
+            'filedump', test_location=location, test_name=name,
             description="RBAC Screenshot error", file_type="rbac_screenshot_error", mode="w",
-            contents_base64=False, contents=sse, display_type="danger", group_id="RBAC")
+            contents_base64=False, contents=sse, display_type="danger", group_id="RBAC",
+            slaveid=SLAVEID)
 
 
 def really_logout():
