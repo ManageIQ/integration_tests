25ed112ca4b1da5ee929deccc306c5f4998ccdca
Pavel Zagalsky <pzagalsk@redhat.com>
2016-11-07 15:24:21 +0200

    Test Basic Metrics test

--- cfme/tests/containers/test_basic_metrics.py
+++ cfme/tests/containers/test_basic_metrics.py
@@ -1,9 +1,8 @@
 import pytest
-import cfme.fixtures.pytest_selenium as sel
 from utils import testgen
-from utils.browser import ensure_browser_open
 from utils import conf
 from utils.version import current_version
+
 
 pytestmark = [
     pytest.mark.uncollectif(lambda provider: current_version() < "5.6"),
@@ -12,14 +11,16 @@ pytest_generate_tests = testgen.generate(
     testgen.container_providers, scope='function')
 
 
-def test_basic_metrics(provider):
+def test_basic_metrics(provider, ssh_client):
     """ Basic Metrics availability test
         This test checks that the Metrics service is up
-        Opens the hawkular status page and checks if it's up
+        Curls the hawkular status page and checks if it's up
         """
-    hostname = 'https://' + conf.cfme_data.get('management_systems', {})[provider.key]\
-        .get('hostname', []) + '/hawkular/metrics'
-    ensure_browser_open()
-    sel.get(hostname)
-    element = sel.ByText('STARTED')
-    assert element
+    username, password = provider.credentials['token'].principal,\
+        provider.credentials['token'].secret
+    hostname = conf.cfme_data.get('management_systems', {})[provider.key]\
+        .get('hostname', [])
+    host_url = 'https://' + hostname + '/hawkular/metrics/'
+    command = 'curl -X GET ' + host_url + ' --insecure'
+    ssh_client = ssh_client(hostname=hostname, username=username, password=password)
+    assert 'Hawkular Metrics' or 'Hawkular-Metrics' in str(ssh_client.run_command(command))
