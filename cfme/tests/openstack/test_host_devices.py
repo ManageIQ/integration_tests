547b5b80befedb142baefac7e7b828e2643ecd56
Ronnie Rasouli <rrasouli@redhat.com>
2016-11-13 14:35:50 +0200

    moved redundant SSA from tests

--- cfme/tests/openstack/test_host_devices.py
+++ cfme/tests/openstack/test_host_devices.py
@@ -1,12 +1,10 @@
 
 import pytest
 from utils import testgen
-from cfme.web_ui import Quadicon, toolbar
+from cfme.web_ui import Quadicon
 from cfme.infrastructure.host import Host
 from cfme.web_ui import InfoBlock
 from cfme.fixtures import pytest_selenium as sel
-from cfme.configure.tasks import is_host_analysis_finished
-from utils.wait import wait_for
 
 
 pytest_generate_tests = testgen.generate(testgen.provider_by_type,
@@ -25,8 +23,5 @@ def test_host_devices(provider):
     assert len(my_quads) > 0
     for quad in my_quads:
         host = Host(name=quad.name)
-        host.run_smartstate_analysis()
-        wait_for(lambda: is_host_analysis_finished(host.name), delay=15,
-                 timeout="10m", fail_func=lambda: toolbar.select('Reload'))
         result = get_integer_value(host.get_detail("Properties", "Devices"))
         assert result > 0
