f975be013806cbc61fbd4c404ec13020abfffcd3
Mike Shriver <mshriver@redhat.com>
2016-11-15 21:23:19 -0500

    Update cfme.config.about, add get_details
    RHCFQE-1245

--- cfme/configure/about.py
+++ cfme/configure/about.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
-from cfme.web_ui import Region
+import re
+
+import cfme.fixtures.pytest_selenium as sel
+from cfme.exceptions import ElementOrBlockNotFound
+from cfme.web_ui import Region, InfoBlock
 from utils import version
+from utils.appliance import current_appliance
+from utils.appliance.implementations.ui import navigate_to
+from utils.log import logger
+from utils.version import current_version
+
 
 product_assistance = Region(
     locators={
@@ -55,3 +64,21 @@ product_assistance = Region(
     identifying_loc='quick_start_guide',
     infoblock_type="form"
 )
+
+
+def get_detail(properties):
+    navigate_to(current_appliance().server, 'About')
+    if current_version() < '5.7':
+        return InfoBlock.text(*properties).encode(
+            "utf-8").strip()
+    else:
+        locator = '//div[@class="product-versions-pf"]//li'
+        sel.wait_for_element(locator)
+        for element in sel.elements(locator):
+            logger.debug('Checking for detail match for "{}" in  "{}"'.format(properties,
+                                                                              element.text))
+            match = re.match("{}\s(?P<value>.*)".format(properties), element.text)
+            if match:
+                return match.group('value')
+        else:
+            raise ElementOrBlockNotFound('Could not match about detail {}'.format(properties))
