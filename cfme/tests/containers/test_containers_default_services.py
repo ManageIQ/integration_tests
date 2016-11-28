6770d3756145d7625beea2c43bd8e765e8693a62
gshefer <gshefer@redhat.com>
2016-11-28 13:43:53 +0200

    test_containers_default_services

---
+++ cfme/tests/containers/test_containers_default_services.py
@@ -0,0 +1,38 @@
+import pytest
+from utils import testgen
+from utils.appliance.implementations.ui import navigate_to
+from utils.version import current_version
+from cfme.containers.service import Service, list_tbl
+
+
+pytestmark = [
+    pytest.mark.uncollectif(lambda provider: current_version() < "5.6"),
+    pytest.mark.usefixtures('setup_provider'),
+    pytest.mark.tier(1)]
+pytest_generate_tests = testgen.generate(
+    testgen.container_providers, scope='function')
+
+
+DefaultServices = ['frontend',
+                   'frontend-prod',
+                   'jenkins',
+                   'kubernetes',
+                   'router']
+
+
+# CMP-9884
+
+def test_containers_default_services():
+    """This test ensures that default container services are existing after provider setup
+
+    Steps:
+        * Go to Compute -> Container services.
+
+    Expected result:
+        ALL DefaultServices above should exist (appear in the table).
+    """
+    navigate_to(Service, 'All')
+    names = [r[2].text for r in list_tbl.rows()]
+    not_in_list = [serv for serv in DefaultServices if serv not in names]
+    if not_in_list:
+        pytest.fail('The following services not found: {}'.format(not_in_list))
