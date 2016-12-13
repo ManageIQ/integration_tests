84c8d59db2154430d35ca1b3ba57b436b4fc2a46
Ronnie Rasouli <rrasouli@redhat.com>
2016-12-13 15:48:23 +0200

    fix navigation to nodes
    CFME 4.2 changed to "Hosts / Nodes"

--- cfme/tests/openstack/test_host_configuration.py
+++ cfme/tests/openstack/test_host_configuration.py
@@ -16,7 +16,7 @@ pytest_generate_tests = testgen.generate(testgen.provider_by_type,
 @pytest.mark.usefixtures("setup_provider_modscope")
 def test_host_configuration(provider, soft_assert):
     provider.load_details()
-    sel.click(InfoBlock.element("Relationships", "Nodes"))
+    sel.click(InfoBlock.element("Relationships", "Hosts / Nodes"))
     my_quads = list(Quadicon.all())
     assert len(my_quads) > 0
     for quad in my_quads:
