b8ad223c7123e55df6b3c274014ecbf0b591f258
Tony Shlifstein <ashlifst@redhat.com>
2016-12-14 23:15:55 +0200

    Minor fixes

--- cfme/tests/containers/test_reload_button_provider.py
+++ cfme/tests/containers/test_reload_button_provider.py
@@ -1,5 +1,6 @@
 import pytest
 
+from cfme.containers.provider import ContainersProvider
 from utils import testgen, version
 from cfme.web_ui import toolbar as tb
 from utils.appliance.implementations.ui import navigate_to
@@ -10,8 +11,7 @@ pytestmark = [
         lambda: version.current_version() < "5.6"),
     pytest.mark.usefixtures('setup_provider'),
     pytest.mark.tier(2)]
-pytest_generate_tests = testgen.generate(
-    testgen.container_providers, scope='function')
+pytest_generate_tests = testgen.generate([ContainersProvider], scope='function')
 
 # CMP-9878
 
@@ -31,21 +31,21 @@ def test_reload_button_provider(provider):
     tb.select('Reload Current Display')
     provider.validate_stats(ui=True)
 
-    l1 = provider.mgmt.list_image()
+    list_img_from_registry = provider.mgmt.list_image()
+    list_img_from_registry_splitted = [i.id.split(
+        '@sha256:')[-1] for i in list_img_from_registry]
 
-    l1splitted = [i[2].split('@sha256:')[-1] for i in l1]
+    list_img_from_openshift = provider.mgmt.list_image_openshift()
+    list_img_from_openshift_splitted = [d['name']
+                                        for d in list_img_from_openshift]
+    list_img_from_openshift_parsed = [i[7:].split(
+        '@sha256:')[-1] for i in list_img_from_openshift_splitted]
 
-    l2 = provider.mgmt.list_image_openshift()
-
-    l2splitted = [d['name'] for d in l2]
-
-    l2splitted_new = [i[7:].split('@sha256:')[-1] for i in l2splitted]
-
-    for s in l2splitted_new:
-        for item in l1splitted:
-            if item not in l2splitted_new:
-                l2splitted_new.append(item)
+    for s in list_img_from_openshift_parsed:
+        for item in list_img_from_registry_splitted:
+            if item not in list_img_from_openshift_parsed:
+                list_img_from_openshift_parsed.append(item)
 
     num_img_in_cfme = provider.summary.relationships.container_images.value
 
-    assert len(l2splitted_new) == num_img_in_cfme
+    assert len(list_img_from_openshift_parsed) == num_img_in_cfme
