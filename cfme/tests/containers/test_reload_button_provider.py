c2560cb708f25db422af24ebff98b64f9598c953
Tony Shlifstein <ashlifst@redhat.com>
2016-12-15 10:58:15 +0200

    Version fix

--- cfme/tests/containers/test_reload_button_provider.py
+++ cfme/tests/containers/test_reload_button_provider.py
@@ -44,8 +44,13 @@ def test_reload_button_provider(provider):
     list_img_from_openshift_parsed.extend([item for item in list_img_from_registry_splitted
                                            if item not in list_img_from_openshift_parsed])
 
-    num_img_in_cfme = provider.summary.relationships.container_images.value
-    assert len(list_img_from_openshift_parsed) == num_img_in_cfme
+    num_img_in_cfme = provider.num_image()
+
+    num_img_cfme_56 = len(provider.mgmt.list_image())
+    num_img_cfme_57 = len(list_img_from_openshift_parsed)
+
+    assert num_img_in_cfme == version.pick({version.LOWEST: num_img_cfme_56,
+                                            '5.7': num_img_cfme_57})
 
     list_all_rgstr = provider.mgmt.list_image_registry()
     list_all_rgstr_revised = [i.host for i in list_all_rgstr]
