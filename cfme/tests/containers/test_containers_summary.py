cb190d3592bf58b1678d1b0ca34102e2c70914c5
Pavel <zagalsky@gmail.com>
2016-12-20 11:45:18 +0200

    containers summary fixes

--- cfme/tests/containers/test_containers_summary.py
+++ cfme/tests/containers/test_containers_summary.py
@@ -43,6 +43,5 @@ def test_containers_summary_objects(provider):
     prov_ui_values = dict()
     for obj_type in container_object:
         prov_ui_values[obj_type] = getattr(provider.summary.relationships, obj_type).value
-        sel.force_navigate('container_dashboard')
         time.sleep(2)
         assert StatusBox(obj_type).value() == prov_ui_values[obj_type]
