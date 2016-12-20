ca2cd32c1070ff732f6ed9b800348976ebf83daa
Oleksandr Kolisnyk <okolisny@redhat.com>
2016-12-20 18:55:31 +0200

    code style changes, refactoring

--- cfme/tests/openstack/cloud/test_volumes.py
+++ cfme/tests/openstack/cloud/test_volumes.py
@@ -1,33 +1,34 @@
 """Tests for cloud provider's volumes"""
 
+import cfme.web_ui.flash as flash
 import pytest
 from fauxfactory import gen_alphanumeric
 from cfme.cloud.instance import Instance
-from cfme.cloud.volume import (creation_form, device_input, get_volume_name,
-                               list_tbl, select_vm, select_volume, Volume)
-from cfme.fixtures import pytest_selenium
-from cfme.web_ui import InfoBlock, toolbar
-from cfme.web_ui.flash import assert_message_contain
-from cfme.web_ui.form_buttons import (attach_volume, detach_volume, FormButton,
+from cfme.cloud.volume import (attach_inst_page_form, attach_vlm_page_form,
+                               creation_form, detach_inst_page_form,
+                               detach_vlm_page_form, get_volume_name, list_tbl,
+                               Volume)
+from cfme.fixtures import pytest_selenium as sel
+from cfme.web_ui import InfoBlock, toolbar as tb
+from cfme.web_ui.form_buttons import (add_ng, attach_volume, detach_volume,
                                       submit_changes)
 from random import choice
-from utils import testgen, version
+from utils import testgen
 from utils.appliance.implementations.ui import navigate_to
+from utils.version import current_version
 from utils.wait import wait_for
 
 pytest_generate_tests = testgen.generate(testgen.provider_by_type,
                                          ['openstack'], scope='module')
 
+pytestmark = [pytest.mark.uncollectif(lambda: current_version() >= '5.7'),
+              pytest.mark.usefixtures("setup_provider_modscope")]
 
-PROD_VERSION = version.current_version().product_version()
 
-
-@pytest.mark.usefixtures("setup_provider_modscope")
-@pytest.mark.uncollectif(lambda: PROD_VERSION > '4.1')
 def test_create_volume(provider):
     """Creates a volume for given cloud provider"""
     navigate_to(Volume, 'All')
-    toolbar.select('Configuration', 'Add a new Cloud Volume')
+    tb.select('Configuration', 'Add a new Cloud Volume')
     vname = gen_alphanumeric()
     vsize = 1
     volume_data = dict(volume_name=vname,
@@ -35,11 +36,11 @@ def test_create_volume(provider):
                        cloud_tenant=provider.mgmt.list_tenant()[0])
     creation_form.fill(volume_data)
 
-    pytest_selenium.click(FormButton('Add', ng_click='addClicked()'))
-    assert_message_contain('Create Cloud Volume')
+    sel.click(add_ng)
+    flash.assert_message_contain('Create Cloud Volume')
 
     wait_for(provider.is_refreshed, [None, 10], delay=5)
-    pytest_selenium.refresh()
+    sel.refresh()
     params = {'Name': vname,
               'Size': '{} GB'.format(vsize),
               'Status': 'available'}
@@ -47,7 +48,6 @@ def test_create_volume(provider):
     assert res, 'Newly created volume doesn\'t appear in UI'
 
 
-@pytest.mark.uncollectif(lambda: PROD_VERSION > '4.1')
 def test_list_volumes_provider_details(provider):
     """Verifies that all provider's volumes are present on details page"""
     # Gather all provider's volumes name
@@ -56,9 +56,8 @@ def test_list_volumes_provider_details(provider):
         vname = provider.mgmt.get_volume(volume).to_dict()['display_name']
         volumes.append(vname)
     provider.load_details()
-    err_msg = 'Details page shows wrong number of provider volumes'
     clv_el = provider.summary.relationships.cloud_volumes
-    assert len(volumes) == clv_el.value, err_msg
+    assert len(volumes) == clv_el.value
 
     clv_el.click()
     err_msg = 'One of volumes is not displayed'
@@ -66,7 +65,6 @@ def test_list_volumes_provider_details(provider):
         assert list_tbl.find_cell('Name', volume), err_msg
 
 
-@pytest.mark.uncollectif(lambda: PROD_VERSION > '4.1')
 def test_attach_volume_from_instance_page(provider):
     """Attaches pre-created volume to an instance from Instance page"""
     vms = filter(lambda vm: vm.power_state == 'ACTIVE', provider.mgmt.all_vms())
@@ -81,23 +79,22 @@ def test_attach_volume_from_instance_page(provider):
     assert volume_name, "Can't find free volume to attach"
 
     Instance(instance_name, provider).load_details()
-    toolbar.select('Configuration', 'Attach a Cloud Volume to this Instance')
-    assert len(select_volume.all_options) > 0
+    tb.select('Configuration', 'Attach a Cloud Volume to this Instance')
+    assert len(attach_inst_page_form.select_volume.all_options) > 0
 
-    select_volume.select_by_value(select_volume.get_value_by_text(volume_name))
-    pytest_selenium.send_keys(device_input, '/dev/vdb')
-    pytest_selenium.click(submit_changes)
-    assert_message_contain('Attaching Cloud Volume')
+    attach_inst_page_form.fill(dict(select_volume=volume_name,
+                                    device_path='/dev/vdb'))
+    sel.click(submit_changes)
+    flash.assert_message_contain('Attaching Cloud Volume')
 
     wait_for(provider.is_refreshed, [None, 10], delay=5)
     info_el = InfoBlock.element('Relationships', 'Cloud Volumes')
-    pytest_selenium.click(info_el)
+    sel.click(info_el)
     params = {'Name': volume_name, 'Status': 'in-use'}
     res = list_tbl.find_rows_by_cells(params)
     assert res, 'Volume does not appear in instance relationships'
 
 
-@pytest.mark.uncollectif(lambda: PROD_VERSION > '4.1')
 def test_detach_volume_from_instance_page(provider):
     """Detaches volume from instance from Instance page"""
     vm_name = None
@@ -110,20 +107,19 @@ def test_detach_volume_from_instance_page(provider):
     assert vm_name, "Can't find any instance with attached volume"
 
     Instance(vm_name, provider).load_details()
-    toolbar.select('Configuration', 'Detach a Cloud Volume from this Instance')
-    v_option = choice(select_volume.all_options[1:])
-    select_volume.select_by_value(v_option[1])
-    pytest_selenium.click(submit_changes)
-    assert_message_contain('Detaching Cloud Volume')
+    tb.select('Configuration', 'Detach a Cloud Volume from this Instance')
+    v_option = choice(detach_inst_page_form.select_volume.all_options[1:])
+    detach_inst_page_form.select_volume.select_by_value(v_option[1])
+    sel.click(submit_changes)
+    flash.assert_message_contain('Detaching Cloud Volume')
 
     wait_for(provider.is_refreshed, [None, 10], delay=5)
     info_el = InfoBlock.element('Relationships', 'Cloud Volumes')
-    pytest_selenium.click(info_el)
+    sel.click(info_el)
     res = list_tbl.find_cell('Name', v_option[0])
     assert not res, 'Volume does not disappear from instance relationships'
 
 
-@pytest.mark.uncollectif(lambda: PROD_VERSION > '4.1')
 def test_attach_volume_from_volume_page(provider):
     """Attaches pre-created volume to an instance from Volume page"""
     vms = filter(lambda vm: vm.power_state == 'ACTIVE', provider.mgmt.all_vms())
@@ -133,13 +129,12 @@ def test_attach_volume_from_volume_page(provider):
               'Cloud Provider': provider.get_yaml_data()['name']}
     list_tbl.click_row_by_cells(params, 'Name')
     vname = get_volume_name()
-    toolbar.select('Configuration', 'Attach this Cloud Volume to an Instance')
-    assert len(select_vm.all_options) > 0
+    tb.select('Configuration', 'Attach this Cloud Volume to an Instance')
+    assert len(attach_vlm_page_form.select_vm.all_options) > 0
 
-    select_vm.select_by_value(select_vm.get_value_by_text(inst_name))
-    pytest_selenium.send_keys(device_input, '/dev/vdb')
-    pytest_selenium.click(attach_volume)
-    assert_message_contain('Attaching Cloud Volume')
+    attach_vlm_page_form.fill(dict(select_vm=inst_name, device_path='/dev/vdb'))
+    sel.click(attach_volume)
+    flash.assert_message_contain('Attaching Cloud Volume')
 
     wait_for(provider.is_refreshed, [None, 10], delay=5)
     navigate_to(Volume, 'All')
@@ -148,7 +143,6 @@ def test_attach_volume_from_volume_page(provider):
     assert res, 'Volume does not marked as in-use'
 
 
-@pytest.mark.uncollectif(lambda: PROD_VERSION > '4.1')
 def test_detach_volume_from_volume_page(provider):
     """Detaches volume from instance from Volume page"""
     navigate_to(Volume, 'All')
@@ -156,11 +150,11 @@ def test_detach_volume_from_volume_page(provider):
               'Cloud Provider': provider.get_yaml_data()['name']}
     list_tbl.click_row_by_cells(params, 'Name')
     vname = get_volume_name()
-    toolbar.select('Configuration', 'Detach this Cloud Volume from an Instance')
-    assert len(select_vm.all_options) > 0
-    select_vm.select_by_index(1)
-    pytest_selenium.click(detach_volume)
-    assert_message_contain('Detaching Cloud Volume')
+    tb.select('Configuration', 'Detach this Cloud Volume from an Instance')
+    assert len(detach_vlm_page_form.select_vm.all_options) > 0
+    detach_vlm_page_form.select_vm.select_by_index(1)
+    sel.click(detach_volume)
+    flash.assert_message_contain('Detaching Cloud Volume')
 
     wait_for(provider.is_refreshed, [None, 10], delay=5)
     navigate_to(Volume, 'All')
@@ -169,7 +163,6 @@ def test_detach_volume_from_volume_page(provider):
     assert res, 'Volume does not marked as available'
 
 
-@pytest.mark.uncollectif(lambda: PROD_VERSION > '4.1')
 def test_delete_volume(provider):
     """Deletes volume"""
     navigate_to(Volume, 'All')
@@ -177,10 +170,9 @@ def test_delete_volume(provider):
               'Cloud Provider': provider.get_yaml_data()['name']}
     list_tbl.click_row_by_cells(params, 'Name')
     vname = get_volume_name()
-    toolbar.select('Configuration', 'Delete this Cloud Volume',
-                   invokes_alert=True)
-    pytest_selenium.handle_alert(cancel=False)
-    assert_message_contain('Delete initiated for 1 Cloud Volume.')
+    tb.select('Configuration', 'Delete this Cloud Volume', invokes_alert=True)
+    sel.handle_alert(cancel=False)
+    flash.assert_message_contain('Delete initiated for 1 Cloud Volume.')
 
     wait_for(provider.is_refreshed, [None, 10], delay=5)
     navigate_to(Volume, 'All')
