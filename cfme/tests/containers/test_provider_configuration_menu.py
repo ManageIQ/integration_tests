9810309ec505498e6178023b4abcabe4da6f17e2
gshefer <gshefer@redhat.com>
2016-12-22 17:27:55 +0200

    Fixes#1

--- cfme/tests/containers/test_provider_configuration_menu.py
+++ cfme/tests/containers/test_provider_configuration_menu.py
@@ -52,8 +52,8 @@ def test_remove_selected_containers_provider():
         success message that the provider has been deleted from VMDB.'''
     name = select_first_provider_and_get_its_name()
     ContainersProvider(name).delete(cancel=False)
-    sel.refresh()
-    assert wait_for(lambda *args: not Quadicon(name).exists,
+    assert wait_for(lambda: not Quadicon(name).exists,
                     'Waiting for containers provider to be removed.',
+                    fail_func=sel.refresh,
                     delay=5,
                     num_sec=60.0), 'Failed to remove containers provider'
