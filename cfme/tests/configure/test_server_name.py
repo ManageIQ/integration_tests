07425ee3aff2fbf165137e91f52ea07c4b5324af
izapolsk <izapolsk@redhat.com>
2017-01-12 19:06:52 +0200

    more test fixes

--- cfme/tests/configure/test_server_name.py
+++ cfme/tests/configure/test_server_name.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+from cfme.configure.about import get_detail
 from cfme.configure.configuration import BasicInformation
 from cfme.fixtures import pytest_selenium as sel
 from fixtures.pytest_store import store
-from cfme.web_ui import flash, InfoBlock
+from cfme.web_ui import flash
 from utils import clear_property_cache
 from utils.appliance import current_appliance
 from utils.appliance.implementations.ui import navigate_to
@@ -25,8 +26,15 @@ def test_server_name():
     # CFME updates about box only after any navigation BZ(1408681)
     navigate_to(current_appliance.server, 'Dashboard')
 
-    navigate_to(current_appliance.server, 'About')
-    assert new_server_name == InfoBlock('Session Information', 'Server Name').text,\
+    # if version.current_version() < '5.7':
+    #     current_server_name = InfoBlock('Session Information', 'Server Name').text
+    #     navigate_to(current_appliance.server, 'About')
+    # else:
+    current_server_name = get_detail('Server Name')
+    close_button = sel.element('//div[contains(@class, "about-modal-pf")]//button[@class="close"]')
+    close_button.click()
+
+    assert new_server_name == current_server_name, \
         "Server name in About section does not match the new name"
 
     clear_property_cache(store.current_appliance, 'configuration_details')
