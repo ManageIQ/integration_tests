910f38c6aafcabc0659cf4a3ba9b2aba7626ee57
Luke Couzens <lcouzens@redhat.com>
2017-01-13 12:03:43 +0000

    changes to tests to request provisions from sprout

--- utils/tests/test_appliance_cli.py
+++ utils/tests/test_appliance_cli.py
@@ -1,18 +1,35 @@
 from fixtures.pytest_store import store
-from utils import version
-from utils.appliance import provision_appliance
+from utils.version import get_stream
+from utils.sprout import SproutClient
+from utils.appliance import current_appliance
 from utils.conf import cfme_data, credentials
 import pytest
 
 
 @pytest.yield_fixture(scope="module")
-def provisioned_appliance():
-    ver_to_prov = str(version.current_version())
-    app = provision_appliance(version=ver_to_prov)
+def sprout_provisioned_appliance():
+    sp = SproutClient.from_config()
+    version = current_appliance.version.vstring
+    stream = get_stream(current_appliance.version)
+    apps, pool_id = sp.provision_appliances(
+        count=1, preconfigured=False, version=version, stream=stream)
 
-    yield app
+    yield apps[0]
 
-    app.destroy()
+    sp.destroy_pool(pool_id)
+
+
+@pytest.yield_fixture(scope="module")
+def sprout_provisioned_appliance2():
+    sp = SproutClient.from_config()
+    version = current_appliance.version.vstring
+    stream = get_stream(current_appliance.version)
+    apps, pool_id = sp.provision_appliances(
+        count=1, preconfigured=True, version=version, stream=stream, provider='rhvqe40')
+
+    yield apps[0]
+
+    sp.destroy_pool(pool_id)
 
 
 @pytest.fixture()
@@ -47,27 +64,29 @@ def test_set_hostname(request):
     assert return_code == 0
 
 
-def test_configure_appliance_internal_fetch_key(request, app_creds, provisioned_appliance):
-    app = provisioned_appliance
+def test_configure_appliance_internal_fetch_key(request, app_creds, sprout_provisioned_appliance):
+    app = sprout_provisioned_appliance
     fetch_key_ip = store.current_appliance.address
-    app.ipapp.ap_cli.configure_appliance_internal_fetch_key(0, 'localhost',
+    app.ap_cli.configure_appliance_internal_fetch_key(0, 'localhost',
         app_creds['username'], app_creds['password'], 'vmdb_production', fetch_key_ip,
         app_creds['sshlogin'], app_creds['sshpass'])
-    app.ipapp.wait_for_evm_service()
-    app.ipapp.wait_for_web_ui()
+    app.wait_for_evm_service()
+    app.wait_for_web_ui()
 
 
-# def test_configure_ipa(request, ipa_creds):
-#     store.current_appliance.ap_cli.configure_ipa(ipa_creds['ipaserver'], ipa_creds['username'],
-#         ipa_creds['password'], ipa_creds['domain'], ipa_creds['realm'])
-#     assert store.current_appliance.ssh_client.run_command("systemctl status sssd | grep running")
-#     return_code, output = store.current_appliance.ssh_client.run_command(
-#         "cat /etc/ipa/default.conf | grep 'enable_ra = True'")
-#     assert return_code == 0   # TODO extend test to login as ipa user
-#
-#
-# def test_uninstall_ipa(request):
-#     store.current_appliance.ap_cli.uninstall_ipa_client()
-#     return_code, output = store.current_appliance.ssh_client.run_command(
-#         "cat /etc/ipa/default.conf")
-#     assert return_code != 0
+def test_configure_ipa(request, ipa_creds, sprout_provisioned_appliance2):
+    app = sprout_provisioned_appliance2
+    app.ap_cli.configure_ipa(ipa_creds['ipaserver'], ipa_creds['username'],
+        ipa_creds['password'], ipa_creds['domain'], ipa_creds['realm'])
+    assert app.ssh_client.run_command("systemctl status sssd | grep running")
+    return_code, output = app.ssh_client.run_command(
+        "cat /etc/ipa/default.conf | grep 'enable_ra = True'")
+    assert return_code == 0   # TODO extend test to login as ipa user
+
+
+def test_uninstall_ipa(request, test_configure_ipa):
+    app = sprout_provisioned_appliance2
+    app.ap_cli.uninstall_ipa_client()
+    return_code, output = app.ssh_client.run_command(
+        "cat /etc/ipa/default.conf")
+    assert return_code != 0
