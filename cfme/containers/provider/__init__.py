9a7b003f5e53038ca78bdd52a1fd651fd446fcc9
jdemon519 <tonysh519@gmail.com>
2017-01-16 13:44:11 +0200

    [RFR]Added test_reload_button_provider.py to test functionality of refresh button (#3921)
    * Added cfme/tests/containers/test_reload_button_provider.py to test the refresh button
    * Added new functionality and comments to cfme/tests/containers/test_reload_button_provider.py
    * Fixed imports/asserts and added template validation
    * Merged 2 single element lists into one 2 element list

--- cfme/containers/provider/__init__.py
+++ cfme/containers/provider/__init__.py
@@ -71,9 +71,9 @@ class ContainersProvider(BaseProvider, Pretty):
         'num_replication_controller',
         'num_pod',
         'num_node',
-        'num_container',
-        'num_image']
-    # TODO add 'num_image_registry' and 'num_volume'
+        'num_image_registry',
+        'num_container']
+    # TODO add 'num_volume'
     string_name = "Containers"
     page_name = "containers"
     detail_page_suffix = 'provider_detail'
