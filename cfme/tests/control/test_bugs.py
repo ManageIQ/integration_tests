cfe2958b9b920249ca665bcf17770e51e7376328
quarckster <dmisharo@redhat.com>
2017-01-16 08:34:46 +0100

    Changed pytest.mark
    Signed-off-by: quarckster <dmisharo@redhat.com>

--- cfme/tests/control/test_bugs.py
+++ cfme/tests/control/test_bugs.py
@@ -17,6 +17,7 @@ from cfme import test_requirements
 from utils.generators import random_vm_name
 from widgetastic.widget import Text
 from utils.appliance import get_or_create_current_appliance
+from utils.blockers import BZ
 
 
 pytestmark = [
@@ -219,7 +220,7 @@ def test_check_compliance_history(request, vmware_provider, vmware_vm):
         vmware_vm.name)
 
 
-@pytest.mark.meta(blockers=[1395965], automates=[1395965])
+@pytest.mark.meta(blockers=[BZ(1395965, forced_streams=["5.6", "5.7"])])
 def test_delete_all_actions_from_compliance_policy(request):
     """We should not allow a compliance policy to be saved
     if there are no actions on the compliance event.
