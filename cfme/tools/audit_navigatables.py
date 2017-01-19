613badb09fd6f74fef40c5d671ce6d6d7b771d9b
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-01-19 16:31:44 +0100

    simplify navigatable type audit data

--- cfme/tools/audit_navigatables.py
+++ cfme/tools/audit_navigatables.py
@@ -3,25 +3,33 @@ import utils.appliance
 import utils.path
 
 
+def relevant_types(module):
+    return sorted(
+        cls for cls in vars(module).values()
+        if isinstance(cls, type) and
+        issubclass(cls, utils.appliance.Navigatable) and
+        cls is not utils.appliance.Navigatable)
+
+
 def main():
 
     for path in utils.path.project_path.join('cfme').visit('**.py'):
         module = path.pyimport()
 
-        for clas in vars(module).values():
-            if isinstance(clas, type):
-                if issubclass(clas, utils.appliance.Navigatable):
-                    print(clas)
-                    methods = [
-                        "{} ({})".format(func[0], func[1].im_func.__module__)
-                        for func in inspect.getmembers(clas, inspect.ismethod)
-                    ]
-                    properties = [
-                        prop[0] for prop in inspect.getmembers(
-                            clas, lambda o: isinstance(o, property))]
-                    if methods:
-                        print("\tMethods:")
-                        print("\t\t{}".format("\n\t\t".join(methods)))
-                    if properties:
-                        print("\tProperties")
-                        print("\t\t{}".format("\n\t\t".join(properties)))
+        for clas in relevant_types(module):
+            methods = []
+            properties = []
+            for name, obj in sorted(vars(clas).items()):
+                if inspect.ismethod(obj):
+                    methods.append(
+                        "{} ({})".format(name, obj.__module__))
+                elif isinstance(obj, property):
+                    properties.append(name)
+            if methods or properties:
+                print(clas)
+            if methods:
+                print("\tMethods:")
+                print("\t\t{}".format("\n\t\t".join(methods)))
+            if properties:
+                print("\tProperties")
+                print("\t\t{}".format("\n\t\t".join(properties)))
