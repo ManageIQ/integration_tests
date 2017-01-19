b05b746b0f3344348839e5f7e275706107fec4de
Pavol Kotvan <patchkez@gmail.com>
2017-01-19 12:27:07 +0100

    adding support for 'curl' - easy install 7

--- integration_tests_install/install.sh
+++ integration_tests_install/install.sh
@@ -3,34 +3,33 @@
 # URL shortener
 # https://github.com/blog/985-git-io-github-url-shortener
 
-
-print (){
-  echo "-----------------------"
-  echo "$1"
-  echo "-----------------------"
-}
-
 if [ -z ${PROJECTS} ];then
-  print "PROJECTS variable not set. This variable sets path to where integration_tests will be installed. Export this variable and re-run this script again."
+  echo -e "PROJECTS variable not set.\n
+This variable sets path to where integration_tests will be installed. Export this variable and re-run this script again."
+
 else
 
   export CFME_TESTS="${PROJECTS}/integration_tests_files"
   mkdir -p ${CFME_TESTS}; cd ${CFME_TESTS}
 
-  print "Cloning integration_tests repo..."
+  echo -e "Cloning integration_tests repo...\n
+-------------------------"
   git clone git://github.com/ManageIQ/integration_tests.git
 
-  print "Fething unmerged PR...[TESTING]"
+  echo -e "Fething unmerged PR...[TESTING]\n
+-------------------------"
   cd integration_tests; git fetch origin pull/3254/head:integration_tests_container
 
-  print "Switching to branch from PR...[TESTING]"
+  echo -e "Switching to branch from PR...[TESTING]\n
+-------------------------"
   git checkout integration_tests_container
 
-  print "Creating symbolic link for wrapper script"
+  echo -e "Creating symbolic link for wrapper script\n
+-------------------------"
   cd ..; ln -s integration_tests/integration_tests_install/integration_tests_init.sh .
 
-  print "integration_tests have been successfully cloned.
-If you have any custom YAML files you want to use, create new directory in ${CFME_TESTS} andcopy them here, e.g.: 
+  echo  -e "integration_tests have been successfully cloned.\n
+If you have any custom YAML files you want to use, create new directory in ${CFME_TESTS} andcopy them here, e.g.:
 
   mkdir ${CFME_TESTS}/my_custom_yaml_files
   cp -r /<path to custom yamls>/*.*y*ml ${CFME_TESTS}/my_custom_yaml_files
