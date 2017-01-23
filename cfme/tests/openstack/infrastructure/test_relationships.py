ad6cbc6d7f0ebd5ffed4ecfa5137acf0334c53d5
Oleksandr Kolisnyk <okolisny@redhat.com>
2017-01-23 20:34:22 +0200

    fixes for undercloud relationships tests

--- cfme/tests/openstack/infrastructure/test_relationships.py
+++ cfme/tests/openstack/infrastructure/test_relationships.py
@@ -19,39 +19,34 @@ pytest_generate_tests = testgen.generate(testgen.provider_by_type,
 @pytest.mark.uncollectif(lambda: version.current_version() < '5.7')
 def test_assigned_roles(provider):
     provider.load_details()
-    result = provider.summary.relationships.clusters.value
-
-    assert result > 0
+    assert provider.get_detail('Relationships', 'Deployment roles') > 0
 
 
 def test_nodes(provider):
     provider.load_details()
-    result = provider.summary.relationships.nodes.value
     """
     todo get the list of VM's from external resource and compare
     it with result - currently not 0
     """
 
-    assert result > 0
+    assert provider.get_detail('Relationships', 'Nodes') > 0
 
 
 def test_templates(provider):
     provider.load_details()
-    result = provider.summary.relationships.templates.value
     """
     todo get the list of images/templates from external resource and compare
     it with result - currently  bigger than 0
     """
 
-    assert result > 0
+    assert provider.get_detail('Relationships', 'Templates') > 0
 
 
 def test_stacks(provider):
     provider.load_details()
-    result = provider.summary.relationships.orchestration_stacks.value
     """
     todo get the list of tenants from external resource and compare
     it with result - currently not 0
     """
 
-    assert result > 0
+    assert provider.get_detail('Relationships', 'Orchestration stacks') > 0
