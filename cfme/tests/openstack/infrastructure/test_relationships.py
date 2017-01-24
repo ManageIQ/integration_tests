80b2f0887de557b08d1d8d08bbbd9407593f50ac
Oleksandr Kolisnyk <okolisny@redhat.com>
2017-01-24 20:31:31 +0200

    fixed tests for relationships table

--- cfme/tests/openstack/infrastructure/test_relationships.py
+++ cfme/tests/openstack/infrastructure/test_relationships.py
@@ -4,52 +4,48 @@ undercloud infra provider, need to compare the cloud providers with the
 results of the relationships
 """
 import pytest
-from utils import testgen
+from utils import testgen, version
 from cfme.infrastructure.provider.openstack_infra import OpenstackInfraProvider
 
 
-pytestmark = [pytest.mark.meta(server_roles='+smartproxy +smartstate')]
+pytestmark = [pytest.mark.meta(server_roles='+smartproxy +smartstate'),
+              pytest.mark.usefixtures("setup_provider_modscope")]
 
 
 pytest_generate_tests = testgen.generate([OpenstackInfraProvider], scope='module')
 
 
-@pytest.mark.usefixtures("setup_provider_modscope")
-def test_assinged_roles(provider):
+@pytest.mark.uncollectif(lambda: version.current_version() < '5.7')
+def test_assigned_roles(provider):
     provider.load_details()
-    result = provider.summary.relationships.deployment_roles.value
-
-    assert result > 0
+    assert int(provider.get_detail('Relationships', 'Deployment Roles')) > 0
 
 
 def test_nodes(provider):
     provider.load_details()
-    result = provider.summary.relationships.nodes.value
     """
     todo get the list of VM's from external resource and compare
     it with result - currently not 0
     """
 
-    assert result > 0
+    assert int(provider.get_detail('Relationships', 'Nodes')) > 0
 
 
 def test_templates(provider):
     provider.load_details()
-    result = provider.summary.relationships.templates.value
     """
     todo get the list of images/templates from external resource and compare
     it with result - currently  bigger than 0
     """
 
-    assert result > 0
+    assert int(provider.get_detail('Relationships', 'Templates')) > 0
 
 
 def test_stacks(provider):
     provider.load_details()
-    result = provider.summary.relationships.orchestration_stacks.value
     """
     todo get the list of tenants from external resource and compare
     it with result - currently not 0
     """
 
-    assert result > 0
+    assert int(provider.get_detail('Relationships', 'Orchestration stacks')) > 0
