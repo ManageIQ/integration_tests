cb37984e45d2c121ad17810a6ab30ac1dfe29ce9
Pavel <zagalsky@gmail.com>
2017-01-29 16:09:19 +0200

    and more

--- cfme/tests/containers/test_containers_templates.py
+++ cfme/tests/containers/test_containers_templates.py
@@ -13,8 +13,8 @@ pytestmark = [
         lambda: current_version() > 5.6),
     pytest.mark.usefixtures('setup_provider'),
     pytest.mark.tier(3)]
-pytest_generate_tests = testgen.generate(
-    testgen.containers_providers, scope='function')
+pytest_generate_tests = testgen.generate([Template], scope='module')
+
 
 list_tbl = CheckboxTable(table_locator="//div[@id='list_grid']//table")
 
