e93e9139871dca2f5103ec0ab2a0fb42d2544a4d
Oleksandr Kolisnyk <okolisny@redhat.com>
2017-02-02 20:23:23 +0200

    small fixes

--- cfme/tests/openstack/infrastructure/test_resources.py
+++ cfme/tests/openstack/infrastructure/test_resources.py
@@ -5,14 +5,14 @@ from cfme.infrastructure.provider.openstack_infra import OpenstackInfraProvider
 from utils import testgen
 
 
-pytestmark = [pytest.mark.meta(server_roles='+smartproxy +smartstate')]
+pytestmark = [pytest.mark.meta(server_roles='+smartproxy +smartstate'),
+              pytest.mark.usefixtures("setup_provider_modscope")]
 
 
 pytest_generate_tests = testgen.generate([OpenstackInfraProvider],
                                          scope='module')
 
 
-@pytest.mark.usefixtures("setup_provider_modscope")
 def test_number_of_cpu(provider, soft_assert):
     navigate_to(provider, 'Details')
     v = provider.get_detail('Properties', 'Aggregate Node CPU Resources')
@@ -20,7 +20,7 @@ def test_number_of_cpu(provider, soft_assert):
     v = provider.get_detail('Properties', 'Aggregate Node CPUs')
     soft_assert(int(v) > 0, "Aggregate Node CPUs is 0")
     v = provider.get_detail('Properties', 'Aggregate Node CPU Cores')
-    soft_assert(int(v) > 0, "Aggregate Node CPU Cores is 0")
+    assert int(v) > 0, "Aggregate Node CPU Cores is 0"
 
 
 def test_node_memory(provider):
