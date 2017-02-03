09cafc0ab2fa8c802f0fd46ca81d013f146e3a79
okolisny <okolisny@redhat.com>
2017-02-03 21:36:08 +0200

    [1LP][RFR] Fix openstack infrastructure tests (#4065)
    * fixed tests for quad icons
    * fixed tests for relationships table
    * gathered tests in common file
    * deleted extra tests
    * added workaround for Openstack providers
    * code-style fixes
    * brought testgen to latest version
    * moved api port test to separate directory
    * added navigation page for Deployment Roles
    * replaced Cluster nav class with Deployment Roles one
    * deleted deprecated .load_details() call
    * changed test name
    * resetting navigations for Deployment Roles
    * different small fixes
    * small fixes
    * replaced soft_assert with assert
    * fixed asserts for tests: hostname, availability zones
    * changed prerequisite for AllForProvider navigation
    * fixed typo in nav address

--- cfme/tests/openstack/infrastructure/test_credentials_quad.py
+++ cfme/tests/openstack/infrastructure/test_credentials_quad.py
@@ -11,7 +11,6 @@ pytest_generate_tests = testgen.generate([OpenstackInfraProvider], scope='module
 
 @pytest.mark.usefixtures("setup_provider_modscope")
 def test_credentials_quads(provider):
-    provider.load_details()
     navigate_to(InfraProvider, 'All')
     quad = Quadicon(provider.name, qtype='infra_prov')
     checked = str(quad.creds).split('-')[0]
