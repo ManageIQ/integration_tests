e3501807aef480b90f8adaaad4d48f329ce9f47f
Pete Savage <psavage@redhat.com>
2017-02-03 11:42:06 +0000

    Cached Template Loader
    * Introduces a new --use-template-cache option
    * Fails over gracefully if there is no cache and pulls from source

--- fixtures/templateloader.py
+++ fixtures/templateloader.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Preloads all templates on all providers that were selected for testing. Useful for test collect.
 """
+import pytest
 from fixtures.pytest_store import store, write_line
 from utils import trackerbot
 from utils.providers import list_provider_keys
@@ -8,11 +9,46 @@ from utils.providers import list_provider_keys
 TEMPLATES = {}
 
 
-def pytest_configure():
+class NoTemplates(object):
+    pass
+
+
+@pytest.mark.tryfirst
+def pytest_addoption(parser):
+    # Create the cfme option group for use in other plugins
+    parser.addoption("--use-template-cache", dest="use_template_cache", action="store_true",
+        default=False, help="Use a cached version of the templates and not redownload them")
+
+
+def pytest_configure(config):
     if store.parallelizer_role == 'master' or trackerbot.conf.get('url') is None:
         return
 
-    write_line("Loading templates from trackerbot")
-    provider_templates = trackerbot.provider_templates(trackerbot.api())
-    for provider in list_provider_keys():
-        TEMPLATES[provider] = provider_templates.get(provider, [])
+    # A further optimization here is to make the calls to trackerbot per provider
+    # and perhaps only pull the providers that are needed, however that will need
+    # to ensure that the tests that just randomly use providers adhere to the filters
+    # which may be too tricky right now.
+
+    count = 0
+
+    if not config.getoption('use_template_cache'):
+        write_line("Loading templates from trackerbot...")
+        provider_templates = trackerbot.provider_templates(trackerbot.api())
+        for provider in list_provider_keys():
+            TEMPLATES[provider] = provider_templates.get(provider, [])
+            config.cache.set(provider, TEMPLATES[provider])
+            count += len(TEMPLATES[provider])
+    else:
+        write_line("Using templates from cache...")
+        provider_templates = None
+        for provider in list_provider_keys():
+            templates = config.cache.get(provider, NoTemplates())
+            if isinstance(templates, NoTemplates):
+                write_line("Loading templates for {} from source as not in cache".format(provider))
+                if not provider_templates:
+                    provider_templates = trackerbot.provider_templates(trackerbot.api())
+                templates = provider_templates.get(provider, [])
+                config.cache.set(provider, templates)
+            count += len(templates)
+            TEMPLATES[provider] = templates
+    write_line("  Loaded {} templates successfully!".format(count))
