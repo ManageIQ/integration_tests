023161459d3c263ae4da8996e1d0efabad60c3be
Jan Kročil <jkrocil@redhat.com>
2017-02-03 12:08:57 +0100

    Actually we want the fixture, not the separate call

--- cfme/tests/test_db_migrate.py
+++ cfme/tests/test_db_migrate.py
@@ -20,14 +20,21 @@ def pytest_generate_tests(metafunc):
     return metafunc.parametrize(argnames=argnames, argvalues=argvalues, ids=idlist)
 
 
+@pytest.fixture(scope="module")
+def extend_db_partition():
+    app = get_or_create_current_appliance()
+    app.stop_evm_service()
+    app.extend_db_partition()
+    app.start_evm_service()
+
+
 @pytest.yield_fixture(scope="module")
-def backup_orig_state():
+def backup_orig_state(extend_db_partition):
     app = get_or_create_current_appliance()
     app.backup_database("/var/www/miq/orig_db.backup")
     app.ssh_client.run_command("cp /var/www/miq/vmdb/GUID{,.bak}")
     app.ssh_client.run_command("cp /var/www/miq/vmdb/REGION{,.bak}")
     app.ssh_client.run_command("cp /var/www/miq/vmdb/certs/v2_key{,.bak}")
-    app.extend_db_partition()
     yield
     app.stop_evm_service()
     app.drop_database()
