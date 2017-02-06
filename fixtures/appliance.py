de9d3e0b3215ad6b0b1341ed88f97380c6a76d93
Jan Kročil <jkrocil@redhat.com>
2017-02-06 13:14:23 +0100

    Add 2 hour default lease time to sprout client Add 3 hour default lease time to appliance fixtures

--- fixtures/appliance.py
+++ fixtures/appliance.py
@@ -1,3 +1,15 @@
+""" This module contains fixtures to use when you need a temporary appliance for testing.
+
+In cases where you cannot run a certain test againts the primary appliance because of the test's
+destructive potential (which could render all subsequent testing useless), you want to use
+a temporary appliance parallel to the primary one.
+
+For tests such as setting up an appliance (or multiple) in a certain setup, you will want to use
+the :py:func:`temp_appliance_unconfig` fixture and then configure the appliance(s) yourself.
+
+For tests where all you need is a single preconfigured appliance to run a database restore on, for
+example, you will want to use the :py:func:`temp_appliance_preconfig` fixture.
+"""
 from contextlib import contextmanager
 
 import pytest
@@ -6,9 +18,16 @@ from cfme.test_framework.sprout.client import SproutClient
 
 
 @contextmanager
-def _temp_appliance(preconfigured=True):
+def _temp_appliance(preconfigured=True, lease_time=180):
+    """ Provisions a temporary appliance for testing
+
+    Args:
+        preconfigured: True if the appliance should be already configured, False otherwise
+        lease_time: Lease time in minutes (3 hours by default)
+    """
     sprout_client = SproutClient.from_config()
-    apps, request_id = sprout_client.provision_appliances(preconfigured=preconfigured)
+    apps, request_id = sprout_client.provision_appliances(
+        lease_time=lease_time, preconfigured=preconfigured)
     app = apps[0]
     app.stop_evm_service()
     app.extend_db_partition()
