f861da2c477383d87b6e6d621939d28caa974bd2
Oleksandr Kolisnyk <okolisny@redhat.com>
2017-02-07 17:28:46 +0200

    minor changes

--- cfme/tests/openstack/infrastructure/test_credentials_quad.py
+++ cfme/tests/openstack/infrastructure/test_credentials_quad.py
@@ -1,17 +1,18 @@
 import pytest
-from cfme.infrastructure.provider import InfraProvider
+
 from cfme.infrastructure.provider.openstack_infra import OpenstackInfraProvider
 from cfme.web_ui import Quadicon
 from utils import testgen
 from utils.appliance.implementations.ui import navigate_to
 
 
-pytest_generate_tests = testgen.generate([OpenstackInfraProvider], scope='module')
+pytest_generate_tests = testgen.generate([OpenstackInfraProvider],
+                                         scope='module')
 
 
 @pytest.mark.usefixtures("setup_provider_modscope")
 def test_credentials_quads(provider):
-    navigate_to(InfraProvider, 'All')
+    navigate_to(provider, 'All')
     quad = Quadicon(provider.name, qtype='infra_prov')
     checked = str(quad.creds).split('-')[0]
     assert checked == 'checkmark'
