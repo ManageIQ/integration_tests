4ff30a4c9bc4194684daddc88dd8704c61995320
gshefer <gshefer@redhat.com>
2017-02-09 12:19:08 +0200

    More test + Fixes (#4124)
    * More tests + Fixes
    
    * Default services test fix

--- cfme/tests/containers/test_relationships_tables.py
+++ cfme/tests/containers/test_relationships_tables.py
@@ -1,18 +1,18 @@
+from random import sample, shuffle
 import pytest
+
 from utils.version import current_version
 from utils import testgen
 from utils.appliance.implementations.ui import navigate_to
-from cfme.containers.pod import Pod, match_page as pod_match_page
+from cfme.containers.pod import Pod
 from cfme.containers.provider import ContainersProvider
-from cfme.containers.service import Service, match_page as service_match_page
-from cfme.containers.node import Node, match_page as node_match_page
-from cfme.containers.replicator import Replicator, match_page as replicator_match_page
-from cfme.containers.image import Image, match_page as image_match_page
-from cfme.containers.project import Project, match_page as project_match_page
-from cfme.fixtures import pytest_selenium as sel
-from cfme.web_ui import CheckboxTable, toolbar as tb
-from random import sample
-from collections import namedtuple
+from cfme.containers.service import Service
+from cfme.containers.node import Node
+from cfme.containers.replicator import Replicator
+from cfme.containers.image import Image
+from cfme.containers.project import Project
+from cfme.containers.template import Template
+from cfme.web_ui import CheckboxTable, toolbar as tb, paginator, summary_title
 
 
 pytestmark = [
@@ -23,44 +23,42 @@ pytestmark = [
 pytest_generate_tests = testgen.generate([ContainersProvider], scope='function')
 
 
-DataSet = namedtuple('DataSet', ['object', 'match_page'])
-
-TEST_OBJECTS = [DataSet(Pod, pod_match_page),
-                DataSet(Service, service_match_page),
-                DataSet(Node, node_match_page),
-                DataSet(Replicator, replicator_match_page),
-                DataSet(Image, image_match_page),
-                DataSet(Project, project_match_page)]
-rel_values = (0, 'Unknown image source', 'registry.access.redhat.com')
-
-# CMP-9930 CMP-9892 CMP-9965 CMP-9983 CMP-9869
+TEST_OBJECTS = [Pod, Service, Node, Replicator, Image, Project, Template]
 
 
-@pytest.mark.parametrize('cls', TEST_OBJECTS)
+# CMP-9930 CMP-9892 CMP-9965 CMP-9983 CMP-9869 CMP-10321
+
+@pytest.mark.parametrize('cls', sample(TEST_OBJECTS, 4))
 def test_relationships_tables(provider, cls):
-    """This module verifies the integrity of the Relationships table.
+    """This test verifies the integrity of the Relationships table.
     clicking on each field in the Relationships table takes the user
     to either Summary page where we verify that the field that appears
     in the Relationships table also appears in the Properties table,
     or to the page where the number of rows is equal to the number
     that is displayed in the Relationships table.
     """
-    navigate_to(cls.object, 'All')
+    if current_version() < "5.7" and cls == Template:
+        pytest.skip('Templates are not exist in CFME version smaller than 5.7. skipping...')
+    navigate_to(cls, 'All')
     tb.select('List View')
     list_tbl = CheckboxTable(table_locator="//div[@id='list_grid']//table")
-    cls_instances = [r.name.text for r in list_tbl.rows()]
-    cls_instances = sample(cls_instances, min(2, len(cls_instances)))
-    for name in cls_instances:
-        obj = cls.object(name, provider)
+    names = [r.name.text for r in list_tbl.rows()]
+    names = sample(names, min(2, len(names)))
+    for name in names:
+        obj = cls(name, provider)
         obj.summary.reload()
-        keys = sample(obj.summary.relationships.keys,
-                      min(1, len(obj.summary.relationships.keys)))
-        for key in keys:
-            # reload summary to prevent StaleElementReferenceException:
-            obj.summary.reload()
-            element = getattr(obj.summary.relationships, key)
-            if element.value in rel_values:
-                continue
-            sel.click(element)
-            # TODO: find a better indication that we are in the right page:
-            sel.is_displayed_text('{} (Summary)'.format(element.text_value))
+        sum_values = obj.summary.relationships.items().values()
+        shuffle(sum_values)
+        for attr in sum_values:
+            if attr.clickable:
+                break
+        link_value = attr.value
+        attr.click()
+        if type(link_value) is int:
+            rec_total = paginator.rec_total()
+            if rec_total != link_value:
+                pytest.fail('Difference between the value({}) in the relationships table in {}'
+                            'to number of records ({}) in the target'
+                            'page'.format(link_value, obj.name, rec_total))
+        else:
+            assert '(Summary)' in summary_title()
