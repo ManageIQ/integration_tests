78d9a529c30609515abe18de63858ede60fd096f
Michael Shriver <mshriver@redhat.com>
2017-02-10 14:54:26 -0500

    [1LP][RFR] Update cloud_infra_common.test_retirement, ProviderFilter (#4159)
    * Update cloud_infra_common.test_retirement, ProviderFilter
    
    * Update common.vm.retire, remove old version picks
    
    * Increase retirement wait time in test_retirement
    
    * Update common.vm.retire, version picked flash message
    
    * Update common.vm, locator in VM.retire_form

--- cfme/tests/cloud_infra_common/test_retirement.py
+++ cfme/tests/cloud_infra_common/test_retirement.py
@@ -7,88 +7,80 @@ from cfme.common.provider import CloudInfraProvider
 from cfme.common.vm import VM
 from utils import testgen
 from utils.generators import random_vm_name
-from utils.providers import setup_a_provider_by_class
+from utils.log import logger
+from utils.providers import ProviderFilter
 from utils.timeutil import parsetime
 from utils.wait import wait_for
-from utils.version import current_version
+
+
+pytest_generate_tests = testgen.generate(
+    gen_func=testgen.providers,
+    filters=[ProviderFilter(classes=[CloudInfraProvider], required_flags=['provision', 'retire'])],
+    scope='module')
+
 
 pytestmark = [
-    pytest.mark.usefixtures('uses_infra_providers', 'uses_cloud_providers'),
-    pytest.mark.tier(2)
+    pytest.mark.usefixtures('setup_provider_modscope'),
+    pytest.mark.tier(2),
+    pytest.mark.long_running
 ]
 
 
-def pytest_generate_tests(metafunc):
-    argnames, argvalues, idlist = testgen.all_providers(metafunc)
-    testgen.parametrize(metafunc, argnames, argvalues, ids=idlist, scope="module")
-
-
-@pytest.fixture(scope="function")
-def vm(request, provider, setup_provider, small_template):
+@pytest.yield_fixture(scope="function")
+def vm(small_template, provider):
     vm_obj = VM.factory(random_vm_name('retire'), provider, template_name=small_template)
+    vm_obj.create_on_provider(find_in_cfme=True, allow_skip="default")
+    yield vm_obj
 
-    @request.addfinalizer
-    def _delete_vm():
+    try:
         if provider.mgmt.does_vm_exist(vm_obj.name):
             provider.mgmt.delete_vm(vm_obj.name)
-    vm_obj.create_on_provider(find_in_cfme=True, allow_skip="default")
-    return vm_obj
+    except Exception:
+        logger.warning('Failed to delete vm from provider: {}'.format(vm_obj.name))
 
 
-@pytest.fixture(scope="function")
-def test_provider(request):
-    return setup_a_provider_by_class(CloudInfraProvider)
-
-
-@pytest.fixture(scope="function")
-def existing_vm(request, test_provider):
+@pytest.yield_fixture(scope="function")
+def existing_vm(provider):
     """ Fixture will be using for set\unset retirement date for existing vm instead of
     creation a new one
     """
-    all_vms = test_provider.mgmt.list_vm()
+    all_vms = provider.mgmt.list_vm()
     need_to_create_vm = True
     for virtual_machine in all_vms:
-        if test_provider.mgmt.is_vm_running(virtual_machine):
-            need_vm = VM.factory(virtual_machine, test_provider)
+        if provider.mgmt.is_vm_running(virtual_machine):
+            vm_obj = VM.factory(virtual_machine, provider)
             need_to_create_vm = False
             break
     if need_to_create_vm:
         machine_name = random_vm_name('retire')
-        need_vm = VM.factory(machine_name, test_provider)
-        need_vm.create_on_provider(find_in_cfme=True, allow_skip="default")
+        vm_obj = VM.factory(machine_name, provider)
+        vm_obj.create_on_provider(find_in_cfme=True, allow_skip="default")
 
-    @request.addfinalizer
-    def _delete_vm():
-        if need_to_create_vm:
-            test_provider.mgmt.delete_vm(need_vm.name)
-    return need_vm
+    yield vm_obj
+
+    try:
+        if need_to_create_vm and provider.mgmt.does_vm_exist(vm_obj.name):
+            provider.mgmt.delete_vm(vm_obj.name)
+    except Exception:
+        logger.warning('Failed to delete vm from provider: {}'.format(vm_obj.name))
 
 
 def verify_retirement(vm):
-    # add condition because of differ behaviour between 5.5 and 5.6
-    if current_version() < "5.6":
-        wait_for(lambda: vm.exists is False, delay=30, num_sec=360,
-                 message="Wait for VM {} removed from provider".format(vm.name))
-    else:
-        # wait for the info block showing a date as retired date
-        wait_for(lambda: vm.is_retired, delay=30, num_sec=720,
-                 message="Wait until VM {} will be retired".format(vm.name))
+    # wait for the info block showing a date as retired date
+    wait_for(lambda: vm.is_retired, delay=15, num_sec=10 * 60,
+             message="Wait until VM {} will be retired".format(vm.name))
 
-        assert vm.summary.power_management.power_state.text_value in {'off', 'suspended', 'unknown'}
+    assert vm.summary.power_management.power_state.text_value in {'off', 'suspended', 'unknown'}
 
-        # make sure retirement date is today
-        retirement_date = vm.retirement_date
-        today = parsetime.now().to_american_date_only()
-        assert retirement_date == today
+    # make sure retirement date is today
+    retirement_date = vm.retirement_date
+    today = parsetime.now().to_american_date_only()
+    assert retirement_date == today
 
 
 @test_requirements.retirement
-@pytest.mark.meta(blockers=[1337697])
 def test_retirement_now(vm):
-    """Tests retirement
-
-    Metadata:
-        test_flag: retire, provision
+    """Tests on-demand retirement of an instance/vm
     """
     vm.retire()
     verify_retirement(vm)
@@ -96,21 +88,15 @@ def test_retirement_now(vm):
 
 @test_requirements.retirement
 def test_set_retirement_date(vm):
-    """Tests retirement
-
-    Metadata:
-        test_flag: retire, provision
+    """Tests retirement by setting a date
     """
     vm.set_retirement_date(datetime.datetime.now(), warn="1 Week before retirement")
     verify_retirement(vm)
 
 
 @test_requirements.retirement
-def test_set_unset_retirement_date_tomorrow(existing_vm):
-    """Tests retirement
-
-    Metadata:
-        test_flag: retire, provision
+def test_unset_retirement_date(existing_vm):
+    """Tests cancelling a scheduled retirement by removing the set date
     """
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
     existing_vm.set_retirement_date(tomorrow)
