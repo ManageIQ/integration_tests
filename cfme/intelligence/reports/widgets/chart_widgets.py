7291c4239b0c7f99aedd3355161bdee0973cf778
quarckster <dmisharo@redhat.com>
2017-02-14 11:04:15 +0100

    Added Chart Widgets

---
+++ cfme/intelligence/reports/widgets/chart_widgets.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""Page model for Cloud Intel / Reports / Dashboard Widgets / Charts"""
+from widgetastic_manageiq import Calendar
+from widgetastic_patternfly import BootstrapSelect
+from utils.appliance.implementations.ui import navigator
+from . import (Widget, DashboardWidgetFormCommon, NewDashboardWidget, EditDashboardWidget,
+    NewDashboardWidgetView, EditDashboardWidgetView)
+
+
+class ChartWidgetFormCommon(DashboardWidgetFormCommon):
+    # Chart Report
+    filter = BootstrapSelect("repfilter_typ")
+    # Timer
+    run = BootstrapSelect("timer_typ")
+    every = BootstrapSelect("timer_hours")
+    time_zone = BootstrapSelect("time_zone")
+    starting_date = Calendar("miq_date_1")
+    starting_hour = BootstrapSelect("start_hour")
+    starting_minute = BootstrapSelect("start_min")
+
+
+class NewChartWidgetView(NewDashboardWidgetView, ChartWidgetFormCommon):
+    pass
+
+
+class EditChartWidgetView(EditDashboardWidgetView, ChartWidgetFormCommon):
+    pass
+
+
+class ChartWidget(Widget):
+
+    TYPE = "Charts"
+    TITLE = "Chart"
+    pretty_attrs = ["title", "description", "filter", "visibility"]
+
+    def __init__(self,
+            title, description=None, active=None, filter=None, timer=None, visibility=None):
+        self.title = title
+        self.description = description
+        self.active = active
+        self.filter = filter
+        self.timer = timer
+        self.visibility = visibility
+
+    @property
+    def fill_dict(self):
+        return {
+            "widget_title": self.title,
+            "description": self.description,
+            "active": self.active,
+            "filter": self.filter,
+            "run": self.timer.get("run"),
+            "every": self.timer.get("hours"),
+            "time_zone": self.timer.get("time_zone"),
+            "starting_date": self.timer.get("starting_date"),
+            "starting_hour": self.timer.get("starting_hour"),
+            "starting_minute": self.timer.get("starting_minute"),
+            "visibility": self.visibility
+        }
+
+
+@navigator.register(ChartWidget, "Add")
+class NewChartWidget(NewDashboardWidget):
+    VIEW = NewChartWidgetView
+
+
+@navigator.register(ChartWidget, "Edit")
+class EditChartWidget(EditDashboardWidget):
+    VIEW = EditChartWidgetView
