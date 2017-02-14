940e52b348f8b1f28c0a439e034080277dbc8484
Mike Shriver <mshriver@redhat.com>
2017-02-14 14:30:21 -0500

    Improve logging in ec2cleanup

--- scripts/ec2cleanup.py
+++ scripts/ec2cleanup.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+from datetime import datetime
 from tabulate import tabulate
 
 from utils.log import logger
@@ -15,8 +16,8 @@ def parse_cmd_line():
                         help='List of EIPs, which should be '
                              'excluded. Allocation_id or public IP are allowed.')
     parser.add_argument("--output", dest="output", help="target file name, default "
-                                                        "'ec2_cleanup.log' in utils.path.log_path",
-                        default=log_path.join('ec2_cleanup.log').strpath)
+                                                        "'cleanup_ec2.log' in utils.path.log_path",
+                        default=log_path.join('cleanup_ec2.log').strpath)
     args = parser.parse_args()
     return args
 
@@ -28,24 +29,28 @@ def delete_disassociated_addresses(provider_mgmt, excluded_eips, output):
         for ip in provider_mgmt.get_all_disassociated_addresses():
             if ip.allocation_id:
                 if excluded_eips and ip.allocation_id in excluded_eips:
-                    print "\n Excluding allocation ID: {}".format(ip.allocation_id)
+                    print "  Excluding allocation ID: {}".format(ip.allocation_id)  # noqa
                     continue
                 else:
                     ip_list.append([provider_name, ip.public_ip, ip.allocation_id])
                     provider_mgmt.release_vpc_address(alloc_id=ip.allocation_id)
             else:
                 if excluded_eips and ip.public_ip in excluded_eips:
-                    print "\n Excluding IP: {}".format(ip.public_ip)
+                    print "  Excluding IP: {}".format(ip.public_ip)  # noqa
                     continue
                 else:
                     ip_list.append([provider_name, ip.public_ip, 'N/A'])
                     provider_mgmt.release_address(address=ip.public_ip)
-        print "\n Released Addresses: \n {}".format(ip_list)
+        print "  Released Addresses:\n  {}".format(ip_list)  # noqa
         with open(output, 'a+') as report:
-            # tabulate ip_list and write it
-            report.write(tabulate(tabular_data=ip_list,
-                                  headers=['Provider Key', 'Public IP', 'Allocation ID'],
-                                  tablefmt='orgtbl'))
+            if ip_list:
+                # tabulate ip_list and write it
+                report.write(tabulate(tabular_data=ip_list,
+                                      headers=['Provider Key', 'Public IP', 'Allocation ID'],
+                                      tablefmt='orgtbl'))
+            else:
+                report.write("\n - No IPs released for {}".format(provider_name))
+
     except Exception as e:
         logger.error(e)
 
@@ -56,29 +61,36 @@ def delete_unattached_volumes(provider_mgmt, excluded_volumes, output):
     try:
         for volume in provider_mgmt.get_all_unattached_volumes():
             if excluded_volumes and volume.id in excluded_volumes:
+                print "  Excluding volume id: {}".format(volume.id)  # noqa
                 continue
             else:
                 volume_list.append([provider_name, volume.id])
                 volume.delete()
-        print "\n Deleted Volumes: \n {}".format(volume_list)
+        print "  Deleted Volumes:\n  {}".format(volume_list)  # noqa
         with open(output, 'a+') as report:
-            # tabulate volume_list and write it
-            report.write(tabulate(tabular_data=volume_list,
-                                  headers=['Provider Key', 'Volume ID'],
-                                  tablefmt='orgtbl'))
+            if volume_list:
+                # tabulate volume_list and write it
+                report.write(tabulate(tabular_data=volume_list,
+                                      headers=['Provider Key', 'Volume ID'],
+                                      tablefmt='orgtbl'))
+            else:
+                report.write("\n - No Volumes released for {}".format(provider_name))
     except Exception as e:
         logger.error(e)
 
 
 def ec2cleanup(exclude_volumes, exclude_eips, output):
+    with open(output, 'a+') as report:
+        report.write('ec2cleanup.py, Address and Volume Cleanup')
+        report.write("\nDate: {}\n".format(datetime.now()))
     for provider_key in list_provider_keys('ec2'):
         provider_mgmt = get_mgmt(provider_key)
-        print "----- Provider: {} -----".format(provider_key)
-        print "Releasing addresses..."
+        print "----- Provider: {} -----".format(provider_key)  # noqa
+        print "Releasing addresses..."  # noqa
         delete_disassociated_addresses(provider_mgmt=provider_mgmt,
                                        excluded_eips=exclude_eips,
                                        output=output)
-        print "Deleting volumes..."
+        print "Deleting volumes..."  # noqa
         delete_unattached_volumes(provider_mgmt=provider_mgmt,
                                   excluded_volumes=exclude_volumes,
                                   output=output)
