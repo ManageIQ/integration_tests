dc2fc08b45898133a27293b7f8db035406274e47
Mike Shriver <mshriver@redhat.com>
2017-02-15 11:24:07 -0500

    Update test_retirement, fixture and date refactor

--- cfme/tests/cloud_infra_common/test_retirement.py
+++ cfme/tests/cloud_infra_common/test_retirement.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-import datetime
 import pytest
+from datetime import date, timedelta, datetime
 
 from cfme import test_requirements
 from cfme.common.provider import CloudInfraProvider
 from cfme.common.vm import VM
+from cfme.web_ui import toolbar as tb
 from utils import testgen
+from utils.blockers import BZ
 from utils.generators import random_vm_name
 from utils.log import logger
 from utils.providers import ProviderFilter
 from utils.timeutil import parsetime
 from utils.wait import wait_for
+from utils.version import pick, current_version
 
 
 pytest_generate_tests = testgen.generate(
@@ -27,78 +30,113 @@ pytestmark = [
 
 
 @pytest.yield_fixture(scope="function")
-def vm(small_template, provider):
-    vm_obj = VM.factory(random_vm_name('retire'), provider, template_name=small_template)
-    vm_obj.create_on_provider(find_in_cfme=True, allow_skip="default")
-    yield vm_obj
+def test_vm(small_template, provider):
+    test_vm = VM.factory(random_vm_name('retire'), provider, template_name=small_template)
+    test_vm.create_on_provider(find_in_cfme=True, allow_skip="default")
+    yield test_vm
 
     try:
-        if provider.mgmt.does_vm_exist(vm_obj.name):
-            provider.mgmt.delete_vm(vm_obj.name)
+        if provider.mgmt.does_vm_exist(test_vm.name):
+            provider.mgmt.delete_vm(test_vm.name)
     except Exception:
-        logger.warning('Failed to delete vm from provider: {}'.format(vm_obj.name))
+        logger.warning('Failed to delete vm from provider: {}'.format(test_vm.name))
 
 
-@pytest.yield_fixture(scope="function")
-def existing_vm(provider):
-    """ Fixture will be using for set\unset retirement date for existing vm instead of
-    creation a new one
-    """
-    all_vms = provider.mgmt.list_vm()
-    need_to_create_vm = True
-    for virtual_machine in all_vms:
-        if provider.mgmt.is_vm_running(virtual_machine):
-            vm_obj = VM.factory(virtual_machine, provider)
-            need_to_create_vm = False
-            break
-    if need_to_create_vm:
-        machine_name = random_vm_name('retire')
-        vm_obj = VM.factory(machine_name, provider)
-        vm_obj.create_on_provider(find_in_cfme=True, allow_skip="default")
-
-    yield vm_obj
-
-    try:
-        if need_to_create_vm and provider.mgmt.does_vm_exist(vm_obj.name):
-            provider.mgmt.delete_vm(vm_obj.name)
-    except Exception:
-        logger.warning('Failed to delete vm from provider: {}'.format(vm_obj.name))
-
-
-def verify_retirement(vm):
+def verify_retirement_state(test_vm):
     # wait for the info block showing a date as retired date
-    wait_for(lambda: vm.is_retired, delay=15, num_sec=10 * 60,
-             message="Wait until VM {} will be retired".format(vm.name))
+    # Use lambda for is_retired since its a property
+    assert wait_for(lambda: test_vm.is_retired, delay=5, num_sec=10 * 60, fail_func=tb.refresh,
+             message="Wait for VM '{}' to enter retired state".format(test_vm.name))
 
-    assert vm.summary.power_management.power_state.text_value in {'off', 'suspended', 'unknown'}
+    # TODO: remove dependency on SummaryMixin and use widgetastic when available
+    assert test_vm.summary.power_management.power_state.text_value in ['off', 'suspended',
+                                                                       'unknown']
 
-    # make sure retirement date is today
-    retirement_date = vm.retirement_date
-    today = parsetime.now().to_american_date_only()
-    assert retirement_date == today
+
+def verify_retirement_date(test_vm, expected_date='Never'):
+    """Verify the retirement date for a variety of situations
+
+    Args:
+        expected_date: a :py:class: `str` or :py:class: `parsetime` date
+            or a dict of :py:class: `parsetime` dates with 'start' and 'end' keys.
+    """
+    if isinstance(expected_date, list):
+        # convert to a parsetime object for comparsion, function depends on version
+        if 'UTC' in pick(VM.RETIRE_DATE_FMT):
+            convert_func = parsetime.from_american_minutes_with_utc
+        else:
+            convert_func = parsetime.from_american_date_only
+        expected_date.update({'retire': convert_func(test_vm.retirement_date)})
+        logger.info('Asserting retire date "%s" is between "%s" and "%s"',
+                    expected_date['retire'],
+                    expected_date['start'],
+                    expected_date['end'])
+
+        assert expected_date['start'] <= expected_date['retire'] <= expected_date['end']
+
+    elif isinstance(expected_date, (parsetime, datetime, date)):
+        test_vm.retirement_date == expected_date.strftime(pick(VM.RETIRE_DATE_FMT))
+    else:
+        test_vm.retirement_date == expected_date
+
+
+def generate_retirement_date(delta=None):
+    gen_date = date.today()
+    if delta:
+        gen_date += timedelta(days=delta)
+    return gen_date
+
+
+def generate_retirement_date_now():
+    return datetime.utcnow()
 
 
 @test_requirements.retirement
-def test_retirement_now(vm):
+@pytest.mark.tier(2)
+def test_retirement_now(test_vm):
     """Tests on-demand retirement of an instance/vm
     """
-    vm.retire()
-    verify_retirement(vm)
+    # For 5.7 capture two times to assert the retire time is within a window.
+    # Too finicky to get it down to minute precision, nor is it really needed here
+    retire_times = dict()
+    retire_times['start'] = generate_retirement_date_now() + timedelta(minutes=-1)
+    test_vm.retire()
+    verify_retirement_state(test_vm)
+    retire_times['end'] = generate_retirement_date_now() + timedelta(minutes=1)
+    if current_version() < '5.7':
+        verify_retirement_date(test_vm,
+                               expected_date=parsetime.now().to_american_date_only())
+    else:
+        verify_retirement_date(test_vm, expected_date=retire_times)
 
 
 @test_requirements.retirement
-def test_set_retirement_date(vm):
-    """Tests retirement by setting a date
+@pytest.mark.tier(2)
+@pytest.mark.meta(blockers=[BZ(1419150, forced_streams='5.6')])
+def test_set_retirement_date(test_vm):
+    """Tests setting retirement date and verifies configured date is reflected in UI
+
+    Note we cannot control the retirement time, just day, so we cannot wait for the VM to retire
     """
-    vm.set_retirement_date(datetime.datetime.now(), warn="1 Week before retirement")
-    verify_retirement(vm)
+    num_days = 2
+    retire_date = generate_retirement_date(delta=num_days)
+    test_vm.set_retirement_date(retire_date, warn="1 Week before retirement")
+    verify_retirement_date(test_vm, expected_date=retire_date)
 
 
 @test_requirements.retirement
-def test_unset_retirement_date(existing_vm):
+@pytest.mark.tier(2)
+def test_unset_retirement_date(test_vm):
     """Tests cancelling a scheduled retirement by removing the set date
     """
-    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    existing_vm.set_retirement_date(tomorrow)
-    existing_vm.set_retirement_date(None)
-    assert existing_vm.get_detail(properties=["Lifecycle", "Retirement Date"]) == "Never"
+    num_days = 3
+    retire_date = generate_retirement_date(delta=num_days)
+    test_vm.set_retirement_date(retire_date)
+    if BZ(1419150, forced_streams='5.6').blocks:
+        # The date is wrong, but we can still test unset
+        logger.warning('Skipping test step verification for BZ 1419150')
+    else:
+        verify_retirement_date(test_vm, expected_date=retire_date)
+
+    test_vm.set_retirement_date(None)
+    verify_retirement_date(test_vm, expected_date='Never')
