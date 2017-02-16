52f1a739c38d52b032f2357b9f86d66dfe0ffdb8
Milan Falešník <mfalesni@redhat.com>
2017-02-16 16:26:37 +0100

    Speed up some automate tests by removing unnecessary finalizers

--- cfme/tests/automate/test_class.py
+++ cfme/tests/automate/test_class.py
@@ -61,7 +61,6 @@ def test_schema_crud(request, namespace):
         display_name=fauxfactory.gen_alphanumeric(),
         description=fauxfactory.gen_alphanumeric()
     )
-    request.addfinalizer(a_class.delete_if_exists)
     f1 = fauxfactory.gen_alpha()
     f2 = fauxfactory.gen_alpha()
     f3 = fauxfactory.gen_alpha()
@@ -88,12 +87,10 @@ def test_same_class_name_different_namespace(request, domain):
         name=fauxfactory.gen_alpha(),
         description=fauxfactory.gen_alpha()
     )
-    request.addfinalizer(ns1.delete_if_exists)
     ns2 = domain.namespaces.create(
         name=fauxfactory.gen_alpha(),
         description=fauxfactory.gen_alpha()
     )
-    request.addfinalizer(ns2.delete_if_exists)
 
     c1 = ns1.classes.create(
         name=fauxfactory.gen_alphanumeric(),
@@ -121,7 +118,6 @@ def test_display_name_unset_from_ui(request, namespace):
         display_name=fauxfactory.gen_alphanumeric(),
         description=fauxfactory.gen_alphanumeric()
     )
-    request.addfinalizer(a_class.delete_if_exists)
     with update(a_class):
         a_class.display_name = fauxfactory.gen_alphanumeric()
     assert a_class.exists
