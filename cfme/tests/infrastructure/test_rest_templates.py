0b38e05c2380ca5e482c5e4f9a85fae24c85ca4a
Martin Kourim <misc.kourim@gmail.com>
2017-02-20 17:23:39 +0100

    [1LP][RFR] fix creating templates in test_rest_templates (#4204)
    fix creating templates in test_rest_templates

--- cfme/tests/infrastructure/test_rest_templates.py
+++ cfme/tests/infrastructure/test_rest_templates.py
@@ -6,6 +6,7 @@ from cfme.rest.gen_data import a_provider as _a_provider
 from cfme.rest.gen_data import mark_vm_as_template
 from cfme.rest.gen_data import vm as _vm
 from utils import error
+from utils.blockers import BZ
 
 pytestmark = [pytest.mark.ignore_stream("5.4"), test_requirements.rest]
 
@@ -15,7 +16,7 @@ def a_provider():
     return _a_provider()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def vm(request, a_provider, rest_api_modscope):
     return _vm(request, a_provider, rest_api_modscope)
 
@@ -35,6 +36,11 @@ def template(request, rest_api, a_provider, vm):
 @pytest.mark.tier(3)
 @pytest.mark.parametrize("from_detail", [True, False], ids=["from_detail", "from_collection"])
 def test_set_ownership(rest_api, template, from_detail):
+    """Tests setting of template ownership.
+
+    Metadata:
+        test_flag: rest
+    """
     if "set_ownership" not in rest_api.collections.templates.action.all:
         pytest.skip("set_ownership action for templates is not implemented in this version")
     group = rest_api.collections.groups.get(description='EvmGroup-super_administrator')
@@ -48,6 +54,7 @@ def test_set_ownership(rest_api, template, from_detail):
     else:
         data["href"] = template.href
         rest_api.collections.templates.action.set_ownership(**data)
+    assert rest_api.response.status_code == 200
     template.reload()
     assert hasattr(template, "evm_owner_id")
     assert template.evm_owner_id == user.id
@@ -56,13 +63,43 @@ def test_set_ownership(rest_api, template, from_detail):
 
 
 @pytest.mark.tier(2)
-@pytest.mark.parametrize("multiple", [False, True], ids=["one_request", "multiple_requests"])
-def test_delete_template(rest_api, template, multiple):
-    if multiple:
+@pytest.mark.meta(blockers=[BZ(1422807, forced_streams=["5.6", "5.7", "upstream"])])
+def test_delete_template_from_detail_post(rest_api, template):
+    """Tests deletion of template from detail using POST method.
+
+    Metadata:
+        test_flag: rest
+    """
+    template.action.delete(force_method="post")
+    assert rest_api.response.status_code == 200
+    with error.expected("ActiveRecord::RecordNotFound"):
+        template.action.delete(force_method="post")
+    assert rest_api.response.status_code == 404
+
+
+@pytest.mark.tier(2)
+def test_delete_template_from_detail_delete(rest_api, template):
+    """Tests deletion of template from detail using DELETE method.
+
+    Metadata:
+        test_flag: rest
+    """
+    template.action.delete(force_method="delete")
+    assert rest_api.response.status_code == 204
+    with error.expected("ActiveRecord::RecordNotFound"):
+        template.action.delete(force_method="delete")
+    assert rest_api.response.status_code == 404
+
+
+@pytest.mark.tier(2)
+def test_delete_template_from_collection(rest_api, template):
+    """Tests deletion of template from collection.
+
+    Metadata:
+        test_flag: rest
+    """
+    rest_api.collections.templates.action.delete(template)
+    assert rest_api.response.status_code == 200
+    with error.expected("ActiveRecord::RecordNotFound"):
         rest_api.collections.templates.action.delete(template)
-        with error.expected("ActiveRecord::RecordNotFound"):
-            rest_api.collections.templates.action.delete(template)
-    else:
-        template.action.delete()
-        with error.expected("ActiveRecord::RecordNotFound"):
-            template.action.delete()
+    assert rest_api.response.status_code == 404
