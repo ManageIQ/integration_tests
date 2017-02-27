81b049f8b5831a0dc63c0e9cd19ca8fc5aa5851d
Pete Savage <psavage@redhat.com>
2017-02-27 12:42:57 +0000

    Rel update

--- release.py
+++ release.py
@@ -8,9 +8,12 @@ print('integration_tests {} Released'.format(new_ver))
 print('')
 print('Includes:')
 
-proc = subprocess.Popen(['git', 'describe', '--tags', '--abbrev=0'],
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-old_tag = proc.stdout.read().strip("\n")
+if len(sys.argv) <= 2:
+    proc = subprocess.Popen(['git', 'describe', '--tags', '--abbrev=0'],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    old_tag = proc.stdout.read().strip("\n")
+else:
+    old_tag = sys.argv[2]
 proc = subprocess.Popen(['git', 'log', '{}..master'.format(old_tag), '--oneline'],
     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 commits = proc.stdout.read()
