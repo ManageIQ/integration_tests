e28be7ad3b3ea2355141e06c48a5342df273876f
Martin Kourim <misc.kourim@gmail.com>
2017-03-01 11:00:38 +0100

    check response status code (#4259)
        check HTTP response status code
        refactoring
        test also DELETE method

--- cfme/tests/configure/test_tag_category.py
+++ cfme/tests/configure/test_tag_category.py
@@ -5,7 +5,7 @@ from cfme.rest.gen_data import categories as _categories
 import pytest
 from utils.update import update
 from utils.wait import wait_for
-from utils import error, version
+from utils import error
 
 
 @pytest.mark.tier(2)
@@ -21,63 +21,90 @@ def test_category_crud():
 
 
 class TestCategoriesViaREST(object):
-
     @pytest.fixture(scope="function")
     def categories(self, request, rest_api):
-        return _categories(request, rest_api, num=5)
+        response = _categories(request, rest_api, num=5)
+        assert rest_api.response.status_code == 200
+        assert len(response) == 5
+        return response
 
     @pytest.mark.tier(3)
-    @pytest.mark.uncollectif(lambda: version.current_version() < '5.5')
+    def test_create_categories(self, rest_api, categories):
+        """Tests creating categories.
+
+        Metadata:
+            test_flag: rest
+        """
+        for ctg in categories:
+            record = rest_api.collections.categories.get(id=ctg.id)
+            assert rest_api.response.status_code == 200
+            assert record.name == ctg.name
+
+    @pytest.mark.tier(3)
     @pytest.mark.parametrize(
         "multiple", [False, True],
         ids=["one_request", "multiple_requests"])
     def test_edit_categories(self, rest_api, categories, multiple):
-        if "edit" not in rest_api.collections.categories.action.all:
-            pytest.skip("Edit categories action is not implemented in this version")
+        """Tests editing categories.
 
+        Metadata:
+            test_flag: rest
+        """
         if multiple:
-            new_names = []
+            new_descriptions = []
             ctgs_data_edited = []
             for ctg in categories:
-                new_name = fauxfactory.gen_alphanumeric().lower()
-                new_names.append(new_name)
+                new_description = "test_category_{}".format(fauxfactory.gen_alphanumeric().lower())
+                new_descriptions.append(new_description)
                 ctg.reload()
                 ctgs_data_edited.append({
                     "href": ctg.href,
-                    "description": "test_category_{}".format(new_name),
+                    "description": new_description,
                 })
             rest_api.collections.categories.action.edit(*ctgs_data_edited)
-            for new_name in new_names:
+            assert rest_api.response.status_code == 200
+            for new_description in new_descriptions:
                 wait_for(
-                    lambda: rest_api.collections.categories.find_by(description=new_name),
+                    lambda: rest_api.collections.categories.find_by(description=new_description),
                     num_sec=180,
                     delay=10,
                 )
         else:
             ctg = rest_api.collections.categories.get(description=categories[0].description)
-            new_name = 'test_category_{}'.format(fauxfactory.gen_alphanumeric().lower())
-            ctg.action.edit(description=new_name)
+            new_description = "test_category_{}".format(fauxfactory.gen_alphanumeric().lower())
+            ctg.action.edit(description=new_description)
+            assert rest_api.response.status_code == 200
             wait_for(
-                lambda: rest_api.collections.categories.find_by(description=new_name),
+                lambda: rest_api.collections.categories.find_by(description=new_description),
                 num_sec=180,
                 delay=10,
             )
 
     @pytest.mark.tier(3)
-    @pytest.mark.uncollectif(lambda: version.current_version() < '5.5')
-    @pytest.mark.parametrize(
-        "multiple", [False, True],
-        ids=["one_request", "multiple_requests"])
-    def test_delete_categories(self, rest_api, categories, multiple):
-        if "delete" not in rest_api.collections.categories.action.all:
-            pytest.skip("Delete categories action is not implemented in this version")
+    @pytest.mark.parametrize("method", ["post", "delete"], ids=["POST", "DELETE"])
+    def test_delete_categories_from_detail(self, rest_api, categories, method):
+        """Tests deleting categories from detail.
 
-        if multiple:
+        Metadata:
+            test_flag: rest
+        """
+        status = 204 if method == "delete" else 200
+        for ctg in categories:
+            ctg.action.delete(force_method=method)
+            assert rest_api.response.status_code == status
+            with error.expected("ActiveRecord::RecordNotFound"):
+                ctg.action.delete(force_method=method)
+            assert rest_api.response.status_code == 404
+
+    @pytest.mark.tier(3)
+    def test_delete_categories_from_collection(self, rest_api, categories):
+        """Tests deleting categories from collection.
+
+        Metadata:
+            test_flag: rest
+        """
+        rest_api.collections.categories.action.delete(*categories)
+        assert rest_api.response.status_code == 200
+        with error.expected("ActiveRecord::RecordNotFound"):
             rest_api.collections.categories.action.delete(*categories)
-            with error.expected("ActiveRecord::RecordNotFound"):
-                rest_api.collections.categories.action.delete(*categories)
-        else:
-            ctg = categories[0]
-            ctg.action.delete()
-            with error.expected("ActiveRecord::RecordNotFound"):
-                ctg.action.delete()
+        assert rest_api.response.status_code == 404
