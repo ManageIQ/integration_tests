c173743ef20af65fa739ffccdb351f17e9ac7000
Oleksandr Kolisnyk <okolisny@redhat.com>
2017-03-06 18:44:32 +0200

    added test for creation of instance

---
+++ cfme/tests/openstack/cloud/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Openstack cloud provider"""
---
+++ cfme/tests/openstack/cloud/test_instances.py
@@ -0,0 +1,29 @@
+"""Tests for Openstack cloud instances"""
+
+import fauxfactory
+import pytest
+
+from cfme.cloud.instance.openstack import OpenStackInstance
+from cfme.cloud.provider.openstack import OpenStackProvider
+from utils import testgen
+from utils.appliance.implementations.ui import navigate_to
+
+
+pytest_generate_tests = testgen.generate([OpenStackProvider],
+                                         scope='module')
+
+pytestmark = [pytest.mark.usefixtures("setup_provider_modscope")]
+
+
+def test_create_instance(provider):
+    """Creates an instance and verifies it appears on UI"""
+    prov_data = provider.get_yaml_data()['provisioning']
+    instance = OpenStackInstance(fauxfactory.gen_alpha(), provider,
+                                 template_name=prov_data['image']['name'])
+    navigate_to(instance, 'Provision')
+    instance.create(fauxfactory.gen_email(), fauxfactory.gen_alpha(),
+                    fauxfactory.gen_alpha(), prov_data['cloud_network'],
+                    prov_data['instance_type'], False,
+                    security_groups='default')
+    instance.wait_to_appear()
+    assert instance.get_detail('Properties', 'Power State') == 'on'
