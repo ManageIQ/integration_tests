8670dab6a285d1e04d2378dd67d50a8e99f88ca8
izapolsk <izapolsk@redhat.com>
2017-03-10 09:37:28 +0200

    slightly updated dockerbot to use frozen.txt instead of old requirements.txt (#4348)

--- scripts/dockerbot/pytestbase/setup.sh
+++ scripts/dockerbot/pytestbase/setup.sh
@@ -63,9 +63,9 @@ do_or_die () {
 # Runs pip update - optionally can make use of wheelhouse
 run_pip_update () {
     if [ -n "$WHEEL_HOST_URL" ]; then
-        run_n_log "PYCURL_SSL_LIBRARY=nss pip install --trusted-host $WHEEL_HOST -f $WHEEL_HOST_URL -Ur $CFME_REPO_DIR/requirements.txt --no-cache-dir"
+        run_n_log "PYCURL_SSL_LIBRARY=nss pip install --trusted-host $WHEEL_HOST -f $WHEEL_HOST_URL -Ur $CFME_REPO_DIR/requirements/frozen.txt --no-cache-dir"
     else
-        run_n_log "PYCURL_SSL_LIBRARY=nss pip install -Ur $CFME_REPO_DIR/requirements.txt --no-cache-dir"
+        run_n_log "PYCURL_SSL_LIBRARY=nss pip install -Ur $CFME_REPO_DIR/requirements/frozen.txt --no-cache-dir"
     fi
 }
 
