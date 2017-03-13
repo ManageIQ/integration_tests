f88147c06a4653ff814e2aec8ff66f4bd4d4158c
Tony S <tonysh519@gmail.com>
2017-03-13 16:31:23 +0200

    update

--- cfme/tests/containers/test_cockpit.py
+++ cfme/tests/containers/test_cockpit.py
@@ -17,11 +17,12 @@ pytest_generate_tests = testgen.generate(
 
 
 @pytest.mark.polarion('CMP-10255')
-def test_cockpit_button_access(provider):
+def test_cockpit_button_access(provider, soft_assert):
     """ The test verifies the existence of cockpit "Web Console"
         button on each node
 
     """
+
     navigate_to(NodeCollection, 'All')
     tb.select('List View')
 
@@ -31,6 +32,8 @@ def test_cockpit_button_access(provider):
     for name in names:
         obj = Node(name, provider)
         obj.load_details()
-        assert tb.exists(
-            'Open a new browser window with Cockpit for this '
-            'VM.  This requires that Cockpit is pre-configured on the VM.')
+        soft_assert(
+            tb.exists(
+                'Open a new browser window with Cockpit for this '
+                'VM.  This requires that Cockpit is pre-configured on the VM.'),
+            'Cockpit "Web Console" button is not found on details page.'.format(name))
