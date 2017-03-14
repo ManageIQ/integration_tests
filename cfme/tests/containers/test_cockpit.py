8cbb8a4520593f5cb7287e1505c283c871ffb89f
Tony S <tonysh519@gmail.com>
2017-03-14 14:30:09 +0200

    update

--- cfme/tests/containers/test_cockpit.py
+++ cfme/tests/containers/test_cockpit.py
@@ -1,37 +1,50 @@
 import pytest
 
-from cfme.containers.node import Node, NodeCollection
+from cfme.containers.node import NodeCollection
 from cfme.containers.provider import ContainersProvider
-from cfme.web_ui import toolbar as tb
+from cfme.fixtures import pytest_selenium as sel
+from cfme.web_ui import CheckboxTable, toolbar as tb
 from utils import testgen, version
 from utils.appliance.implementations.ui import navigate_to
+from utils.blockers import BZ
 
 
 pytestmark = [
     pytest.mark.uncollectif(
-        lambda provider: version.current_version() < "5.6"),
+        lambda provider: version.current_version() < "5.7"),
     pytest.mark.usefixtures('setup_provider'),
     pytest.mark.tier(1)]
 pytest_generate_tests = testgen.generate(
     [ContainersProvider], scope='function')
 
 
-@pytest.mark.polarion('CMP-10255')
+@pytest.mark.polarion('CMP-10469')
+@pytest.mark.meta(blockers=[BZ(1406772)])
 def test_cockpit_button_access(provider, soft_assert):
     """ The test verifies the existence of cockpit "Web Console"
-        button on each node
+        button on master node, then presses on the button and
+        opens up the cockpit main page in a new window. Then
+        we verify the title of the main cockpit page. The test
+        will not work until the single sign-on bug is fixed
 
     """
 
-    view = navigate_to(NodeCollection, 'All')
+    navigate_to(NodeCollection, 'All')
+    list_tbl = CheckboxTable(table_locator="//div[@id='list_grid']//table")
+    names = [r for r in list_tbl.rows()]
 
-    names = [r.name.text for r in view.nodes]
+    names[0].row_element.click()
 
-    for name in names:
-        obj = Node(name, provider)
-        obj.load_details()
-        soft_assert(
-            tb.exists(
-                'Open a new browser window with Cockpit for this '
-                'VM.  This requires that Cockpit is pre-configured on the VM.'),
-            'Cockpit "Web Console" button {} is not found on details page.'.format(name))
+    soft_assert(
+        tb.exists(
+            'Open a new browser window with Cockpit for this '
+            'VM.  This requires that Cockpit is pre-configured on the VM.'),
+        'No "Web Console" button found')
+    tb.select('Open a new browser window with Cockpit for this '
+              'VM.  This requires that Cockpit is pre-configured on the VM.')
+
+    port_num = ':9090/system'
+    url_cockpit = provider.hostname + port_num
+    sel.get(url_cockpit)
+    title_pg = sel.title()
+    soft_assert(title_pg == 'Cockpit', 'Cockpit main page failed to open')
