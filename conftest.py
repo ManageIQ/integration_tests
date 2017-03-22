25f98c4cd6150b756ad65297a146096aa7ae4854
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-03-22 13:20:53 +0100

    conftest: remove the unused _pytest_plugins_generator

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ Top-level conftest.py does a couple of things:
 1) Add cfme_pages repo to the sys.path automatically
 2) Load a number of plugins and fixtures automatically
 """
-from pkgutil import iter_modules
 
 import pytest
 import requests
@@ -157,15 +156,6 @@ def appliance_police():
         rdb_kwargs = {}
     Rdb(msg).set_trace(**rdb_kwargs)
     store.slave_manager.message('Resuming testing following remote debugging')
-
-
-def _pytest_plugins_generator(*extension_pkgs):
-    # Finds all submodules in pytest extension packages and loads them
-    for extension_pkg in extension_pkgs:
-        path = extension_pkg.__path__
-        prefix = '%s.' % extension_pkg.__name__
-        for importer, modname, is_package in iter_modules(path, prefix):
-            yield modname
 
 
 pytest_plugins = (
