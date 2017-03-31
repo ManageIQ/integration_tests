6feb4340e06a6965ce72dc7a7edcc5c24cb758b3
Michael Shriver <mshriver@redhat.com>
2017-03-31 17:20:21 +0000

    Update test_appliance_console.test_black_console (#4485)
    Version pick CFME / ManageIQ for grepping

--- cfme/tests/cli/test_appliance_console.py
+++ cfme/tests/cli/test_appliance_console.py
@@ -10,12 +10,15 @@ TimedCommand = namedtuple('TimedCommand', ['command', 'timeout'])
 @pytest.mark.smoke
 def test_black_console(appliance):
     """'exec > >(tee /tmp/opt.txt)' saves stdout to file, 'ap' launch appliance_console."""
-
+    appliance_title = 'CFME' if version.appliance_is_downstream() else 'ManageIQ'
     command_set = ('exec > >(tee /tmp/opt.txt)', 'ap')
     appliance.appliance_console.run_commands(command_set)
-    assert appliance.ssh_client.run_command("cat /tmp/opt.txt | grep 'CFME Virtual Appliance'")
-    assert appliance.ssh_client.run_command("cat /tmp/opt.txt | grep 'CFME Database:'")
-    assert appliance.ssh_client.run_command("cat /tmp/opt.txt | grep 'CFME Version:'")
+    assert appliance.ssh_client.run_command("cat /tmp/opt.txt | egrep '{} Virtual Appliance'"
+                                            .format(appliance_title))
+    assert appliance.ssh_client.run_command("cat /tmp/opt.txt | egrep '{} Database:'"
+                                            .format(appliance_title))
+    assert appliance.ssh_client.run_command("cat /tmp/opt.txt | egrep '{} Version:'"
+                                            .format(appliance_title))
 
 
 def test_black_console_set_hostname(appliance):
