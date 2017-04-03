829ba988d24699fe9d545879b3679ae2ee0a3d7f
rbabyuk <rbabyuk@redhat.com>
2017-04-03 14:34:34 +0300

    Fix tree navigation in test_myservice.py

--- cfme/services/catalogs/service_catalogs.py
+++ cfme/services/catalogs/service_catalogs.py
@@ -135,7 +135,7 @@ class ServiceCatalogDetails(CFMENavigateStep):
     def step(self):
         try:
             self.prerequisite_view.service_catalogs.tree.click_path("All Services",
-                self.obj.catalog.name, self.obj.name)
+                    self.obj.catalog, self.obj.name)
         except:
             raise NoSuchElementException()
 
