73280edda1aae99f308be669b7d3751b8d321677
Tony S <tonysh519@gmail.com>
2017-04-03 12:58:30 +0300

    update

--- cfme/containers/provider/__init__.py
+++ cfme/containers/provider/__init__.py
@@ -72,6 +72,7 @@ properties_form_58 = TabStripForm(
     })
 
 
+
 prop_region = Region(
     locators={
         'properties_form': {
@@ -97,7 +98,8 @@ class ContainersProvider(BaseProvider, Pretty):
         'num_replication_controller',
         'num_pod',
         'num_node',
-        'num_container']
+        'num_container',
+        'num_image_registry']
     # TODO add 'num_volume', 'num_image_registry'
     string_name = "Containers"
     page_name = "containers"
@@ -216,7 +218,8 @@ class ContainersProvider(BaseProvider, Pretty):
 
     @variable(alias='db')
     def num_container(self):
-        # Containers are linked to providers through container definitions and then through pods
+        # Containers are linked to providers through container definitions and
+        # then through pods
         res = self.appliance.db.engine.execute(
             "SELECT count(*) "
             "FROM ext_management_systems, container_groups, container_definitions, containers "
@@ -256,7 +259,8 @@ class All(CFMENavigateStep):
     prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
 
     def step(self):
-        self.prerequisite_view.navigation.select('Compute', 'Containers', 'Providers')
+        self.prerequisite_view.navigation.select(
+            'Compute', 'Containers', 'Providers')
 
     def resetter(self):
         # Reset view and selection
@@ -345,6 +349,7 @@ class TopologyFromDetails(CFMENavigateStep):
 class ContainersTestItem(object):
     """This is a generic test item. Especially used for parametrized functions
     """
+
     def __init__(self, obj, polarion_id):
         """Args:
             * obj: The container object in this test (e.g. Image)
--- cfme/tests/containers/test_reload_button_provider.py
+++ cfme/tests/containers/test_reload_button_provider.py
@@ -1,8 +1,8 @@
 import pytest
 
 from cfme.containers.provider import ContainersProvider
-from utils import testgen, version
 from cfme.web_ui import toolbar as tb
+from utils import testgen, version
 from utils.appliance.implementations.ui import navigate_to
 
 
@@ -11,7 +11,8 @@ pytestmark = [
         lambda: version.current_version() < "5.6"),
     pytest.mark.usefixtures('setup_provider'),
     pytest.mark.tier(2)]
-pytest_generate_tests = testgen.generate([ContainersProvider], scope='function')
+pytest_generate_tests = testgen.generate(
+    [ContainersProvider], scope='function')
 
 
 @pytest.mark.polarion('CMP-9878')
@@ -19,11 +20,12 @@ def test_reload_button_provider(provider):
     """ This test verifies the data integrity of the fields in
         the Relationships table after clicking the "reload"
         button. Fields that are being verified as part of provider.validate.stats():
-        Projects, Routes, Container Services, Replicators, Pods, Containers, and Nodes.
+        Projects, Routes, Container Services, Replicators, Pods, Image Registries,
+        Containers, and Nodes.
         Images are being validated separately, since the total
-        number of images in CFME 5.7 includes all images from the OSE registry as well
+        number of images in CFME 5.7 and CFME 5.8 includes all images from the OSE registry as well
         as the images that are being created from the running pods. The images are searched
-        according to the @sha. Image Registries are also validated separately.
+        according to the @sha.
     """
 
     navigate_to(provider, 'Details')
@@ -42,22 +44,17 @@ def test_reload_button_provider(provider):
     list_img_from_registry_splitted_new = set(list_img_from_registry_splitted)
     list_img_from_openshift_parsed_new = set(list_img_from_openshift_parsed)
 
-    list_img_from_openshift_parsed_new.update(list_img_from_registry_splitted_new)
+    list_img_from_openshift_parsed_new.update(
+        list_img_from_registry_splitted_new)
 
     num_img_in_cfme = provider.num_image()
     # TODO Fix num_image_ui()
 
     num_img_cfme_56 = len(provider.mgmt.list_image())
     num_img_cfme_57 = len(list_img_from_openshift_parsed_new)
+    num_img_cfme_58 = len(list_img_from_openshift_parsed_new)
 
     assert num_img_in_cfme == version.pick({version.LOWEST: num_img_cfme_56,
-                                            '5.7': num_img_cfme_57})
-
-    # validate the number of image registries
-    list_all_rgstr = provider.mgmt.list_image_registry()
-    list_all_rgstr_revised = [i.host for i in list_all_rgstr]
-    list_all_rgstr_new = filter(lambda ch: 'openshift3' not in ch, list_all_rgstr_revised)
-
-    num_rgstr_in_cfme = provider.summary.relationships.image_registries.value
-
-    assert len(list_all_rgstr_new) == num_rgstr_in_cfme
+                                            '5.7': num_img_cfme_57,
+                                            '5.8': num_img_cfme_58
+                                            })
