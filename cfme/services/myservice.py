ed247122d725ab4ce1a42902c7df27941ac46840
Shveta Sachdeva <sshveta@redhat.com>
2017-04-07 09:08:16 -0700

    MyService widgetastic conversion

--- cfme/services/myservice.py
+++ cfme/services/myservice.py
@@ -1,162 +1,202 @@
-from functools import partial
-
-from navmazing import NavigateToAttribute, NavigateToSibling
-
-from cfme import web_ui as ui
+from widgetastic.widget import Text, View
+from widgetastic_manageiq import Accordion, ManageIQTree, Calendar, SummaryTable
+from widgetastic_patternfly import Input, BootstrapSelect, Dropdown, Button, CandidateNotFound
+from cfme.web_ui import toolbar as tb, Quadicon
 from cfme.fixtures import pytest_selenium as sel
-from cfme.web_ui import \
-    accordion, flash, Quadicon, InfoBlock, Form, fill, form_buttons, match_location, toolbar as tb
-from widgetastic_patternfly import CandidateNotFound
-from utils import version
-from utils.appliance import Navigatable, current_appliance
-from utils.appliance.implementations.ui import CFMENavigateStep, navigator, navigate_to
-from utils.log import logger
+from navmazing import NavigateToAttribute, NavigateToSibling
+from cfme import BaseLoggedInPage
 from utils.update import Updateable
+from utils.appliance import Navigatable, current_appliance
+from utils.appliance.implementations.ui import navigator, CFMENavigateStep, navigate_to
 from utils.wait import wait_for
+from utils import version
 
-cfg_btn = partial(tb.select, "Configuration")
-pol_btn = partial(tb.select, "Policy")
-life_btn = partial(tb.select, "Lifecycle")
-down_btn = partial(tb.select, "download_choice")
 
-my_service_tree = partial(accordion.tree, "Services")
+class MyServicesView(BaseLoggedInPage):
+    def in_myservices(self):
+        return (
+            self.logged_in_as_current_user and
+            self.navigation.currently_selected == ['Services', 'MyServices'])
 
-match_page = partial(match_location, controller='service', title='Services')
+    @property
+    def is_displayed(self):
+        return self.in_myservices and self.configuration.is_displayed and not\
+            self.myservice.is_dimmed
 
-retirement_form = Form(
-    fields=[
-        ('retirement_date', ui.Calendar('retirementDate')),
-        ('retirement_warning', ui.Select("select#retirement_warn"))
-    ])
+    @View.nested
+    class myservice(Accordion):  # noqa
+        ACCORDION_NAME = "Services"
 
-edit_service_form = Form(
-    fields=[
-        ("name", ui.Input("name")),
-        ("description", ui.Input("description"))
-    ])
+        tree = ManageIQTree()
 
-set_ownership_form = Form(
-    fields=[
-        ("select_owner", {
-            version.LOWEST: ui.Select("select#user_name"),
-            '5.7': ui.AngularSelect("user_name")
-        }),
-        ("select_group", {
-            version.LOWEST: ui.Select("select#group_name"),
-            '5.7': ui.AngularSelect("group_name")
-        }),
-    ])
+    configuration = Dropdown('Configuration')
+    policy_btn = Dropdown('Policy')
+    lifecycle_btn = Dropdown('Lifecycle')
+    download_choice = Dropdown('Download')
 
-edit_tags_form = Form(
-    fields=[
-        ("select_tag", ui.Select("select#tag_cat")),
-        ("select_value", ui.Select("select#tag_add"))
-    ])
+
+class ServiceRetirementForm(MyServicesView):
+    title = Text('#explorer_title_text')
+
+    retirement_date = Calendar('retirementDate')
+    retirement_warning = BootstrapSelect('retirement_warn')
+
+
+class ServiceEditForm(MyServicesView):
+    title = Text('#explorer_title_text')
+
+    name = Input(name='name')
+    description = Input(name='description')
+
+
+class SetOwnershipForm(MyServicesView):
+    title = Text('#explorer_title_text')
+
+    select_owner = BootstrapSelect('user_name')
+    select_group = BootstrapSelect('group_name')
+
+
+class EditTagsForm(MyServicesView):
+    title = Text('#explorer_title_text')
+
+    select_tag = BootstrapSelect('tag_cat')
+    select_value = BootstrapSelect('tag_add')
+
+
+class MyServiceDetailView(MyServicesView):
+    title = Text("#explorer_title_text")
+
+    properties_detail = SummaryTable(title='Properties')
+    lifecycle_detail = SummaryTable(title='Lifecycle')
+    relationships_detail = SummaryTable(title='Relationships')
+    vm_detail = SummaryTable(title='Totals for Service VMs ')
+    smart_mgmt_detail = SummaryTable(title='Smart Management')
+
+    @property
+    def is_displayed(self):
+        return (
+            self.in_myservices and self.myservice.is_opened and
+            self.title.text == 'Service "{}"'.format(self.context['object'].name)
+        )
+
+
+class EditMyServiceView(ServiceEditForm):
+    title = Text("#explorer_title_text")
+
+    save_button = Button('Save')
+    reset_button = Button('Reset')
+    cancel_button = Button('Cancel')
+
+    @property
+    def is_displayed(self):
+        return (
+            self.in_myservices and self.myservice.is_opened and
+            self.title.text == 'Editing Service "{}"'.format(self.context['object'].name)
+        )
+
+
+class SetOwnershipView(SetOwnershipForm):
+    title = Text("#explorer_title_text")
+
+    save_button = Button('Save')
+
+    @property
+    def is_displayed(self):
+        return (
+            self.in_myservices and self.myservice.is_opened and
+            self.title.text == 'Set Ownership of Service "{}"'.format(self.context['object'].name)
+        )
+
+
+class EditTagsView(EditTagsForm):
+    title = Text("#explorer_title_text")
+
+    save_button = Button('Save')
+
+    @property
+    def is_displayed(self):
+        return (
+            self.in_myservices and self.myservice.is_opened and
+            self.title.text == 'Edit Tags of Service "{}"'.format(self.context['object'].name)
+        )
+
+
+class ServiceRetirementView(ServiceRetirementForm):
+    title = Text("#explorer_title_text")
+
+    save_button = Button('Save')
+
+    @property
+    def is_displayed(self):
+        return (
+            self.in_myservices and self.myservice.is_opened and
+            self.myservice.tree.currently_selected == self.context['object'].name and
+            self.title.text == 'Set/Remove retirement date for Service')
+
+
+class ReconfigureServiceView(SetOwnershipForm):
+    title = Text("#explorer_title_text")
+
+    save_button = Button('Save')
+
+    @property
+    def is_displayed(self):
+        return (
+            self.in_myservices and self.myservice.is_opened and
+            self.title.text == 'Reconfigure Service "{}"'.format(self.context['object'].name)
+        )
 
 
 class MyService(Updateable, Navigatable):
-    """Create,Edit and Delete Button Groups
 
-    Args:
-        service_name: The name of service to retire.
-        vm_name: Name of vm in the service.
-        retirement_date: Date to retire service.
-    """
-
-    def __init__(self, service_name, vm_name=None, appliance=None):
-        self.service_name = service_name
-        self.vm_name = vm_name
+    def __init__(self, name, description=None, vm_name=None, appliance=None):
         Navigatable.__init__(self, appliance=appliance)
-
-    def get_detail(self, properties=None, icon_href=False):
-        """ Gets details from the details infoblock
-
-        Args:
-            *properties: An InfoBlock title, followed by the Key name e.g. "Relationships", "Images"
-            icon_href: Boolean indicating to return icon_href instead of text
-        Returns: A string representing the contents of the InfoBlock's value.
-        """
-        navigate_to(self, 'Details')
-        if icon_href:
-            return InfoBlock.icon_href(*properties)
-        else:
-            return InfoBlock.text(*properties)
-
-    def wait_for_vm_retire(self):
-        """ Waits for self.vm_name to go to the off state and returns state
-
-        Args:
-        Returns: A string representing the VM state
-        """
-        def get_vm_details():
-            power_state = InfoBlock.text('Power Management', 'Power State')
-            logger.debug('Service VM power state: {}'.format(power_state))
-            if power_state == 'unknown':
-                # The VM power state is unknown, check lifecycle instead of power
-                retire_state = InfoBlock.text('Lifecycle', 'Retirement state')
-                return retire_state == 'Retired'
-            else:
-                # VM power is a known state, use it
-                return power_state == 'off'
-
-        quadicon = Quadicon(self.vm_name, "vm")
-        sel.click(quadicon)
-
-        wait_for(
-            get_vm_details,
-            fail_func=tb.refresh,
-            num_sec=120 * 60,
-            delay=10,
-            message="Service VM power off wait"
-        )
-        return InfoBlock.text('Power Management', 'Power State')
+        self.name = name
+        self.description = description
+        self.vm_name = vm_name
 
     def retire(self):
-        navigate_to(self, 'Details')
-        life_btn("Retire this Service", invokes_alert=True)
-        sel.handle_alert()
-        flash.assert_success_message(
-            'Retirement initiated for 1 Service from the {} Database'.format(
-                current_appliance.product_name)
-        )        # wait for service to retire
+        view = navigate_to(self, 'Details')
+        view.lifecycle_btn.item_select("Retire this Service", handle_alert=True)
+        view.flash.assert_no_error()
+        if self.appliance.version < '5.8':
+            view.flash.assert_success_message(
+                'Retirement initiated for 1 Service from the {} Database'.format(
+                    current_appliance.product_name))
+        # wait for service to retire
         wait_for(
-            lambda: self.get_detail(properties=('Lifecycle', 'Retirement State')) == 'Retired',
+            lambda: view.lifecycle_detail.get_text_of("Retirement State") == 'Retiring',
             fail_func=tb.refresh,
             num_sec=10 * 60,
             delay=3,
             message='Service Retirement wait'
         )
-        # wait for any vms to shutdown, this method leaves the page on the VM details
-        if self.vm_name is not None:
-            vm_state = self.wait_for_vm_retire()
-            assert vm_state in ['off', 'unknown']
 
     def retire_on_date(self, retirement_date):
-        navigate_to(self, 'SetRetirement')
-        fill(retirement_form, {'retirement_date': retirement_date},
-             action=form_buttons.save)
-        navigate_to(self, 'Details')
+        view = navigate_to(self, 'SetRetirement')
+        view.retirement_date.fill(retirement_date)
+        view.save_button.click()
+        view = navigate_to(self, 'Details')
         wait_for(
-            lambda: InfoBlock.text('Lifecycle', 'Retirement State') == 'Retiring',
+            lambda: view.lifecycle_detail.get_text_of("Retirement State") == 'Retiring',
             fail_func=tb.refresh,
             num_sec=5 * 60,
             delay=5,
             message='Service Retirement'
         )
-        # wait for any vms to shutdown, this method leaves the page on the VM details
-        vm_state = self.wait_for_vm_retire()
-        assert vm_state in ['off', 'unknown']
 
     def update(self, updates):
-        navigate_to(self, 'Edit')
-        updated_name = updates.get('service_name', self.service_name + '_edited')
-        updated_description = updates.get('description', 'Updated description')
-        fill(edit_service_form, {'name': updated_name,
-                                 'description': updated_description},
-             action=form_buttons.angular_save)
-        wait_for(flash.get_messages, timeout=10, delay=2, fail_condition=[], fail_func=tb.refresh())
-        if flash.assert_success_message('Service "{}" was saved'.format(updated_name)):
-            setattr(self, 'service_name', updated_name)
+        view = navigate_to(self, 'Edit')
+        changed = view.fill_with(updates, on_change=view.save_button, no_change=view.cancel_button)
+        view.flash.assert_no_error()
+        if changed:
+            view.flash.assert_success_message('Service "{}" was saved'.format
+            (updates.get('name', self.name)))
+        else:
+            view.flash.assert_success_message('Edit of Service "{}" was cancelled by the \
+                user'.format(updates.get('description', self.description)))
+        view = self.create_view(MyServiceDetailView, override=updates)
+        assert view.is_displayed
 
     def exists(self):
         try:
@@ -166,116 +206,122 @@ class MyService(Updateable, Navigatable):
             return False
 
     def delete(self):
-        navigate_to(self, 'Details')
-        cfg_btn(
+        view = navigate_to(self, 'Details')
+        view.configuration.item_select(
             version.pick({
                 version.LOWEST: 'Remove Service from the VMDB',
                 '5.7': 'Remove Service'}),
-            invokes_alert=True)
-        sel.handle_alert()
-        flash.assert_success_message('Service "{}": Delete successful'.format(self.service_name))
+            handle_alert=True)
+        view = self.create_view(MyServicesView)
+        view.flash.assert_no_error()
+        assert view.is_displayed
+        view.flash.assert_success_message(
+            'Service "{}": Delete successful'.format(self.name))
 
     def set_ownership(self, owner, group):
-        navigate_to(self, 'SetOwnership')
-        fill(set_ownership_form, {'select_owner': owner,
-                                  'select_group': group},
-             action=form_buttons.save)
-        flash.assert_success_message('Ownership saved for selected Service')
+        view = navigate_to(self, 'SetOwnership')
+        view.fill({'select_owner': owner,
+                   'select_group': group})
+        view.save_button.click()
+        view = self.create_view(MyServiceDetailView)
+        assert view.is_displayed
+        view.flash.assert_no_error()
+        view.flash.assert_success_message('Ownership saved for selected Service')
 
     def edit_tags(self, tag, value):
-        navigate_to(self, 'EditTags')
-        fill(edit_tags_form, {'select_tag': tag,
-                              'select_value': value},
-             action=form_buttons.save)
-        flash.assert_success_message('Tag edits were successfully saved')
+        view = navigate_to(self, 'EditTags')
+        view.fill({'select_tag': tag,
+                   'select_value': value})
+        view.save_button.click()
+        view = self.create_view(MyServiceDetailView)
+        assert view.is_displayed
+        view.flash.assert_no_error()
+        view.flash.assert_success_message('Tag edits were successfully saved')
 
     def check_vm_add(self, add_vm_name):
-        navigate_to(self, 'Details')
+        view = navigate_to(self, 'Details')
+        # TODO - replace Quadicon later
         quadicon = Quadicon(add_vm_name, "vm")
         sel.click(quadicon)
-        flash.assert_no_errors()
-
-    def reconfigure_service(self):
-        navigate_to(self, 'Reconfigure')
+        view.flash.assert_no_error()
 
     def download_file(self, extension):
-        navigate_to(self, 'All')
-        down_btn("Download as " + extension)
-
-    @property
-    def all_services_label(self):
-        if self.appliance.version < '5.8':
-            return 'All Services'
-        else:
-            return 'Active Services'
+        view = navigate_to(self, 'All')
+        view.download_choice.item_select("Download as " + extension)
+        view.flash.assert_no_error()
 
 
 @navigator.register(MyService, 'All')
 class MyServiceAll(CFMENavigateStep):
-    prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
+    VIEW = MyServicesView
 
-    def am_i_here(self):
-        return match_page(summary=self.obj.all_services_label)
+    prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
 
     def step(self, *args, **kwargs):
         self.prerequisite_view.navigation.select('Services', 'My Services')
 
-    def resetter(self, *args, **kwargs):
-        if self.obj.appliance.version < '5.7':
-            my_service_tree().click_path(self.obj.all_services_label)
-        tb.refresh()
-
 
 @navigator.register(MyService, 'Details')
 class MyServiceDetails(CFMENavigateStep):
+    VIEW = MyServiceDetailView
+
     prerequisite = NavigateToSibling('All')
 
-    def am_i_here(self):
-        return match_page(summary='Service "{}"'.format(self.obj.service_name))
-
-    def step(self, *args, **kwargs):
-        logger.debug('Clicking tree for service: {}'.format(self.obj.service_name))
-        my_service_tree().click_path(self.obj.all_services_label, self.obj.service_name)
-
-    def resetter(self, *args, **kwargs):
-        tb.refresh()
+    def step(self):
+        if self.appliance.version > '5.8':
+            self.prerequisite_view.myservice.tree.click_path("Active Services", self.obj.name)
+        else:
+            self.prerequisite_view.myservice.tree.click_path("All Services", self.obj.name)
 
 
 @navigator.register(MyService, 'Edit')
 class MyServiceEdit(CFMENavigateStep):
+    VIEW = EditMyServiceView
+
     prerequisite = NavigateToSibling('Details')
 
-    def step(self, *args, **kwargs):
-        cfg_btn('Edit this Service')
+    def step(self):
+        self.prerequisite_view.configuration.item_select('Edit this Service')
 
 
 @navigator.register(MyService, 'SetOwnership')
 class MyServiceSetOwnership(CFMENavigateStep):
+    VIEW = SetOwnershipView
+
     prerequisite = NavigateToSibling('Details')
 
-    def step(self, *args, **kwargs):
-        cfg_btn('Set Ownership')
+    def step(self):
+        self.prerequisite_view.configuration.item_select('Set Ownership')
 
 
 @navigator.register(MyService, 'EditTags')
 class MyServiceEditTags(CFMENavigateStep):
+    VIEW = EditTagsView
+
     prerequisite = NavigateToSibling('Details')
 
-    def step(self, *args, **kwargs):
-        pol_btn('Edit Tags')
+    def step(self):
+        self.prerequisite_view.policy_btn.item_select('Edit Tags')
 
 
 @navigator.register(MyService, 'SetRetirement')
 class MyServiceSetRetirement(CFMENavigateStep):
+    VIEW = ServiceRetirementView
+
     prerequisite = NavigateToSibling('Details')
 
-    def step(self, *args, **kwargs):
-        life_btn('Set Retirement Date')
+    def step(self):
+        if self.appliance.version > '5.8':
+            self.prerequisite_view.lifecycle_btn.item_select('Set Retirement Date for this Service')
+        else:
+            self.prerequisite_view.lifecycle_btn.item_select('Set Retirement Date')
 
 
 @navigator.register(MyService, 'Reconfigure')
 class MyServiceReconfigure(CFMENavigateStep):
+    VIEW = ReconfigureServiceView
+
     prerequisite = NavigateToSibling('Details')
 
-    def step(self, *args, **kwargs):
-        cfg_btn('Reconfigure this Service')
+    def step(self):
+        self.prerequisite_view.configuration.item_select('Reconfigure this Service')
