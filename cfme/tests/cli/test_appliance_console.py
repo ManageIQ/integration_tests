79a76df3337bb595086de6ce9308a9a8a497a8fe
Luke Couzens <lcouzens@redhat.com>
2017-04-07 09:30:58 +0100

    Adding tests for scap

--- cfme/tests/cli/test_appliance_console.py
+++ cfme/tests/cli/test_appliance_console.py
@@ -239,89 +239,133 @@ def test_black_console_external_auth_all(app_creds, ipa_crud):
     evm_tail.validate_logs()
 
 
-def test_black_console_scap_accounts(temp_appliance_preconfig_funcscope):
+def test_black_console_scap_accounts(appliance_pre_scap):
     """'ap' launches appliance_console, '' clears info screen, '14/17' Hardens appliance using SCAP
     configuration, '' complete."""
 
-    if temp_appliance_preconfig_funcscope.version >= "5.8":
-        command_set = ('ap', '', '10', '')
+    if appliance_pre_scap.version >= "5.8":
+        command_set = ('ap', '', '14', '')
     else:
         command_set = ('ap', '', '17', '')
-    temp_appliance_preconfig_funcscope.appliance_console.run_commands(command_set)
+    appliance_pre_scap.appliance_console.run_commands(command_set)
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/pam.d/postlogin | grep 'pam_lastlog.so silent noupdate showfailed'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/security/pwquality.conf | grep 'dcredit = -1'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/security/pwquality.conf | grep 'ucredit = -1'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/security/pwquality.conf | grep 'ocredit = -1'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/security/pwquality.conf | grep 'lcredit = -1'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/security/pwquality.conf | grep 'difok = 4'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/default/useradd | grep 'INACTIVE=35'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/login.defs | grep 'PASS_MAX_DAYS     60'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/login.defs | grep 'PASS_MIN_DAYS     1'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/login.defs | grep 'PASS_MIN_LEN     14'")
 
 
-def test_black_console_scap_audit(temp_appliance_preconfig_funcscope):
+def test_black_console_scap_audit(appliance_pre_scap):
     """'ap' launches appliance_console, '' clears info screen, '14/17' Hardens appliance using SCAP
     configuration, '' complete."""
 
-    if temp_appliance_preconfig_funcscope.version >= "5.8":
-        command_set = ('ap', '', '10', '')
+    if appliance_pre_scap.version >= "5.8":
+        command_set = ('ap', '', '14', '')
     else:
         command_set = ('ap', '', '17', '')
-    temp_appliance_preconfig_funcscope.appliance_console.run_commands(command_set)
+    appliance_pre_scap.appliance_console.run_commands(command_set)
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/security/limits.conf  | grep '* hard core 0'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/audit/audit.rules | grep '-w /etc/localtime -p wa -k audit_time_rules'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/audit/audit.rules | grep '-w /etc/group -p wa -k audit_account_changes'")
+-w /etc/passwd -p wa -k audit_account_changes
+-w /etc/gshadow -p wa -k audit_account_changes
+-w /etc/shadow -p wa -k audit_account_changes
+-w /etc/security/opasswd -p wa -k audit_account_changes
+-w /etc/selinux/ -p wa -k MAC-policy
+-w /etc/sudoers -p wa -k actions
+-w /sbin/insmod -p x -k modules
+-w /sbin/rmmod -p x -k modules
+-w /sbin/modprobe -p x -k modules
+-a always,exit -F arch=b64 -S init_module -S delete_module -k modules
+-a always,exit -F arch=b64 -S settimeofday -S clock_settime -k audit_time_rules
+-a always,exit -F arch=b64 -S adjtimex -k audit_time_rules
+-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=500 -F auid!=4294967295 -k access
+-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=500 -F auid!=4294967295 -k access
+'")
+    assert appliance_pre_scap.ssh_client.run_command(
+        "cat /etc/login.defs | grep 'PASS_MIN_LEN     14'")
 
 
-def test_black_console_scap_boot(temp_appliance_preconfig_funcscope):
+
+def test_black_console_scap_boot(appliance_pre_scap):
     """'ap' launches appliance_console, '' clears info screen, '14/17' Hardens appliance using SCAP
     configuration, '' complete."""
 
-    if temp_appliance_preconfig_funcscope.version >= "5.8":
-        command_set = ('ap', '', '10', '')
+    if appliance_pre_scap.version >= "5.8":
+        command_set = ('ap', '', '14', '')
     else:
         command_set = ('ap', '', '17', '')
-    temp_appliance_preconfig_funcscope.appliance_console.run_commands(command_set)
+    appliance_pre_scap.appliance_console.run_commands(command_set)
 
 
-def test_black_console_scap_user(temp_appliance_preconfig_funcscope):
+def test_black_console_scap_user(appliance_pre_scap):
     """'ap' launches appliance_console, '' clears info screen, '14/17' Hardens appliance using SCAP
     configuration, '' complete."""
 
-    if temp_appliance_preconfig_funcscope.version >= "5.8":
-        command_set = ('ap', '', '10', '')
+    if appliance_pre_scap.version >= "5.8":
+        command_set = ('ap', '', '14', '')
     else:
         command_set = ('ap', '', '17', '')
-    temp_appliance_preconfig_funcscope.appliance_console.run_commands(command_set)
+    appliance_pre_scap.appliance_console.run_commands(command_set)
 
 
-def test_black_console_scap_sshd(temp_appliance_preconfig_funcscope):
+def test_black_console_scap_sshd(appliance_pre_scap):
     """'ap' launches appliance_console, '' clears info screen, '14/17' Hardens appliance using SCAP
     configuration, '' complete."""
 
-    if temp_appliance_preconfig_funcscope.version >= "5.8":
-        command_set = ('ap', '', '10', '')
+    if appliance_pre_scap.version >= "5.8":
+        command_set = ('ap', '', '14', '')
     else:
         command_set = ('ap', '', '17', '')
-    temp_appliance_preconfig_funcscope.appliance_console.run_commands(command_set)
+    appliance_pre_scap.appliance_console.run_commands(command_set)
 
 
-def test_black_console_scap_kernel(temp_appliance_preconfig_funcscope):
+def test_black_console_scap_kernel(appliance_pre_scap):
     """'ap' launches appliance_console, '' clears info screen, '14/17' Hardens appliance using SCAP
     configuration, '' complete."""
 
-    if temp_appliance_preconfig_funcscope.version >= "5.8":
-        command_set = ('ap', '', '10', '')
+    if appliance_pre_scap.version >= "5.8":
+        command_set = ('ap', '', '14', '')
     else:
         command_set = ('ap', '', '17', '')
-    temp_appliance_preconfig_funcscope.appliance_console.run_commands(command_set)
+    appliance_pre_scap.appliance_console.run_commands(command_set)
 
 
-def test_black_console_scap_ipv6(temp_appliance_preconfig_funcscope):
+def test_black_console_scap_ipv6(appliance_pre_scap):
     """'ap' launches appliance_console, '' clears info screen, '14/17' Hardens appliance using SCAP
     configuration, '' complete."""
 
-    if temp_appliance_preconfig_funcscope.version >= "5.8":
-        command_set = ('ap', '', '10', '')
+    if appliance_pre_scap.version >= "5.8":
+        command_set = ('ap', '', '14', '')
     else:
         command_set = ('ap', '', '17', '')
-    temp_appliance_preconfig_funcscope.appliance_console.run_commands(command_set)
+    appliance_pre_scap.appliance_console.run_commands(command_set)
 
 
-def test_black_console_scap_ipv4(temp_appliance_preconfig_funcscope):
+def test_black_console_scap_ipv4(appliance_pre_scap):
     """'ap' launches appliance_console, '' clears info screen, '14/17' Hardens appliance using SCAP
     configuration, '' complete."""
 
-    if temp_appliance_preconfig_funcscope.version >= "5.8":
-        command_set = ('ap', '', '10', '')
+    if appliance_pre_scap.version >= "5.8":
+        command_set = ('ap', '', '14', '')
     else:
         command_set = ('ap', '', '17', '')
-    temp_appliance_preconfig_funcscope.appliance_console.run_commands(command_set)
+    appliance_pre_scap.appliance_console.run_commands(command_set)
