a1f62cb466db9cbc8045945fc045be232aa6cff7
Matous Mojzis <mmojzis@redhat.com>
2017-04-10 17:24:54 +0200

    Python3 print statements

--- scripts/ec2cleanup.py
+++ scripts/ec2cleanup.py
@@ -3,7 +3,6 @@ import sys
 from datetime import datetime
 from tabulate import tabulate
 
-from utils.log import logger
 from utils.path import log_path
 from utils.providers import list_provider_keys, get_mgmt
 
@@ -33,19 +32,19 @@ def delete_disassociated_addresses(provider_mgmt, excluded_eips, output):
         for ip in provider_mgmt.get_all_disassociated_addresses():
             if ip.allocation_id:
                 if excluded_eips and ip.allocation_id in excluded_eips:
-                    print "  Excluding allocation ID: {}".format(ip.allocation_id)  # noqa
+                    print("  Excluding allocation ID: {}".format(ip.allocation_id))
                     continue
                 else:
                     ip_list.append([provider_name, ip.public_ip, ip.allocation_id])
                     provider_mgmt.release_vpc_address(alloc_id=ip.allocation_id)
             else:
                 if excluded_eips and ip.public_ip in excluded_eips:
-                    print "  Excluding IP: {}".format(ip.public_ip)  # noqa
+                    print("  Excluding IP: {}".format(ip.public_ip))
                     continue
                 else:
                     ip_list.append([provider_name, ip.public_ip, 'N/A'])
                     provider_mgmt.release_address(address=ip.public_ip)
-        print "  Released Addresses: {}".format(ip_list)  # noqa
+        print("  Released Addresses: {}".format(ip_list))
         with open(output, 'a+') as report:
             if ip_list:
                 # tabulate ip_list and write it
@@ -63,12 +62,12 @@ def delete_unattached_volumes(provider_mgmt, excluded_volumes, output):
     try:
         for volume in provider_mgmt.get_all_unattached_volumes():
             if excluded_volumes and volume.id in excluded_volumes:
-                print "  Excluding volume id: {}".format(volume.id)  # noqa
+                print("  Excluding volume id: {}".format(volume.id))
                 continue
             else:
                 volume_list.append([provider_name, volume.id])
                 volume.delete()
-        print "  Deleted Volumes: {}".format(volume_list)  # noqa
+        print("  Deleted Volumes: {}".format(volume_list))
         with open(output, 'a+') as report:
             if volume_list:
                 # tabulate volume_list and write it
@@ -85,12 +84,12 @@ def delete_unused_loadbalancers(provider_mgmt, excluded_elbs, output):
     try:
         for elb in provider_mgmt.get_all_unused_loadbalancers():
             if excluded_elbs and elb.name in excluded_elbs:
-                print "  Excluding Elastic LoadBalancer id: {}".format(elb.name)  # noqa
+                print("  Excluding Elastic LoadBalancer id: {}".format(elb.name))
                 continue
             else:
                 elb_list.append([provider_name, elb.name])
                 provider_mgmt.delete_loadbalancer(loadbalancer=elb)
-        print "  Deleted Elastic LoadBalancers: {}".format(elb_list)  # noqa
+        print("  Deleted Elastic LoadBalancers: {}".format(elb_list))
         with open(output, 'a+') as report:
             if elb_list:
                 # tabulate volume_list and write it
@@ -107,12 +106,12 @@ def delete_unused_network_interfaces(provider_mgmt, excluded_enis, output):
     try:
         for eni in provider_mgmt.get_all_unused_network_interfaces():
             if excluded_enis and eni.id in excluded_enis:
-                print "  Excluding Elastic Network Interface id: {}".format(eni.id)  # noqa
+                print("  Excluding Elastic Network Interface id: {}".format(eni.id))
                 continue
             else:
                 eni_list.append([provider_name, eni.id])
                 eni.delete()
-        print "  Deleted Elastic Network Interfaces: {}".format(eni_list)  # noqa
+        print("  Deleted Elastic Network Interfaces: {}".format(eni_list))
         with open(output, 'a+') as report:
             if eni_list:
                 # tabulate volume_list and write it
@@ -129,20 +128,20 @@ def ec2cleanup(exclude_volumes, exclude_eips, exclude_elbs, exclude_enis, output
         report.write("\nDate: {}\n".format(datetime.now()))
     for provider_key in list_provider_keys('ec2'):
         provider_mgmt = get_mgmt(provider_key)
-        print "----- Provider: {} -----".format(provider_key)  # noqa
-        print "Deleting volumes..."  # noqa
+        print("----- Provider: {} -----".format(provider_key))
+        print("Deleting volumes...")
         delete_unattached_volumes(provider_mgmt=provider_mgmt,
                                   excluded_volumes=exclude_volumes,
                                   output=output)
-        print "Deleting Elastic LoadBalancers..."  # noqa
+        print("Deleting Elastic LoadBalancers...")
         delete_unused_loadbalancers(provider_mgmt=provider_mgmt,
                                   excluded_elbs=exclude_elbs,
                                   output=output)
-        print "Deleting Elastic Network Interfaces..."  # noqa
+        print("Deleting Elastic Network Interfaces...")
         delete_unused_network_interfaces(provider_mgmt=provider_mgmt,
                                     excluded_enis=exclude_enis,
                                     output=output)
-        print "Releasing addresses..."  # noqa
+        print("Releasing addresses...")
         delete_disassociated_addresses(provider_mgmt=provider_mgmt,
                                        excluded_eips=exclude_eips,
                                        output=output)
