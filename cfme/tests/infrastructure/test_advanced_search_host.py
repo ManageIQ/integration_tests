1d5bd7f6d361a248d205510440809b9fa51caddb
Oleksandr Kolisnyk <okolisny@redhat.com>
2017-04-20 11:36:39 +0300

    actualized a find_quadicon call with new parameters

--- cfme/tests/infrastructure/test_advanced_search_host.py
+++ cfme/tests/infrastructure/test_advanced_search_host.py
@@ -25,7 +25,8 @@ def hosts_with_vm_count(hosts):
     """Returns a list of tuples (hostname, vm_count)"""
     hosts_with_vm_count = []
     for host_name in hosts:
-        hosts_with_vm_count.append((host_name, int(host.find_quadicon(host_name, True).no_vm)))
+        h = Host(host_name)
+        hosts_with_vm_count.append((host_name, int(host.find_quadicon(h, True).no_vm)))
     return sorted(hosts_with_vm_count, key=lambda tup: tup[1])
 
 
