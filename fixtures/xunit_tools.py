dc3566aac4fd29a87b810837ab518fa1df0d7251
Pete Savage <psavage@redhat.com>
2017-04-20 19:15:31 +0100

    Added new xUnit exporter tool
    * --generate-xmls is a new option that will generate two xml files
        during collection time.

---
+++ fixtures/xunit_tools.py
@@ -0,0 +1,252 @@
+import pytest
+
+from collections import defaultdict
+from lxml import etree
+import re
+from utils.conf import xunit
+from utils.pytest_shortcuts import extract_fixtures_values
+
+
+default_custom_fields = {
+    "caseautomation": "automated",
+    "caseimportance": "high",
+    "caselevel": "component",
+    "caseposneg": "positive",
+    "testtype": "functional",
+    "subtype1": "-",
+    "subtype2": "-"
+}
+
+
+def pytest_addoption(parser):
+    # Create the cfme option group for use in other plugins
+    parser.getgroup('cfme')
+    parser.addoption("--generate-xmls", action="store_true", default=False,
+        help="generate the xml files for import")
+
+
+def testcase_gen(
+        test_name, description=None, parameters=None, custom_fields=None, linked_items=None):
+    linked_items = linked_items or []
+    custom_fields_update = custom_fields or {}
+    custom_fields = default_custom_fields.copy()
+    custom_fields.update(custom_fields_update)
+    parameters = parameters or []
+    testcase = etree.Element('testcase', id=test_name)
+    title = etree.Element('title')
+    title.text = test_name
+    description_el = etree.Element('description')
+    description_el.text = description or ""
+    testcase.append(title)
+    testcase.append(description_el)
+    test_steps = etree.Element('test-steps')
+    test_step = etree.Element('test-step')
+    test_step_col = etree.Element('test-step-column', id="step")
+    for param in parameters:
+        param_el = etree.Element('parameter', name=param, scope="local")
+        test_step_col.append(param_el)
+    test_step.append(test_step_col)
+    test_steps.append(test_step)
+    testcase.append(test_steps)
+    custom_fields_el = etree.Element('custom-fields')
+    for id, content in custom_fields.iteritems():
+        custom_field = etree.Element('custom-field', id=id, content=content)
+        custom_fields_el.append(custom_field)
+    testcase.append(custom_fields_el)
+    if linked_items:
+        work_items = etree.Element('linked-work-items')
+        for work_item in linked_items:
+            work_item_el = etree.Element('linked-work-item')
+            work_item_el.attrib['workitem-id'] = work_item['id']
+            work_item_el.attrib['role-id'] = work_item['role']
+            work_items.append(work_item_el)
+        testcase.append(work_items)
+    return testcase
+
+
+prop_dict = {
+    'testrun-template-id': xunit['testrun_template_id'],
+    'testrun-title': xunit['testrun_title'],
+    'testrun-id': xunit['testrun_id'],
+    'project-id': xunit['project_id'],
+    'dry-run': xunit['dry_run'],
+    'testrun-status-id': xunit['testrun_status_id'],
+    'lookup-method': xunit['lookup_method']
+}
+
+
+def testresult_gen(test_name, parameters=None, result=None):
+    testcase = etree.Element('testcase', name=test_name)
+    extra = None
+    if result == "skipped" or not result:
+        extra = etree.Element('skipped', message='Skipped', type='skipped')
+        testcase.append(extra)
+    elif result == "error":
+        extra = etree.Element('error', message="Error", type='error')
+        testcase.append(extra)
+    elif result == "failed":
+        extra = etree.Element('failure', message="Failed", type='failure')
+        testcase.append(extra)
+    properties = etree.Element('properties')
+    testcase_id = etree.Element('property', name="polarion-testcase-id", value=test_name)
+    properties.append(testcase_id)
+    for param, value in parameters.iteritems():
+        param_el = etree.Element(
+            'property', name="polarion-parameter-{}".format(param), value=value)
+        properties.append(param_el)
+    testcase.append(properties)
+    return testcase
+
+
+def testrun_gen(tests, filename, collectonly=True):
+    testsuites = etree.Element("testsuites")
+    testsuite = etree.Element("testsuite")
+    properties = etree.Element("properties")
+    property_resp = etree.Element(
+        'property', name='polarion-response-{}'.format(
+            xunit['response']['id']), value=xunit['response']['value'])
+    properties.append(property_resp)
+    for prop_name, prop_value in prop_dict.iteritems():
+        prop_el = etree.Element(
+            'property', name="polarion-{}".format(prop_name), value=str(prop_value))
+        properties.append(prop_el)
+    testsuites.append(properties)
+    testsuites.append(testsuite)
+
+    no_tests = 0
+    results_count = {
+        'passed': 0,
+        'skipped': 0,
+        'failure': 0,
+        'error': 0
+    }
+    for data in tests:
+        no_tests += 1
+        if collectonly:
+            testsuite.append(testresult_gen(data['name'], data['params']))
+            results_count['skipped'] += 1
+        else:
+            testsuite.append(testresult_gen(data['name'], data['params'], result=data['result']))
+            results_count[data['result']] += 1
+    testsuite.attrib['tests'] = str(no_tests)
+    testsuite.attrib['failures'] = str(results_count['failure'])
+    testsuite.attrib['skipped'] = str(results_count['skipped'])
+    testsuite.attrib['errors'] = str(results_count['error'])
+    testsuite.attrib['name'] = "cfme-tests"
+    xml = etree.ElementTree(testsuites)
+    xml.write(filename, pretty_print=True)
+
+
+def get_name(obj):
+    if hasattr(obj, '_param_name'):
+        return getattr(obj, '_param_name')
+    elif hasattr(obj, 'name'):
+        return obj.name
+    else:
+        return str(obj)
+
+
+caselevels = {
+    '0': 'component',
+    '1': 'integration',
+    '2': 'system',
+    '3': 'acceptance'
+}
+
+
+@pytest.mark.trylast
+def pytest_collection_modifyitems(session, config, items):
+    if not config.getoption('generate_xmls'):
+        return
+    a = defaultdict(dict)
+    ntr = []
+    for item in items:
+        a[item.location[0]][re.sub('\[.*\]', '', item.location[2])] = a[item.location[0]].get(
+            re.sub('\[.*\]', '', item.location[2]), 0) + 1
+    with open('duplicates.log', 'w') as f:
+        for module, tests in a.iteritems():
+            for test in tests:
+                if test not in ntr:
+                    ntr.append(test)
+                else:
+                    f.write("{}\n".format(test))
+
+    test_name = []
+
+    testcases = etree.Element("testcases")
+    testcases.attrib['project-id'] = xunit['project_id']
+    response_properties = etree.Element("response-properties")
+    response_property = etree.Element(
+        "response-property", name=xunit['response']['id'], value=xunit['response']['value'])
+    response_properties.append(response_property)
+    properties = etree.Element("properties")
+    lookup = etree.Element("property", name="lookup-method", value="custom")
+    properties.append(lookup)
+    testcases.append(response_properties)
+    testcases.append(properties)
+
+    for item in items:
+        work_items = []
+        custom_fields = {}
+        try:
+            description = item.function.func_doc
+        except:
+            description = ""
+        try:
+            requirement = item.get_marker('requirement').args[0]
+            from utils.conf import cfme_data
+            requirement_id = cfme_data['requirements'][requirement]
+            work_items.append({'id': requirement_id, 'role': 'verifies'})
+        except:
+            pass
+        try:
+            tier = item.get_marker('tier').args[0]
+            tier_id = caselevels[str(tier)]
+            custom_fields['caselevel'] = tier_id
+        except:
+            pass
+
+        param_list = extract_fixtures_values(item).keys()
+
+        try:
+            automated = item.get_marker('automated').args[0]
+        except:
+            automated = True
+        if automated:
+            # The master here should probably link the latest "commit" eventually
+            automation_script = 'http://github.com/{}/{}/blob/master/{}#L{}'.format(
+                xunit['gh_owner'],
+                xunit['gh_repo'],
+                item.location[0],
+                item.function.func_code.co_firstlineno
+            )
+            custom_fields['caseautomation'] = "automated"
+            custom_fields['automation_script'] = automation_script
+        description = '{}<br/><br/><a href="{}">Test Source</a>'.format(
+            description, automation_script)
+
+        name = item.name
+        name = re.sub('\[.*\]', '', name)
+        if name not in test_name:
+            test_name.append(name)
+            testcases.append(
+                testcase_gen(
+                    name,
+                    description=description,
+                    parameters=param_list,
+                    linked_items=work_items,
+                    custom_fields=custom_fields))
+
+    xml = etree.ElementTree(testcases)
+    xml.write('test_case_import.xml', pretty_print=True)
+
+    tests = []
+    for item in items:
+        name = re.sub('\[.*\]', '', item.name)
+        try:
+            params = item.callspec.params
+            param_dict = {p: get_name(v) for p, v in params.iteritems()}
+        except:
+            param_dict = {}
+        tests.append({'name': name, 'params': param_dict, 'result': None})
+    testrun_gen(tests, 'test_run_import.xml', collectonly=True)
