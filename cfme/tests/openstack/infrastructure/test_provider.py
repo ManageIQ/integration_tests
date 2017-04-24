4363ef7f1c4a50f7f20b9da7fa1adf909084db84
Oleksandr Kolisnyk <okolisny@redhat.com>
2017-04-24 13:29:16 +0300

    minor change

--- cfme/tests/openstack/infrastructure/test_provider.py
+++ cfme/tests/openstack/infrastructure/test_provider.py
@@ -3,7 +3,7 @@
 import pytest
 
 from cfme.infrastructure.provider.openstack_infra import OpenstackInfraProvider
-from cfme.web_ui import Quadicon, toolbar as tb
+from cfme.web_ui import Quadicon
 from utils import testgen
 from utils.appliance.implementations.ui import navigate_to
 
@@ -27,6 +27,6 @@ def test_credentials_quads(provider):
 
 def test_delete_provider(provider):
     provider.delete(cancel=False)
-    tb.refresh()
+    provider.wait_for_delete()
     navigate_to(provider, 'All')
     assert provider.name not in [q.name for q in Quadicon.all()]
