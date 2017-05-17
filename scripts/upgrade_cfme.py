b7b0e81c3e8a04722d631b9b9970e20a7c009dbc
Shalom Naim <snaim@redhat.com>
2017-05-17 16:57:25 +0300

    script is worknig with logging

--- scripts/upgrade_cfme.py
+++ scripts/upgrade_cfme.py
@@ -3,62 +3,104 @@ import yaml
 import time
 import threading
 import requests
-import logging
-import datetime
+from datetime import datetime
 from utils.appliance import IPAppliance
+import logging
+
+# setting script logging object
+# logging all the event to script and progress messages to screen
+formatter = "[%(asctime)s][%(levelname)s][%(funcName)s] - %(message)s"
+log_name = "cfme-upgrade-{date}.log".format(date=datetime.now().strftime("%y:%m:%d::%H:%M:%y"))
+logging.basicConfig(filename=log_name, filemode="w",
+                    level=logging.DEBUG, format=formatter)
+logger = logging.getLogger("cfme_logger")
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(logging.Formatter(formatter))
+logger.addHandler(console_handler)
 
 
-class cfme_upgrade_maneger(IPAppliance):
+class cfmeUpgradeManeger(IPAppliance):
 
     def __init__(self, appliance_ip, repo_list, dest_version):
-        super(cfme_upgrade_maneger, self).__init__(address=appliance_ip)
+        super(cfmeUpgradeManeger, self).__init__(address=appliance_ip)
         self.repo_list = repo_list
         self.dest_version = dest_version
 
-        self.logger = logging.basicConfig(
-            filename='/log/cfme-upgrade-{date}.log'.format(
-                date=datetime.datetime.now().strftime("%d/%m/%y-%H:%M:%S")),
-            level=logging.INFO)
-        self.logger.getLogger().addHandler(logging.StreamHandler())
     def exec_commaned(self, cmd, expect_failure=False, ignore_failure=False):
+        '''
+        execute command on the appliance and logging the command with it output to log file
+        :param cmd: command
+        :param expect_failure: is the command have to fail
+        :param ignore_failure: not raise exception on command failure
+        :return: command results
+        '''
 
+        logger.debug("exec_commaned: {cmd}".format(cmd=cmd))
         result = self.ssh_client.run_command(cmd)
-        if bool(result.rc) != expect_failure and not ignore_failure:
-            raise RuntimeError(
-                "command {cmd} failed!!\n\n\n{command_output}".format(cmd=cmd,
-                                                                      command_output=result.output))
+        try:
+            if bool(result.rc) != expect_failure and not ignore_failure:
+                exception_msg = "command {cmd} failed!!\n\n\n" \
+                                "{command_output}".format(cmd=cmd,
+                                                          command_output=result.output)
+                raise RuntimeError(exception_msg)
+        except Exception as ex:
+            logger.exception(ex.message)
+
+        logger.debug(result.output)
         return result
 
     def add_yum_repo(self):
-        print "adding repositories to yum"
+        '''
+        setting all yumrepos before starting the ugrade
+        :return: None
+        '''
+
+        logger.info("adding repositories to yum")
         for curr_repo_url in self.repo_list:
             # Add all the repos to yum repo
             self.exec_commaned("yum-config-manager "
                                "--add-repo {repo_url}".format(repo_url=curr_repo_url))
 
     def update_yum(self):
-
+        '''
+        initiate upgrading process
+        :return: None
+        '''
         def monitor_yum():
+            '''
+            give real time monitoring on yum update (give the user experience
+            that the script is still running)
+            :return: None
+            '''
+
             time.sleep(5)
             # do as long yum is running on the appliance
             res = self.exec_commaned(""" ps -ef | grep "yum update" | grep -v grep """,
                                      ignore_failure=True)
             while not bool(res.rc):
-                print "yum still running"
+                logger.info("yum still running")
                 time.sleep(10)
                 res = self.exec_commaned(""" ps -ef | grep "yum update" | grep -v grep """,
                                          ignore_failure=True)
 
-        print "importing all gpg keys for yum repositories"
+        logger.info("importing all gpg keys for yum repositories")
         self.ssh_client.run_command("rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-*")
 
+        # start thr monitoring thread
         monitor = threading.Thread(target=monitor_yum)
         monitor.start()
-        print "initiating yum update"
+
+        logger.info("initiating yum update")
         self.ssh_client.run_command("yum update -y")
 
     def stop_cfme(self):
-        print "stopping the cfme engine (evmserverd)"
+        '''
+        stop CFME engine process
+        :return:
+        '''
+        logger.info("stopping the cfme engine (evmserverd)")
 
         result = self.exec_commaned("systemctl status evmserverd | "
                                     "grep Active: | awk {'print $2'}", ignore_failure=True)
@@ -67,8 +109,11 @@ class cfme_upgrade_maneger(IPAppliance):
             self.exec_commaned("systemctl stop evmserverd")
 
     def start_cfme(self):
-
-        print "starting the cfme engine (evmserverd)"
+        '''
+        start CFME engine process
+        :return: None
+        '''
+        logger.info("starting the cfme engine (evmserverd)")
         result = self.exec_commaned("systemctl status evmserverd | grep Active: | awk {'print $2'}",
                                     ignore_failure=True)
 
@@ -76,22 +121,33 @@ class cfme_upgrade_maneger(IPAppliance):
             self.exec_commaned("systemctl start evmserverd")
 
     def yum_register(self, username, password):
+        '''
+        registaer to subscription manager
+        :param username: username to sign with
+        :param password: password to sign with
+        :return: None
+        '''
         self.exec_commaned("subscription-manager register --username={username} "
                            "--password={password} "
                            "--force".format(username=username, password=password))
 
     def validate_cfme_version(self):
-        print "waiting for 120 seconds for main proccess to load before runnig verrsion valiadation"
+        '''
+        Validating if the cfme version  is fit
+        :return:
+        '''
+        logger.info("waiting for 120 seconds for main process to load "
+                    "before running version validation")
         time.sleep(120)
-        print "start CFME version validation"
+        logger.info("start CFME version validation")
         api_url = "https://{ip}/api/".format(ip=self.address)
         auth = ("admin", "smartvm")
         si = requests.get(api_url, verify=False, auth=auth).json()["server_info"]
         assert si['version'].split("-", 1)[0] == self.dest_version, "The upgrade filed!"
-        print "The upgrade finished successfully"
+        logger.info("The upgrade finished successfully")
 
     def restart_components(self):
-        print "restarting componates after updating components"
+        logger.info("restarting componates after updating components")
 
         self.exec_commaned("systemctl restart $APPLIANCE_PG_SERVICE")
 
@@ -108,13 +164,12 @@ def main():
     args = parser.parse_args()
 
     params = {}
-
-    print "loading config file"
-    with open(args.config, 'r') as stream:
-        try:
+    try:
+        logger.info("loading config file")
+        with open(args.config, 'r') as stream:
             params = yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+    except Exception as ex:
+        logger.exception(ex.message)
 
     # filed name: (error msg, is mandatory, default value)
     requierd_fildes = {"address": {"msg": "Appliance address is missing",
@@ -134,15 +189,18 @@ def main():
                                 "is_mandatory": True, "default": None}}
     for key in requierd_fildes:
         if key not in params:
-            if requierd_fildes[key]["is_mandatory"]:
-                raise RuntimeError(requierd_fildes.get(key).get("msg"))
+            try:
+                if requierd_fildes[key]["is_mandatory"]:
+                    raise RuntimeError(requierd_fildes.get(key).get("msg"))
+            except Exception as ex:
+                logger.exception(ex.message)
             else:
                 params.update({key: requierd_fildes.get(key).get("default")})
 
-    print "Starting system upgrade"
-    cfme_upgrader = cfme_upgrade_maneger(params["address"],
-                                         repo_list=params["repo"],
-                                         dest_version=params["dest_version"])
+    logger.info("Starting system upgrade")
+    cfme_upgrader = cfmeUpgradeManeger(params["address"],
+                                       repo_list=params["repo"],
+                                       dest_version=params["dest_version"])
 
     cfme_upgrader.add_yum_repo()
     cfme_upgrader.stop_cfme()
