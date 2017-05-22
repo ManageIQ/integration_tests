07ef900b5ace117879f7315f29e853f78e1dc0a9
Oleksandr Kolisnyk <okolisny@redhat.com>
2017-05-22 18:28:38 +0300

    returned back fail_func param for wait_for

--- cfme/cloud/tenant.py
+++ cfme/cloud/tenant.py
@@ -74,7 +74,7 @@ class Tenant(Navigatable):
 
     def wait_for_appear(self, timeout=600):
         return wait_for(self.exists, timeout=timeout, message='Wait for cloud tenant to appear',
-                        delay=10)
+                        delay=10, fail_func=sel.refresh)
 
     def update(self, updates, wait=True):
         navigate_to(self, 'Edit')
