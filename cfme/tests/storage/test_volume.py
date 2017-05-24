e35ed9a8f844a2f5dd9248e82fe5bf0c196628b3
Mike Shriver <mshriver@redhat.com>
2017-05-24 14:04:08 -0400

    Update storage.test_volume

--- cfme/tests/storage/test_volume.py
+++ cfme/tests/storage/test_volume.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import pytest
+from widgetastic.exceptions import NoSuchElementException
 
 from cfme import test_requirements
 from cfme.cloud.provider import CloudProvider
 from cfme.cloud.provider.openstack import OpenStackProvider
 from cfme.storage.volume import Volume
+
 from utils import testgen
 from utils.appliance.implementations.ui import navigate_to
 
@@ -19,19 +21,18 @@ pytestmark = [pytest.mark.tier(3),
 
 @pytest.mark.uncollectif(lambda provider: not provider.one_of(OpenStackProvider))
 def test_volume_navigation(openstack_provider):
+    # grab a volume name, the table returns a generator so use next
     view = navigate_to(Volume, 'All')
+    try:
+        volume_name = view.entities.table.rows().next().name.text
+    except (StopIteration, NoSuchElementException):
+        pytest.skip('Skipping volume navigation for details, no volumes present')
+    volume = Volume(name=volume_name, provider=openstack_provider)
+
+    assert view.is_displayed
+
+    view = navigate_to(volume, 'Details')
     assert view.is_displayed
 
     view = navigate_to(Volume, 'Add')
-    assert view.is_displayed
-
-    # grab a volume name, the table returns a generator so use next
-    view = navigate_to(Volume, 'All')
-    volume_name = view.entities.table.rows().next().name.text
-    if not volume_name:
-        pytest.skip('Skipping volume navigation for details, could not find configured volume name')
-
-    volume = Volume(name=volume_name, provider=openstack_provider)
-
-    view = navigate_to(volume, 'Details')
     assert view.is_displayed
