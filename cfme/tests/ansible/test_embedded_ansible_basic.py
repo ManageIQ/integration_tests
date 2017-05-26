29f133a0d41428ed9e1513d63ebf7ad85796834c
quarckster <dmisharo@redhat.com>
2017-05-26 14:52:16 +0200

    Fixture embedded_ansible_role_enabled has been replaced by appliance. Added finalizer into test_embedded_ansible_repository_crud

--- cfme/tests/ansible/test_embedded_ansible_basic.py
+++ cfme/tests/ansible/test_embedded_ansible_basic.py
@@ -12,34 +12,27 @@ from cfme import test_requirements
 pytestmark = [
     pytest.mark.long_running,
     pytest.mark.meta(server_roles=["+embedded_ansible"]),
-    pytest.mark.uncollectif(current_version() < "5.8"),
+    pytest.mark.uncollectif(lambda: current_version() < "5.8"),
     pytest.mark.ignore_stream("upstream"),
     test_requirements.ansible
 ]
 
 
-@pytest.fixture(scope="module")
-def embedded_ansible_role_enabled(appliance):
-
-    def _wait_for_embedded_ansible():
-        rc, _ = appliance.ssh_client.run_command("systemctl status supervisord")
-        if rc == 0:
-            return True
-        else:
-            return False
-
-    wait_for(_wait_for_embedded_ansible, timeout="30m", delay=60,
-        message="Embedded ansible role enabled.")
-
-
 @pytest.mark.tier(1)
-def test_embedded_ansible_repository_crud(embedded_ansible_role_enabled):
+def test_embedded_ansible_repository_crud(request, appliance):
+    appliance.wait_for_embedded_ansible()
     repositories = RepositoryCollection()
     repository = repositories.create(
         fauxfactory.gen_alpha(),
         "https://github.com/quarckster/ansible_playbooks",
         description=fauxfactory.gen_alpha()
     )
+
+    @request.addfinalizer
+    def _delete_if_exists():
+        if repository.exists:
+            repository.delete()
+
     updated_description = "edited_{}".format(fauxfactory.gen_alpha())
     with update(repository):
         repository.description = updated_description
