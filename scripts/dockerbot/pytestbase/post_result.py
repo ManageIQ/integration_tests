dc1bfbc53b132d2fbbcf9ff04ce605ac79389f46
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-06-08 15:33:25 +0200

    dockerbot: sort out path issues in result posting
    we place coverage paths into logs
    we use py.path apis more

--- scripts/dockerbot/pytestbase/post_result.py
+++ scripts/dockerbot/pytestbase/post_result.py
@@ -1,24 +1,26 @@
 #!/cfme_pristine_venv/bin/python2
-import os
+from __future__ import print_function
 import sys
 
 try:
     from cfme.utils import safe_string
     from cfme.utils.trackerbot import post_task_result
+    from cfme.utils.path import log_path
 except ImportError:
     from utils import safe_string
     from utils.trackerbot import post_task_result
+    from utils.path import log_path
 
-with open("{}/setup.txt".format(os.environ['ARTIFACTOR_DIR'])) as f:
-    data = f.read()
+setup_data = log_path.join("setup.txt").read()
 
 coverage = 0.0
 try:
-    with open("{}/coverage_result.txt".format(os.environ['CFME_REPO_DIR'])) as f:
-        data = f.read().strip("\n")
-    coverage = float(data)
-except:
-    pass
 
-print sys.argv[1], sys.argv[2], coverage
-post_task_result(sys.argv[1], sys.argv[2], safe_string(data), coverage)
+    with log_path.join("coverage_result.txt").open() as f:
+        coverage_data = f.read().strip("\n")
+    coverage = float(coverage_data)
+except Exception as e:
+    print(e)
+
+print(sys.argv[1], sys.argv[2], coverage)
+post_task_result(sys.argv[1], sys.argv[2], safe_string(setup_data), coverage)
