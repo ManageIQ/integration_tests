a56e8e38ea469e050c911c2218c63c1268c686ff
Pete Savage <psavage@redhat.com>
2017-06-09 14:32:39 +0100

    Added conf sub command

--- cfme/scripting/miq.py
+++ cfme/scripting/miq.py
@@ -1,5 +1,6 @@
 import click
 from artifactor.__main__ import main as art_main
+from cfme.scripting.conf import main as conf_main
 from cfme.scripting.ipyshell import main as shell_main
 from scripts.dockerbot.sel_container import main as sel_con_main
 from scripts.release import main as rel_main
@@ -14,6 +15,7 @@ cli.add_command(sel_con_main, name="selenium-container")
 cli.add_command(art_main, name="artifactor-server")
 cli.add_command(rel_main, name="release")
 cli.add_command(shell_main, name="shell")
+cli.add_command(conf_main, name="conf")
 
 if __name__ == '__main__':
     cli()
