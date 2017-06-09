d3e9e98eefdd76678d0183842950d9c4d16156d4
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-06-09 14:27:13 +0200

    Dockerbot: split dockerfile into multiple layers

--- scripts/dockerbot/build-container.py
+++ scripts/dockerbot/build-container.py
@@ -2,9 +2,27 @@ import subprocess
 import py
 import sys
 
-here = py.path.local(__file__).dirpath()
+BASE = py.path.local(__file__).dirpath().join('pytestbase')
 
 
-sys.exit(subprocess.call([
-    'docker', 'build', '-t', 'py_test_base', str(here / 'pytestbase')
-]))
+def build_image(input, tag, no_cache=False):
+    command = [
+        'docker', 'build',
+        '-t', tag + ':latest',
+        '-t', tag,
+    ]
+    if no_cache:
+        command.append('--no-cache')
+    if input.check(file=1):
+        with input.open() as fp:
+            return subprocess.call(command + ['-'], stdin=fp)
+    else:
+        return subprocess.call(command + [str(input)])
+
+
+sys.exit(
+    build_image(BASE / "Dockerfile.base", 'cfmeqe/dockerbot-base') or
+
+    build_image(BASE / "Dockerfile.checkouts", 'cfmeqe/dockerbot-checkouts') or
+    build_image(BASE, 'py_test_base', no_cache=True)
+)
