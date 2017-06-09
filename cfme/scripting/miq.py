0996a81c9f8a32939b350de6c9fbd5806a073b2d
Milan Falešník <mfalesni@redhat.com>
2017-06-09 12:27:44 +0200

    miq shell

--- cfme/scripting/miq.py
+++ cfme/scripting/miq.py
@@ -1,5 +1,6 @@
 import click
 from artifactor.__main__ import main as art_main
+from cfme.scripting.ipyshell import main as shell_main
 from scripts.dockerbot.sel_container import main as sel_con_main
 from scripts.release import main as rel_main
 
@@ -12,6 +13,7 @@ def cli():
 cli.add_command(sel_con_main, name="selenium-container")
 cli.add_command(art_main, name="artifactor-server")
 cli.add_command(rel_main, name="release")
+cli.add_command(shell_main, name="shell")
 
 if __name__ == '__main__':
     cli()
