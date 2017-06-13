e9ba2538b3f79d3c3b5c8019c20c08dd97ee3d71
Shveta Sachdeva <sshveta@redhat.com>
2017-06-13 11:55:35 -0700

    SUI Beginnings

---
+++ cfme/tests/ssui/test_ssui_login.py
@@ -0,0 +1,12 @@
+import pytest
+
+from utils.appliance import get_or_create_current_appliance
+from utils.appliance import ViaSSUI
+
+
+@pytest.mark.parametrize('context', [ViaSSUI])
+def test_ssui_login(context):
+    appliance = get_or_create_current_appliance()
+
+    with appliance.context.use(context):
+        appliance.server.login()
