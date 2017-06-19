074da61f4296702939e2b6a508130124a7a6c8b4
quarckster <dmisharo@redhat.com>
2017-06-19 17:07:30 +0200

    Added a couple smoke tests for Control

---
+++ cfme/control/log.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from navmazing import NavigateToSibling
+from widgetastic.widget import Text
+from widgetastic_patternfly import Button
+
+from cfme.base import Server
+from cfme.base.login import BaseLoggedInPage
+from utils.appliance.implementations.ui import navigator, CFMENavigateStep
+
+
+class ControlLogView(BaseLoggedInPage):
+    """Basic view for Control/Log tab."""
+    title = Text(".//div[@id='main_content']//h1")
+    subtitle = Text(".//div[@id='main_div']/h3")
+    refresh_button = Button(id="refresh_log")
+    download_button = Button(id="fetch_log")
+
+    @property
+    def is_displayed(self):
+        return (
+            self.title.text == "Log" and
+            "Last 1000 lines from server" in self.subtitle.text and
+            self.refresh_button.is_displayed and
+            self.download_button.is_displayed
+        )
+
+
+@navigator.register(Server)
+class ControlLog(CFMENavigateStep):
+    VIEW = ControlLogView
+    prerequisite = NavigateToSibling("LoggedIn")
+
+    def step(self):
+        self.view.navigation.select("Control", "Log")
