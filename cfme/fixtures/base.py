2801e2b4921de2280b5b24d3a2709c5c6f3cb90d
izapolsk <izapolsk@redhat.com>
2017-06-29 23:35:28 +0300

    it was decided to remove conficting providers check

--- cfme/fixtures/base.py
+++ cfme/fixtures/base.py
@@ -10,20 +10,6 @@ from utils.log import logger
 from utils.path import data_path
 
 
-def pytest_sessionstart(session):
-    appliance = get_or_create_current_appliance()
-    if not session.config.getvalue('no_provider_check'):
-        try:
-            appliance.check_no_conflicting_providers()
-        except ApplianceException as e:
-            raise pytest.UsageError("Conflicting providers were found: {}".format(e))
-
-
-def pytest_addoption(parser):
-    parser.addoption("--no-provider-check", action="store_true", default=False,
-                     help="Will not check validity of existing providers, when specified")
-
-
 @pytest.fixture(scope="session")
 def appliance():
     return get_or_create_current_appliance()
