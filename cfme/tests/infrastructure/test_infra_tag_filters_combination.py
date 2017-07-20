c3c2d56af7ac93f60b315d4242156911c86a1776
rbabyuk <rbabyuk@redhat.com>
2017-07-20 14:35:35 +0300

    small review changes

--- cfme/tests/infrastructure/test_infra_tag_filters_combination.py
+++ cfme/tests/infrastructure/test_infra_tag_filters_combination.py
@@ -25,25 +25,26 @@ def a_provider(request):
     return setup_one_or_skip(request, filters=[prov_filter])
 
 
-@pytest.fixture(params=test_items, ids=[str(test_id[0]) for test_id in test_items],
+@pytest.fixture(params=test_items, ids=[collection_type for collection_type, __ in test_items],
                 scope='function')
 def testing_vis_object(request, a_provider, appliance):
     """ Fixture creates class object for tag visibility test
+
     Returns: class object of certain type
     """
     collection_name, param_class = request.param
-    test_items = getattr(appliance.rest_api.collections, collection_name)
+    collection_rest = getattr(appliance.rest_api.collections, collection_name)
 
     if not test_items:
         pytest.skip('No content found for test!')
 
     if collection_name == 'templates':
         item_type = a_provider.data['provisioning']['catalog_item_type'].lower()
-        for test_item_value in test_items:
-            if test_item_value.vendor == item_type:
-                return param_class(name=test_item_value.name, provider=a_provider)
+        for resource in collection_rest:
+            if resource.vendor == item_type:
+                return param_class(name=resource.name, provider=a_provider)
     else:
-        return param_class(name=test_items[0].name, provider=a_provider)
+        return param_class(name=collection_rest[0].name, provider=a_provider)
 
 
 @pytest.fixture(scope='module')
@@ -56,6 +57,7 @@ def group_tag_datacenter_combination(group_with_tag, a_provider):
 def test_tagvis_tag_datacenter_combination(testing_vis_object, group_tag_datacenter_combination,
                                 check_item_visibility, visibility):
     """ Tests template visibility with combination  of tag and selected
+
         datacenter filters in the group
         Prerequisites:
             Catalog, tag, role, group and restricted user should be created
