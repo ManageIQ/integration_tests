679947ddaac04165c540ac88b62008510f736db8
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-07-21 10:39:32 +0200

    markers: directly declare plugin list

--- markers/__init__.py
+++ markers/__init__.py
@@ -1,25 +1,25 @@
-pytest_plugins = """
-    markers.composite
-    markers.crud
-    markers.fixtureconf
-    markers.meta
-    markers.stream_excluder
-    markers.requires
-    markers.sauce
-    markers.skipper
-    markers.uses
-    markers.uncollect
-    markers.smoke
-    markers.composite
-    markers.crud
-    markers.fixtureconf
-    markers.meta
-    markers.stream_excluder
-    markers.requires
-    markers.sauce
-    markers.skipper
-    markers.uses
-    markers.uncollect
-    markers.smoke
-    markers.polarion
-""".strip().split()
+pytest_plugins = [
+    "markers.composite",
+    "markers.crud",
+    "markers.fixtureconf",
+    "markers.meta",
+    "markers.stream_excluder",
+    "markers.requires",
+    "markers.sauce",
+    "markers.skipper",
+    "markers.uses",
+    "markers.uncollect",
+    "markers.smoke",
+    "markers.composite",
+    "markers.crud",
+    "markers.fixtureconf",
+    "markers.meta",
+    "markers.stream_excluder",
+    "markers.requires",
+    "markers.sauce",
+    "markers.skipper",
+    "markers.uses",
+    "markers.uncollect",
+    "markers.smoke",
+    "markers.polarion",
+]
