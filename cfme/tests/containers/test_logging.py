075ec48c90bcbc1254615f2f2edb944136c37b4a
shalomnaim1 <snaim@redhat.com>
2017-07-24 12:40:39 +0300

    logging test (#4890)
    * finish editing validation function
    * refactor code to work with WT
    * fix asserssion condition
    * set global variable NUM_OF_DEFAULT_LOG_ROUTES
    * order imports

---
+++ cfme/tests/containers/test_logging.py
@@ -0,0 +1,35 @@
+import pytest
+
+from utils import testgen
+from utils.version import current_version
+from utils.appliance.implementations.ui import navigate_to
+
+from cfme.containers.provider import ContainersProvider
+
+NUM_OF_DEFAULT_LOG_ROUTES = 2
+pytestmark = [
+    pytest.mark.uncollectif(lambda provider: current_version() < "5.8"),
+    pytest.mark.usefixtures('setup_provider'),
+    pytest.mark.tier(1)]
+pytest_generate_tests = testgen.generate([ContainersProvider], scope='function')
+
+
+@pytest.fixture(scope="function")
+def validate_logging_up_and_running(provider):
+    routers = [router for router in provider.mgmt.o_api.get('route')[1]['items']
+               if "logging" in router["metadata"]["name"]]
+    all_routers_up = all([router["status"]["ingress"][0]["conditions"][0]["status"]
+                          for router in routers])
+
+    assert len(routers) <= NUM_OF_DEFAULT_LOG_ROUTES, "some logging route is missing"
+    assert all_routers_up, "some logging route is off"
+
+
+@pytest.mark.polarion('CMP-10643')
+def test_external_logging_activated(provider, validate_logging_up_and_running):
+    view = navigate_to(provider, 'Details')
+
+    assert view.monitor.item_enabled('External Logging'), (
+        "Monitoring --> External Logging not activated")
+
+    view.monitor.item_select('External Logging')
