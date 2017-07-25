e3de12ffa216ae6474f386bca72c81784d67d8b4
Dmitry Misharov <quarckster@gmail.com>
2017-07-25 14:38:48 +0200

    Fixed EditPolicyEventAssignments view (#4944)
    * Added BootstrapswitchSelect view, fixed EditPolicyEventAssignments
    
    * _get_bs_description added to BootstrapSwitchSelect
    
    * Fixed multiline docstrings to follow pep257
    
    * Bumped widgetastic.patternfly version
    
    * Fixed imports in widgetastic_manageiq
    
    * Fixed Version.lowest() in policies.py

--- widgetastic_manageiq/__init__.py
+++ widgetastic_manageiq/__init__.py
@@ -34,8 +34,8 @@ from widgetastic.widget import (
     do_not_read_this_widget)
 from widgetastic.xpath import quote
 from widgetastic_patternfly import (
-    Accordion as PFAccordion, CandidateNotFound, BootstrapTreeview, Button, Input, BootstrapSelect,
-    CheckableBootstrapTreeview, FlashMessages)
+    Accordion as PFAccordion, CandidateNotFound, BootstrapSwitch, BootstrapTreeview, Button, Input,
+    BootstrapSelect, CheckableBootstrapTreeview, FlashMessages)
 
 from cfme.exceptions import ItemNotFound, ManyEntitiesFound
 
@@ -599,6 +599,61 @@ class CheckboxSelect(Widget):
     def read(self):
         """Only selected checkboxes."""
         return [cb for cb in self.checkboxes if cb.selected]
+
+
+class BootstrapSwitchSelect(CheckboxSelect):
+    """BootstrapSwitchSelect view.
+
+    This view is very similar to parent CheckboxSelect view. BootstrapSwitches used instead of
+    usual Checkboxes. It can be found in the same policy's events assignment screen since
+    CFME 5.8.1.
+
+    """
+
+    BS_TEXT = '/../../following-sibling::text()[1]'
+
+    def _get_bs_description(self, bs):
+        """Returns text description of the BootstrapSwitch widget.
+
+        We have to use such hack with the script execution, because Selenium cannot return text of a
+        text node itself.
+
+        Returns: str
+
+        """
+        return bs._label or self.browser.execute_script(
+            "{script} return xpath(null, {arg}).textContent;".format(
+                script=DynaTree.XPATH,
+                arg=quote(bs.ROOT.locator + self.BS_TEXT)
+            )).strip()
+
+    @property
+    def checkboxes(self):
+        """All bootstrap switches."""
+        return {BootstrapSwitch(self, id=el.get_attribute("id")) for el in self.browser.elements(
+            ".//input[@type='checkbox']", parent=self)}
+
+    def checkbox_by_id(self, id):
+        """Finds bootstrap switch by id."""
+        return BootstrapSwitch(self, id=id)
+
+    @property
+    def selected_text(self):
+        """Only selected bootstrap switches' text descriptions."""
+        return {self._get_bs_description(bs) for bs in self.selected_checkboxes}
+
+    def checkbox_by_text(self, text):
+        """Returns bootstrap switch searched by its text."""
+        if self._access_func is not None:
+            for cb in self.checkboxes:
+                txt = self._access_func(cb)
+                if txt == text:
+                    return cb
+            else:
+                raise NameError("Bootstrap switch with text {} not found!".format(text))
+        else:
+            # Has to be only single
+            return BootstrapSwitch(self, label=text)
 
 
 # ManageIQ table objects definition
