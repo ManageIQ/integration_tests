4dbebe2f0e9ec3d2eea3b0df5ae8cd33a25f72ed
rbabyuk <rbabyuk@redhat.com>
2017-07-27 12:36:34 +0300

    Red Hat Updates to widgetastic

--- cfme/configure/configuration/region_settings.py
+++ cfme/configure/configuration/region_settings.py
@@ -1,7 +1,10 @@
+import re
+
 from navmazing import NavigateToAttribute, NavigateToSibling
 from widgetastic_patternfly import Input, BootstrapSelect, Button, BootstrapSwitch
 # TODO replace with dynamic table
-from widgetastic_manageiq import VanillaTable
+from widgetastic_manageiq import VanillaTable, SummaryFormItem, Table
+from widgetastic.widget import Checkbox, Text
 
 from cfme.base.ui import RegionView
 from utils.appliance import Navigatable
@@ -428,6 +431,349 @@ class MapTagsEdit(CFMENavigateStep):
 
     def step(self):
         self.prerequisite_view.table.row(tag_category=self.obj.category).click()
+
+
+# ====================Red Hat Updates===================================
+
+
+class RedHatUpdatesView(RegionView):
+    """Red Hat Updates details view"""
+    title = Text('//div[@id="main-content"]//h3[1]')
+    available_update_version = Text('//td[contains(text(), "Available Product version:")]')
+    edit_registration = Button('Edit Registration')
+    refresh = Button('Refresh List')
+    check_for_updates = Button('Check for Updates')
+    register = Button('Register')
+    apply_cfme_update = Button('Apply CFME Update')
+    updates_table = Table('.table.table-striped.table-bordered')
+    repository_names_info = SummaryFormItem('Red Hat Software Updates', 'Repository Name(s)')
+
+    @property
+    def is_displayed(self):
+        return (
+            self.redhatupdates.is_active() and
+            self.edit_registration.is_displayed and
+            self.title.text == 'Red Hat Software Updates'
+        )
+
+
+class RedHatUpdatesEditView(RegionView):
+    """Red Hat Updates edit view"""
+    title = Text('//div[@id="main-content"]//h3[1]')
+
+    register_to = BootstrapSelect(id='register_to')
+    url = Input(id='server_url')
+    repo_name = Input(id='repo_name')
+    use_proxy = Checkbox('use_proxy')
+    proxy_url = Input(id='proxy_address')
+    proxy_username = Input(id='proxy_userid')
+    proxy_password = Input(id='proxy_password')
+    proxy_password_verify = Input(id='proxy_password2')
+    username = Input(id='customer_userid')
+    password = Input(id='customer_password')
+    password_verify = Input(id='customer_password2')
+
+    repo_default_name = Button(id='repo_default_name')
+    rhn_default_url = Button(id='rhn_default_button')
+
+    validate_button = Button('Validate')
+    reset_button = Button('Reset')
+    save_button = Button('Save')
+    cancel_button = Button('Cancel')
+
+    @property
+    def is_displayed(self):
+        return (
+            self.redhatupdates.is_active() and
+            self.validate_button.is_displayed and
+            self.title.text == 'Red Hat Software Updates'
+        )
+
+
+class RedHatUpdates(Navigatable, Pretty):
+    """ Class represents a Red Hat updates tab in CFME UI
+
+             Args:
+                 service: Service type (registration method).
+                 url: Service server URL address.
+                 username: Username to use for registration.
+                 password: Password to use for registration.
+                 password_verify: 2nd entry of password for verification. Same as 'password' if None.
+                 repo_name: Repository/channel to enable.
+                 organization: Organization (sat6 only).
+                 use_proxy: `True` if proxy should be used, `False` otherwise (default `False`).
+                 proxy_url: Address of the proxy server.
+                 proxy_username: Username for the proxy server.
+                 proxy_password: Password for the proxy server.
+                 proxy_password_verify: 2nd entry of proxy server password for verification.
+                    Same as 'proxy_password' if None.
+                 set_default_rhsm_address: Click the Default button connected to
+                    the RHSM (only) address if `True`
+                 set_default_repository: Click the Default button connected to the repo/channel if `True`
+
+                Note:
+                    With satellite 6, it is necessary to validate credentials to obtain
+                    available organizations from the server.
+                    With satellite 5, 'validate' parameter is ignored because there is
+                    no validation button available.
+            """
+
+    pretty_attrs = ['service', 'url', 'username', 'password']
+    service_types = {
+        'rhsm': 'Red Hat Subscription Management',
+        'sat6': 'Red Hat Satellite 6'
+    }
+
+    def __init__(self, service, url, username, password, password_verify=None, repo_name=None,
+                 organization=None, use_proxy=False, proxy_url=None, proxy_username=None,
+                 proxy_password=None, proxy_password_verify=None,
+                 set_default_rhsm_address=False,
+                 set_default_repository=False, appliance=None):
+        self.service = service
+        self.url = url
+        self.username = username
+        self.password = password
+        self.password_verify = password_verify
+        self.repo_name = repo_name
+        self.organization = organization
+        self.use_proxy = use_proxy
+        self.proxy_url = proxy_url
+        self.proxy_username = proxy_username
+        self.proxy_password = proxy_password
+        self.proxy_password_verify = proxy_password_verify
+        self.set_default_rhsm_address = set_default_rhsm_address
+        self.set_default_repository = set_default_repository
+        Navigatable.__init__(self, appliance=appliance)
+
+    def update_registration(self, validate=True, cancel=False):
+        """ Fill in the registration form, validate and save/cancel
+
+        Args:
+            validate: Click the Validate button and check the
+                      flash message for errors if `True` (default `True`)
+            cancel: Click the Cancel button if `True` or the Save button
+                    if `False` (default `False`)
+        """
+        assert self.service in self.service_types, "Unknown service type '{}'".format(
+            self.service)
+        service_value = self.service_types[self.service]
+
+        password_verify = self.password_verify or self.password
+        proxy_password_verify = self.proxy_password_verify or self.proxy_password
+
+        view = navigate_to(self, 'Edit')
+        details = {
+            'register_to': service_value,
+            'url': self.url,
+            'username': self.username,
+            'password': self.password,
+            'password_verify': password_verify,
+            'repo_name': self.repo_name,
+            'use_proxy': self.use_proxy,
+            'proxy_url': self.proxy_url,
+            'proxy_username': self.proxy_username,
+            'proxy_password': self.proxy_password,
+            'proxy_password_verify': proxy_password_verify
+        }
+
+        view.fill(details)
+
+        if self.set_default_rhsm_address:
+            view.rhn_default_url.click()
+
+        if self.set_default_repository:
+            view.repo_default_name.click()
+
+        if validate:
+            view.validate_button.click()
+
+        if cancel:
+            view.cancel_button.click()
+            flash_message = 'Edit of Customer Information was cancelled'
+        else:
+            view.save_button.click()
+            flash_message = 'Customer Information successfully saved'
+
+        view = self.create_view(RedHatUpdatesView)
+        assert view.is_displayed
+        view.flash.assert_message(flash_message)
+
+    def refresh(self):
+        """ Click refresh button to update statuses of appliances """
+        view = navigate_to(self, 'Details')
+        view.refresh.click()
+
+    def register_appliances(self, *appliance_names):
+        """ Register appliances by names
+
+        Args:
+            appliance_names: Names of appliances to register; will register all if empty
+        """
+        view = navigate_to(self, 'Details')
+        self.select_appliances(*appliance_names)
+        view.register.click()
+        view.flash.assert_message("Registration has been initiated for the selected Servers")
+
+    def update_appliances(self, *appliance_names):
+        """ Update appliances by names
+
+        Args:
+            appliance_names: Names of appliances to update; will update all if empty
+        """
+        view = navigate_to(self, 'Details')
+        self.select_appliances(*appliance_names)
+        view.apply_updates.click()
+        view.flash.assert_message("Update has been initiated for the selected Servers")
+
+    def check_updates(self, *appliance_names):
+        """ Run update check on appliances by names
+
+        Args:
+            appliance_names: Names of appliances to check; will check all if empty
+        """
+        view = navigate_to(self, 'Details')
+        self.select_appliances(*appliance_names)
+        view.check_updates.click()
+        view.flash.assert_message(
+            "Check for updates has been initiated for the selected Servers")
+
+    def is_registering(self, *appliance_names):
+        """ Check if at least one appliance is registering """
+        view = navigate_to(self, 'Details')
+        for appliance_name in appliance_names:
+            row = view.updates_table.row(appliance=appliance_name)
+            if row.last_message.text.lower() == 'registering':
+                return True
+        else:
+            return False
+
+    def is_registered(self, *appliance_names):
+        """ Check if each appliance is registered
+
+        Args:
+            appliance_names: Names of appliances to check; will check all if empty
+        """
+        view = navigate_to(self, 'Details')
+        for appliance_name in appliance_names:
+            row = view.updates_table.row(appliance=appliance_name)
+            if row.last_message.text.lower() == 'registered':
+                return True
+        else:
+            return False
+
+    def is_subscribed(self, *appliance_names):
+        """ Check if appliances are subscribed
+
+        Args:
+            appliance_names: Names of appliances to check; will check all if empty
+        """
+        for row in self.get_appliance_rows(*appliance_names):
+            if row.update_status.text.lower() in {'not registered', 'unsubscribed'}:
+                return False
+        return True
+
+    def versions_match(self, version, *appliance_names):
+        """ Check if versions of appliances match version
+
+        Args:
+            version: Version to match against
+            appliance_names: Names of appliances to check; will check all if empty
+        """
+        for row in self.get_appliance_rows(*appliance_names):
+            if row.cfme_version.text != version:
+                return False
+        return True
+
+    def checked_updates(self, *appliance_names):
+        """ Check if appliances checked if there is an update available
+
+        Args:
+            appliance_names: Names of appliances to check; will check all if empty
+        """
+        for row in self.get_appliance_rows(*appliance_names):
+            if row.last_checked_for_updates.text == '':
+                return False
+        return True
+
+    def platform_updates_available(self, *appliance_names):
+        """ Check if appliances have a platform update available
+
+        Args:
+            appliance_names: Names of appliances to check; will check all if empty
+        """
+        for row in self.get_appliance_rows(*appliance_names):
+            if row.platform_updates_available.text.lower() != 'yes':
+                return False
+        return True
+
+    def get_available_version(self):
+        """ Get available version printed on the page
+
+        Returns:
+            `None` if not available; string with version otherwise
+             e.g. ``1.2.2.3``
+        """
+        view = navigate_to(self, 'Details')
+        available_version_raw = view.available_update_version.text()
+        available_version_search_res = re.search(r"([0-9]+\.)*[0-9]+", available_version_raw)
+        if available_version_search_res:
+            return available_version_search_res.group(0)
+        return None
+
+    def get_repository_names(self):
+        """Get available repositories names
+
+        Returns:
+            string: summary info for repositories names
+        """
+        view = navigate_to(self, 'Details')
+        return view.repository_names_info.text
+
+    def select_appliances(self, *appliance_names):
+        """ Select appliances by names
+
+        Args:
+            appliance_names: Names of appliances to select; will select all if empty
+        """
+        view = navigate_to(self, 'Details')
+        if appliance_names:
+            view.updates_table.uncheck_all()
+            for name in appliance_names:
+                view.updates_table.row(appliance=name)[0].click()
+        else:
+            view.updates_table.check_all()
+
+    def get_appliance_rows(self, *appliance_names):
+        """ Get appliances as table rows
+
+        Args:
+            appliance_names: Names of appliances to get; will get all if empty
+        """
+        view = navigate_to(self, 'Details')
+        if appliance_names:
+            rows = [row for row in view.updates_table.rows()
+                    if row.appliance.text in appliance_names]
+        else:
+            rows = view.updates_table.rows()
+        return rows
+
+
+@navigator.register(RedHatUpdates)
+class Details(CFMENavigateStep):
+    VIEW = RedHatUpdatesView
+    prerequisite = NavigateToAttribute('appliance.server.zone.region', 'Details')
+
+    def step(self):
+        self.prerequisite_view.redhatupdates.select()
+
+
+@navigator.register(RedHatUpdates)
+class Edit(CFMENavigateStep):
+    VIEW = RedHatUpdatesEditView
+    prerequisite = NavigateToSibling('Details')
+
+    def step(self):
+        self.prerequisite_view.edit_registration.click()
 
 
 # ====================C and U===================================
