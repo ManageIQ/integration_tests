c483f1ecafa984df0fe4e1d8c8fe946943ca0066
Milan Falešník <mfalesni@redhat.com>
2017-08-05 15:19:08 +0200

    Sprout - STATICFILES_DIRS is not required and screws things up

--- sprout/sprout/settings.py
+++ sprout/sprout/settings.py
@@ -115,9 +115,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# Unnecessary
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 # STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
