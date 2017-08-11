0ab92fb9859027b69849f9997d1c6a71c9064aa2
Luke Couzens <lcouzens@redhat.com>
2017-08-11 10:07:03 +0100

    Creating yum update test for minor version testing

---
+++ cfme/tests/cli/test_appliance_update.py
@@ -0,0 +1,76 @@
+import tempfile
+
+import pytest
+
+from cfme.test_framework.sprout.client import SproutClient, SproutException
+from scripts.repo_gen import process_url, build_file
+from utils.version import Version
+from utils.appliance import current_appliance
+from utils.log import logger
+from utils.conf import cfme_data
+from utils import os
+
+
+versions = []
+
+'''The following lines generate appliance versions based from the current build. appliance version
+is split and minor_build is picked out for generating each version and appending it to the empty
+versions list'''
+
+version = current_appliance.version
+split_ver = str(version).split(".")
+minor_build = split_ver[2]
+
+for i in range(int(minor_build) - 1, -1, -1):
+    versions.append("{}.{}.{}".format(split_ver[0], split_ver[1], i))
+
+
+@pytest.yield_fixture(scope="function")
+def appliance_preupdate(old_version):
+
+    '''Requests appliance from sprout based on old_versions, edits partitions and adds repo file
+    for update'''
+    usable = []
+    sp = SproutClient.from_config()
+    available_versions = set(sp.call_method('available_cfme_versions'))
+    for ver in versions:
+            for a in available_versions:
+                if a.startswith(ver):
+                    usable.append(a)
+    usable = sorted([Version(i) for i in usable], reverse=True)
+    try:
+        apps, pool_id = sp.provision_appliances(count=1, preconfigured=True, lease_time=180,
+            version=str(usable[0]))
+    except Exception as e:
+        logger.warning("Couldn't provision appliance with following error:")
+        logger.warning("{}".format(e))
+    finally:
+        logger.error("Couldn't provision an appliance at all")
+        raise SproutException('No provision available')
+    yield apps[0]
+
+    apps[0].db.extend_partition()
+    urls = process_url(cfme_data['basic_info']['update_url'])
+    output = build_file(urls)
+    with tempfile.NamedTemporaryFile('w') as f:
+        f.write(output)
+        f.flush()
+        os.fsync(f.fileno())
+        apps[0].ssh_client.put_file(
+            f.name, '/etc/yum.repos.d/update.repo')
+
+    apps[0].ssh_client.close()
+    sp.destroy_pool(pool_id)
+
+
+@pytest.mark.parametrize('old_version', versions)
+def test_update_yum(appliance_preupdate, appliance):
+
+    '''Tests appliance update between versions'''
+
+    appliance_preupdate.evmserverd.stop()
+    with appliance_preupdate.ssh_client as ssh:
+        rc, out = ssh.run_command('yum update -y', timeout=3600)
+        assert rc == 0, "update failed {}".format(out)
+    appliance_preupdate.start_evm_service()
+    assert appliance.version == appliance_preupdate.version
