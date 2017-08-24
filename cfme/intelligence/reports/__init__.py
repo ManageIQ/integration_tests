b2f66bd29665b311ad7cf34e2931e4a487105ac2
Dmitry Misharov <quarckster@gmail.com>
2017-08-24 14:58:10 +0200

    Deleted old code Cloud Intelligence / Reports widgetastic conversion (#5094)

--- cfme/intelligence/reports/__init__.py
+++ cfme/intelligence/reports/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from navmazing import NavigateToSibling, NavigateToAttribute
+from navmazing import NavigateToSibling
 from widgetastic.utils import Parameter
 from widgetastic.widget import View
 from widgetastic_manageiq import ManageIQTree, MultiBoxSelect
@@ -7,8 +7,6 @@ from widgetastic_patternfly import Accordion, Button, Dropdown, FlashMessages
 
 from cfme.base import Server
 from cfme.base.login import BaseLoggedInPage
-from cfme.web_ui import accordion
-from utils.appliance import Navigatable
 from utils.appliance.implementations.ui import navigator, CFMENavigateStep
 
 
@@ -73,75 +71,3 @@ class CloudIntelReports(CFMENavigateStep):
 class ReportsMultiBoxSelect(MultiBoxSelect):
     move_into_button = Button(title=Parameter("@move_into"))
     move_from_button = Button(title=Parameter("@move_from"))
-
-
-class Report(Navigatable):
-    """
-        This is fake class mainly needed for navmazing navigation
-    """
-    def __init__(self, appliance=None):
-        Navigatable.__init__(self, appliance)
-
-
-@navigator.register(Report, 'All')
-class All(CFMENavigateStep):
-    prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
-
-    def step(self):
-        self.prerequisite_view.navigation.select('Cloud Intel', 'Reports')
-
-
-@navigator.register(Report, 'SavedReports')
-class SavedReports(CFMENavigateStep):
-    prerequisite = NavigateToSibling('All')
-
-    def step(self):
-        accordion.tree("Saved Reports", "All Saved Reports")
-
-
-@navigator.register(Report, 'Reports')
-class Reports(CFMENavigateStep):
-    prerequisite = NavigateToSibling('All')
-
-    def step(self):
-        accordion.tree("Reports", "All Reports")
-
-
-@navigator.register(Report, 'Schedules')
-class Schedules(CFMENavigateStep):
-    prerequisite = NavigateToSibling('All')
-
-    def step(self):
-        accordion.tree("Schedules", "All Schedules")
-
-
-@navigator.register(Report, 'Dashboards')
-class Dashboards(CFMENavigateStep):
-    prerequisite = NavigateToSibling('All')
-
-    def step(self):
-        accordion.tree("Dashboards", "All Dashboards")
-
-
-@navigator.register(Report, 'DashboardWidgets')
-class DashboardWidgets(CFMENavigateStep):
-    prerequisite = NavigateToSibling('All')
-
-    def step(self):
-        accordion.tree("Dashboard Widgets", "All Widgets")
-
-
-@navigator.register(Report, 'EditReportMenus')
-class EditReportMenus(CFMENavigateStep):
-    prerequisite = NavigateToSibling('All')
-
-    def step(self):
-        accordion.tree("Edit Report Menus", "All EVM Groups")
-
-
-@navigator.register(Report, 'ImportExport')
-class ImportExport(CFMENavigateStep):
-    prerequisite = NavigateToSibling('All')
-
-    def step(self):
-        accordion.tree("Import/Export", "Import / Export")
