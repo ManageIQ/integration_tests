9b3b6e9e955844cc269ad6b6e7a2d8d4f22b6162
Nikhil Dhandre <ndhandre@redhat.com>
2017-08-24 15:18:39 +0530

    Deployment Role model page widgetastic convertion

--- cfme/infrastructure/deployment_roles.py
+++ cfme/infrastructure/deployment_roles.py
@@ -1,29 +1,244 @@
 """ A model of an Infrastructure Deployment roles in CFME"""
 
 from functools import partial
+
 from navmazing import NavigateToAttribute, NavigateToSibling
-from selenium.common.exceptions import NoSuchElementException
+from widgetastic.exceptions import NoSuchElementException
+from widgetastic.utils import Version, VersionPick
+from widgetastic.widget import ParametrizedView, View
+from widgetastic_manageiq import (Accordion,
+                                  BaseEntitiesView,
+                                  BaseListEntity,
+                                  BaseQuadIconEntity,
+                                  BaseTileIconEntity,
+                                  BootstrapSelect,
+                                  BootstrapTreeview,
+                                  BreadCrumb,
+                                  ItemsToolBarViewSelector,
+                                  JSBaseEntity,
+                                  NonJSBaseEntity,
+                                  SummaryTable,
+                                  Table,
+                                  Text
+                                  )
+from widgetastic_patternfly import (BootstrapNav,
+                                    Button,
+                                    Dropdown,
+                                    FlashMessages
+                                    )
 
-from cfme.exceptions import ListAccordionLinkNotFound
-from cfme.fixtures import pytest_selenium as sel
+from cfme.base.ui import BaseLoggedInPage
+from cfme.exceptions import ItemNotFound, RoleNotFound
 from cfme.infrastructure.provider.openstack_infra import OpenstackInfraProvider
+from utils.appliance.implementations.ui import CFMENavigateStep, navigator, navigate_to
 from utils.appliance import Navigatable
-from utils.appliance.implementations.ui import (CFMENavigateStep, navigate_to,
-                                                navigator)
-from cfme.web_ui import (listaccordion as list_acc, match_location, Quadicon,
-                         Region, toolbar as tb)
-from utils import version
-from utils.pretty import Pretty
 
 
-details_page = Region(infoblock_type='detail')
+class DeploymentRoleToolbar(View):
+    """The toolbar on the Deployment Role page"""
+    configuration = Dropdown('Configuration')
+    policy = Dropdown('Policy')
+    download = Dropdown('Download')
+    view_selector = View.nested(ItemsToolBarViewSelector)
 
-cfg_btn = partial(tb.select, 'Configuration')
-pol_btn = partial(tb.select, 'Policy')
-match_page = partial(match_location, controller='ems_cluster')
+
+class DeploymentRoleDetailsToolbar(View):
+    """The toolbar on the Deployment Role details page"""
+    configuration = Dropdown('Configuration')
+    policy = Dropdown('Policy')
+    monitoring = Dropdown('Monitoring')
+    download = Button(title='Download summary in PDF format')
 
 
-class DeploymentRoles(Pretty, Navigatable):
+class DeploymentRoleComparisonToolbar(View):
+    """The toolbar on Comparison Page of roles"""
+    all_attributes = Button(title="All attributes")
+    different_values_attributes = Button(title="Attributes with different values")
+    same_values_attributes = Button(title="Attributes with same values")
+
+    details_mode = Button(title="Details Mode")
+    exists_mode = Button(title="Exists Mode")
+    download = Dropdown('Download')
+
+
+class DeploymentRoleDetailsAccordion(View):
+    """The accordion on the Deployment Role details page"""
+
+    @View.nested
+    class properties(Accordion):  # noqa
+        nav = BootstrapNav('//div[@id="ems_prop"]//ul')
+
+    @View.nested
+    class relationships(Accordion):  # noqa
+        nav = BootstrapNav('//div[@id="ems_rel"]//ul')
+
+
+class DepRoleQuadIconEntity(BaseQuadIconEntity):
+    @property
+    def data(self):
+        return self.browser.get_attribute("alt", self.QUADRANT.format(pos="a"))
+
+
+class DepRoleTileIconEntity(BaseTileIconEntity):
+    quad_icon = ParametrizedView.nested(DepRoleQuadIconEntity)
+
+
+class DepRoleListEntity(BaseListEntity):
+    pass
+
+
+class NonJSDepRoleEntity(NonJSBaseEntity):
+    quad_entity = DepRoleQuadIconEntity
+    list_entity = DepRoleListEntity
+    tile_entity = DepRoleTileIconEntity
+
+
+def DeploymentRoleEntity():  # noqa
+    """Temporary wrapper for Deployment Role Entity during transition to JS based Entity """
+    return VersionPick({
+        Version.lowest(): NonJSDepRoleEntity,
+        '5.9': JSBaseEntity,
+    })
+
+
+class DeploymentRoleEntitiesView(BaseEntitiesView):
+    """The entities on the main list Deployment Role page"""
+
+    @property
+    def entity_class(self):
+        return DeploymentRoleEntity().pick(self.browser.product_version)
+
+
+class DeploymentRoleDetailsEntities(View):
+    """The entities on the Deployment Role details page"""
+    breadcrumb = BreadCrumb()
+    relationships = SummaryTable(title='Relationships')
+    total_for_node = SummaryTable(title='Totals for Nodes')
+    total_for_vm = SummaryTable(title='Totals for VMs')
+    smart_management = SummaryTable(title='Smart Management')
+
+
+class DeploymentRoleComparisonEntities(View):
+    """The entities on compare Deployment role page"""
+    breadcrumb = BreadCrumb()
+    table = Table('//*[@id="compare-grid"]/table')
+
+
+class DeploymentRoleView(BaseLoggedInPage):
+    """A base view for all the Deployment Role pages"""
+    title = Text('.//div[@id="center_div" or @id="main-content"]//h1')
+    flash = FlashMessages(
+        './/div[@id="flash_msg_div"]/div[@id="flash_text_div" or '
+        'contains(@class, "flash_text_div")]')
+
+    @property
+    def in_dep_role(self):
+        """Determine if the Deployment page is currently open"""
+        return (
+            self.logged_in_as_current_user and
+            self.navigation.currently_selected == ['Compute', 'Infrastructure',
+                                                   'Deployment Roles'])
+
+
+class DeploymentRoleAllView(DeploymentRoleView):
+    """The all Deployment Role page"""
+    toolbar = View.nested(DeploymentRoleToolbar)
+    including_entities = View.include(DeploymentRoleEntitiesView, use_parent=True)
+
+    @property
+    def is_displayed(self):
+        """This is page currently being displayed"""
+        return (
+            self.in_dep_role and
+            (self.title.text == 'Deployment Roles' or
+             self.title.text == 'All Cluster / Deployment Role'))
+
+
+class DeploymentRoleAllForProviderView(DeploymentRoleView):
+    """The Deployment Role for Provider page"""
+    breadcrumb = BreadCrumb()
+    toolbar = View.nested(DeploymentRoleToolbar)
+    sidebar = View.nested(DeploymentRoleDetailsAccordion)
+    including_entities = View.include(DeploymentRoleEntitiesView, use_parent=True)
+
+    @property
+    def is_displayed(self):
+        """Is this page currently being displayed"""
+        expected_title = '{} (All Deployment Roles)'.format(self.context['object'].provider.name)
+
+        return (
+            self.logged_in_as_current_user and
+            self.breadcrumb.active_location == expected_title)
+
+
+class DeploymentRoleDetailsView(DeploymentRoleView):
+    """The details page for a Deployment Roles"""
+    toolbar = View.nested(DeploymentRoleDetailsToolbar)
+    sidebar = View.nested(DeploymentRoleDetailsAccordion)
+    entities = View.nested(DeploymentRoleDetailsEntities)
+
+    @property
+    def is_displayed(self):
+        """Is this page currently being displayed"""
+        expected_title = '{} (Summary)'.format(self.context['object'].name)
+        return (
+            self.in_dep_role and
+            self.title.text == expected_title and
+            self.entities.breadcrumb.active_location == expected_title)
+
+
+class DeploymentRoleComparisonView(DeploymentRoleView):
+    """The page for comparison of Deployment Role"""
+    toolbar = View.nested(DeploymentRoleComparisonToolbar)
+    entities = View.nested(DeploymentRoleComparisonEntities)
+
+    @property
+    def is_displayed(self):
+        """Is this page currently being displayed"""
+        expected_title = 'Compare Cluster / Deployment Role'
+        return (
+            self.in_dep_role and
+            self.title.text == expected_title and
+            self.entities.breadcrumb.active_location == expected_title)
+
+
+class DeploymentRoleEditTagsView(DeploymentRoleView):
+    """The edit tags of Deployment Role"""
+    breadcrumb = BreadCrumb()
+    title = Text('#explorer_title_text')
+    select_tag = BootstrapSelect('tag_cat')
+    select_value = BootstrapSelect('tag_add')
+    save_button = Button('Save')
+    reset_button = Button('Reset')
+    cancel = Button('Cancel')
+
+    @property
+    def is_displayed(self):
+        """Is this page currently being displayed"""
+        return (
+            self.in_dep_role and
+            self.breadcrumb.active_location == 'Tag Assignment')
+
+
+class DeploymentRoleManagePoliciesView(DeploymentRoleView):
+    """Deployment role Manage Policies view."""
+    breadcrumb = BreadCrumb()
+    policies = BootstrapTreeview("protectbox")
+    save_button = Button("Save")
+    reset_button = Button("Reset")
+    cancel_button = Button("Cancel")
+
+    @property
+    def is_displayed(self):
+        """Is this page currently displayed"""
+        return (
+            self.in_dep_role and
+            (self.breadcrumb.active_location == "'Cluster / Deployment Role' Policy Assignment" or
+             self.breadcrumb.active_location == "'Deployment Role' Policy Assignment")
+        )
+
+
+class DeploymentRoles(Navigatable):
     """ Model of an infrastructure deployment roles in cfme
 
     Args:
@@ -31,39 +246,31 @@ class DeploymentRoles(Pretty, Navigatable):
         provider: provider this role is attached to
             (deployment roles available only for Openstack!).
     """
-    pretty_attrs = ['name', 'provider']
 
     def __init__(self, name, provider, appliance=None):
-        Navigatable.__init__(self, appliance=appliance)
-        if not isinstance(provider, OpenstackInfraProvider):
-            raise NotImplementedError('Deployment roles available only '
-                                      'for Openstack provider')
         self.name = name
         self.provider = provider
 
-    def get_detail(self, *ident):
-        """ Gets details from the details InfoBlock
+        if not provider.one_of(OpenstackInfraProvider):
+            raise NotImplementedError('Deployment roles available only '
+                                      'for Openstack provider')
+        Navigatable.__init__(self, appliance=appliance)
 
-        Args:
-            ident: An InfoBlock title, followed by the Key name,
-                   e.g. "Relationships", "All VMs"
+    def delete(self, cancel=False):
+        view = navigate_to(self, 'Details')
+        view.toolbar.configuration.item_select('Remove item',
+                                               handle_alert=not cancel)
 
-        Returns: A string representing the contents of the InfoBlock's value.
-        """
-        navigate_to(self, 'Details')
-        return details_page.infoblock.text(*ident)
-
-    def delete(self):
-        navigate_to(self, 'Details')
-        menu_item = version.pick({version.LOWEST: 'Remove from the VMDB',
-                                  '5.7': 'Remove item'})
-        cfg_btn(menu_item, invokes_alert=True)
-        sel.handle_alert(wait=60)
+        if not cancel:
+            view = self.create_view(DeploymentRoleAllView)
+            assert view.is_displayed
+            view.flash.assert_success_message("The selected Clusters / "
+                                              "Deployment Roles was deleted")
 
 
 @navigator.register(DeploymentRoles, 'All')
 class All(CFMENavigateStep):
-
+    VIEW = DeploymentRoleAllView
     prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
 
     def step(self):
@@ -73,59 +280,39 @@ class All(CFMENavigateStep):
         except NoSuchElementException:
             nav_select('Clusters / Deployment Roles')
 
-    def am_i_here(self):
-        def match(option):
-            return match_page(title=option, summary=option)
 
-        return match('Clusters / Deployment Roles') or match('Deployment Roles')
+@navigator.register(DeploymentRoles, 'Details')
+class Details(CFMENavigateStep):
+    VIEW = DeploymentRoleDetailsView
+    prerequisite = NavigateToSibling('All')
+
+    def step(self, *args, **kwargs):
+        """Navigate to the details page of Role"""
+        try:
+            self.prerequisite_view.entities.get_entity(by_name=self.obj.name).click()
+        except ItemNotFound:
+            raise RoleNotFound("Deployment Role {} not found".format(self.obj.name))
 
 
 @navigator.register(DeploymentRoles, 'AllForProvider')
 class AllForProvider(CFMENavigateStep):
-
-    def prerequisite(self):
-        navigate_to(self.obj.provider, 'Details')
+    VIEW = DeploymentRoleAllForProviderView
+    prerequisite = NavigateToAttribute('provider', 'Details')
 
     def step(self):
-        def list_acc_select(option):
-            list_acc.select('Relationships', 'Show all managed {}'.format(option), by_title=True,
-                            partial=False)
-
         try:
-            list_acc_select('Deployment Roles')
-        except ListAccordionLinkNotFound:
-            list_acc_select('Clusters / Deployment Roles')
-
-    def am_i_here(self):
-        def match(option):
-            summary_ptrn = "{} (All {})"
-            return match_page(summary=summary_ptrn.format(self.obj.provider.name, option),
-                              title=option)
-
-        return match('Deployment Roles') or match('Clusters / Deployment Roles')
-
-
-@navigator.register(DeploymentRoles, 'Details')
-class Details(CFMENavigateStep):
-
-    prerequisite = NavigateToSibling('All')
-
-    def step(self):
-        tb.select('Grid View')
-        sel.click(Quadicon(self.obj.name))
-
-    def am_i_here(self):
-        return match_page(summary="{} (Summary)".format(self.obj.name))
+            self.prerequisite_view.contents.relationships.click_at('Deployment Roles')
+        except NameError:
+            self.prerequisite_view.contents.relationships.click_at('Clusters / Deployment Roles')
 
 
 @navigator.register(DeploymentRoles, 'DetailsFromProvider')
 class DetailsFromProvider(CFMENavigateStep):
-
+    VIEW = DeploymentRoleDetailsView
     prerequisite = NavigateToSibling('AllForProvider')
 
     def step(self):
-        tb.select('Grid View')
-        sel.click(Quadicon(self.obj.name))
-
-    def am_i_here(self):
-        return match_page(summary="{} (Summary)".format(self.obj.name))
+        try:
+            self.prerequisite_view.entities.get_entity(by_name=self.obj.name).click()
+        except ItemNotFound:
+            raise RoleNotFound("Deployment Role {} not found".format(self.obj.name))
