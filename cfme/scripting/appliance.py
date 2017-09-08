bd6f11d38c5339b40caffe4021f5ea8c33bcb6ad
Luke Couzens <lcouzens@redhat.com>
2017-09-08 10:34:01 +0100

    Fixed line spaces

--- cfme/scripting/appliance.py
+++ cfme/scripting/appliance.py
@@ -71,125 +71,82 @@ def setup_embedded_ansible(appliance_ip, license):
 def setup_appliances(cfme_version, mode):
     """setup up appliances in a desired configuration"""
     TimedCommand = namedtuple('TimedCommand', ['command', 'timeout'])
-
     pwd = credentials['database']['password']
     cfme_version = cfme_version or get_or_create_current_appliance().version
     provider = cfme_data['basic_info']['ha_provider']
-
     if mode == 'distributed':
         """multi appliance single region configuration (distributed setup, 1st appliance has
         a local database and workers, 2nd appliance has workers pointing at 1st appliance)"""
-
         print "Provisioning and configuring distributed environment"
-
         apps = provision_appliances(count=2, cfme_version=cfme_version)
         opt = '5' if cfme_version >= "5.8" else '8'
         ip0 = apps[0].address
         port = (ip0, '') if cfme_version >= "5.8" else (ip0,)
-
         command_set0 = ('ap', '', opt, '1', '1', 'y', '1', 'n', '1', pwd,
             TimedCommand(pwd, 360), '')
-
         apps[0].appliance_console.run_commands(command_set0)
         apps[0].wait_for_evm_service()
         apps[0].wait_for_web_ui()
-
-        print "VMDB appliance provision and configured"
-
+        print "VMDB appliance provisioned and configured"
         command_set1 = ('ap', '', opt, '2', ip0, '', pwd, '', '3') + port + ('', '',
             pwd, TimedCommand(pwd, 360), '')
-
         apps[1].appliance_console.run_commands(command_set1)
         apps[1].wait_for_evm_service()
         apps[1].wait_for_web_ui()
-
         print "Non-VMDB appliance provisioned and configured"
-
     elif mode == 'ha':
         """multi appliance setup consisting of dedicated primary and standy databases with a single
         UI appliance."""
-
         print "Provisioning and configuring HA environment"
-
         apps = provision_appliances(count=3, cfme_version=cfme_version,
             provider=provider)
         ip0 = apps[0].address
         ip2 = apps[2].address
         opt = '5' if cfme_version >= "5.8" else '8'
         port = (ip0, '') if cfme_version >= "5.8" else (ip0,)
-
         command_set0 = ('ap', '', opt, '1', '1', 'y', '1', 'y', pwd, TimedCommand(pwd, 360), '')
-
         apps[0].appliance_console.run_commands(command_set0)
         wait_for(lambda: apps[0].db.is_dedicated_active)
-
         print "Dedicated database provisioned and configured"
-
         command_set1 = ('ap', '', opt, '1', '2', '1', 'y') + port + ('', '', pwd,
-                            TimedCommand(pwd, 360), '')
-
+            TimedCommand(pwd, 360), '')
         apps[1].appliance_console.run_commands(command_set1)
         apps[1].wait_for_evm_service()
         apps[1].wait_for_web_ui()
-
         print "Non-VMDB appliance provisioned and region created"
-
         command_set2 = ('ap', '', '6', '1', '1', '', '', pwd, pwd, ip0, 'y', '')
-
         apps[0].appliance_console.run_commands(command_set2)
-
         print "Primary HA node configured"
-
         command_set3 = ('ap', '', '6', '2', '1', '2', '', '', pwd, pwd, ip0, ip2, 'y', 'y', '')
-
         apps[2].appliance_console.run_commands(command_set3)
-
         print "Secondary HA node configured"
-
         command_set4 = ('ap', '', '9', '1', '')
-
         apps[1].appliance_console.run_commands(command_set4)
-
         print "HA configuration complete"
-
     elif mode == 'replication':
-
         """Multi appliance setup with multi region and replication from remote to global"""
-
         print "Provisioning and configuring replication environment"
-
         apps = provision_appliances(count=2, cfme_version=cfme_version)
         ip0 = apps[0].address
         opt = '5' if cfme_version >= "5.8" else '8'
-
         command_set0 = ('ap', '', opt, '1', '1', 'y', '1', 'n', '99', pwd,
             TimedCommand(pwd, 360), '')
-
         apps[0].appliance_console.run_commands(command_set0)
         apps[0].wait_for_evm_service()
         apps[0].wait_for_web_ui()
-
         print "Global region appliance provisioned and configured"
-
         command_set1 = ('ap', '', opt, '2', ip0, '', pwd, '', '1', 'y', '1', 'n', '1', pwd,
             TimedCommand(pwd, 360), '')
-
         apps[1].appliance_console.run_commands(command_set1)
         apps[1].wait_for_evm_service()
         apps[1].wait_for_web_ui()
-
         print "Remote region appliance provisioned and configured"
-
-        # Config remote
         print("Setup - Replication on remote appliance")
         apps[1].set_pglogical_replication(replication_type=':remote')
-
-        # Add subscription to global
         print("Setup - Replication on global appliance")
         apps[0].set_pglogical_replication(replication_type=':global')
         apps[0].add_pglogical_replication_subscription(apps[1].address)
         print("Done!")
-
     else:
         print 'You must select a mode, such as --ha / --distrubuted / --replication'
         sys.exit(127)
