cd21cbec3007d82d0a66b86c3bc8ef8312b2fc3e
Nikhil Dhandre <ndhandre@redhat.com>
2017-09-21 16:15:49 +0530

    Initial Commit

---
+++ cfme/storage/manager.py
@@ -0,0 +1,239 @@
+from cfme.utils.appliance import NavigatableMixin
+from navmazing import NavigateToAttribute, NavigateToSibling
+from cfme.utils.appliance.implementations.ui import CFMENavigateStep, navigator, navigate_to
+from widgetastic.utils import Version, VersionPick
+from cfme.base.ui import BaseLoggedInPage
+from widgetastic_manageiq import (Accordion,
+                                  BootstrapSelect,
+                                  BootstrapTreeview,
+                                  BreadCrumb,
+                                  ManageIQTree,
+                                  PaginationPane,
+                                  SummaryTable,
+                                  Table
+                                  )
+from widgetastic_patternfly import (Button,
+                                    Dropdown,
+                                    FlashMessages
+                                    )
+from widgetastic.widget import View, NoSuchElementException, Text
+from cfme.exceptions import ItemNotFound
+
+
+class ManagerToolbar(View):
+    """The toolbar on the Storage Manager or Provider page"""
+    configuration = Dropdown('Configuration')
+    policy = Dropdown('Policy')
+
+
+class ManagerDetailsToolbar(View):
+    """The toolbar on the Storage Manager or Provider detail page"""
+    reload = Button(title='Reload Current Display')
+    configuration = Dropdown('Configuration')
+    policy = Dropdown('Policy')
+    monitoring = Dropdown('Monitoring')
+    download = Button(title='Download summary in PDF format')
+
+
+class ManagerEntities(View):
+    """The entities on the main list Storage Manager or Provider page"""
+    table = Table(".//div[@id='list_grid']/table")
+
+
+class ManagerDetailsEntities(View):
+    """The entities on the Storage Manager or Provider details page"""
+    breadcrumb = BreadCrumb()
+    properties = SummaryTable('Properties')
+    relationships = SummaryTable('Relationships')
+    smart_management = SummaryTable('Smart Management')
+    status = SummaryTable('Status')
+
+
+class ManagerDetailsAccordion(View):
+    """The accordion on the Storage Manager or Provider details page"""
+    @View.nested
+    class properties(Accordion):  # noqa
+        tree = ManageIQTree()
+
+    @View.nested
+    class relationships(Accordion):  # noqa
+        tree = ManageIQTree()
+
+
+class ManagerView(BaseLoggedInPage):
+    """A base view for all the Storage Manager or Provider pages"""
+    title = Text('.//div[@id="center_div" or @id="main-content"]//h1')
+
+    flash = FlashMessages(
+        './/div[@id="flash_msg_div"]/div[@id="flash_text_div" or '
+        'contains(@class, "flash_text_div")]')
+
+    @property
+    def in_manager(self):
+        nav = self.context['object'].nav.pick(self.context['object'].appliance.version)
+        return(
+            self.logged_in_as_current_user and
+            self.navigation.currently_selected == nav)
+
+
+class ManagerAllView(ManagerView):
+    """The all Storage Manager or Provider page"""
+    @property
+    def is_displayed(self):
+        """Is this page currently being displayed"""
+        return (
+            self.in_manager and
+            self.title.text in ('Storage Managers', self.context['object'].type))
+
+    toolbar = View.nested(ManagerToolbar)
+    entities = View.nested(ManagerEntities)
+    paginator = PaginationPane()
+
+
+class ManagerDetailsView(ManagerView):
+    """The details page for Storage Manager or Provider"""
+    @property
+    def is_displayed(self):
+        expected_title = '{} (Summary)'.format(self.context['object'].name)
+
+        return(
+            self.title.text == expected_title and
+            self.entities.breadcrumb.active_location == expected_title)
+
+    toolbar = View.nested(ManagerDetailsToolbar)
+    sidebar = View.nested(ManagerDetailsAccordion)
+    entities = View.nested(ManagerDetailsEntities)
+
+
+class ManagerTagsView(ManagerView):
+    """The tag page for Storage Manager or Provider"""
+    breadcrumb = BreadCrumb()
+    select_tag = BootstrapSelect('tag_cat')
+    select_value = BootstrapSelect('tag_add')
+    save_button = Button('Save')
+    reset_button = Button('Reset')
+    cancel = Button('Cancel')
+
+    @property
+    def is_displayed(self):
+        """Is this page currently being displayed"""
+        return (
+            self.in_manager and
+            self.breadcrumb.active_location == 'Tag Assignment')
+
+
+class ManagePoliciesView(ManagerView):
+    """The policies page for Storage Manager or Provider"""
+    breadcrumb = BreadCrumb()
+    policies = BootstrapTreeview("protectbox")
+    save_button = Button("Save")
+    reset_button = Button("Reset")
+    cancel_button = Button("Cancel")
+
+    @property
+    def is_displayed(self):
+        """Is this page currently displayed"""
+        return (
+            self.in_manager and
+            self.breadcrumb.active_location == "'Storage Manager' Policy Assignment")
+
+
+class Manager(NavigatableMixin):
+    """ Its represents Base Manager class for block and object manager classes.
+    It should be initialized by the child classes.
+    """
+    def __init__(self, name, provider, appliance, nav, type):
+        self.name = name
+        self.provider = provider
+        self.appliance = appliance
+        self.nav = nav
+        self.type = type
+        self.name_vari = VersionPick({Version.lowest(): 'Storage Provider',
+                                  '5.8': 'Storage Manager'}).pick(self.appliance.version)
+
+    def refresh(self, cancel=False):
+        """Refresh storage manager"""
+        view = navigate_to(self, 'Details')
+        view.toolbar.configuration.item_select('Refresh Relationships and Power States',
+                                               handle_alert=not cancel)
+
+        if not cancel:
+            msg = "Refresh Provider initiated for 1 {} from the CFME Database"\
+                .format(self.name_vari)
+            view.flash.assert_success_message(msg)
+
+    def delete(self, wait=True):
+        """Delete storage manager"""
+        view = navigate_to(self, 'Details')
+        view.toolbar.configuration.item_select(
+            'Remove this {}'.format(self.name_vari), handle_alert=True)
+
+        msg = "Delete initiated for 1 {} from the CFME Database".format(self.name_vari)
+        view.flash.assert_success_message(msg)
+
+
+class BlockManager(Manager):
+    """ Model of an storage block manager in cfme
+
+    Args:
+        name: Name of the block manager.
+        provider: provider
+        appliance: appliance
+    """
+    def __init__(self, name, provider, appliance):
+        self.nav = VersionPick({
+            Version.lowest(): ['Storage', 'Storage Providers'],
+            '5.8': ['Storage', 'Block Storage', 'Managers']})
+        self.type = 'Block Storage Managers'
+        self.provider = provider
+        self.appliance = appliance
+        self.name = name
+
+        super(BlockManager, self).__init__(name=self.name, provider=self.provider,
+                                            appliance=self.appliance,nav=self.nav, type=self.type)
+
+
+class ObjectManager(Manager):
+    """ Model of an storage object manager in cfme
+
+    Args:
+        name: Name of the object manager.
+        provider: provider
+        appliance: appliance
+    """
+
+    def __init__(self, name, provider, appliance):
+        self.nav = VersionPick({
+            Version.lowest(): ['Storage', 'Storage Providers'],
+            '5.8': ['Storage', 'Object Storage', 'Managers']})
+        self.type = 'Object Storage Managers'
+        self.provider = provider
+        self.appliance = appliance
+        self.name = name
+
+        super(ObjectManager, self).__init__(name=self.name, provider=self.provider,
+                                            appliance=self.appliance,nav=self.nav, type=self.type)
+
+
+@navigator.register(Manager, 'All')
+class ManagerAll(CFMENavigateStep):
+    VIEW = ManagerAllView
+    prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
+
+    def step(self, *args, **kwargs):
+        nav = self.obj.nav.pick(self.obj.appliance.version)
+        self.prerequisite_view.navigation.select(*nav)
+
+
+@navigator.register(Manager, 'Details')
+class ManagerDetails(CFMENavigateStep):
+    VIEW = ManagerDetailsView
+    prerequisite = NavigateToSibling('All')
+
+    def step(self, *args, **kwargs):
+        try:
+            row = self.prerequisite_view.paginator.find_row_on_pages(
+                self.prerequisite_view.entities.table, Name=self.obj.name)
+            row.click()
+        except NoSuchElementException:
+            raise ItemNotFound('Could not locate {}'.format(self.obj.name))
---
+++ cfme/tests/storage/test_manager.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from cfme import test_requirements
+from cfme.cloud.provider import CloudProvider
+from cfme.cloud.provider.openstack import OpenStackProvider
+from cfme.storage.manager import ObjectManager, BlockManager
+from cfme.utils import testgen
+from cfme.utils.appliance.implementations.ui import navigate_to
+
+
+pytest_generate_tests = testgen.generate([CloudProvider])
+
+
+pytestmark = [pytest.mark.tier(3),
+              test_requirements.storage,
+              pytest.mark.usefixtures('openstack_provider', 'setup_provider')]
+
+
+@pytest.mark.uncollectif(lambda provider: not provider.one_of(OpenStackProvider))
+def test_block_manager_navigation(openstack_provider, appliance):
+    """Simple test for navigation destinations"""
+    block_name = openstack_provider.name + ' Cinder Manager'
+    block_manager = BlockManager(name=block_name, provider=openstack_provider, appliance=appliance)
+
+    view = navigate_to(block_manager, 'All')
+    assert view.is_displayed
+
+    view = navigate_to(block_manager, 'Details')
+    assert view.is_displayed
+
+    block_manager.refresh()
+
+
+@pytest.mark.uncollectif(lambda provider: not provider.one_of(OpenStackProvider))
+def test_object_manager_navigation(openstack_provider, appliance):
+    """Simple test for navigation destinations"""
+    object_name = openstack_provider.name + ' Swift Manager'
+    object_manager = ObjectManager(name=object_name, provider=openstack_provider,
+                                  appliance=appliance)
+
+    view = navigate_to(object_manager, 'All')
+    assert view.is_displayed
+
+    view = navigate_to(object_manager, 'Details')
+    assert view.is_displayed
+
+    object_manager.refresh()
