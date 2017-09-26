b616e760bd47f011e13f0f47ad54cfad2164df64
Ruslana Babyuk <rbabyuk@redhat.com>
2017-09-26 15:25:54 +0300

    Fix for test_tag_vis_vm (#5444)
    * Fix for test_tag_vis_vm
    
    * filters update

--- cfme/tests/cloud_infra_common/test_tag_visibility.py
+++ cfme/tests/cloud_infra_common/test_tag_visibility.py
@@ -1,88 +1,28 @@
-import fauxfactory
 import pytest
 
-import cfme.configure.access_control as ac
 from cfme import test_requirements
-from cfme.base.credential import Credential
 from cfme.common.vm import VM
-from cfme.configure.configuration import Tag, Category
 from cfme.utils import testgen
 
 
 def pytest_generate_tests(metafunc):
-    argnames, argvalues, idlist = testgen.all_providers(metafunc, required_fields=['ownership_vm'])
+    argnames, argvalues, idlist = testgen.all_providers(metafunc, required_fields=['cap_and_util'])
     testgen.parametrize(metafunc, argnames, argvalues, ids=idlist, scope="module")
-
 
 pytestmark = [test_requirements.tag]
 
 
 @pytest.yield_fixture(scope="module")
-def new_category():
-    category = Category(name="tag_vis_" + fauxfactory.gen_alpha(8).lower(),
-                        description="tag_vis_" + fauxfactory.gen_alphanumeric(),
-                        display_name="tag_vis_" + fauxfactory.gen_alphanumeric())
-    category.create()
-    yield category
-    category.delete(cancel=False)
-
-
-@pytest.yield_fixture(scope="module")
-def new_tag(new_category):
-    category = Category(name=new_category.name, display_name=new_category.display_name)
-    tag = Tag(name="tag_vis_" + fauxfactory.gen_alphanumeric().lower(),
-              display_name="tag_vis_" + fauxfactory.gen_alphanumeric().lower(),
-              category=category)
-    tag.create()
-    yield tag
-    tag.delete(cancel=False)
-
-
-@pytest.yield_fixture(scope="module")
-def new_role():
-    role = ac.Role(name='tag_vis_role_' + fauxfactory.gen_alphanumeric())
-    role.create()
-    yield role
-    role.delete()
-
-
-@pytest.yield_fixture(scope="module")
-def new_group(new_tag, new_role):
-    group = ac.Group(description='tag_vis_group_' + fauxfactory.gen_alphanumeric(),
-                     role=new_role.name)
-    group.create()
-    group.edit_tags(new_tag.category.display_name + " *", new_tag.display_name)
-    yield group
-    group.delete()
-
-
-def new_credential():
-    return Credential(principal='uid' + fauxfactory.gen_alphanumeric(), secret='redhat')
-
-
-@pytest.yield_fixture(scope="module")
-def new_user(new_group):
-    user = ac.User(name='user_' + fauxfactory.gen_alphanumeric(),
-                   credential=new_credential(),
-                   email='abc@redhat.com',
-                   group=new_group)
-    user.create()
-    yield user
-    user.appliance.server.login_admin()
-    user.delete()
-
-
-@pytest.yield_fixture(scope="module")
-def tagged_vm(new_tag, setup_provider_modscope, provider):
-    ownership_vm = provider.data['ownership_vm']
+def tagged_vm(tag, setup_provider_modscope, provider):
+    ownership_vm = provider.data.cap_and_util.capandu_vm
     tag_vm = VM.factory(ownership_vm, provider)
-    tag_vm.add_tag(new_tag)
+    tag_vm.add_tag(tag)
     yield tag_vm
     tag_vm.appliance.server.login_admin()
-    tag_vm.remove_tag(new_tag)
+    tag_vm.remove_tag(tag)
 
 
 @pytest.mark.tier(3)
-def test_tag_vis_vm(request, tagged_vm, new_user):
-    with new_user:
+def test_tag_vis_vm(tagged_vm, user_restricted):
+    with user_restricted:
         assert tagged_vm.exists, "vm not found"
