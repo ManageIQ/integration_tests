840a4be4c17fa96611a9aef6480403ce790e6b10
Nikhil Dhandre <ndhandre@redhat.com>
2017-10-03 19:06:11 +0530

    Lint

--- cfme/tests/storage/test_object_store_container.py
+++ cfme/tests/storage/test_object_store_container.py
@@ -34,4 +34,3 @@ def test_add_remove_tag(containers):
     container.remove_tag('Department', 'Communication')
     tag_available = container.get_tags()
     assert 'Department' not in tag_available and 'Communication' not in tag_available
-    
