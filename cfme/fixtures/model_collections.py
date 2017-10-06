2f4602c93d74f650d8ee50bfdeb7dd4f09f9df47
NIKHIL DHANDRE <ndhandre@redhat.com>
2017-10-06 15:37:42 +0530

    [1LP][RFR] Object Store Object WT and Collection (#5487)
    * Initial Commit OSO
    
    * Lint
    
    * uncollectif <5.8
    
    * change test name
    
    * Oject use avoid, add fixture to model_collections
    
    * miss line spece
    
    * correct fixture
    
    * removed changes in BaseEntityView
    
    * accordian to sidebar

--- cfme/fixtures/model_collections.py
+++ cfme/fixtures/model_collections.py
@@ -6,3 +6,9 @@ import pytest
 def dashboards(appliance):
     from cfme.dashboard import DashboardCollection
     return DashboardCollection(appliance=appliance)
+
+
+@pytest.fixture(scope="function")
+def objects(appliance):
+    from cfme.storage.object_store_object import ObjectStoreObjectCollection
+    return ObjectStoreObjectCollection(appliance=appliance)
---
+++ cfme/tests/storage/test_object_store_object.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import pytest
+import random
+
+from cfme.cloud.provider.openstack import OpenStackProvider
+from cfme.utils import testgen, version
+
+
+pytestmark = pytest.mark.usefixtures("setup_provider")
+
+
+pytest_generate_tests = testgen.generate([OpenStackProvider], scope="module")
+
+
+@pytest.mark.uncollectif(lambda: version.current_version() < '5.8')
+@pytest.mark.tier(3)
+def test_object_add_remove_tag(objects, provider):
+    all_objects = objects.all(provider)
+    obj = random.choice(all_objects)
+
+    # add tag with category Department and tag communication
+    obj.add_tag('Department', 'Communication')
+    tag_available = obj.get_tags()
+    assert 'Department' in tag_available and 'Communication' in tag_available
+
+    # remove assigned tag
+    obj.remove_tag('Department', 'Communication')
+    tag_available = obj.get_tags()
+    assert 'Department' not in tag_available and 'Communication' not in tag_available
