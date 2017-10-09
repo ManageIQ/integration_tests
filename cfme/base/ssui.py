3c12363750536d76812dd3c5c2648476c30b34c4
shveta <sshveta@redhat.com>
2017-10-09 13:53:59 -0700

    Sentaku Implementation for Login

--- cfme/base/ssui.py
+++ cfme/base/ssui.py
@@ -1,8 +1,10 @@
 from . import Server
 
+from selenium.webdriver.common.keys import Keys
+
 from navmazing import NavigateToSibling
 from widgetastic.widget import View, ParametrizedView
-from widgetastic_patternfly import NavDropdown, FlashMessages, Input, Button
+from widgetastic_patternfly import NavDropdown, FlashMessages, Input, Button, Text
 from widgetastic_manageiq import SSUIVerticalNavigation
 from widgetastic.utils import ParametrizedLocator
 
@@ -25,6 +27,7 @@ class SSUIBaseLoggedInPage(View):
     help = NavDropdown('.//li[./a[@id="dropdownMenu1"]]')
     navigation = SSUIVerticalNavigation('//ul[@class="list-group"]')
     domain_switcher = Button(id="domain-switcher")
+    shopping_cart = Text('.//li/a[@title="Shopping cart"]')
 
     @ParametrizedView.nested
     class settings(ParametrizedView):  # noqa
@@ -68,7 +71,7 @@ class SSUIBaseLoggedInPage(View):
     def logged_in(self):
         return (
             self.settings(self.extra.appliance.user.credential.principal).is_displayed() and
-            self.domain_switcher.is_displayed)
+            self.shopping_cart.is_displayed)
 
     @property
     def logged_out(self):
@@ -86,6 +89,31 @@ class LoginPage(View):
     password = Input(id='inputPassword')
     login = Button('Log In')
 
+    def submit_login(self, method='click_on_login'):
+        if method == 'click_on_login':
+            self.login.click()
+        elif method == 'press_enter_after_password':
+            self.browser.send_keys(Keys.ENTER, self.password)
+        else:
+            raise ValueError('Unknown method {}'.format(method))
+        if self.flash.is_displayed:
+            self.flash.assert_no_error()
+
+    def log_in(self, user, method='click_on_login'):
+        self.fill({
+            'username': user.credential.principal,
+            'password': user.credential.secret,
+        })
+        self.submit_login(method)
+        logged_in_view = self.browser.create_view(SSUIBaseLoggedInPage)
+        if logged_in_view.logged_in:
+            if user.name is None:
+                name = logged_in_view.current_fullname
+                self.logger.info(
+                    'setting the appliance.user.name to %r because it was not specified', name)
+                user.name = name
+            self.extra.appliance.user = user
+
 
 @Server.address.external_implementation_for(ViaSSUI)
 def address(self):
@@ -93,8 +121,11 @@ def address(self):
     return 'https://{}/self_service/'.format(self.appliance.address)
 
 
+LOGIN_METHODS = ['click_on_login', 'press_enter_after_password']
+
+
 @Server.login.external_implementation_for(ViaSSUI)
-def login(self, user=None, **kwargs):
+def login(self, user=None, method=LOGIN_METHODS[-1]):
     if not user:
         username = conf.credentials['default']['username']
         password = conf.credentials['default']['password']
@@ -110,15 +141,30 @@ def login(self, user=None, **kwargs):
         logged_in_view.logout()
 
     login_view = navigate_to(self.appliance.server, 'LoginScreen')
-    login_view.fill({
-        'username': user.credential.principal,
-        'password': user.credential.secret,
-    })
-    login_view.login.click()
+    login_view.log_in(user, method=method)
     # Without this the login screen just exits after logging in
     time.sleep(3)
     login_view.flash.assert_no_error()
     self.browser.plugin.ensure_page_safe()
+    assert logged_in_view.is_displayed
+    return logged_in_view
+
+
+@Server.login_admin.external_implementation_for(ViaSSUI)
+def login_admin(self, **kwargs):
+    """
+    Convenience function to log into CFME using the admin credentials from the yamls.
+    Args:
+        kwargs: A dict of keyword arguments to supply to the :py:meth:`login` method.
+    """
+    username = conf.credentials['default']['username']
+    password = conf.credentials['default']['password']
+    cred = Credential(principal=username, secret=password)
+    from cfme.configure.access_control import User
+    user = User(credential=cred)
+    user.name = 'Administrator'
+    logged_in_view = self.login(user, **kwargs)
+    return logged_in_view
 
 
 @navigator.register(Server)
