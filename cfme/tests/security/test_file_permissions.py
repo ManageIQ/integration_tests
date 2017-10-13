5097c2608143f62f1912a1edac83b1f5e75dad02
Alex Newman <anewman@redhat.com>
2017-10-13 16:53:15 -0400

    Rework for review

--- cfme/tests/security/test_file_permissions.py
+++ cfme/tests/security/test_file_permissions.py
@@ -1,10 +1,5 @@
-import pytest
-from cfme.utils import version
-
-
-@pytest.mark.uncollectif(lambda: version.current_version() < '5.6', reason='Only valid for >5.7')
 def test_v2_key_permissions(appliance):
     """Verifies that the v2_key has proper permissions"""
     stdout = appliance.ssh_client.run_command(
-        "stat --format '%a' /var/www/miq/vmdb/certs/v2_key")[1]
+        "stat --format '%a' /var/www/miq/vmdb/certs/v2_key").output
     assert int(stdout) == 400
