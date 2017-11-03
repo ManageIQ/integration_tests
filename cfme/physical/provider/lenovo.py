2a5031d0b975c3723cda183d357e61985c08f90c
Pete Savage <psavage@redhat.com>
2017-11-03 21:26:36 +0000

    Added Lenovo Provider base views etc

--- cfme/physical/provider/lenovo.py
+++ cfme/physical/provider/lenovo.py
@@ -1,14 +1,37 @@
+from cfme.common.provider import DefaultEndpoint, DefaultEndpointForm
+
 from . import PhysicalProvider
 
 
+class LenovoEndpoint(DefaultEndpoint):
+    pass
+
+
+class LenovoEndpointForm(DefaultEndpointForm):
+    pass
+
+
 class LenovoProvider(PhysicalProvider):
-    def __init__(self, appliance, name):
-        super(LenovoProvider, self).__init__(appliance, name)
+    type_name = 'lenovo'
+    endpoints_form = LenovoEndpointForm
+    string_name = "Ems Physical Infras"
+
+    def __init__(self, appliance, name=None, key=None, endpoints=None):
+        super(LenovoProvider, self).__init__(
+            appliance=appliance, name=name, key=key, endpoints=endpoints
+        )
 
     @classmethod
     def from_config(cls, prov_config, prov_key, appliance=None):
-        # Will need an endpoint created
-        # endpoint = VirtualCenterEndpoint(**prov_config['endpoints']['default'])
-
+        endpoint = LenovoEndpoint(**prov_config['endpoints']['default'])
         return cls(name=prov_config['name'],
+                   endpoints={endpoint.name: endpoint},
+                   key=prov_key,
                    appliance=appliance)
+
+    @property
+    def view_value_mapping(self):
+        return {
+            'name': self.name,
+            'prov_type': 'Lenovo XClarity'
+        }
