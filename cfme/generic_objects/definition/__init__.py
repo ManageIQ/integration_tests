3c0569449a10c14fb1b5a930fb691896ca9d80cb
Martin Kourim <mkourim@redhat.com>
2017-11-10 15:13:48 +0100

    REST only ``exists``; getting rid of ``Pretty``

--- cfme/generic_objects/definition/__init__.py
+++ cfme/generic_objects/definition/__init__.py
@@ -5,20 +5,18 @@ import attr
 import sentaku
 
 from cfme.modeling.base import BaseCollection, BaseEntity
-from cfme.utils.pretty import Pretty
 from cfme.utils.update import Updateable
 
 
 @attr.s
-class GenericObjectDefinition(Pretty, BaseEntity, Updateable, sentaku.modeling.ElementMixin):
+class GenericObjectDefinition(BaseEntity, Updateable, sentaku.modeling.ElementMixin):
     """Generic Objects Definition class to context switch between UI and REST.
 
     Read/Update/Delete functionality.
     """
-    pretty_attrs = ['name', 'description', 'attributes', 'associations', 'methods']
-
     update = sentaku.ContextualMethod()
     delete = sentaku.ContextualMethod()
+    exists = sentaku.ContextualProperty()
 
     name = attr.ib(default=None)
     description = attr.ib(default=None)
@@ -28,11 +26,6 @@ class GenericObjectDefinition(Pretty, BaseEntity, Updateable, sentaku.modeling.E
 
     def __attrs_post_init__(self):
         self.rest_response = None
-
-    @property
-    def exists(self):
-        return bool(
-            self.appliance.rest_api.collections.generic_object_definitions.find_by(name=self.name))
 
 
 @attr.s
