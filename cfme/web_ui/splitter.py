3b569d26db98f37067237dfa2d97d4b75f653096
Pete Savage <psavage@redhat.com>
2017-11-15 10:55:06 +0000

    Converting splitter to WT
    * Splitter is now WT
    * Splitter test was causing failures where the test failed and the
      view was not reset
    * Disabled locations that didn't work due to navigation steps requiring
      the sidebar

--- cfme/web_ui/splitter.py
+++
@@ -1,9 +0,0 @@
-from cfme.fixtures import pytest_selenium as sel
-
-
-def pull_splitter_left():
-    sel.click("//span[@class='fa fa-angle-left']")
-
-
-def pull_splitter_right():
-    sel.click("//span[@class='fa fa-angle-right']")
