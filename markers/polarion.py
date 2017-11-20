a9aa2832b38ec999f35f1f7421da8a47ca6a0760
Shalom Naim <snaim@redhat.com>
2017-11-20 14:11:29 +0200

    Add a polation marker

--- markers/polarion.py
+++ markers/polarion.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """polarion(\*tcid): Marker for marking tests as automation for polarion test cases."""
+import pytest
 
 
 def pytest_configure(config):
@@ -13,3 +14,13 @@ def extract_polarion_ids(item):
         return None
 
     return map(str, polarion.args)
+
+
+@pytest.fixture(autouse=True)
+def _update_polarion_in_junit(request, record_xml_property):
+    """Adds the supplied test case id to the xunit file as a property"""
+    from functools import partial
+    record_id_to_xml = partial(record_xml_property, ("test_id",))
+    ids = extract_polarion_ids(request.node)
+    if ids is not None:
+        map(record_id_to_xml, ids)
