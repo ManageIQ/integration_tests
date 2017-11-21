3ff9d9e722345b986a2a579197d55114cac531cf
skovic <skovic@lenovo.com>
2017-11-21 14:56:57 -0500

    Add test for physical servers page dropdown menus

--- cfme/tests/physical_infrastructure/test_physical_server.py
+++ cfme/tests/physical_infrastructure/test_physical_server.py
@@ -19,3 +19,32 @@ def test_physical_servers_view_displayed(physical_server_collection):
     """Navigate to the physical servers page and verify that servers are displayed"""
     physical_servers_view = navigate_to(physical_server_collection, 'All')
     assert physical_servers_view.is_displayed
+
+def test_physical_servers_view_dropdowns(physical_server_collection):
+    """Navigate to the physical servers page and verify that the dropdown menus are present"""
+    physical_servers_view = navigate_to(physical_server_collection, 'All')
+
+    configuration_items = physical_servers_view.toolbar.configuration.items
+    assert "Refresh Relationships and Power States" in configuration_items
+    assert "Remove Physical Servers from Inventory" in configuration_items
+
+    power_items = physical_servers_view.toolbar.power.items
+    assert "Power On" in power_items
+    assert "Power Off" in power_items
+    assert "Power Off Immediately" in power_items
+    assert "Restart" in power_items
+    assert "Restart Immediately" in power_items
+    assert "Restart to System Setup" in power_items
+    assert "Restart Management Controller" in power_items
+
+    identify_items = physical_servers_view.toolbar.identify.items
+    assert "Blink LED" in identify_items
+    assert "Turn On LED" in identify_items
+    assert "Turn Off LED" in identify_items
+
+    policy_items = physical_servers_view.toolbar.policy.items
+    assert "Manage Policies" in policy_items
+    assert "Edit Tags" in policy_items
+
+    lifecycle_items = physical_servers_view.toolbar.lifecycle.items
+    assert "Provision Physical Server" in lifecycle_items
