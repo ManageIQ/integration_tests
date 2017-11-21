a1614c9f2cf0ff70a960811cc3019b08e6485710
Shalom Naim <snaim@redhat.com>
2017-11-21 14:41:22 +0200

    move partial improt to the top of the module

--- markers/polarion.py
+++ markers/polarion.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """polarion(\*tcid): Marker for marking tests as automation for polarion test cases."""
 import pytest
+from functools import partial
 
 
 def pytest_configure(config):
@@ -19,8 +20,8 @@ def extract_polarion_ids(item):
 @pytest.fixture(autouse=True)
 def _update_polarion_in_junit(request, record_xml_property):
     """Adds the supplied test case id to the xunit file as a property"""
-    from functools import partial
-    record_id_to_xml = partial(record_xml_property, ("test_id",))
+
+    record_id_to_xml = partial(record_xml_property, "test_id")
     ids = extract_polarion_ids(request.node)
     if ids is not None:
         map(record_id_to_xml, ids)
