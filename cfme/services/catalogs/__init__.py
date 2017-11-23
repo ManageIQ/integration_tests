c9125544d6b56c2d688f4b533d248090066acca3
izapolsk <izapolsk@redhat.com>
2017-11-23 17:09:24 +0200

    fix for catalogs view which doesn't follow our conventions about view structure

--- cfme/services/catalogs/__init__.py
+++ cfme/services/catalogs/__init__.py
@@ -18,7 +18,8 @@ class ServicesCatalogView(BaseLoggedInPage):
 
     @property
     def is_displayed(self):
-        return self.in_explorer and self.configuration.is_displayed and not self.catalogs.is_dimmed
+        return (self.in_explorer and self.toolbar.configuration.is_displayed and
+                not self.catalogs.is_dimmed)
 
     @View.nested
     class service_catalogs(Accordion):  # noqa
@@ -42,8 +43,16 @@ class ServicesCatalogView(BaseLoggedInPage):
     class catalogs(Accordion):  # noqa
         tree = ManageIQTree()
 
-    configuration = Dropdown('Configuration')
-    policy = Dropdown('Policy')
+    @View.nested
+    class toolbar(View):
+        configuration = Dropdown('Configuration')
+        policy = Dropdown('Policy')
+
+    # for backward compatibility. it is difficult to figure out where those are used
+    # TODO: this should be fixed by this code owner
+    configuration = toolbar.configuration
+    policy = toolbar.policy
+
     flash = FlashMessages(".//div[@id='flash_msg_div']/div")
 
 
