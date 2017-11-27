4685b89c829653e2936e4d7089b5e0a0d9c37fa5
Anurag Sinha <anuragsinhab@gmail.com>
2017-11-27 12:37:23 +0530

    Making changes related to exceptions.

--- cfme/infrastructure/datastore.py
+++ cfme/infrastructure/datastore.py
@@ -91,7 +91,7 @@ class JSDatastoreEntity(JSBaseEntity):
                 data_dict['no_vm'] = quad_data.xpath(self.QUADRANT.format(pos="b"))[0].get()
                 data_dict['no_host'] = quad_data.xpath(self.QUADRANT.format(pos="c"))[0].get()
             return data_dict
-        except (IndexError, TypeError, NoSuchElementException):
+        except IndexError:
             return {}
 
 
