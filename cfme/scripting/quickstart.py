ba63c2e5ec8048d9b8317603c2c68b82f9d40c4e
Ronnie Rasouli <rrasouli@redhat.com>
2017-11-29 15:58:54 +0200

    Update quickstart.py
    adding libpng-devel that was missing

--- cfme/scripting/quickstart.py
+++ cfme/scripting/quickstart.py
@@ -66,6 +66,7 @@ REDHAT_PACKAGES_SPECS = [
      " libxslt-devel zeromq3-devel libcurl-devel"
      " redhat-rpm-config gcc-c++ openssl-devel"
      " libffi-devel python-devel tesseract"
+     " libpng-devel"
      " freetype-devel")
 ]
 
