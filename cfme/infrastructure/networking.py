3fcd3ace1b83246473e25717707ab08895125d38
Pete Savage <psavage@redhat.com>
2017-11-29 12:27:41 +0000

    Removed web_ui from networking

--- cfme/infrastructure/networking.py
+++ cfme/infrastructure/networking.py
@@ -1,7 +1,11 @@
-from cfme.utils.appliance import Navigatable
 from navmazing import NavigateToAttribute
+from widgetastic.widget import View
+from widgetastic_patternfly import Dropdown
+
+from cfme.base.ui import BaseLoggedInPage
+from cfme.utils.appliance import Navigatable
 from cfme.utils.appliance.implementations.ui import navigator, CFMENavigateStep
-from cfme.web_ui import toolbar as tb
+from widgetastic_manageiq import PaginationPane, ItemsToolBarViewSelector, Text
 
 
 class InfraNetworking(Navigatable):
@@ -9,8 +13,46 @@ class InfraNetworking(Navigatable):
         Navigatable.__init__(self, appliance)
 
 
+class InfraNetworkingView(BaseLoggedInPage):
+    """Base view for header and nav checking, navigatable views should inherit this"""
+
+    @property
+    def in_infra_networking(self):
+        nav_chain = ['Compute', 'Infrastructure', 'Networking']
+        return (
+            self.logged_in_as_current_user and
+            self.navigation.currently_selected == nav_chain)
+
+
+class InfraNetworkingToolbar(View):
+    """The toolbar on the main page"""
+    policy = Dropdown('Policy')
+
+    view_selector = View.nested(ItemsToolBarViewSelector)
+
+
+class InfraNetworkingEntities(View):
+    """Entities on the main page"""
+    title = Text('//div[@id="main-content"]//h1')
+
+
+class InfraNetworkingAllView(InfraNetworkingView):
+    """The "all" view -- a list"""
+    @property
+    def is_displayed(self):
+        return (
+            self.in_infra_networking and
+            self.entities.title.text == 'All Switches')
+
+    toolbar = View.nested(InfraNetworkingToolbar)
+    entities = View.nested(InfraNetworkingEntities)
+    paginator = PaginationPane()
+
+
 @navigator.register(InfraNetworking, 'All')
 class All(CFMENavigateStep):
+    VIEW = InfraNetworkingAllView
+
     prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
 
     def step(self):
@@ -18,4 +60,4 @@ class All(CFMENavigateStep):
 
     def resetter(self):
         # Reset view and selection
-        tb.select("Grid View")
+        self.view.toolbar.view_selector.select('Grid View')
