66c3b24ec97dfc410b2db58db0044f781b32d78f
Eugene Zapolsky <izapolsk@redhat.com>
2017-12-22 13:20:38 +0200

    [RFR] make azure clean up script remove stale resources from any subscription and any resource groups (#6232)
    * updated requirements to include fresh wrapanapi
    
    * added overriding default logger

--- scripts/azure_cleanup.py
+++ scripts/azure_cleanup.py
@@ -1,11 +1,11 @@
 import argparse
+import logging
 import sys
 import traceback as tb
 from datetime import datetime
-from tabulate import tabulate
 
 from cfme.utils.path import log_path
-from cfme.utils.providers import list_provider_keys, get_mgmt
+from cfme.utils.providers import list_provider_keys, get_crud, get_mgmt
 
 
 def parse_cmd_line():
@@ -27,74 +27,102 @@ def parse_cmd_line():
     return args
 
 
-def azure_cleanup(nic_template, pip_template, days_old, output):
-    with open(output, 'w') as report:
-        report.write('azure_cleanup.py, NICs, PIPs, Disks and Stack Cleanup')
-        report.write("\nDate: {}\n".format(datetime.now()))
+def azure_cleanup(nic_template, pip_template, days_old):
+        logger.info('azure_cleanup.py, NICs, PIPs, Disks and Stack Cleanup')
+        logger.info("Date: {}".format(datetime.now()))
         try:
-            for provider_key in list_provider_keys('azure'):
-                provider_mgmt = get_mgmt(provider_key)
-                nic_list = provider_mgmt.list_free_nics(nic_template)
-                report.write("----- Provider: {} -----\n".format(provider_key))
-                if nic_list:
-                    report.write("\nRemoving Nics with the name \'{}\':\n".format(nic_template))
-                    report.write(tabulate(tabular_data=[[nic] for nic in nic_list], headers=[
-                        "Name"], tablefmt='orgtbl'))
-                    provider_mgmt.remove_nics_by_search(nic_template)
-                else:
-                    report.write("No \'{}\' NICs were found\n".format(nic_template))
-                pip_list = provider_mgmt.list_free_pip(pip_template)
-                if pip_list:
-                    report.write("\nRemoving Public IPs with the name \'{}\':\n".
-                                 format(pip_template))
-                    report.write(tabulate(tabular_data=[[pip] for pip in pip_list], headers=[
-                        "Name"], tablefmt='orgtbl'))
-                    provider_mgmt.remove_pips_by_search(pip_template)
-                else:
-                    report.write("No \'{}\' Public IPs were found\n".format(pip_template))
-                stack_list = provider_mgmt.list_stack(days_old=days_old)
+            for prov_key in list_provider_keys('azure'):
+                logger.info("----- Provider: {} -----".format(prov_key))
+                prov = get_crud(prov_key)
+                mgmt = prov.mgmt
+                mgmt.logger = logger
+                for name, scr_id in mgmt.list_subscriptions():
+                    logger.info("subscription {s} is chosen".format(s=name))
+                    prov_data = prov.data
+                    prov_data['subscription_id'] = scr_id
+                    mgmt = get_mgmt(prov.key, providers={prov.key: prov_data})
+                    mgmt.logger = logger
+                    # removing stale nics
+                    removed_nics = mgmt.remove_nics_by_search(nic_template)
+                    if removed_nics:
+                        logger.info('following nics were removed:')
+                        for nic in removed_nics:
+                            logger.info(nic[0])
+                    else:
+                        logger.info("No '{}' NICs were found".format(nic_template))
+
+                    # removing public ips
+                    removed_pips = mgmt.remove_pips_by_search(pip_template)
+                    if removed_pips:
+                        logger.info('following pips were removed:')
+                        for pip in removed_pips:
+                            logger.info(pip[0])
+                    else:
+                        logger.info("No '{}' Public IPs were found".format(pip_template))
+
+                # removing stale stacks
+                stack_list = mgmt.list_stack(days_old=days_old)
                 if stack_list:
-                    report.write("\nRemoving empty Stacks:\n")
+                    logger.info("Removing empty Stacks:")
                     removed_stacks = []
                     for stack in stack_list:
-                        if provider_mgmt.is_stack_empty(stack):
+                        if mgmt.is_stack_empty(stack):
                             removed_stacks.append(stack)
-                            provider_mgmt.delete_stack(stack)
-                    report.write(tabulate(tabular_data=[[st] for st in removed_stacks], headers=[
-                        "Name"], tablefmt='orgtbl')) if len(removed_stacks) > 0 else None
+                            mgmt.delete_stack(stack)
+
+                    logger.info('following stacks were removed:')
+                    for stack in removed_stacks:
+                        logger.info([stack])
                 else:
-                    report.write("\nNo stacks older than \'{}\' days were found\n".format(
+                    logger.info("No stacks older than '{}' days were found".format(
                         days_old))
 
                 """
                 Blob removal section
                 """
-                report.write("\nRemoving 'bootdiagnostics-test*' containers\n")
+                # TODO: update it later to use different subscriptions and resource groups
+                logger.info("Removing 'bootdiagnostics-test*' containers")
                 bootdiag_list = []
-                for container in provider_mgmt.container_client.list_containers():
+                for container in mgmt.container_client.list_containers():
                     if container.name.startswith('bootdiagnostics-test'):
                         bootdiag_list.append(container.name)
-                        provider_mgmt.container_client.delete_container(
+                        mgmt.container_client.delete_container(
                             container_name=container.name)
-                report.write(tabulate(tabular_data=[[disk] for disk in bootdiag_list], headers=[
-                    "Name"], tablefmt='orgtbl'))
-                report.write("\nRemoving unused blobs and disks\n")
-                removed_disks = provider_mgmt.remove_unused_blobs()
+
+                logger.info('following disks were removed:')
+                for disk in bootdiag_list:
+                    logger.info([disk])
+
+                logger.info("Removing unused blobs and disks")
+                removed_disks = mgmt.remove_unused_blobs()
                 if len(removed_disks['Managed']) > 0:
-                    report.write('Managed disks:\n')
-                    report.write(tabulate(tabular_data=removed_disks['Managed'], headers="keys",
-                                          tablefmt='orgtbl'))
+                    logger.info('Managed disks:')
+                    logger.info(removed_disks['Managed'])
+
                 if len(removed_disks['Unmanaged']) > 0:
-                    report.write('\nUnmanaged blobs:\n')
-                    report.write(tabulate(tabular_data=removed_disks['Unmanaged'], headers="keys",
-                                          tablefmt='orgtbl'))
+                    logger.info('Unmanaged blobs:')
+                    logger.info(removed_disks['Unmanaged'])
             return 0
         except Exception:
-            report.write("Something bad happened during Azure cleanup\n")
-            report.write(tb.format_exc())
+            logger.info("Something bad happened during Azure cleanup")
+            logger.info(tb.format_exc())
             return 1
 
 
 if __name__ == "__main__":
     args = parse_cmd_line()
-    sys.exit(azure_cleanup(args.nic_template, args.pip_template, args.days_old, args.output))
+
+    # setup logging
+    formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    file_handler = logging.FileHandler(args.output)
+    file_handler.setFormatter(formatter)
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(stdout_handler)
+    logger.addHandler(file_handler)
+
+    sys.exit(azure_cleanup(args.nic_template, args.pip_template, args.days_old))
