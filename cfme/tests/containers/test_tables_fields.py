ab925906b9a8db8fc2ce6313caf32da72567d757
Pete Savage <psavage@redhat.com>
2018-01-03 11:25:34 +0000

    Fix for container objects

--- cfme/tests/containers/test_tables_fields.py
+++ cfme/tests/containers/test_tables_fields.py
@@ -1,19 +1,16 @@
 import pytest
 
-from cfme.containers.pod import Pod, PodCollection
+from cfme.containers.container import Container
+from cfme.containers.image import Image
+from cfme.containers.image_registry import (ImageRegistry)
+from cfme.containers.node import Node
+from cfme.containers.pod import Pod
+from cfme.containers.project import Project
 from cfme.containers.provider import ContainersProvider, ContainersTestItem
-from cfme.containers.service import Service, ServiceCollection
-from cfme.containers.replicator import Replicator, ReplicatorCollection
-from cfme.containers.image import Image, ImageCollection
-from cfme.containers.project import Project, ProjectCollection
-from cfme.containers.container import Container, ContainerCollection
-from cfme.containers.image_registry import (ImageRegistry,
-    ImageRegistryCollection)
-from cfme.containers.route import Route, RouteCollection
-from cfme.containers.node import Node, NodeCollection
-
+from cfme.containers.replicator import Replicator
+from cfme.containers.route import Route
+from cfme.containers.service import Service
 from cfme.utils.appliance.implementations.ui import navigate_to
-
 
 pytestmark = [
     pytest.mark.usefixtures('setup_provider'),
@@ -37,13 +34,13 @@ TEST_ITEMS = [
     pytest.mark.polarion('CMP-10651')(
         ContainersTestItem(
             Route, 'CMP-10651', fields_to_verify=['provider', 'project_name'],
-            collection_obj=RouteCollection
+            collection_obj='container_routes'
         )
     ),
     pytest.mark.polarion('CMP-9943')(
         ContainersTestItem(
             Container, 'CMP-9943', fields_to_verify=['pod_name', 'image', 'state'],
-            collection_obj=ContainerCollection
+            collection_obj='containers'
         )
     ),
     pytest.mark.polarion('CMP-9909')(
@@ -52,7 +49,7 @@ TEST_ITEMS = [
                 'provider', 'project_name', 'ready', 'containers',
                 'phase', 'restart_policy', 'dns_policy'
             ],
-            collection_obj=PodCollection
+            collection_obj='container_pods'
         )
     ),
     pytest.mark.polarion('CMP-9889')(
@@ -61,7 +58,7 @@ TEST_ITEMS = [
                 'provider', 'project_name', 'type', 'portal_ip',
                 'session_affinity', 'pods'
             ],
-            collection_obj=ServiceCollection
+            collection_obj='container_services'
         )
     ),
     pytest.mark.polarion('CMP-9967')(
@@ -70,26 +67,26 @@ TEST_ITEMS = [
                 'provider', 'ready', 'operating_system', 'kernel_version',
                 'runtime_version'
             ],
-            collection_obj=NodeCollection
+            collection_obj='container_nodes'
         )
     ),
     pytest.mark.polarion('CMP-9920')(
         ContainersTestItem(
             Replicator, 'CMP-9920',
             fields_to_verify=['provider', 'project_name', 'replicas', 'current_replicas'],
-            collection_obj=ReplicatorCollection
+            collection_obj='container_replicators'
         )
     ),
     pytest.mark.polarion('CMP-9975')(
         ContainersTestItem(
             Image, 'CMP-9975', fields_to_verify=['provider', 'tag', 'id', 'image_registry'],
-            collection_obj=ImageCollection
+            collection_obj='container_images'
         )
     ),
     pytest.mark.polarion('CMP-9985')(
         ContainersTestItem(
             ImageRegistry, 'CMP-9985', fields_to_verify=['port', 'provider'],
-            collection_obj=ImageRegistryCollection
+            collection_obj='container_image_registries'
         )
     ),
     pytest.mark.polarion('CMP-10652')(
@@ -98,7 +95,7 @@ TEST_ITEMS = [
                 'provider', 'container_routes', 'container_services',
                 'container_replicators', 'pods', 'containers', 'images'
             ],
-            collection_obj=ProjectCollection
+            collection_obj='container_projects'
         )
     )
 ]
@@ -106,10 +103,13 @@ TEST_ITEMS = [
 
 @pytest.mark.parametrize('test_item', TEST_ITEMS,
                          ids=[ti.args[1].pretty_id() for ti in TEST_ITEMS])
-def test_tables_fields(provider, test_item, soft_assert):
+def test_tables_fields(provider, test_item, soft_assert, appliance):
+
+    if test_item.collection_obj:
+        nav_obj = getattr(appliance, test_item.collection_obj)
 
     view = navigate_to((test_item.obj if isinstance(test_item.obj, ContainersProvider)
-                        else test_item.collection_obj), 'All')
+                        else nav_obj), 'All')
     view.toolbar.view_selector.select('List View')
     for row in view.table.rows():
         name_field = getattr(row, 'name', getattr(row, 'host', None))
