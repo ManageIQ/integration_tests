c57e5021e872f340554f3d6559d9e68ea4d03d80
Nazar <nzayats@redhat.com>
2018-01-05 17:48:23 +0200

    Updated due to comment

--- cfme/tests/services/test_catalog_item.py
+++ cfme/tests/services/test_catalog_item.py
@@ -110,14 +110,14 @@ def test_edit_tags(catalog_item):
     catalog_item.remove_tag("Cost Center *", "Cost Center 001")
 
 
-@pytest.mark.meta(blockers=[BZ(1531512, forced_streams=["5.7", "5.8", "5.9", "upstream"])])
+@pytest.mark.meta(blockers=[BZ(1531512, forced_streams=["5.8", "5.9", "upstream"])])
 def test_catalog_item_duplicate_name(catalog_item):
     catalog_item.create()
     with error.expected("Name has already been taken"):
         catalog_item.create()
 
 
-@pytest.mark.meta(blockers=[BZ(1460891, forced_streams=["5.7", "5.8", "upstream"])])
+@pytest.mark.meta(blockers=[BZ(1460891, forced_streams=["5.8", "upstream"])])
 def test_permissions_catalog_item_add(catalog_item):
     """Test that a catalog can be added only with the right permissions."""
     tac.single_task_permission_test([['Everything', 'Services', 'Catalogs Explorer',
