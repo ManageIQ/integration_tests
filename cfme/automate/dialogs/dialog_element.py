85151a1cb1298191eabb66c8bcb17e9ef41bfbe9
Shveta Sachdeva <sshveta@redhat.com>
2018-01-05 12:12:50 -0800

    Added flash message test

--- cfme/automate/dialogs/dialog_element.py
+++ cfme/automate/dialogs/dialog_element.py
@@ -181,9 +181,10 @@ class ElementCollection(BaseCollection):
             view.fill(element)
             view.ele_save_button.click()
         view.save_button.click()
-        view.flash.assert_no_error()
-        view.flash.assert_message('{} was saved'.
-                                  format(self.parent.tab.dialog.label))
+        try:
+            view.flash.assert_no_error()
+        except AssertionError:
+            raise
         return self.instantiate(element_data=element_data)
 
     def set_element_type(self, view, element):
