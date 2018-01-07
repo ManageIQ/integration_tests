6e8a3c547f19830a96cc1f3087b8b683be2860a2
root <root@seal04.qa.lab.tlv.redhat.com>
2018-01-07 16:26:59 +0200

    test api widgetastic

--- cfme/tests/openstack/infrastructure/test_provider.py
+++ cfme/tests/openstack/infrastructure/test_provider.py
@@ -13,8 +13,7 @@ pytestmark = [
 
 
 def test_api_port(provider):
-    
-    view_details=navigate_to(provider,"Details")
+    view_details = navigate_to(provider, "Details")
     port = provider.get_yaml_data()['endpoints']['default']['api_port']
     assert int(view_details.entities.properties.get_text_of('API Port')) == port, 'Invalid API Port'
 
