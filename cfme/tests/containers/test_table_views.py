c07d6c3a9f4d68bdbeab544cf2cb643db673cbaf
gshefer <gshefer@redhat.com>
2018-01-08 14:49:24 +0200

    Fix test_table_views.py (collections)

--- cfme/tests/containers/test_table_views.py
+++ cfme/tests/containers/test_table_views.py
@@ -3,18 +3,9 @@ from collections import OrderedDict
 
 import pytest
 
-from cfme.modeling.base import BaseCollection
 from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.configure.settings import DefaultView
-from cfme.containers.container import Container
-from cfme.containers.image import Image
-from cfme.containers.image_registry import ImageRegistry
 from cfme.containers.provider import ContainersProvider
-from cfme.containers.service import Service
-from cfme.containers.replicator import Replicator
-from cfme.containers.pod import Pod
-from cfme.containers.route import Route
-from cfme.containers.project import Project
 
 
 pytestmark = [
@@ -29,16 +20,15 @@ VIEWS = ('Grid View', 'Tile View', 'List View')
 # to DefaultView and keep the order of the LUT
 objects_mapping = OrderedDict({  # <object> : <ui name>
     ContainersProvider: 'Containers Providers',
-    Image: 'Container Images',
-    ImageRegistry: 'Image Registries',
-    Project: 'Projects',
-    Route: 'Routes',
-    # TODO Add Node back into the list when other classes are updated to use WT views and widgets.
-    # NodeCollection: 'Nodes',
-    Pod: 'Pods',
-    Service: 'Services',
-    Container: 'Containers',
-    Replicator: 'Replicators'
+    'container_images': 'Container Images',
+    'container_image_registries': 'Image Registries',
+    'container_projects': 'Projects',
+    'container_routes': 'Routes',
+    'container_nodes': 'Nodes',
+    'container_pods': 'Pods',
+    'container_services': 'Services',
+    'containers': 'Containers',
+    'container_replicators': 'Replicators'
 })
 
 
@@ -48,9 +38,9 @@ def random_default_views():
     Revert the default views to the original on exit"""
     # Collecting the original default views and Generating random views LUT for test:
     original_default_views, tested_default_views = OrderedDict(), OrderedDict()
-    for obj, ui_name in objects_mapping.items():
-        original_default_views[obj] = DefaultView.get_default_view(ui_name)
-        tested_default_views[obj] = choice(VIEWS)
+    for collection_name, ui_name in objects_mapping.items():
+        original_default_views[collection_name] = DefaultView.get_default_view(ui_name)
+        tested_default_views[collection_name] = choice(VIEWS)
     DefaultView.set_default_view(objects_mapping.values(), tested_default_views.values())
     yield tested_default_views
     # setting back the default views to the original state:
@@ -58,22 +48,26 @@ def random_default_views():
 
 
 @pytest.mark.polarion('CMP-10568')
-def test_default_views(random_default_views):
-    for obj in objects_mapping.keys():
+def test_default_views(appliance, random_default_views):
+    for collection_name in objects_mapping.keys():
+        obj = (ContainersProvider if collection_name is ContainersProvider
+               else getattr(appliance.collections, collection_name))
         view = navigate_to(obj, 'All', use_resetter=False)
-        assert random_default_views[obj].lower() == view.toolbar.view_selector.selected.lower(), (
-            "Failed to setup default view \"{}\" for {}".format(view, objects_mapping[obj])
+        assert (random_default_views[collection_name].lower() ==
+                view.toolbar.view_selector.selected.lower()), (
+            "Failed to setup default view \"{}\" for {}".format(
+                view, objects_mapping[collection_name])
         )
 
 
 @pytest.mark.polarion('CMP-10570')
 def test_table_views(appliance):
-    for obj in objects_mapping.keys():
-        if isinstance(obj, BaseCollection):
-            obj = appliance.get(obj)
+    for collection_name in objects_mapping.keys():
+        obj = (ContainersProvider if collection_name is ContainersProvider
+               else getattr(appliance.collections, collection_name))
         view = navigate_to(obj, 'All')
         view_to_select = choice(VIEWS)
         view.toolbar.view_selector.select(view_to_select)
         assert view_to_select.lower() == view.toolbar.view_selector.selected.lower(), (
-            "Failed to set view \"{}\" For {}".format(view, obj.__name__)
+            "Failed to set view \"{}\" For {}".format(view, collection_name)
         )
