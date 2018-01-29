0a016d9c0cc2c075e33c4ebc0f8cbe90d97ce3ca
Mike Shriver <mshriver@redhat.com>
2018-01-29 12:02:53 -0500

    Update auth yaml use in fixtures/configure_auth_mode

--- cfme/fixtures/configure_auth_mode.py
+++ cfme/fixtures/configure_auth_mode.py
@@ -1,6 +1,6 @@
 import pytest
 
-from cfme.utils.conf import cfme_data, credentials
+from cfme.utils.conf import credentials, auth_data
 from cfme.utils.appliance import current_appliance
 from cfme.utils.ext_auth import (disable_external_auth_ipa, disable_external_auth_openldap,
     setup_external_auth_ipa, setup_external_auth_openldap)
@@ -17,7 +17,7 @@ def get_auth_settings(appliance):
 
 @pytest.fixture(scope='session')
 def available_auth_modes():
-    return cfme_data.get('auth_modes', {}).keys()
+    return auth_data.get('auth_providers', {}).keys()
 
 
 @pytest.yield_fixture(scope='module')
@@ -25,7 +25,7 @@ def configure_ldap_auth_mode(browser, available_auth_modes):
     """Configure LDAP authentication mode"""
     if 'miq_ldap' in available_auth_modes:
         auth_mode = current_appliance.server.authentication
-        server_data = cfme_data.get('auth_modes', {})['miq_ldap']
+        server_data = auth_data.get('auth_providers', {}).get('miq_ldap')
         auth_mode.configure_auth(**server_data)
         yield
         current_appliance.server.login_admin()
@@ -39,7 +39,7 @@ def configure_openldap_auth_mode(browser, available_auth_modes):
     """Configure LDAP authentication mode"""
     if 'miq_openldap' in available_auth_modes:
         auth_mode = current_appliance.server.authentication
-        server_data = cfme_data.get('auth_modes', {})['miq_openldap']
+        server_data = auth_data.get('auth_providers', {}).get('miq_openldap')
         auth_mode.configure_auth(**server_data)
         yield
         current_appliance.server.login_admin()
@@ -53,7 +53,7 @@ def configure_openldap_auth_mode_default_groups(browser, available_auth_modes):
     """Configure LDAP authentication mode"""
     if 'miq_openldap' in available_auth_modes:
         auth_mode = current_appliance.server.authentication
-        server_data = cfme_data.get('auth_modes', {})['miq_openldap']
+        server_data = auth_data.get('auth_providers', {}).get('miq_openldap')
         server_data['get_groups'] = False
         server_data['default_groups'] = 'EvmRole-user'
         auth_mode.configure_auth(**server_data)
@@ -73,7 +73,7 @@ def configure_aws_iam_auth_mode(appliance, available_auth_modes):
         orig_mode = auth_settings.auth_mode
         orig_settings = auth_settings.auth_settings
         orig_settings.pop('title')
-        yaml_data = cfme_data.auth_modes.miq_aws.copy()
+        yaml_data = auth_data.get('auth_providers', {}).get('miq_aws', {}).copy()
         creds = credentials[yaml_data.pop('credentials')]  # remove cred key from fill_data with pop
         fill_data = {
             'access_key': creds['username'],
@@ -96,7 +96,7 @@ def configure_aws_iam_auth_mode(appliance, available_auth_modes):
 # TODO remove this fixture, its doing what the above fixtures are doing but taking an argument
 @pytest.fixture()
 def configure_auth(appliance, request, auth_mode):
-    data = cfme_data['auth_modes'].get(auth_mode, {})
+    data = auth_data['auth_providers'].get(auth_mode, {})
     app_auth = appliance.server.authentication
     if auth_mode == 'ext_ipa':
         request.addfinalizer(disable_external_auth_ipa)
