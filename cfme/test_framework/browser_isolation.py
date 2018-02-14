83e175565ea5d91841e920bb0a85be0a5dfc64e1
Milan Falešník <mfalesni@redhat.com>
2018-02-14 15:55:45 +0100

    moar doc

--- cfme/test_framework/browser_isolation.py
+++ cfme/test_framework/browser_isolation.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-"""Plugin enabling us to isolate browser sessions per test."""
+"""Plugin enabling us to isolate browser sessions per test.
+
+If active, then when each test ends, the browser gets killed. That ensures that whatever way the
+browser session could be tainted after a test, the next test should not be affected.
+"""
 import pytest
 
 
@@ -8,7 +12,10 @@ def pytest_addoption(parser):
         '--browser-isolation',
         action='store_true',
         default=False,
-        help='Isolate browser sessions for each test.')
+        help=(
+            'Isolate browser sessions for each test. That makes sure that whatever state the '
+            'browser is in after a test, it will be killed so the next test will have to check out '
+            'a fresh browser session.'))
 
 
 @pytest.mark.hookwrapper(trylast=True)
