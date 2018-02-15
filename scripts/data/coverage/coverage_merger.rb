f4709ec25f48f62d11489a0fe8ef08650e91adf0
James Olin Oden <james.oden@gmail.com>
2018-02-15 10:49:41 -0500

    Minor changes to coverage_merger.rb (#6646)
    - Update comments in script to properly explain format of
      .resultset.json files and the merged .resultset.json file.
    - The location of the coverage data is no longer hard coded, and now
      defaults to ./coverage.  Also added a --coverageRoot option.
    - Merged file output is now pretty printed.  I found this useful when
      I was trying to understand the file.  Someone else may also at
      some point.
    
    Additionally some comment changes were made to fixtures/ui_coverage.py.
    It has an explanation of almost the whole end to end process for
    gathering and reporting coverage statistics and I wanted to make that
    accurate.

--- scripts/data/coverage/coverage_merger.rb
+++ scripts/data/coverage/coverage_merger.rb
@@ -1,21 +1,85 @@
-# goes into the rails root
-# merges coverage results from multiple processes, potentially on multiple appliances
-# This expects the layout used by our coverage hook: "RAILS_ROOT/coverage/[ipaddress]/[pid]/"
+# This script merges coverage results from multiple processes, potentially on multiple appliances
+# This expects the layout used by our coverage hook: "coverage/[ipaddress]/[pid]/.resultset.json".
+# It will look for the coverage archive either under the current directory or the one specified.
 # It does the report merging manually based on that layout, and can be run at any time
-# to compile reports.
+# to compile reports.   
+#
+# The basic format of the .resultset.json files is:
+#   
+#   {
+#       "$ip-$pid": {
+#           "coverage": {
+#              "$file1": {
+#                   $coverage_data_line1,
+#                   .
+#                   .
+#                   .
+#                   $coverage_data_lineN
+#               },
+#               .
+#               .
+#               .
+#              "$fileN": {
+#                   $coverage_data_line1,
+#                   .
+#                   .
+#                   .
+#                   $coverage_data_lineN
+#               },
+#           }
+#       }
+#   } 
+#
+# Note coverage data is either:
+#
+#      - 0: line not covered
+#      - > 0: Number of times line covered.
+#      - null: Not coverable (e.g. a comment)
+#
+# The merge will actually still keep the data separate between the 
+# appliance-processes, but they are all in the same file.   sonar-scanner 
+# can apparently handle this fine.   So the file will look like:
+# 
+#   {
+#       "$ip1-$pid1": {
+#           "coverage": {
+#               ...
+#           }
+#       }
+#       .
+#       .
+#       .
+#       "$ipM-$pidN": {
+#           "coverage": {
+#               ...
+#           }
+#       }
+#   } 
+#
 require 'fileutils'
 require 'json'
+require 'optparse'
 require 'simplecov'
 require 'simplecov/result'
 
 rails_root = '/var/www/miq/vmdb'
 results = Hash.new
-coverage_root = "/var/www/miq/vmdb/coverage"
-puts "Scanning for results in #{coverage_root}"
+
+# Parse command line arguments:
+options = {}
+options[:coverage_root] = './coverage'
+OptionParser.new do |parser|
+  parser.on("-c", "--coverageRoot DIR", "Path to the coverage directory.") do |v|
+    options[:coverage_root] = v
+  end
+end.parse!
+
+puts "Scanning for results in #{options[:coverage_root]}"
 json_files = Dir.glob(
-  File.join(coverage_root, "*", "*", ".resultset.json")
+  File.join(options[:coverage_root], "*", "*", ".resultset.json")
 )
-merged_dir = File.join(coverage_root, 'merged')
+merged_dir = File.join(options[:coverage_root], 'merged')
+merged_result_set = File.join(merged_dir, '.resultset.json')
 
 for json_file in json_files
   begin
@@ -30,12 +94,12 @@ end
 if results.empty?
   abort "No results found for merging."
 else
-  puts "All results merged, compiling report."
+  puts "All results merged, compiling report: #{merged_result_set}"
 end
 
 FileUtils.mkdir_p(merged_dir)
-File.open(File.join(merged_dir, '.resultset.json'), "w") do |f|
-  JSON.dump(results, f)
+File.open(merged_result_set, "w") do |f|
+  f.puts JSON.pretty_generate(results)
 end
 
 # Set up simplecov to use the merged results, then fire off the formatters
