2b6191a67917fedf8b39f25f34a81bc23a564688
Douglas Gabriel <douglasxgabriel@gmail.com>
2018-02-16 17:01:02 -0300

    Adding Physical Servers Inventory API Tests

---
+++ cfme/tests/physical_infrastructure/api/test_physical_server_inventory_api.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from cfme.physical.provider.lenovo import LenovoProvider
+from cfme.utils.rest import assert_response
+
+pytestmark = [
+    pytest.mark.tier(3),
+    pytest.mark.provider([LenovoProvider], scope='module')
+]
+
+
+@pytest.fixture(scope="module")
+def physical_server(setup_provider_modscope, appliance):
+    physical_server = appliance.rest_api.collections.physical_servers[0]
+    return physical_server
+
+
+def test_get_hardware(appliance, physical_server):
+    physical_server.reload(attributes=['hardware'])
+    assert_response(appliance)
+    assert physical_server.hardware is not None
+
+
+@pytest.mark.parametrize('attribute', ['firmwares', 'nics', 'ports'])
+def test_get_hardware_attributes(appliance, physical_server, attribute):
+    expanded_attribute = 'hardware.{}'.format(attribute)
+    physical_server.reload(attributes=[expanded_attribute])
+    assert_response(appliance)
+    assert physical_server.hardware[attribute] is not None
+
+
+def test_get_asset_details(appliance, physical_server):
+    physical_server.reload(attributes=['asset_details'])
+    assert_response(appliance)
+    assert physical_server.asset_details is not None
