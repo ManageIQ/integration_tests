bcfba9216a4ab228af3b2ecf547623a8a123f822
izapolsk <izapolsk@redhat.com>
2018-02-16 10:56:30 +0200

    pod appliance currently has different db and http hosts. So, this check isn't appliacable to pod appliances

--- cfme/test_framework/appliance_police.py
+++ cfme/test_framework/appliance_police.py
@@ -28,36 +28,39 @@ def appliance_police():
     if not store.slave_manager:
         return
     try:
-        port_numbers = {
-            'ssh': ports.SSH,
-            'https': store.current_appliance.ui_port,
-            'postgres': ports.DB}
-        port_results = {pn: net_check(pp, force=True) for pn, pp in port_numbers.items()}
+        appliance = store.current_appliance
+        available_ports = {
+            'ssh': (appliance.hostname, ports.SSH),
+            'https': (appliance.hostname, appliance.ui_port),
+            'postgres': (appliance.db_host or appliance.hostname, ports.DB)}
+        port_results = {pn: net_check(addr=pp[0], port=pp[1], force=True)
+                        for pn, pp in available_ports.items()}
         for port, result in port_results.items():
-            if port == 'ssh' and store.current_appliance.is_pod:
+            if port == 'ssh' and appliance.is_pod:
                 # ssh is not available for podified appliance
                 continue
             if not result:
-                raise AppliancePoliceException('Unable to connect', port_numbers[port])
+                raise AppliancePoliceException('Unable to connect', available_ports[port][1])
 
         try:
-            status_code = requests.get(store.current_appliance.url, verify=False,
+            status_code = requests.get(appliance.url, verify=False,
                                        timeout=120).status_code
         except Exception:
-            raise AppliancePoliceException('Getting status code failed', port_numbers['https'])
+            raise AppliancePoliceException('Getting status code failed',
+                                           available_ports['https'][1])
 
         if status_code != 200:
             raise AppliancePoliceException('Status code was {}, should be 200'.format(
-                status_code), port_numbers['https'])
+                status_code), available_ports['https'][1])
         return
     except AppliancePoliceException as e:
         # special handling for known failure conditions
         if e.port == 443:
             # Lots of rdbs lately where evm seems to have entirely crashed
             # and (sadly) the only fix is a rude restart
-            store.current_appliance.restart_evm_service(rude=True)
+            appliance.restart_evm_service(rude=True)
             try:
-                store.current_appliance.wait_for_web_ui(900)
+                appliance.wait_for_web_ui(900)
                 store.write_line('EVM was frozen and had to be restarted.', purple=True)
                 return
             except TimedOutError:
@@ -68,7 +71,7 @@ def appliance_police():
 
     # Regardles of the exception raised, we didn't return anywhere above
     # time to call a human
-    msg = 'Help! My appliance {} crashed with: {}'.format(store.current_appliance.url, e_message)
+    msg = 'Help! My appliance {} crashed with: {}'.format(appliance.url, e_message)
     store.slave_manager.message(msg)
     if 'appliance_police_recipients' in rdb:
         rdb_kwargs = {
