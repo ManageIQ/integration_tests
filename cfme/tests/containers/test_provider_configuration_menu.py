762a0051f4cdc4563a283ff571a41e16f7a2d0d4
brahmanim <brahmani@redhat.com>
2018-02-22 12:29:09 +0200

    Delete the part of test that removes selected container provider

--- cfme/tests/containers/test_provider_configuration_menu.py
+++ cfme/tests/containers/test_provider_configuration_menu.py
@@ -22,16 +22,3 @@ def test_edit_selected_containers_provider(provider):
     view = navigate_to(provider, 'Edit')
     assert view.is_displayed
     view.cancel.click()
-
-
-@pytest.mark.polarion('CMP-9881')
-def test_remove_selected_containers_provider(provider):
-    '''Testing Configuration -> Remove... button functionality
-    Step:
-        In Providers summary page - click configuration menu and select
-        "Remove this container provider from VMDB"
-    Expected result:
-        The user should be shown a warning message following a
-        success message that the provider has been deleted from VMDB.'''
-    provider.delete(cancel=False)
-    # The assertion of success is inside the delete function
