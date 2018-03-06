8fbd92f19453918bf3d7ce838e2fbf8aae5172a1
Ruslana <rbabyuk@redhat.com>
2018-03-06 14:01:05 +0200

    fix navigation

--- cfme/tests/infrastructure/test_datastore_analysis.py
+++ cfme/tests/infrastructure/test_datastore_analysis.py
@@ -108,16 +108,15 @@ def test_run_datastore_analysis(setup_provider, datastore, soft_assert, datastor
     #             'Datastore type does not match the type defined in yaml:' +
     #             'expected "{}" but was "{}"'.format(datastore.type.upper(), c_datastore))
 
-    wait_for(lambda: details_view.entities.summary('Content').get_text_of(CONTENT_ROWS_TO_CHECK[0]),
+    wait_for(lambda: details_view.entities.content.get_text_of(CONTENT_ROWS_TO_CHECK[0]),
              delay=15, timeout="3m",
              fail_condition='0',
              fail_func=appliance.server.browser.refresh)
-    managed_vms = details_view.entities.summary('Relationships').get_text_of('Managed VMs')
+    managed_vms = details_view.entities.relationships.get_text_of('Managed VMs')
     if managed_vms != '0':
         for row_name in CONTENT_ROWS_TO_CHECK:
-            value = details_view.entities.summary('Content').get_text_of(row_name)
+            value = details_view.entities.content.get_text_of(row_name)
             soft_assert(value != '0',
                         'Expected value for {} to be non-empty'.format(row_name))
     else:
-        assert details_view.entities.summary('Content').get_text_of(
-            CONTENT_ROWS_TO_CHECK[-1]) != '0'
+        assert details_view.entities.content.get_text_of(CONTENT_ROWS_TO_CHECK[-1]) != '0'
