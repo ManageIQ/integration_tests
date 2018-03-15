d78f2c71fffd27300852773b2efa8386c1277afd
Nikhil Dhandre <ndhandre@redhat.com>
2018-03-15 15:43:56 +0530

    manager tag and remove tests

--- cfme/tests/storage/test_manager.py
+++ cfme/tests/storage/test_manager.py
@@ -4,6 +4,7 @@ import pytest
 from cfme import test_requirements
 from cfme.cloud.provider import CloudProvider
 from cfme.cloud.provider.openstack import OpenStackProvider
+from cfme.storage.manager import StorageManagerDetailsView
 from cfme.utils.appliance.implementations.ui import navigate_to
 
 
@@ -40,3 +41,48 @@ def test_manager_navigation(collection_manager):
     assert view.is_displayed
 
     manager.refresh()
+
+
+@pytest.mark.uncollectif(lambda provider: not provider.one_of(OpenStackProvider))
+def test_storage_manager_edit_tag(collection_manager):
+    """ Test add and remove tag to storage manager
+
+    prerequisites:
+        * Storage provider
+
+    Steps:
+        * Add tag and check
+        * Remove tag and check
+    """
+
+    manager = collection_manager[1]
+    # add tag with category Department and tag communication
+    manager.add_tag('Department', 'Communication')
+    tag_available = manager.get_tags()
+    assert tag_available[0].display_name == 'Communication'
+    assert tag_available[0].category.display_name == 'Department'
+
+    # remove assigned tag
+    manager.remove_tag('Department', 'Communication')
+    tag_available = manager.get_tags()
+    assert not tag_available
+
+
+@pytest.mark.uncollectif(lambda provider: not provider.one_of(OpenStackProvider))
+def test_storage_manager_delete(collection_manager):
+    """ Test delete storage manager
+
+    prerequisites:
+        * Storage provider
+
+    Steps:
+        * Delete storage manager from inventory
+        * Assert flash message
+        * Check storage manager exists or not
+    """
+    manager = collection_manager[1]
+    manager.delete()
+    view = manager.create_view(StorageManagerDetailsView)
+    view.flash.assert_success_message(
+        'Delete initiated for 1 Storage Manager from the CFME Database')
+    assert not manager.exists
