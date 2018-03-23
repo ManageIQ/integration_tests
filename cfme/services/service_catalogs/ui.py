1e78f39aafe7f3af1886d4fcc8b713071e3f2d41
Shveta Sachdeva <sshveta@redhat.com>
2018-03-23 11:21:24 -0700

    Added wait for view

--- cfme/services/service_catalogs/ui.py
+++ cfme/services/service_catalogs/ui.py
@@ -81,7 +81,7 @@ class OrderServiceCatalogView(OrderForm):
 
 @MiqImplementationContext.external_for(ServiceCatalogs.order, ViaUI)
 def order(self):
-    view = navigate_to(self, 'Order')
+    view = navigate_to(self, 'Order', wait_for_view=True)
     if self.stack_data:
         view.fill(self.stack_data)
     if self.dialog_values:
