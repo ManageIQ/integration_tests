742dbe7bb573cd1409d1b18a4c82a060154b63e2
izapolsk <izapolsk@redhat.com>
2018-03-28 19:39:07 +0300

    basic manual pod tests

---
+++ cfme/tests/pod/test_appliance_crud.py
@@ -0,0 +1,71 @@
+import pytest
+
+
+@pytest.mark.manual
+def test_crud_pod_appliance():
+    """
+    deploys pod appliance
+    checks that it is alive
+    deletes pod appliance
+    """
+    pass
+
+
+@pytest.mark.manual
+def test_crud_pod_appliance_ext_db():
+    """
+    deploys pod appliance
+    checks that it is alive
+    deletes pod appliance
+    """
+    pass
+
+
+@pytest.mark.manual
+def test_crud_pod_appliance_custom_config():
+    """
+    overriding default values in template and deploys pod appliance
+    checks that it is alive
+    deletes pod appliance
+    """
+    pass
+
+
+@pytest.mark.manual
+def test_pod_appliance_config_upgrade():
+    """
+    appliance config update should cause appliance re-deployment
+    """
+    pass
+
+
+@pytest.mark.manual
+def test_pod_appliance_image_upgrade():
+    """
+    one of appliance images has been changed. it should cause pod re-deployment
+    """
+    pass
+
+
+@pytest.mark.manual
+def test_pod_appliance_start_stop():
+    """
+    appliance should start w/o issues
+    """
+    pass
+
+
+@pytest.mark.manual
+def test_pod_appliance_scale():
+    """
+    appliance should work correctly after scale up/down
+    """
+    pass
+
+
+@pytest.mark.manual
+def test_aws_smartstate_pod():
+    """
+    deploy aws smartstate pod and that it works
+    """
+    pass
---
+++ cfme/tests/pod/test_ext_auth.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.mark.manual
+def test_pod_appliance_basic_ipa_auth():
+    """
+    auth from ipa server should work
+    """
+    pass
