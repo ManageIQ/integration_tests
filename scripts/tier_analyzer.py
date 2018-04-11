ad85d0b7e3d41bd878b961c316cfe30efc2d198a
Jan Zmeskal <janzmeskal@email.cz>
2018-04-11 11:49:28 +0200

    Review changes for tier analyzer

--- scripts/tier_analyzer.py
+++ scripts/tier_analyzer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+"""This simple script lists all tests generated for the given provider. Then it lists all the tests
+marked with given tier marker(s). In the end it simply compares those two list, showing you tests
+that are generated for provider but NOT marked with the tier(s)."""
+
 import argparse
 import re
 import subprocess
@@ -7,15 +11,22 @@ import sys
 
 
 def check_virtualenv():
-    """Check if we are in virtualenv and if not, exit."""
+    """Check if we are in virtualenv and if not, raise an error."""
     if not hasattr(sys, 'real_prefix'):
-        print('You must activate CFME virtualenv in oder to run this script.')
-        sys.exit(1)
+        raise EnvironmentError('You must activate CFME virtualenv in oder to run this script.')
 
 
-def get_pytest_output(args, use_tier_marker=False):
-    """Get string output of pytest command."""
-    print('Getting pytest output {} tier marker.'.format('with' if use_tier_marker else 'without'))
+def get_pytest_collect_only_output(args, use_tier_marker=False):
+    """
+    Get a string that is returned by pytest after invoking it with --collect-only argument.
+    If use_tier_marker is set to False (default), all the tests for given provider are collected.
+    If use_tier_marker is set to True, only the marked tests are collected.
+    """
+    print('Getting pytest --collect-only output for provider {} {} tier marker {}.'
+        .format(
+            args.provider,
+            'with' if use_tier_marker else 'without',
+            args.tier_marker))
     pytest_args = ['pytest',
         args.test_path,
         '--collect-only',
@@ -31,12 +42,13 @@ def get_pytest_output(args, use_tier_marker=False):
     return subprocess.check_output(pytest_args)
 
 
-def parse_pytest_output(output):
-    """Parse raw pytest output to get list of parametrized test cases."""
+def get_testcases_from_pytest_output(output):
+    """Parse raw pytest --collect-only output to get list of parametrized test cases."""
     test_cases = []
-    tc_regex = re.compile(r"\s*<Function '(?P<test_case_name>test_\w+)(?P<parameters>\[.+\])?'>")
+    testcase_regex = \
+        re.compile(r"\s*<Function '(?P<test_case_name>test_\w+)(?P<parameters>\[.+\])?'>")
     for line in output.splitlines():
-        match = re.match(pattern=tc_regex, string=line)
+        match = re.match(pattern=testcase_regex, string=line)
         if match:
             test_cases.append('{}{}'.format(
                 match.group('test_case_name'),
@@ -45,7 +57,7 @@ def parse_pytest_output(output):
     return test_cases
 
 
-def compare_parsed_outputs(all, tiers):
+def get_diff_from_lists(all, tiers):
     """Get sorted list of test cases that are not marked with tiers."""
     diff = set(all).difference(set(tiers))
     return sorted(diff)
@@ -75,10 +87,7 @@ def print_message(args, diff, all_parsed, all_count, tiers_parsed, tiers_count):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="""This simple script lists all tests generated
-    for the given provider. Then it lists all the tests makred with given tier marker(s).
-    In the end it simply compares thhose two list, showing you tests that are generated for provider
-    but NOT marked with the tier(s).""")
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-p', '--provider', action='store', default='rhv_cfme_integration')
     parser.add_argument('-t', '--test-path', action='store', default='cfme/tests')
@@ -87,14 +96,14 @@ if __name__ == '__main__':
 
     check_virtualenv()
 
-    output_with_tiers = get_pytest_output(args, use_tier_marker=True)
-    output_without_tiers = get_pytest_output(args, use_tier_marker=False)
-    tiers_parsed = parse_pytest_output(output_with_tiers)
-    all_parsed = parse_pytest_output(output_without_tiers)
+    output_with_tiers = get_pytest_collect_only_output(args, use_tier_marker=True)
+    output_without_tiers = get_pytest_collect_only_output(args, use_tier_marker=False)
+    tiers_parsed = get_testcases_from_pytest_output(output_with_tiers)
+    all_parsed = get_testcases_from_pytest_output(output_without_tiers)
 
     tiers_count = len(tiers_parsed)
     all_count = len(all_parsed)
 
-    final_diff = compare_parsed_outputs(all_parsed, tiers_parsed)
+    final_diff = get_diff_from_lists(all_parsed, tiers_parsed)
 
     print_message(args, final_diff, all_parsed, all_count, tiers_parsed, tiers_count)
