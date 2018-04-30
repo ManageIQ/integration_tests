3911ec07aadf232a3ea5d4b4c6cc0562138ec04f
Nikhil <ndhandre@redhat.com>
2018-04-30 16:02:36 +0530

    Intitial Commit host graph automation

--- cfme/common/candu_views.py
+++ cfme/common/candu_views.py
@@ -1,5 +1,5 @@
 from widgetastic_manageiq import LineChart
-from widgetastic.widget import View, Text, TextInput
+from widgetastic.widget import ConditionalSwitchableView, Text, TextInput, View
 from widgetastic_patternfly import BootstrapSelect
 
 
@@ -37,3 +37,53 @@ class VMUtilizationAllView(VMUtilizationView):
             )
         else:
             return "Capacity & Utilization data for Virtual Machine" in self.title.text
+
+
+class HostInfraUtilizationView(View):
+    """View for Infrastructure provider Host Utilization"""
+    title = Text(".//div[@id='main-content']//h1")
+    options = View.nested(OptionForm)
+    interval_type = ConditionalSwitchableView(reference='options.interval')
+
+    @interval_type.register('Daily', default=True)
+    class HostInfraDailyUtilizationView(View):
+        """A view for Daily Interval Host Utilization"""
+        host_cpu = LineChart(id='miq_chart_parent_candu_0')
+        host_cpu_vm_avg = LineChart(id='miq_chart_candu_0_2')
+        host_cpu_state = LineChart(id='miq_chart_parent_candu_1')
+        host_cpu_state_vm_avg = LineChart(id='miq_chart_candu_1_2')
+        host_memory = LineChart(id='miq_chart_parent_candu_2')
+        host_memory_vm_avg = LineChart(id='miq_chart_candu_2_2')
+        host_disk = LineChart(id='miq_chart_parent_candu_3')
+        host_disk_vm_avg = LineChart(id='miq_chart_candu_3_2')
+        host_network = LineChart(id='miq_chart_parent_candu_4')
+        host_network_vm_avg = LineChart(id='miq_chart_candu_4_2')
+        host_vm = LineChart(id='miq_chart_parent_candu_5')
+
+    @interval_type.register('Hourly')
+    class HostInfraHourlyUtilizationView(View):
+        """A view for Hourly Interval Host Utilization"""
+        host_cpu = LineChart(id='miq_chart_parent_candu_0')
+        host_cpu_vm_avg = LineChart(id='miq_chart_candu_0_2')
+        host_cpu_state = LineChart(id='miq_chart_parent_candu_1')
+        host_cpu_state_vm_avg = LineChart(id='miq_chart_candu_1_2')
+        host_memory = LineChart(id='miq_chart_parent_candu_2')
+        host_memory_vm_avg = LineChart(id='miq_chart_candu_2_2')
+        host_disk = LineChart(id='miq_chart_parent_candu_3')
+        host_disk_vm_avg = LineChart(id='miq_chart_candu_3_2')
+        host_network = LineChart(id='miq_chart_parent_candu_4')
+        host_network_vm_avg = LineChart(id='miq_chart_candu_4_2')
+        host_vm = LineChart(id='miq_chart_parent_candu_5')
+
+    @interval_type.register('Most Recent Hour')
+    class HostInfraRecentHourUtilizationView(View):
+        """A view for Most Recent Hour Interval Host Utilization"""
+        host_cpu = LineChart(id='miq_chart_parent_candu_0')
+        host_memory = LineChart(id='miq_chart_parent_candu_2')
+        host_disk = LineChart(id='miq_chart_parent_candu_3')
+        host_network = LineChart(id='miq_chart_parent_candu_4')
+
+    @property
+    def is_displayed(self):
+        expected_title = "{} Capacity & Utilization".format(self.context['object'].name)
+        return self.title.text == expected_title
---
+++ cfme/tests/candu/test_host_graph.py
@@ -0,0 +1,84 @@
+import pytest
+
+from cfme import test_requirements
+from cfme.infrastructure.provider.rhevm import RHEVMProvider
+from cfme.infrastructure.provider.virtualcenter import VMwareProvider
+from cfme.utils.appliance.implementations.ui import navigate_to
+from cfme.utils.wait import wait_for
+
+
+pytestmark = [
+    pytest.mark.tier(3),
+    test_requirements.c_and_u,
+    pytest.mark.usefixtures('setup_provider'),
+    pytest.mark.provider([VMwareProvider, RHEVMProvider],
+                         required_fields=[(['cap_and_util', 'capandu_vm'], 'cu-24x7')])
+]
+
+
+HOST_RECENT_HR_GRAPHS = ['host_cpu',
+                         'host_memory',
+                         'host_disk',
+                         'host_network']
+
+
+@pytest.fixture(scope='function')
+def host(appliance, provider):
+    collection = appliance.collections.hosts
+    for test_host in provider.data['hosts']:
+        if not test_host.get('test_fleece', False):
+            continue
+        return collection.instantiate(name=test_host.name, provider=provider)
+
+
+@pytest.mark.uncollectif(lambda provider, graph_type:
+                         provider.one_of(RHEVMProvider) and
+                         graph_type == "host_disk")
+@pytest.mark.parametrize('graph_type', HOST_RECENT_HR_GRAPHS)
+def test_host_most_recent_hour_graph_screen(graph_type, provider, host, enable_candu):
+    """ Test Host graphs for most recent hour displayed or not
+
+    prerequisites:
+        * C&U enabled appliance
+
+    Steps:
+        * Navigate to Host Utilization Page
+        * Check graph displayed or not
+        * Check legends hide and display properly or not
+        * Check data for legends collected or not
+    """
+
+    host.wait_candu_data_available(timeout=1200)
+
+    view = navigate_to(host, 'candu')
+    view.options.interval.fill('Most Recent Hour')
+
+    if graph_type == 'host_cpu':
+        graph = view.interval_type.host_cpu
+    elif graph_type == 'host_memory':
+        graph = view.interval_type.host_memory
+    elif graph_type == 'host_disk':
+        graph = view.interval_type.host_disk
+    else:
+        graph = view.interval_type.host_network
+
+    assert graph.is_displayed
+
+    def refresh():
+        provider.browser.refresh()
+        view.options.interval.fill('Most Recent Hour')
+
+    # wait, some time graph take time to load
+    wait_for(lambda: len(graph.all_legends) > 0,
+             delay=5, timeout=600, fail_func=refresh)
+
+    graph_data = 0
+    for leg in graph.all_legends:
+        graph.hide_legends(leg)
+        assert not graph.legend_is_displayed(leg)
+        graph.display_legends(leg)
+        assert graph.legend_is_displayed(leg)
+
+        for data in graph.data_for_legends(leg).values():
+            graph_data += float(data[leg].replace(',', '').replace('%', '').split()[0])
+    assert graph_data > 0
