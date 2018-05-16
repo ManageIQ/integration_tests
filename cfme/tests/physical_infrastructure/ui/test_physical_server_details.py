0563a0796b0e6bafae2d9f04bfab8ee046d3a14b
skovic <skovic@lenovo.com>
2018-05-16 16:00:46 -0400

    Refactor test_network_devices and test_storage_devices

--- cfme/tests/physical_infrastructure/ui/test_physical_server_details.py
+++ cfme/tests/physical_infrastructure/ui/test_physical_server_details.py
@@ -55,23 +55,21 @@ def test_physical_server_details_dropdowns(physical_server):
 def test_network_devices(physical_server):
     """Navigate to the Network Devices page and verify that the page is displayed"""
 
-    if physical_server.num_network_devices() != "0":
-        network_device_view = navigate_to(physical_server, 'NetworkDevices')
-        assert network_device_view.is_displayed
-    else:
-        network_device_view = navigate_to(physical_server, 'NetworkDevices')
-        assert not network_device_view.is_displayed
+    num_network_devices = physical_server.num_network_devices()
+    network_device_view = navigate_to(physical_server, 'NetworkDevices')
+
+    assert(network_device_view.is_displayed if num_network_devices != "0" else
+           not network_device_view.is_displayed)
 
 
 def test_storage_devices(physical_server):
     """Navigate to the Storage Devices page and verify that the page is displayed"""
 
-    if physical_server.num_storage_devices() != "0":
-        storage_device_view = navigate_to(physical_server, 'StorageDevices')
-        assert storage_device_view.is_displayed
-    else:
-        storage_device_view = navigate_to(physical_server, 'StorageDevices')
-        assert not storage_device_view.is_displayed
+    num_storage_devices = physical_server.num_storage_devices()
+    storage_device_view = navigate_to(physical_server, 'StorageDevices')
+
+    assert(storage_device_view.is_displayed if num_storage_devices != "0" else
+           not storage_device_view.is_displayed)
 
 
 def test_physical_server_details_stats(physical_server):
