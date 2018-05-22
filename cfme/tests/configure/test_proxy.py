a932d325d8732d66e0d2bd91c7a73339479f9f7a
otsuman <otsuman@redhat.com>
2018-05-22 12:32:20 +0300

    fix failing tests

--- cfme/tests/configure/test_proxy.py
+++ cfme/tests/configure/test_proxy.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import fauxfactory
 import pytest
-import time
 
 from cfme.cloud.provider import CloudProvider
+from cfme.common.provider import DefaultEndpointForm
 from cfme.utils import conf
+from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.utils.providers import get_mgmt
 from cfme.utils.ssh import SSHClient
 from cfme.utils.virtual_machines import deploy_template
+from cfme.utils.wait import wait_for
 
 pytestmark = [
     pytest.mark.tier(3),
@@ -15,7 +17,7 @@ pytestmark = [
 ]
 
 
-@pytest.yield_fixture(scope="module")
+@pytest.fixture(scope="module")
 def proxy_machine():
     """ Deploy vm for proxy test
 
@@ -37,20 +39,35 @@ def proxy_machine():
 
 def validate_provider(provider, valid_proxy=True):
     provider.refresh_provider_relationships()
+    wait_for(lambda: provider.last_refresh_error() is None, fail_condition=not valid_proxy,
+             fail_func=provider.refresh_provider_relationships, timeout=300, delay=20,
+             message='Waiting to provider refresh')
     result = provider.last_refresh_error() is None if valid_proxy else provider.last_refresh_error()
     assert result
+
+
+def validate_provider_credentials(provider):
+    # need to validate credentials after incorrect proxy settings to be able to refresh provider
+    view = navigate_to(provider, 'Edit')
+    endp_view = provider.create_view(DefaultEndpointForm)
+    endp_view.validate.click()
+    view.cancel.click()
 
 
 def validate_proxy_logs(proxy_ip, appliance_ip, valid_proxy=True):
     proxy_ssh = SSHClient(hostname=proxy_ip, **conf.credentials['proxy_vm'])
     proxy_ssh.run_command('echo "" > /var/log/squid/access.log')
+
+    def _is_ip_in_log():
+        log_file = proxy_ssh.run_command("cat /var/log/squid/access.log").output
+        return appliance_ip in log_file
     # need to wait until requests will occur in access.log or check if its empty after some time
-    time.sleep(60)
-    log_file = proxy_ssh.run_command("cat /var/log/squid/access.log").output
-    assert (appliance_ip in log_file) == valid_proxy
+    wait_for(func=_is_ip_in_log, fail_condition=not valid_proxy, num_sec=300, delay=10,
+             message='Waiting for requests from appliance in logs')
+    assert _is_ip_in_log() == valid_proxy
 
 
-@pytest.yield_fixture(scope="function")
+@pytest.fixture(scope="function")
 def prepare_proxy(proxy_machine):
     proxy_ip, proxy_port = proxy_machine
     proxy_ssh = SSHClient(hostname=proxy_ip, **conf.credentials['proxy_vm'])
@@ -59,6 +76,7 @@ def prepare_proxy(proxy_machine):
     proxy_ssh.run_command('echo "" > /var/log/squid/access.log')
 
 
+@pytest.mark.tier(3)
 def test_proxy_valid(appliance, proxy_machine, prepare_proxy, provider, setup_provider, request):
     proxy_ip, proxy_port = proxy_machine
     prov_type = provider.type
@@ -68,14 +86,17 @@ def test_proxy_valid(appliance, proxy_machine, prepare_proxy, provider, setup_pr
     validate_proxy_logs(proxy_ip, appliance.hostname)
 
 
+@pytest.mark.tier(3)
 def test_proxy_invalid(appliance, proxy_machine, prepare_proxy, setup_provider, provider, request):
     proxy_ip, proxy_port = proxy_machine
     prov_type = provider.type
     appliance.set_proxy('1.1.1.1', proxy_port, prov_type=prov_type)
     request.addfinalizer(lambda: appliance.set_proxy(None, None, prov_type=prov_type))
+    request.addfinalizer(lambda: validate_provider_credentials(provider))
     validate_provider(provider, valid_proxy=False)
 
 
+@pytest.mark.tier(3)
 def test_proxy_remove(appliance, proxy_machine, prepare_proxy, provider, setup_provider, request):
     proxy_ip, proxy_port = proxy_machine
     prov_type = provider.type
@@ -84,10 +105,12 @@ def test_proxy_remove(appliance, proxy_machine, prepare_proxy, provider, setup_p
     validate_provider(provider)
     validate_proxy_logs(proxy_ip, appliance.hostname)
     appliance.set_proxy(None, None, prov_type=prov_type)
+    validate_provider_credentials(provider)
     validate_provider(provider)
     validate_proxy_logs(proxy_ip, appliance.hostname, valid_proxy=False)
 
 
+@pytest.mark.tier(3)
 def test_proxy_override(appliance, proxy_machine, prepare_proxy, provider, setup_provider, request):
     proxy_ip, proxy_port = proxy_machine
     prov_type = provider.type
@@ -96,5 +119,6 @@ def test_proxy_override(appliance, proxy_machine, prepare_proxy, provider, setup
     validate_provider(provider, valid_proxy=False)
     appliance.set_proxy(proxy_ip, proxy_port, prov_type=prov_type)
     request.addfinalizer(lambda: appliance.set_proxy(None, None, prov_type=prov_type))
+    validate_provider_credentials(provider)
     validate_provider(provider)
     validate_proxy_logs(proxy_ip, appliance.hostname)
