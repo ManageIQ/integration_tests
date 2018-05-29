fcd3aa780846686cfd7c552bb55eda1ffb5ce17b
Anurag Sinha <ansinha@redhat.com>
2018-05-29 18:53:56 +0530

    Making changes to resolve failure.

---
+++ cfme/tests/infrastructure/test_quota.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+import fauxfactory
+import pytest
+from riggerlib import recursive_update
+
+from cfme.infrastructure.provider.rhevm import RHEVMProvider
+from cfme.infrastructure.provider.virtualcenter import VMwareProvider
+from cfme.markers.env_markers.provider import ONE_PER_TYPE
+from cfme.provisioning import do_vm_provisioning
+from cfme.utils.generators import random_vm_name
+from cfme.utils.update import update
+
+pytestmark = [
+    pytest.mark.provider([RHEVMProvider, VMwareProvider], scope="module", selector=ONE_PER_TYPE)
+]
+
+
+@pytest.fixture(scope='module')
+def admin_email(appliance):
+    """Required for user quota tagging services to work, as it's mandatory for it's functioning."""
+    user = appliance.collections.users
+    admin = user.instantiate(name='Administrator')
+    with update(admin):
+        admin.email = 'xyz@redhat.com'
+    yield
+    with update(admin):
+        admin.email = ''
+
+
+@pytest.fixture
+def vm_name():
+    return random_vm_name(context='quota')
+
+
+@pytest.fixture
+def template_name(provider):
+    if provider.one_of(RHEVMProvider):
+        return provider.data.templates.get('full_template')['name']
+    elif provider.one_of(VMwareProvider):
+        return provider.data.templates.get('big_template')['name']
+
+
+@pytest.fixture()
+def prov_data():
+    return {
+        "catalog": {'vm_name': ''},
+        "environment": {'automatic_placement': True},
+    }
+
+
+@pytest.fixture(scope='module')
+def test_domain(appliance):
+    domain = appliance.collections.domains.create('test_{}'.format(fauxfactory.gen_alphanumeric()),
+                                                  'description_{}'.format(
+                                                      fauxfactory.gen_alphanumeric()),
+                                                  enabled=True)
+    yield domain
+    if domain.exists:
+        domain.delete()
+
+
+@pytest.fixture(scope='module')
+def max_quota_test_instance(appliance, test_domain):
+    miq = appliance.collections.domains.instantiate('ManageIQ')
+
+    original_instance = miq. \
+        namespaces.instantiate('System'). \
+        namespaces.instantiate('CommonMethods'). \
+        classes.instantiate('QuotaMethods'). \
+        instances.instantiate('quota_source')
+    original_instance.copy_to(domain=test_domain)
+
+    original_instance = miq. \
+        namespaces.instantiate('System'). \
+        namespaces.instantiate('CommonMethods'). \
+        classes.instantiate('QuotaStateMachine'). \
+        instances.instantiate('quota')
+    original_instance.copy_to(domain=test_domain)
+
+    instance = test_domain. \
+        namespaces.instantiate('System'). \
+        namespaces.instantiate('CommonMethods'). \
+        classes.instantiate('QuotaStateMachine'). \
+        instances.instantiate('quota')
+    yield instance
+
+
+def set_entity_quota_source(max_quota_test_instance, entity):
+    with update(max_quota_test_instance):
+        max_quota_test_instance.fields = {'quota_source_type': {'value': entity}}
+
+
+@pytest.fixture(params=[('groups', 'group', 'EvmGroup-super_administrator'),
+                        ('users', 'user', 'Administrator')],
+                ids=['group', 'user'],
+                scope='module')
+def entities(appliance, request, max_quota_test_instance):
+    collection, entity, description = request.param
+    set_entity_quota_source(max_quota_test_instance, entity)
+    yield getattr(appliance.collections, collection).instantiate(description)
+
+
+@pytest.mark.rhv2
+@pytest.mark.parametrize(
+    ['custom_prov_data'],
+    [
+        [{'hardware': {'memory': '4096'}}],
+        [{}],
+        [{'hardware': {'vm_num': '21'}}],
+        [{'hardware': {'num_sockets': '8'}}]
+    ],
+    ids=['max_memory',
+         'max_storage',
+         'max_vm',
+         'max_cpu']
+)
+def test_quota(appliance, provider, setup_provider, custom_prov_data, vm_name, admin_email,
+               entities, template_name, prov_data):
+    recursive_update(prov_data, custom_prov_data)
+    do_vm_provisioning(appliance, template_name=template_name, provider=provider, vm_name=vm_name,
+                       provisioning_data=prov_data, smtp_test=False, wait=False, request=None)
+
+    # nav to requests page to check quota validation
+    request_description = 'Provision from [{}] to [{}]'.format(template_name, vm_name)
+    provision_request = appliance.collections.requests.instantiate(request_description)
+    provision_request.wait_for_request(method='ui')
+    assert provision_request.row.reason.text == "Quota Exceeded"
