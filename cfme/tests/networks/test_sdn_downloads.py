4d3957343552dfc00e6b5ff33d1072d0e6b7c72e
Nikhil <ndhandre@redhat.com>
2018-06-05 13:52:44 +0530

    no need of instantiate

--- cfme/tests/networks/test_sdn_downloads.py
+++ cfme/tests/networks/test_sdn_downloads.py
@@ -1,10 +1,10 @@
 import pytest
+import random
 
 from cfme.cloud.provider.azure import AzureProvider
 from cfme.cloud.provider.ec2 import EC2Provider
 from cfme.cloud.provider.gce import GCEProvider
 from cfme.cloud.provider.openstack import OpenStackProvider
-from cfme.exceptions import ManyEntitiesFound
 from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.utils.blockers import BZ
 from cfme.utils.version import LATEST
@@ -57,10 +57,9 @@ def test_download_lists_base(filetype, collection_type, appliance):
 def test_download_pdf_summary(appliance, collection_type, provider):
     """ Download the summary details of specific object """
     collection = getattr(appliance.collections, collection_type)
-    if collection.all():
-        random_obj = collection.all()[0].name
-        try:
-            obj = collection.instantiate(name=random_obj)
-            download_summary(obj)
-        except ManyEntitiesFound:
-            pass
+    all_entities = collection.all()
+    if all_entities:
+        random_obj = random.choice(all_entities)
+        download_summary(random_obj)
+    else:
+        pytest.skip('{} entities not available'.format(collection_type))
