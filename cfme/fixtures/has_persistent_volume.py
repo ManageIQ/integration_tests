8d09cc2b374abe31a3b298623870dc3299676e12
Justin Watts <juwatts@redhat.com>
2018-06-11 14:07:20 -0400

    Fix CMQE Persistent Volume Fixture

--- cfme/fixtures/has_persistent_volume.py
+++ cfme/fixtures/has_persistent_volume.py
@@ -1,30 +1,12 @@
 import pytest
 
-from wrapanapi.containers.volume import Volume as VolumeApi
-from cfme.containers.volume import Volume
 
-
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='function')
 def has_persistent_volume(provider, appliance):
     """Verifying that some persistent volume exists"""
-    vols = provider.mgmt.list_volume()
+    vols = provider.mgmt.list_persistent_volume()
     vols_count = len(vols)
     if vols_count:
-        yield Volume(vols[0].name, provider, appliance)
+        yield
     else:
-        name = 'pv-{}'.format(vols_count + 1)
-        payload = {
-            'metadata': {'name': name},
-            'spec': {
-                'accessModes': ['ReadWriteOnce'],
-                'capacity': {'storage': '1Gi'},
-                'nfs': {
-                    'path': '/tmp',
-                    'server': '12.34.56.78'
-                }
-            },
-            'persistentVolumeReclaimPolicy': 'Retain'
-        }
-        volume = VolumeApi.create(provider.mgmt, payload)
-        yield Volume(volume.name, provider, appliance)
-        volume.delete()
+        pytest.skip('No Persistent Volumes Detected on OpenShift Provider {}'.format(provider.name))
