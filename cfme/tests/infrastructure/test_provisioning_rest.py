001728c000aac56943662841d4657e63d7586826
Jan Zmeskal <janzmeskal@email.cz>
2018-06-21 11:13:04 +0200

    Wrapping test_provision_attributes with BZ1592326 (#7382)

--- cfme/tests/infrastructure/test_provisioning_rest.py
+++ cfme/tests/infrastructure/test_provisioning_rest.py
@@ -4,7 +4,7 @@ import pytest
 from cfme import test_requirements
 from cfme.infrastructure.provider.rhevm import RHEVMProvider
 from cfme.infrastructure.provider.virtualcenter import VMwareProvider
-from cfme.utils.blockers import GH
+from cfme.utils.blockers import BZ
 from cfme.utils.rest import assert_response, query_resource_attributes
 from cfme.utils.wait import wait_for
 
@@ -181,7 +181,7 @@ def test_create_pending_provision_requests(request, appliance, provider, small_t
 
 
 @pytest.mark.rhv3
-@pytest.mark.meta(blockers=[GH('ManageIQ/integration_tests:7372')])
+@pytest.mark.meta(blockers=[BZ(1592326, forced_stream=['5.8', '5.9', '5.10'])])
 def test_provision_attributes(appliance, provider, small_template, soft_assert):
     """Tests that it's possible to display additional attributes in /api/provision_requests/:id.
 
