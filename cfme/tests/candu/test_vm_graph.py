5ff2f86ef7368b9a3057cc756d50901eaf4d1a18
NIKHIL DHANDRE <ndhandre@redhat.com>
2018-06-23 02:30:19 +0530

    [1LP][RFR] Daily and Hourly graph tests (#7357)
    * Initial commit
    
    * keyerror fix
    
    * return and scope

--- cfme/tests/candu/test_vm_graph.py
+++ cfme/tests/candu/test_vm_graph.py
@@ -6,6 +6,7 @@ from cfme.cloud.provider.ec2 import EC2Provider
 from cfme.infrastructure.provider.rhevm import RHEVMProvider
 from cfme.infrastructure.provider.virtualcenter import VMwareProvider
 from cfme.utils.appliance.implementations.ui import navigate_to
+from cfme.utils.log import logger
 from cfme.utils.wait import wait_for
 
 
@@ -18,6 +19,7 @@ pytestmark = [
 ]
 
 VM_GRAPHS = ['vm_cpu', 'vm_cpu_state', 'vm_memory', 'vm_disk', 'vm_network']
+INTERVAL = ['Hourly', 'Daily']
 
 
 # ToDo: Add support for GCE provider once BZ-1511099 fixed
@@ -76,4 +78,59 @@ def test_vm_most_recent_hour_graph_screen(graph_type, provider, enable_candu):
         # Note: legend like %Ready have less value some time zero. so sum data for all legend
         for data in graph.data_for_legends(leg).values():
             graph_data += float(data[leg].replace(',', '').replace('%', '').split()[0])
+    assert graph_data > 0
+
+
+# To-Do add support for other provider
+@pytest.mark.provider([VMwareProvider], scope='module', override=True)
+@pytest.mark.parametrize('interval', INTERVAL)
+@pytest.mark.parametrize('graph_type', VM_GRAPHS)
+def test_graph_screen(provider, interval, graph_type, enable_candu, collect_data):
+    """Test VM graphs for hourly and Daily
+
+    prerequisites:
+        * C&U enabled appliance
+
+    Steps:
+        * Navigate to VM (cu-24x7) Utilization Page
+        * Check graph displayed or not
+        * Check legends hide and display properly or not
+        * Check data for legends collected or not
+    """
+    collection = provider.appliance.provider_based_collection(provider)
+    vm = collection.instantiate('cu-24x7', provider)
+    vm.wait_candu_data_available(timeout=1200)
+
+    view = navigate_to(vm, 'candu')
+    view.options.interval.fill(interval)
+
+    graph = getattr(view, graph_type)
+    assert graph.is_displayed
+
+    def refresh():
+        provider.browser.refresh()
+        view = navigate_to(vm, 'candu')
+        view.options.interval.fill(interval)
+
+    # wait, some time graph took time to load
+    wait_for(lambda: len(graph.all_legends) > 0,
+             delay=5, timeout=600, fail_func=refresh)
+
+    # Check for legend hide property and graph data
+    graph_data = 0
+    for leg in graph.all_legends:
+        # check legend hide or not
+        graph.hide_legends(leg)
+        assert not graph.legend_is_displayed(leg)
+        # check legend display or not
+        graph.display_legends(leg)
+        assert graph.legend_is_displayed(leg)
+
+        # check graph display data or not
+        # Note: legend like %Ready have less value some time zero. so sum data for all legend
+        for data in graph.data_for_legends(leg).values():
+            try:
+                graph_data += float(data[leg].replace(',', '').replace('%', '').split()[0])
+            except KeyError:
+                logger.warning("{} has not data point on graph".format(leg))
     assert graph_data > 0
