42db963c3bef0247bc71f5d76d540ec5666eab0f
Shalom Naim <snaim@redhat.com>
2018-07-18 16:39:31 +0300

    Add a test for Embedded Ansible

--- cfme/tests/containers/test_provider_configuration_menu.py
+++ cfme/tests/containers/test_provider_configuration_menu.py
@@ -2,7 +2,7 @@ import pytest
 
 from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.containers.provider import ContainersProvider
-
+from cfme.utils.wait import wait_for
 
 pytestmark = [
     pytest.mark.usefixtures('setup_provider'),
@@ -22,3 +22,15 @@ def test_edit_selected_containers_provider(provider):
     view = navigate_to(provider, 'Edit')
     assert view.is_displayed
     view.cancel.click()
+
+def test_start_embedded_ansible(appliance):
+
+    def monitor_container(appliance):
+        appliance.collections.containers.all()
+
+    # Enable embedded ansible
+    args = ["embedded_ansible"]
+    appliance.server.settings.enable_server_roles(*args)
+
+    # Waiting for container to start
+    assert wait_for(monitor_container, delay=30, timeout="10m"), "No ansible container started"
