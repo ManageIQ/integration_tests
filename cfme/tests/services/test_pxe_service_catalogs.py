4054f7779e332501afedcd6c30752d2403fd47d1
Jan Zmeskal <janzmeskal@email.cz>
2018-07-26 17:24:21 +0200

    Adding GH

--- cfme/tests/services/test_pxe_service_catalogs.py
+++ cfme/tests/services/test_pxe_service_catalogs.py
@@ -8,6 +8,7 @@ from cfme.infrastructure.provider import InfraProvider
 from cfme.infrastructure.pxe import get_pxe_server_from_config, get_template_from_config
 from cfme.services.service_catalogs import ServiceCatalogs
 from cfme.utils import testgen
+from cfme.utils.blockers import GH
 from cfme.utils.conf import cfme_data
 from cfme.utils.generators import random_vm_name
 from cfme.utils.log import logger
