c2b04376580691f01d01fdf4badbe49de1e0ba77
Shalom Naim <snaim@redhat.com>
2018-07-27 12:17:01 +0300

    Test OCP operator role out-of-the-box

--- cfme/tests/containers/test_provider_configuration_menu.py
+++ cfme/tests/containers/test_provider_configuration_menu.py
@@ -22,3 +22,14 @@ def test_edit_selected_containers_provider(provider):
     view = navigate_to(provider, 'Edit')
     assert view.is_displayed
     view.cancel.click()
+
+
+def test_ocp_operator_out_of_the_box(appliance):
+
+    roles_collection = appliance.collections.roles
+    view = navigate_to(roles_collection, "All")
+    role_name_prefix = "container_operator"
+    is_role_found = bool(filter(lambda row: role_name_prefix in row.name.text.lower(),
+                                view.table.rows()))
+
+    assert is_role_found, "No {role} found".format(role=role_name_prefix)
