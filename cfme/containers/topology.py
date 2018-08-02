4f95e85749f3cff300e4395e092998c2b450909d
amritrajck <amritrajck@gmail.com>
2018-08-02 19:59:55 +0530

    Update topology.py
    Conversion to Collection v3

--- cfme/containers/topology.py
+++ cfme/containers/topology.py
@@ -1,23 +1,34 @@
 # -*- coding: utf-8 -*-
+import attr
+
 from navmazing import NavigateToAttribute
 
+from cfme.modeling.base import BaseCollection, BaseEntity
 from cfme.common import TopologyMixin
 from cfme.utils.appliance.implementations.ui import CFMENavigateStep, navigator, navigate_to
-from cfme.utils.appliance import Navigatable
 
 
-class Topology(TopologyMixin, Navigatable):
 
-    def __init__(self, appliance=None):
-        Navigatable.__init__(self, appliance=appliance)
+class Topology(TopologyMixin, BaseEntity):
+
+    region = attr.ib(default=None)
+    provider = attr.ib(default=None)
+    datastore = attr.ib(default=None)
 
     @classmethod
     def load_topology_page(cls):
         navigate_to(cls, 'All')
+        
+        
+@attr.s
+class TopologyCollection(BaseCollection):
+    def __init__(self, appliance=None):	    
+        ENTITY = Topology
 
 
 @navigator.register(Topology, 'All')
 class All(CFMENavigateStep):
+    
     prerequisite = NavigateToAttribute('appliance.server', 'LoggedIn')
 
     def step(self):
