913e237d5f693ea137db11fcc879b74fd4372ac3
izapolsk <izapolsk@redhat.com>
2018-08-03 12:43:10 +0300

    glance upload template modules produced errors in travis. so, those were added to ignore list. this commit removes them from ignore list

--- cfme/tests/test_modules_importable.py
+++ cfme/tests/test_modules_importable.py
@@ -19,15 +19,6 @@ KNOWN_FAILURES = set(ROOT.dirpath().join(x) for x in[
     'cfme/fixtures/widgets.py',
     'cfme/dashboard.py',
     'cfme/configure/tasks.py',
-    'cfme/utils/template/template_upload.py',
-    'cfme/utils/template/ec2.py',
-    'cfme/utils/template/gce.py',
-    'cfme/utils/template/openstack.py',
-    'cfme/utils/template/rhevm.py',
-    'cfme/utils/template/rhopenshift.py',
-    'cfme/utils/template/scvmm.py',
-    'cfme/utils/template/virtualcenter.py',
-    'cfme/utils/template/base.py',
 ])
 
 
