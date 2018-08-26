e1780c55118bb3d42466df54dd91a6c9dbf65747
tkhamis <tkhamis@redhat.com>
2018-08-26 12:11:35 +0300

    1. Refactored check_ems_state_in_diagnostics 2. Refactored pause and resume button clicking

--- cfme/tests/containers/test_pause_resume_workers.py
+++ cfme/tests/containers/test_pause_resume_workers.py
@@ -11,32 +11,13 @@ pytestmark = [
 ]
 
 
-def get_pause_resume_buttons(view):
-    resume_option = [menu_item for menu_item in view.toolbar.configuration.items if
-                     "resume this containers provider" in menu_item.lower()]
-    # ensure resume option is exists
-    assert resume_option, "No resume provider option is available in the configuration menu"
-    resume_option = resume_option.pop()
-
-    pause_option = [menu_item for menu_item in view.toolbar.configuration.items if
-                    "pause this containers provider" in menu_item.lower()]
-    # ensure pause option is exists
-    assert pause_option, "No pause provider option is available in the configuration menu"
-    pause_option = pause_option.pop()
-
-    return pause_option, resume_option
-
-
 def check_ems_state_in_diagnostics(appliance, provider):
     workers_view = navigate_to(appliance.collections.diagnostic_workers, 'AllDiagnosticWorkers')
     workers_view.browser.refresh()
     try:
-        rows = workers_view.workers_table.rows(
-            name='Event Monitor for Provider: {}'.format(provider.name))
-        for r in rows:
+        if workers_view.workers_table.rows(
+                name='Event Monitor for Provider: {}'.format(provider.name)).next():
             return True
-        else:
-            return False
     except Exception:
         return False
     return False
@@ -56,14 +37,13 @@ def test_pause_and_resume_provider_workers(appliance, provider):
         8. Validate the ems_ workers are started
     """
     view = navigate_to(provider, "Details")
-    pause_option, resume_option = get_pause_resume_buttons(view)
     # pause the provider
-    view.toolbar.configuration.item_select(pause_option, handle_alert=True)
+    view.toolbar.configuration.item_select(provider.pause_provider_text, handle_alert=True)
     ems_worker_state = check_ems_state_in_diagnostics(appliance, provider)
     assert not ems_worker_state, "Diagnostics shows that workers are running after pause provider"
 
     view = navigate_to(provider, "Details")
     # resume the provider
-    view.toolbar.configuration.item_select(resume_option, handle_alert=True)
+    view.toolbar.configuration.item_select(provider.resume_provider_text, handle_alert=True)
     ems_worker_state = check_ems_state_in_diagnostics(appliance, provider)
     assert ems_worker_state, "Diagnostics shows that workers are not running after resume provider"
