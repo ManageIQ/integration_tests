636a7979a68524bfab14a72a312aa7fc06242b03
Matej Artač <matej.artac@xlab.si>
2018-08-29 16:21:58 +0200

    Add test for redfish physical server details
    This test validates the statistics of a selected physical server
    that has been inventorised by the Redfish provider as it is shown
    in the GUI. It checks the displayed values against the ones
    accessible through the Redfish API client.

---
+++ cfme/tests/physical_infrastructure/test_redfish_physical_server_details.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from cfme.physical.provider.redfish import RedfishProvider
+
+pytestmark = [pytest.mark.provider([RedfishProvider], scope="function")]
+
+
+@pytest.fixture(scope="function")
+def physical_server(appliance, provider, setup_provider_funcscope):
+    # Get and return the first physical server
+    yield appliance.collections.redfish_physical_servers.all(provider)[0]
+
+
+def test_redfish_physical_server_details_stats(physical_server):
+    """Navigate to the physical server details page and verify that the stats match"""
+    physical_server.validate_stats(ui=True)
