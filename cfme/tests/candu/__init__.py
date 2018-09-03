77b57a3e56a6f80bb71b88a041634abaf421a385
Nikhil <ndhandre@redhat.com>
2018-09-03 14:38:25 +0530

    added comments

--- cfme/tests/candu/__init__.py
+++ cfme/tests/candu/__init__.py
@@ -44,24 +44,35 @@ def compare_data_with_unit(table_data, graph_data, legends, tolerance=1):
         for key, data in graph_data.items():
             if any([re.match(key, item) for item in row["Date/Time"].split()]):
                 for leg in legends:
+                    # For table item data separating unit, removing `,` and removing "%" Unit
+                    # like `1,000 MB` to [1000, "MB"]
                     table_item = re.split(" |%", row[leg].replace(",", ""))
                     tb_value, tb_unit = table_item if len(table_item) > 1 else (table_item[0], None)
                     if table_item:
+                        # For graph item data separating unit, removing `,` and removing "%" Unit
+                        # like `1,000 MB` to [1000, "MB"]
                         graph_item = re.split(" |%", data[leg].replace(",", ""))
                         gp_value, gp_unit = (
                             graph_item if len(graph_item) > 1 else (graph_item[0], None)
                         )
 
+                        # Workaround for BZ-1367560; In absence of table item unit.
                         if not tb_unit:
                             if gp_unit == "GHz":
+                                # By observation, if Graph unit in GHz table value unit is in MHz.
                                 tb_value = round(float(tb_value) * 10 ** -3, 1)
                                 tb_unit = "GHz"
                             elif gp_unit == "GB":
+                                # By observation, if Graph unit in GB table value unit is in MB.
                                 tb_value = round(float(tb_value) / 1024, 1)
                                 tb_unit = "GB"
                             elif gp_unit in ["MHz", "MB", "B", "KBps"]:
+                                # By observation, if Graph unit MHz, MB, B or KBps then table value
+                                # unit no change means same unit as graph value.
                                 tb_unit = gp_unit
                         elif gp_unit == "":
+                            # We removed % unit for CPU ready graph in unit separation due to no
+                            # no separation between value and unit like 1%. Reassign unit.
                             gp_unit = tb_unit = "%"
                         elif tb_unit == "Bytes" and gp_unit == "B":
                             tb_unit = gp_unit
