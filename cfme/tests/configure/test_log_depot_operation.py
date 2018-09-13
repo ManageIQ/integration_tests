a8c96335c13239d5c7ef03a28c872b65d7f1a6ad
Angelina Nikiforova <anikifor@redhat.com>
2018-09-13 10:11:21 +0200

    removed refresh for 5.8

--- cfme/tests/configure/test_log_depot_operation.py
+++ cfme/tests/configure/test_log_depot_operation.py
@@ -212,10 +212,7 @@ def ansible_repository(appliance, wait_for_ansible):
         url="https://github.com/quarckster/ansible_playbooks",
         description=fauxfactory.gen_alpha())
     view = navigate_to(repository, "Details")
-    if appliance.version < "5.9":
-        refresh = view.browser.refresh
-    else:
-        refresh = view.toolbar.refresh.click
+    refresh = view.toolbar.refresh.click
     wait_for(
         lambda: view.entities.summary("Properties").get_text_of("Status") == "successful",
         timeout=60,
