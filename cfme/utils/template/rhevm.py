37a7552d2428390a8f52936b7a71ea7a447a8b2a
nachandr <nachandr@redhat.com>
2018-10-04 06:28:33 -0500

    [RFR] Fix ovirt template upload failure (#7900)
    * Fix ovirt template upload failure
    
    * Fix ovirt template upload failure
    
    * Fix ovirt template upload failure

--- cfme/utils/template/rhevm.py
+++ cfme/utils/template/rhevm.py
@@ -12,7 +12,8 @@ from cfme.utils.log import logger
 class RHEVMTemplateUpload(ProviderTemplateUpload):
     provider_type = 'rhevm'
     log_name = 'RHEVM'
-    image_pattern = re.compile(r'<a href="?\'?([^"\']*(?:rhevm[^"\']*\.[qcow2|ova])[^"\'>]*)')
+    image_pattern = re.compile(
+        r'<a href="?\'?([^"\']*(?:(?:rhevm|ovirt)[^"\']*\.(?:qcow2|qc2))[^"\'>]*)')
 
     @log_wrap('add glance to rhevm provider')
     def add_glance_to_provider(self):
