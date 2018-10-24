ea091a734d8a63db5d00eacaa681b54a1fcf21cd
lina-nikiforova <42433123+lina-nikiforova@users.noreply.github.com>
2018-10-24 16:21:08 +0200

    [1LP][WIPTEST] new test_provider_log_level (#7890)
    * new test_provider_log_level
    
    * added assert for log_exists and success for ssh command result
    
    * cast log_count to int
    
    * added blocker and wait_for for log level change

--- cfme/tests/configure/test_logs.py
+++ cfme/tests/configure/test_logs.py
@@ -5,6 +5,7 @@ from cfme.cloud.provider.azure import AzureProvider
 from cfme.cloud.provider.ec2 import EC2Provider
 from cfme.infrastructure.provider.rhevm import RHEVMProvider
 from cfme.infrastructure.provider.scvmm import SCVMMProvider
+from cfme.utils.blockers import BZ
 from cfme.utils.wait import wait_for
 
 pytestmark = [
@@ -49,16 +50,16 @@ def test_provider_log_rotate(appliance, provider, log_exists):
     Metadata:
         test_flag: log
     """
-    if log_exists:
-        appliance.ssh_client.run_command("logrotate -f /etc/logrotate.d/miq_logs.conf")
-        logs_count = appliance.ssh_client.run_command(
-            "ls -l /var/www/miq/vmdb/log/{}.log*|wc -l".format(
-                provider.log_name
-            )
-        ).output
-        assert logs_count > 1, "{}.log wasn't rotated by default miq_logs.conf".format(
+    assert log_exists, "Log file {}.log doesn't exist".format(provider.log_name)
+    appliance.ssh_client.run_command("logrotate -f /etc/logrotate.d/miq_logs.conf")
+    logs_count = int(appliance.ssh_client.run_command(
+        "ls -l /var/www/miq/vmdb/log/{}.log*|wc -l".format(
             provider.log_name
         )
+    ).output.rstrip())
+    assert logs_count > 1, "{}.log wasn't rotated by default miq_logs.conf".format(
+        provider.log_name
+    )
 
 
 def test_provider_log_updated(appliance, provider, log_exists):
@@ -74,17 +75,52 @@ def test_provider_log_updated(appliance, provider, log_exists):
     Metadata:
         test_flag: log
     """
-    if log_exists:
-        log_before = appliance.ssh_client.run_command(
-            "md5sum /var/www/miq/vmdb/log/{}.log | awk '{{ print $1 }}'".format(
-                provider.log_name
-            )
-        ).output
+    assert log_exists, "Log file {}.log doesn't exist".format(provider.log_name)
+    log_before = appliance.ssh_client.run_command(
+        "md5sum /var/www/miq/vmdb/log/{}.log | awk '{{ print $1 }}'".format(
+            provider.log_name
+        )
+    ).output
 
-        wait_for(provider.is_refreshed, func_kwargs=dict(refresh_delta=10), timeout=600)
-        log_after = appliance.ssh_client.run_command(
-            "md5sum /var/www/miq/vmdb/log/{}.log | awk '{{ print $1 }}'".format(
-                provider.log_name
-            )
-        ).output
-        assert log_before != log_after, "Log hashes are the same"
+    wait_for(provider.is_refreshed, func_kwargs=dict(refresh_delta=10), timeout=600)
+    log_after = appliance.ssh_client.run_command(
+        "md5sum /var/www/miq/vmdb/log/{}.log | awk '{{ print $1 }}'".format(
+            provider.log_name
+        )
+    ).output
+    assert log_before != log_after, "Log hashes are the same"
+
+
+@pytest.mark.meta(blockers=[BZ(1633656,
+                               unblock=lambda provider: provider.one_of(AzureProvider, EC2Provider),
+                               forced_streams=["5.9", "5.10", "upstream"]),
+                            BZ(1640718,
+                               unblock=lambda provider: not provider.one_of(AzureProvider),
+                               forced_streams=["5.9"])
+                            ]
+                  )
+def test_provider_log_level(appliance, provider, log_exists):
+    """
+    Tests that log level in advanced settings affects log files
+
+    Steps:
+    1. Change log level to debug
+    2. Refresh provider
+    3. Check logs contain debug level
+    4. Reset level back
+
+    Metadata:
+        test_flag: log
+    """
+    assert log_exists, "Log file {}.log doesn't exist".format(provider.log_name)
+    log_level = appliance.server.advanced_settings['log']['level_{}'.format(provider.log_name)]
+    # set log level to debug
+    wait_for(lambda: appliance.server.update_advanced_settings(
+        {'log': {'level_{}'.format(provider.log_name): 'debug'}}), timeout=300)
+    wait_for(provider.is_refreshed, func_kwargs=dict(refresh_delta=10), timeout=600)
+    debug_in_logs = appliance.ssh_client.run_command(
+        "cat /var/www/miq/vmdb/log/{}.log | grep DEBUG".format(provider.log_name))
+    # set log level back
+    appliance.server.update_advanced_settings(
+        {'log': {'level_{}'.format(provider.log_name): log_level}})
+    assert debug_in_logs.success
