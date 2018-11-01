815dba2662bb011ba96179f2758bdd1aab167d91
John Dupuy <jdupuy@redhat.com>
2018-11-01 08:23:11 -0400

    Add pytest.mark.tier(3) for all tests in test_embedded_ansible_automate

--- cfme/tests/ansible/test_embedded_ansible_automate.py
+++ cfme/tests/ansible/test_embedded_ansible_automate.py
@@ -22,7 +22,8 @@ pytestmark = [
     pytest.mark.provider([VMwareProvider], selector=ONE_PER_TYPE, scope="module"),
     test_requirements.ansible,
     pytest.mark.uncollectif(lambda appliance: appliance.version < "5.9" and appliance.is_pod,
-                            reason="5.8 pod appliance doesn't support embedded ansible")
+                            reason="5.8 pod appliance doesn't support embedded ansible"),
+    pytest.mark.tier(3)
 ]
 
 
