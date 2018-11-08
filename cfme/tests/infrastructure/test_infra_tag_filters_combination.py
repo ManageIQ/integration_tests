1c1610a57dd1d0d018c6d0d3c23fffce14704125
Angelina Nikiforova <anikifor@redhat.com>
2018-11-08 16:39:02 +0100

    fixed ProviderTemplatesView and removed BZ blocker

--- cfme/tests/infrastructure/test_infra_tag_filters_combination.py
+++ cfme/tests/infrastructure/test_infra_tag_filters_combination.py
@@ -51,7 +51,7 @@ def group_tag_datacenter_combination(group_with_tag, provider):
                                         provider.data['datacenters'][0]], True)
 
 
-@pytest.mark.meta(blockers=[BZ(1533391, forced_streams=["5.9", "upstream"])])
+@pytest.mark.meta(blockers=[BZ(1648658, forced_streams=["5.9"])])
 @pytest.mark.parametrize('visibility', [True, False], ids=['visible', 'not_visible'])
 def test_tagvis_tag_datacenter_combination(testing_vis_object, group_tag_datacenter_combination,
                                 check_item_visibility, visibility):
