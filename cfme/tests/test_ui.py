e404f9e4f660cce1be68ebe9a959c706a635270e
Angelina Nikiforova <anikifor@redhat.com>
2018-12-07 11:28:01 +0100

    added check for the meta tag for IE11

--- cfme/tests/test_ui.py
+++ cfme/tests/test_ui.py
@@ -1,3 +1,4 @@
+from cfme.fixtures.soft_assert import soft_assert
 from cfme.utils.appliance.implementations.ui import navigate_to
 
 
@@ -23,6 +24,9 @@ def test_each_page(appliance):
         initialEstimate: None
     """
     view = navigate_to(appliance.server, 'Dashboard')
+    # test meta is here and CFME will be displayed correctly in IE11
+    edge_header = view.browser.element('//meta[@content="IE=edge"]')
+    soft_assert(edge_header.get_attribute('http-equiv') == 'X-UA-Compatible')
     tree = view.navigation.nav_item_tree()
     paths = []
     traverse(tree, paths, path=None)
