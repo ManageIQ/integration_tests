0143f64440f17e2abb7d0b0145ac95f093349230
NIKHIL DHANDRE <ndhandre@redhat.com>
2018-12-28 20:11:28 +0530

    [1LP][RFR] CB: Added test for open url (#8267)
    * added test for open url
    
    * to-do related to selenium use
    
    * fix instance method
    
    * docblock
    
    * use of iana domain
    
    * added finalizer for widows clean
    
    * symetirc diff removed

--- cfme/tests/automate/custom_button/test_infra_objects.py
+++ cfme/tests/automate/custom_button/test_infra_objects.py
@@ -1,8 +1,10 @@
 import pytest
 import fauxfactory
+from textwrap import dedent
 
 from widgetastic_patternfly import Dropdown
 
+from cfme.automate.explorer.domain import DomainCollection
 from cfme.infrastructure.provider.virtualcenter import VMwareProvider
 from cfme.markers.env_markers.provider import ONE_PER_TYPE
 from cfme.tests.automate.custom_button import log_request_check, TextInputDialogView
@@ -26,6 +28,45 @@ DISPLAY_NAV = {
 }
 
 SUBMIT = ["Submit all", "One by one"]
+
+
+@pytest.fixture(scope="module")
+def cls(appliance):
+    domain = appliance.collections.domains.create(
+        name="domain_{}".format(fauxfactory.gen_alphanumeric(4)), enabled=True
+    )
+    original_class = (
+        domain.parent.instantiate(name="ManageIQ")
+        .namespaces.instantiate(name="System")
+        .classes.instantiate(name="Request")
+    )
+    original_class.copy_to(domain=domain)
+    yield domain.namespaces.instantiate(name="System").classes.instantiate(name="Request")
+    if domain.exists:
+        domain.delete()
+
+
+@pytest.fixture(scope="module")
+def method(cls):
+    meth = cls.methods.create(
+        name="meth_{}".format(fauxfactory.gen_alphanumeric(4)),
+        script=dedent(
+            """
+            # add google url to open
+            vm = $evm.root['vm']
+            $evm.log(:info, "Opening url")
+            vm.remote_console_url = "http://example.com"
+            """
+        ),
+    )
+
+    instance = cls.instances.create(
+        name="inst_{}".format(fauxfactory.gen_alphanumeric(4)),
+        fields={"meth1": {"value": meth.name}},
+    )
+    yield instance
+    meth.delete_if_exists()
+    instance.delete_if_exists()
 
 
 @pytest.fixture(
@@ -339,3 +380,59 @@ def test_custom_button_expression(appliance, request, setup_obj, button_group, e
             assert not custom_button_group.is_displayed
             setup_obj.add_tag(tag)
             assert custom_button_group.is_displayed
+
+
+@pytest.mark.uncollectif(lambda button_group: "VM_INSTANCE" not in button_group)
+def test_open_url(request, setup_obj, button_group, method):
+    """ Test Open url functionality of custom button.
+
+    Polarion:
+        assignee: ndhandre
+        initialEstimate: 1/2
+        caseimportance: high
+        testSteps:
+            1. Appliance with Infra provider
+            2. Create ruby method for url functionality
+            3. Create custom button group with the Object type
+            4. Create a custom button with open_url option and respective method
+            5. Navigate to object Detail page
+            6. Execute custom button
+            7. Check new tab open or not with respective url
+    """
+
+    group, obj_type = button_group
+    button = group.buttons.create(
+        text=fauxfactory.gen_alphanumeric(),
+        hover=fauxfactory.gen_alphanumeric(),
+        open_url=True,
+        display_for="Single entity",
+        system="Request",
+        request=method.name,
+    )
+    request.addfinalizer(button.delete_if_exists)
+
+    view = navigate_to(setup_obj, "Details")
+    custom_button_group = Dropdown(view, group.hover)
+    assert custom_button_group.has_item(button.text)
+
+    # TODO: Move windows handling functionality to browser
+    initial_count = len(view.browser.selenium.window_handles)
+    main_window = view.browser.selenium.current_window_handle
+    custom_button_group.item_select(button.text)
+
+    wait_for(
+        lambda: len(view.browser.selenium.window_handles) > initial_count,
+        timeout=120,
+        message="Check for window open",
+    )
+    open_url_window = set(view.browser.selenium.window_handles) - {main_window}
+
+    view.browser.selenium.switch_to_window(open_url_window.pop())
+
+    @request.addfinalizer
+    def _reset_window():
+        if view.browser.selenium.current_window_handle != main_window:
+            view.browser.selenium.close()
+            view.browser.selenium.switch_to_window(main_window)
+
+    assert "example.com" in view.browser.url
