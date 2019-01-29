24332af7e1e1905e63993ea032d861cbdc6a62a7
Yadnyawalkya <ytale@redhat.com>
2019-01-29 17:20:47 +0530

    Changes with black

--- cfme/tests/v2v/test_rest_migrations.py
+++ cfme/tests/v2v/test_rest_migrations.py
@@ -27,10 +27,12 @@ def get_clusters(appliance, v2v_providers):
     clusters = {}
     source_cluster = v2v_providers.vmware_provider.data.get("clusters")[0]
     target_cluster = v2v_providers.rhv_provider.data.get("clusters")[0]
-    cluster_db = {cluster.name: cluster for cluster in appliance.rest_api.collections.clusters.all}
+    cluster_db = {
+        cluster.name: cluster for cluster in appliance.rest_api.collections.clusters.all
+    }
 
     if source_cluster in cluster_db.keys():
-        clusters['source'] = cluster_db[source_cluster].href
+        clusters["source"] = cluster_db[source_cluster].href
     if target_cluster in cluster_db.keys():
         clusters["destination"] = cluster_db[target_cluster].href
     print clusters
@@ -44,7 +46,9 @@ def get_datastores(appliance, v2v_providers):
         i.name for i in v2v_providers.vmware_provider.data.datastores if i.type == "nfs"][0]
     target_ds = [
         i.name for i in v2v_providers.rhv_provider.data.datastores if i.type == "nfs"][0]
-    datastore_db = {ds.name: ds for ds in appliance.rest_api.collections.data_stores.all}
+    datastore_db = {
+        ds.name: ds for ds in appliance.rest_api.collections.data_stores.all
+    }
 
     if source_ds in datastore_db.keys():
         datastores["source"] = datastore_db[source_ds].href
@@ -59,12 +63,14 @@ def get_networks(appliance, v2v_providers):
     networks = {}
     source_network = v2v_providers.vmware_provider.data.get("vlans", [None])[0]
     target_network = v2v_providers.rhv_provider.data.get("vlans", [None])[0]
-    network_db = {network.name: network for network in appliance.rest_api.collections.lans.all}
+    network_db = {
+        network.name: network for network in appliance.rest_api.collections.lans.all
+    }
 
     if source_network in network_db.keys():
         networks["source"] = network_db[source_network].href
     if target_network in network_db.keys():
-            networks["destination"] = network_db[target_network].href
+        networks["destination"] = network_db[target_network].href
     print networks
     return networks
 
