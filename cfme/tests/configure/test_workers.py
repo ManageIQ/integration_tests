79f89136b763ec5a23a38fbf537bba098ed76590
john-dupuy <jdupuy@redhat.com>
2019-02-13 09:21:57 -0500

    Add in a test-cases for a BZ

--- cfme/tests/configure/test_workers.py
+++ cfme/tests/configure/test_workers.py
@@ -27,3 +27,33 @@ def test_restart_workers(appliance):
     wait_for(lambda: len(pids) == len(worker.get_all_worker_pids()),
              fail_func=worker.parent.reload_workers_page, num_sec=1800, delay=10,
              message="Wait for all original workers are back online")
+
+
+@pytest.mark.manual
+@pytest.mark.tier(0)
+def test_set_memory_threshold_in_ui():
+    """
+    Bugzillas:
+        * 1656873
+
+    Polarion:
+        assignee: anikifor
+        casecomponent: WebUI
+        caseimportance: medium
+        initialEstimate: 1/30h
+        testSteps:
+            1. Navigate to Configuration
+            2. Select the workers tab
+            3. Set memory threshold of "Generic Workers" to a value above 1 GB
+            4. Hit save
+            5. Set the memory threshold of "Event Monitor" to a value above 1 GB
+            6. Hit save
+        expectedResults:
+            1.
+            2.
+            3.
+            4. the change should be reflected in the dropdown
+            5.
+            6. same as 4.
+    """
+    pass
