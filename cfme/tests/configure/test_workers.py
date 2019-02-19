6ff3f5a93367aa2b73f6ae7fcca1a60790ae3593
john-dupuy <44065123+john-dupuy@users.noreply.github.com>
2019-02-19 10:45:47 -0500

    [1LP][RFR] Add test cases for BZ (#8468)
    * Add in a test-cases for a BZ
    
    * Automate this manual test case
    
    * Update to use namedtuple and add some extra test checks

--- cfme/tests/configure/test_workers.py
+++ cfme/tests/configure/test_workers.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+from collections import namedtuple
+from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.utils.blockers import BZ
 from cfme.utils.wait import wait_for
+
+Dropdown = namedtuple('Dropdown', 'dropdown id')
+
+DROPDOWNS = [
+    Dropdown('generic_worker_threshold', 'generic'),
+    Dropdown('cu_data_collector_worker_threshold', 'cu_data_coll'),
+    Dropdown('event_monitor_worker_threshold', 'event_monitor'),
+    Dropdown('connection_broker_worker_threshold', 'conn_broker'),
+    Dropdown('reporting_worker_threshold', 'reporting'),
+    Dropdown('web_service_worker_threshold', 'web_service'),
+    Dropdown('priority_worker_threshold', 'priority'),
+    Dropdown('cu_data_processor_worker_threshold', 'cu_data_proc'),
+    Dropdown('refresh_worker_threshold', 'refresh'),
+    Dropdown('vm_analysis_collectors_worker_threshold', 'vm_analysis')
+]
 
 
 @pytest.mark.tier(3)
@@ -27,3 +44,45 @@ def test_restart_workers(appliance):
     wait_for(lambda: len(pids) == len(worker.get_all_worker_pids()),
              fail_func=worker.parent.reload_workers_page, num_sec=1800, delay=10,
              message="Wait for all original workers are back online")
+
+
+@pytest.mark.tier(2)
+@pytest.mark.parametrize("dropdown", [x.dropdown for x in DROPDOWNS], ids=[x.id for x in DROPDOWNS])
+@pytest.mark.meta(blockers=[BZ(1656873, forced_streams="5.9")])
+def test_set_memory_threshold_in_ui(appliance, dropdown):
+    """
+    Bugzillas:
+        * 1656873
+
+    Polarion:
+        assignee: anikifor
+        casecomponent: WebUI
+        caseimportance: medium
+        initialEstimate: 1/6h
+        testSteps:
+            1. Navigate to Configuration
+            2. Select the workers tab
+            3. Set memory threshold of any dropdown to a value above 1 GB
+            4. Hit save
+        expectedResults:
+            1.
+            2.
+            3.
+            4. the change should be reflected in the dropdown
+    """
+    view = navigate_to(appliance.server, 'Workers')
+    mem_threshold = getattr(view.workers, dropdown)
+    before = mem_threshold.selected_option
+    change = "1.1 GB"
+    if before == change:
+        change = "1.2 GB"
+    mem_threshold.select_by_visible_text(change)
+    view.workers.save.click()
+    view.wait_displayed()
+    after = mem_threshold.selected_option
+
+    assert before != after
+    assert after == change
+
+    # reset the mem threshold after the test
+    view.workers.reset.click()
