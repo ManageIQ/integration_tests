d5b96cee8c65a36a0772795bd689618bac230609
Aljaž Košir <aljazkosir5@gmail.com>
2019-03-07 15:07:07 +0100

    Refactor Nuage enterprise sandbox so that it also returns count values
    We move hardcoded values inside tests that uses nuage sandbox fixture to
    fixture where we use python dictionary to store count values

--- cfme/fixtures/nuage.py
+++ cfme/fixtures/nuage.py
@@ -1,3 +1,5 @@
+from argparse import Namespace
+
 import pytest
 from wrapanapi.utils.random import random_name
 
@@ -6,44 +8,62 @@ from cfme.utils.wait import wait_for
 
 
 def create_basic_sandbox(nuage):
-    box = {}
+    box = Namespace()
 
     # Create empty enterprise aka 'sandbox'.
-    enterprise = box['enterprise'] = nuage.create_enterprise()
-    logger.info('Created sandbox enterprise %s (%s)', enterprise.name, enterprise.id)
+    box.enterprise = get_object_dictionary(nuage.create_enterprise())
+    logger.info('Created sandbox enterprise %s (%s)',
+                box.enterprise['name'], box.enterprise['id'])
 
     # Fill the sandbox with some entities.
     # Method `create_child` returns a tuple (object, connection) and we only need object.
-    box['template'] = enterprise.create_child(
-        nuage.vspk.NUDomainTemplate(name=random_name()))[0]
-    box['domain'] = enterprise.create_child(
-        nuage.vspk.NUDomain(name=random_name(), template_id=box['template'].id))[0]
-    box['zone'] = box['domain'].create_child(
-        nuage.vspk.NUZone(name=random_name()))[0]
-    box['subnet'] = box['zone'].create_child(
+    box.template = get_object_dictionary(box.enterprise['obj'].create_child(
+        nuage.vspk.NUDomainTemplate(name=random_name()))[0])
+    box.domain = get_object_dictionary(box.enterprise['obj'].create_child(
+        nuage.vspk.NUDomain(name=random_name(), template_id=box.template['obj'].id))[0])
+    box.domain.update(num_subnets=1, num_security_groups=1)
+    box.zone = get_object_dictionary(box.domain['obj'].create_child(
+        nuage.vspk.NUZone(name=random_name()))[0])
+    box.subnet = get_object_dictionary(box.zone['obj'].create_child(
         nuage.vspk.NUSubnet(
             name=random_name(),
             address='192.168.0.0',
             netmask='255.255.255.0',
-            gateway='192.168.0.1'))[0]
-    box['cont_vport'] = box['subnet'].create_child(
-        nuage.vspk.NUVPort(name=random_name(), type='CONTAINER'))[0]
-    box['vm_vport'] = box['subnet'].create_child(
-        nuage.vspk.NUVPort(name=random_name(), type='VM'))[0]
-    box['l2_template'] = enterprise.create_child(
-        nuage.vspk.NUL2DomainTemplate(name=random_name()))[0]
-    box['l2_domain'] = enterprise.create_child(
-        nuage.vspk.NUL2Domain(name=random_name(), template_id=box['l2_template'].id))[0]
-    box['l2_cont_vport'] = box['l2_domain'].create_child(
-        nuage.vspk.NUVPort(name=random_name(), type='CONTAINER'))[0]
-    box['l2_vm_vport'] = box['l2_domain'].create_child(
-        nuage.vspk.NUVPort(name=random_name(), type='VM'))[0]
-    box['group'] = box['domain'].create_child(
-        nuage.vspk.NUPolicyGroup(name=random_name()))[0]
-    box['l2_group'] = box['l2_domain'].create_child(
-        nuage.vspk.NUPolicyGroup(name=random_name()))[0]
+            gateway='192.168.0.1'))[0])
+    box.subnet.update(num_ports=2, num_security_groups=0)
+    box.cont_vport = get_object_dictionary(box.subnet['obj'].create_child(
+        nuage.vspk.NUVPort(name=random_name(), type='CONTAINER'))[0])
+    box.cont_vport.update(num_subnets=1)
+    box.vm_vport = get_object_dictionary(box.subnet['obj'].create_child(
+        nuage.vspk.NUVPort(name=random_name(), type='VM'))[0])
+    box.vm_vport.update(num_subnets=1)
+    box.l2_template = get_object_dictionary(box.enterprise['obj'].create_child(
+        nuage.vspk.NUL2DomainTemplate(name=random_name()))[0])
+    box.l2_domain = get_object_dictionary(box.enterprise['obj'].create_child(
+        nuage.vspk.NUL2Domain(name=random_name(), template_id=box.l2_template['obj'].id))[0])
+    box.l2_domain.update(num_ports=2, num_security_groups=1)
+    box.l2_cont_vport = get_object_dictionary(box.l2_domain['obj'].create_child(
+        nuage.vspk.NUVPort(name=random_name(), type='CONTAINER'))[0])
+    box.l2_cont_vport.update(num_subnets=1)
+    box.l2_vm_vport = get_object_dictionary(box.l2_domain['obj'].create_child(
+        nuage.vspk.NUVPort(name=random_name(), type='VM'))[0])
+    box.l2_vm_vport.update(num_subnets=1)
+    box.group = get_object_dictionary(box.domain['obj'].create_child(
+        nuage.vspk.NUPolicyGroup(name=random_name()))[0])
+    box.l2_group = get_object_dictionary(box.l2_domain['obj'].create_child(
+        nuage.vspk.NUPolicyGroup(name=random_name()))[0])
 
+    box.counts = {
+        'num_subnets': 2,
+        'num_network_routers': 1,
+        'num_security_groups': 2,
+        'num_network_ports': 4
+    }
     return box
+
+
+def get_object_dictionary(obj):
+    return dict(obj=obj, name=obj.name, id=obj.id)
 
 
 @pytest.fixture
@@ -55,7 +75,7 @@ def with_nuage_sandbox(networks_provider):
     yield sandbox
 
     # Destroy the sandbox.
-    enterprise = sandbox['enterprise']
+    enterprise = sandbox.enterprise['obj']
     nuage.delete_enterprise(enterprise)
     logger.info('Destroyed sandbox enterprise %s (%s)', enterprise.name, enterprise.id)
 
@@ -64,8 +84,9 @@ def with_nuage_sandbox(networks_provider):
 def with_nuage_sandbox_modscope(appliance, setup_provider_modscope, provider):
     nuage = provider.mgmt
     sandbox = create_basic_sandbox(nuage)
-    enterprise = sandbox['enterprise']
-    logger.info('Performing a full refresh, so sandbox %s appears in the database', enterprise.name)
+    enterprise = sandbox.enterprise
+    logger.info('Performing a full refresh, so sandbox %s appears in the database',
+                enterprise['name'])
     provider.refresh_provider_relationships()
     wait_for(provider.is_refreshed, func_kwargs=dict(refresh_delta=5), timeout=600, delay=10)
 
@@ -73,12 +94,13 @@ def with_nuage_sandbox_modscope(appliance, setup_provider_modscope, provider):
     tenants_table = appliance.db.client['cloud_tenants']
     tenant = (appliance.db.client.session
               .query(tenants_table.name, tenants_table.ems_ref)
-              .filter(tenants_table.ems_ref == enterprise.id).first())
-    assert tenant is not None, 'Nuage sandbox tenant inventory missing: {}'.format(enterprise.name)
+              .filter(tenants_table.ems_ref == enterprise['id']).first())
+    assert tenant is not None, 'Nuage sandbox tenant inventory missing: {}'.format(
+        enterprise['name'])
 
     # Let integration test do whatever it needs to do.
     yield sandbox
 
     # Destroy the sandbox.
-    nuage.delete_enterprise(enterprise)
-    logger.info('Destroyed sandbox enterprise %s (%s)', enterprise.name, enterprise.id)
+    nuage.delete_enterprise(enterprise['obj'])
+    logger.info('Destroyed sandbox enterprise %s (%s)', enterprise['name'], enterprise['id'])
