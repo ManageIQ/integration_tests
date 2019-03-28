5fb1aee353f5ed39a1b7f2aa6c3dadc3b23e3809
john-dupuy <jdupuy@redhat.com>
2019-03-28 12:28:12 -0400

    Include test cases to assert removal of RSS and Timelines pages

---
+++ cfme/tests/intelligence/test_timelines_rss_removed.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import pytest
+from selenium.common.exceptions import NoSuchElementException
+
+from cfme.utils.appliance.implementations.ui import navigate_to
+
+
+@pytest.mark.tier(2)
+def test_timelines_removed(appliance):
+    """
+    Test that Cloud Intel->Timelines have been removed in upstream and 5.11 builds.
+    Designed to pass for CFME 5.10.
+
+    Bugzillas:
+        * 1672933
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/12h
+        casecomponent: WebUI
+        caseimportance: medium
+    """
+    if appliance.is_downstream and appliance.version < "5.11":
+        navigate_to(appliance.server, "CloudIntelTimelines")
+    else:
+        with pytest.raises(NoSuchElementException):
+            navigate_to(appliance.server, "CloudIntelTimelines")
+
+
+@pytest.mark.tier(2)
+def test_rss_removed(appliance):
+    """
+    Test that Cloud Intel->RSS has been removed in upstream and 5.11 builds.
+    Designed to pass for CFME 5.10.
+
+    Bugzillas:
+        * 1672933
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/12h
+        casecomponent: WebUI
+        caseimportance: medium
+    """
+    if appliance.is_downstream and appliance.version < "5.11":
+        navigate_to(appliance.server, "RSS")
+    else:
+        with pytest.raises(NoSuchElementException):
+            navigate_to(appliance.server, "RSS")
