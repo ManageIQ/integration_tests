45fe711a0367ab8ffb3711100a6bad6ca72ebaf1
Jaroslav Henner <jhenner@redhat.com>
2019-04-01 14:58:48 +0200

    Dedup provider_app_crud

--- cfme/tests/cli/test_appliance_console_db_restore.py
+++ cfme/tests/cli/test_appliance_console_db_restore.py
@@ -5,6 +5,7 @@ import pytest
 from wait_for import wait_for
 
 from cfme.cloud.provider.ec2 import EC2Provider
+from cfme.fixtures.cli import provider_app_crud
 from cfme.fixtures.cli import replicated_appliances_with_providers
 from cfme.fixtures.cli import waiting_for_ha_monitor_started
 from cfme.infrastructure.provider.virtualcenter import VMwareProvider
@@ -20,16 +21,6 @@ from cfme.utils.ssh import SSHClient
 TimedCommand = namedtuple('TimedCommand', ['command', 'timeout'])
 
 evm_log = '/var/www/miq/vmdb/log/evm.log'
-
-
-def provider_app_crud(provider_class, appliance):
-    try:
-        prov = list_providers_by_class(provider_class)[0]
-        logger.info('using provider {}'.format(prov.name))
-        prov.appliance = appliance
-        return prov
-    except IndexError:
-        pytest.skip("No {} providers available (required)".format(provider_class.type))
 
 
 def provision_vm(request, provider):
