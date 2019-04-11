7d7538bce8dd19961c46e59f582271596093613c
Satyajit Bulage <sbulage@redhat.com>
2019-04-11 20:31:00 +0530

    Fixed Embedded Ansible CRUD test file by re-ordering sub-tests

--- cfme/tests/ansible/test_embedded_ansible_crud.py
+++ cfme/tests/ansible/test_embedded_ansible_crud.py
@@ -41,39 +41,6 @@ def test_embedded_ansible_enable(embedded_appliance):
         container=embedded_appliance.ansible_pod_name)
 
 
-@pytest.mark.tier(3)
-def test_embedded_ansible_disable(embedded_appliance):
-    """Tests whether the embedded ansible role and all workers have stopped correctly
-
-    Polarion:
-        assignee: sbulage
-        casecomponent: Ansible
-        caseimportance: critical
-        initialEstimate: 1/6h
-        tags: ansible_embed
-    """
-    assert wait_for(lambda: embedded_appliance.rabbitmq_server.running, num_sec=30)
-    assert wait_for(lambda: embedded_appliance.nginx.running, num_sec=30)
-    embedded_appliance.disable_embedded_ansible_role()
-
-    if not embedded_appliance.is_pod:
-        assert wait_for(lambda: embedded_appliance.supervisord.running,
-                        fail_cond=True,
-                        num_sec=180)
-        assert wait_for(lambda: embedded_appliance.rabbitmq_server.running,
-                        fail_cond=True,
-                        num_sec=60)
-        assert wait_for(lambda: embedded_appliance.nginx.running,
-                        fail_cond=True,
-                        num_sec=30)
-    else:
-        @wait_for_decorator(num_sec=300)
-        def is_ansible_pod_stopped():
-            # todo: implement appropriate methods in appliance
-            return embedded_appliance.ssh_client.run_command(
-                'oc get pods|grep ansible', ensure_host=True).failed
-
-
 @pytest.mark.tier(1)
 def test_embedded_ansible_event_catcher_process(embedded_appliance):
     """
@@ -133,3 +100,36 @@ def test_embedded_ansible_logs(embedded_appliance):
     # Retrieving setup log file from list and asserting with length
     # Setup log file contains date/time string in it.
     assert "setup" in diff[0]
+
+
+@pytest.mark.tier(3)
+def test_embedded_ansible_disable(embedded_appliance):
+    """Tests whether the embedded ansible role and all workers have stopped correctly
+
+    Polarion:
+        assignee: sbulage
+        casecomponent: Ansible
+        caseimportance: critical
+        initialEstimate: 1/6h
+        tags: ansible_embed
+    """
+    assert wait_for(lambda: embedded_appliance.rabbitmq_server.running, num_sec=30)
+    assert wait_for(lambda: embedded_appliance.nginx.running, num_sec=30)
+    embedded_appliance.disable_embedded_ansible_role()
+
+    if not embedded_appliance.is_pod:
+        assert wait_for(lambda: embedded_appliance.supervisord.running,
+                        fail_cond=False,
+                        num_sec=180)
+        assert wait_for(lambda: embedded_appliance.rabbitmq_server.running,
+                        fail_cond=False,
+                        num_sec=80)
+        assert wait_for(lambda: embedded_appliance.nginx.running,
+                        fail_cond=False,
+                        num_sec=30)
+    else:
+        @wait_for_decorator(num_sec=300)
+        def is_ansible_pod_stopped():
+            # todo: implement appropriate methods in appliance
+            return embedded_appliance.ssh_client.run_command(
+                'oc get pods|grep ansible', ensure_host=True).failed
