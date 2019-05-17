0d2c28a6f9be942a9922f92532b6c274f5a61713
Nikhil Dhandre <ndhandre@redhat.com>
2019-05-17 16:47:58 +0530

    test for dynamic dialog with custom button
    Signed-off-by: Nikhil Dhandre <ndhandre@redhat.com>

--- cfme/tests/automate/custom_button/test_service_vm_custom_button.py
+++ cfme/tests/automate/custom_button/test_service_vm_custom_button.py
@@ -2,6 +2,7 @@ import fauxfactory
 import pytest
 from widgetastic_patternfly import Dropdown
 
+from cfme import test_requirements
 from cfme.infrastructure.provider.virtualcenter import VMwareProvider
 from cfme.markers.env_markers.provider import ONE
 from cfme.utils.appliance import ViaSSUI
@@ -12,6 +13,7 @@ from cfme.utils.appliance.implementations.ui import navigate_to as ui_nav
 
 pytestmark = [
     pytest.mark.tier(2),
+    test_requirements.custom_button,
     pytest.mark.provider([VMwareProvider], selector=ONE, scope="module"),
     pytest.mark.usefixtures("setup_provider_modscope"),
 ]
@@ -79,3 +81,46 @@ def test_custom_button_display_service_vm(request, appliance, service_vm, button
             custom_button_group = Dropdown(view, button_group.text)
             assert custom_button_group.is_displayed
             assert custom_button_group.has_item(button.text)
+
+
+@test_requirements.customer_stories
+@pytest.mark.manual
+@pytest.mark.tier(1)
+def test_custom_button_with_dynamic_dialog_vm():
+    """ Test custom button combination with dynamic dialog for VM entity.
+
+    Polarion:
+        assignee: ndhandre
+        initialEstimate: 1/2h
+        caseimportance: high
+        caseposneg: positive
+        testtype: functional
+        startsin: 5.9
+        casecomponent: CustomButton
+        testSteps:
+            1. Create new domain and copy 'Request' class from ManageIQ domain
+            2. Create class method under new domain with following ruby method
+            ```ruby
+              @vm = $evm.root['vm']
+              dialog_hash = {}
+              dialog_hash[@vm.id] = @vm.name
+              $evm.object['default_value'] = dialog_hash.first[0]
+              $evm.object['values'] = dialog_hash
+            ```
+            3. Create Instance Pointing class method
+            4. Create Dropdown type Service Dialog (dynamic) with entry point as above method
+            5. Create Custom button with Service Dialog on VM/Instance Object
+            6. Execute button on VM with OPS UI and SSUI.
+        expectedResults:
+            1.
+            2.
+            3.
+            4. Check service dialog created or not
+            5.
+            6. Check dialog should take vm/instance name automatically as per method.
+               Check automation log for button execution.
+
+    Bugzilla:
+        1687061
+    """
+    pass
