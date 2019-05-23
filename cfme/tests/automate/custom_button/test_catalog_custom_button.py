d4b51171176cd6322c763239cc8a12c925961d3d
NIKHIL DHANDRE <ndhandre@redhat.com>
2019-05-23 22:09:06 +0530

    [1LP][RFR] Catalog level unassigned btn test (#8829)
    * global fixture for generic service order
    
    Signed-off-by: Nikhil Dhandre <ndhandre@redhat.com>
    
    * catalog level unassinged btn test
    
    Signed-off-by: Nikhil Dhandre <ndhandre@redhat.com>

--- cfme/tests/automate/custom_button/test_catalog_custom_button.py
+++ cfme/tests/automate/custom_button/test_catalog_custom_button.py
@@ -1,8 +1,14 @@
 import pytest
 from fauxfactory import gen_numeric_string
+from widgetastic_patternfly import Button
 
 from cfme import test_requirements
 from cfme.services.catalogs.catalog_items import DetailsCatalogItemView
+from cfme.utils.appliance import ViaSSUI
+from cfme.utils.appliance import ViaUI
+from cfme.utils.appliance.implementations.ssui import navigate_to as ssui_nav
+from cfme.utils.appliance.implementations.ui import navigate_to as ui_nav
+
 
 pytestmark = [pytest.mark.tier(2), test_requirements.custom_button]
 
@@ -76,3 +82,42 @@ def test_custom_button_on_catalog_item_crud(generic_catalog_item):
     generic_catalog_item.delete_button(btn)
     # TODO(BZ-1687289): add deletion flash assertion as BZ fix.
     assert not generic_catalog_item.button_exists(btn)
+
+
+def test_custom_button_unassigned_behavior_catalog_level(appliance, generic_service):
+    """ Test unassigned custom button behavior catalog level
+
+    Note: At catalog level unassigned button (not part of any group) should displayed
+    for both OPS UI and SSUI.
+
+    Polarion:
+        assignee: ndhandre
+        initialEstimate: 1/6h
+        caseimportance: medium
+        caseposneg: positive
+        testtype: functional
+        startsin: 5.9
+        casecomponent: CustomButton
+        testSteps:
+            1. Create custom button directly on catalog item.
+            2. Check service details page for both OPS UI and SSUI; button should display.
+    Bugzilla:
+        1653195
+    """
+    service, catalog_item = generic_service
+
+    btn_data = {
+        "text": "button_{}".format(gen_numeric_string(3)),
+        "hover": "hover_{}".format(gen_numeric_string(3)),
+        "image": "fa-user",
+    }
+
+    btn = catalog_item.add_button(**btn_data)
+    assert catalog_item.button_exists(btn)
+
+    for context in [ViaUI, ViaSSUI]:
+        navigate_to = ssui_nav if context is ViaSSUI else ui_nav
+        with appliance.context.use(context):
+            view = navigate_to(service, "Details")
+            button = Button(view, btn)
+            assert button.is_displayed
