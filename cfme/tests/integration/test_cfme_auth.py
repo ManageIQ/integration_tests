31c1ca577745f0aef45cd93f11a584a6343fb508
Antonin Pagac <apagac@redhat.com>
2019-06-04 13:12:45 +0200

    Run auth tests

--- cfme/tests/integration/test_cfme_auth.py
+++ cfme/tests/integration/test_cfme_auth.py
@@ -28,7 +28,8 @@ pytestmark = [
                auth_mode in ['external', 'ldaps'] and
                auth_data.auth_providers[prov_key].type == 'openldaps')),
         BZ(1593171)]),  # 510z groups page doesn't load
-    pytest.mark.usefixtures('prov_key', 'auth_mode', 'auth_provider', 'configure_auth', 'auth_user')
+    pytest.mark.usefixtures('prov_key', 'auth_mode', 'auth_provider', 'configure_auth', 'auth_user'),
+    pytest.mark.ignore_stream("5.11")
 ]
 
 # map auth provider types, auth_modes, and user_types for test matrix
