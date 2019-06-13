c27378f97c16d8654d0734d054a39db407d9406c
Satyajit Bulage <sbulage@redhat.com>
2019-06-13 18:13:40 +0530

    [RFR] Added VDDK 6.7 checks and updated getter method (#8917)
    * Added VDDK 6.7 and checks for getter testing vddk
    
    * Added host credentials update via UI workaround for BZ:1718209
    
    * Removed VDDK 5.5 version as longer supported.

--- cfme/tests/infrastructure/test_ssa_vddk.py
+++ cfme/tests/infrastructure/test_ssa_vddk.py
@@ -3,12 +3,14 @@ import pytest
 from wrapanapi import VmState
 
 from cfme import test_requirements
+from cfme.infrastructure import host as host_ui
 from cfme.infrastructure.provider.virtualcenter import VMwareProvider
 from cfme.markers.env_markers.provider import ONE_PER_VERSION
 from cfme.utils import conf
 from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.utils.generators import random_vm_name
 from cfme.utils.log import logger
+from cfme.utils.update import update
 
 pytestmark = [
     pytest.mark.tier(3),
@@ -19,9 +21,9 @@ pytestmark = [
 ]
 
 vddk_versions = [
-    ('v5_5'),
     ('v6_0'),
-    ('v6_5')
+    ('v6_5'),
+    ('v6_7')
 ]
 
 
@@ -47,18 +49,29 @@ def ssa_analysis_profile(appliance):
 @pytest.fixture(params=vddk_versions, ids=([item for item in vddk_versions]), scope='function')
 def configure_vddk(request, appliance, provider, vm):
     vddk_version = request.param
-    vddk_url = conf.cfme_data.get("basic_info").get("vddk_url").get(vddk_version)
+    vddk_url = conf.cfme_data.get("basic_info", {}).get("vddk_url", {}).get(vddk_version, None)
+    if vddk_url is None:
+        pytest.skip('Could not locate vddk url in cfme_data')
+    else:
+        appliance.install_vddk(vddk_url=vddk_url)
     view = navigate_to(vm, 'Details')
     host_name = view.entities.summary("Relationships").get_text_of("Host")
     host, = [host for host in provider.hosts.all() if host.name == host_name]
     host_data, = [data for data in provider.data['hosts'] if data['name'] == host.name]
-    host.update_credentials_rest(credentials=host_data['credentials'])
-    appliance.install_vddk(vddk_url=vddk_url)
+    # TODO: Remove Host UI validation BZ:1718209
+    # host.update_credentials_rest(credentials=host_data['credentials'])
+    host_collection = appliance.collections.hosts
+    host_obj = host_collection.instantiate(name=host.name, provider=provider)
+    with update(host_obj, validate_credentials=True):
+        host_obj.credentials = {'default': host_ui.Host.Credential.from_config(
+                                host_data['credentials']['default'])}
 
     @request.addfinalizer
     def _finalize():
         appliance.uninstall_vddk()
-        host.remove_credentials_rest()
+        with update(host_obj):
+            host_obj.credentials = {'default': host_ui.Host.Credential(
+                principal="", secret="", verify_secret="")}
 
 
 @pytest.fixture(scope="function")
