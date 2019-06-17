b3fb4b3ad411bac801c1ccff91751014897f6f45
Ganesh Hubale <ghubale@redhat.com>
2019-06-17 15:09:48 +0530

    New Test: - Testing invalid value for assertion field in class schema

--- cfme/tests/automate/test_simulation.py
+++ cfme/tests/automate/test_simulation.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import fauxfactory
 import pytest
 
 from cfme import test_requirements
+from cfme.automate.simulation import simulate
 from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.utils.blockers import BZ
 
@@ -34,3 +36,51 @@ def test_object_attributes(appliance):
             view.target_type.select_by_visible_text(object_type.text)
             # Checking whether dependent objects(object attribute selection) are loaded or not
             assert view.target_object.all_options > 0
+
+
+@pytest.fixture(scope='function')
+def copy_class(domain):
+    # Take the 'Request' class and copy it to custom domain.
+    domain.parent.instantiate(name="ManageIQ").namespaces.instantiate(
+        name="System"
+    ).classes.instantiate(name="Request").copy_to(domain.name)
+    klass = domain.namespaces.instantiate(name="System").classes.instantiate(name="Request")
+    return klass
+
+
+@pytest.mark.tier(1)
+def test_assert_failed_substitution(copy_class):
+    """
+    Polarion:
+        assignee: ghubale
+        casecomponent: Automate
+        caseimportance: medium
+        initialEstimate: 1/4h
+        caseposneg: negative
+        tags: automate
+
+    Bugzilla:
+        1335669
+    """
+    # Adding instance and invalid value for assertion field - 'guard'
+    instance = copy_class.instances.create(
+        name=fauxfactory.gen_alphanumeric(),
+        display_name=fauxfactory.gen_alphanumeric(),
+        description=fauxfactory.gen_alphanumeric(),
+        fields={'guard': {'value': "${/#this_value_does_not_exist}"}}
+    )
+
+    # Executing automate instance using simulation
+    with pytest.raises(AssertionError,
+                       match="Automation Error: Attribute this_value_does_not_exist not found"):
+        simulate(
+            appliance=copy_class.appliance,
+            attributes_values={
+                "namespace": copy_class.namespace.name,
+                "class": copy_class.name,
+                "instance": instance.name,
+            },
+            message="create",
+            request="Call_Instance",
+            execute_methods=True,
+        )
