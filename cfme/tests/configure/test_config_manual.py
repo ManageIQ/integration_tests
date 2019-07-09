c5d2830341ef9953b7b52d7c2a7688e5fd0e45e5
Ievgen Zapolskyi <izapolsk@redhat.com>
2019-07-09 18:30:03 +0300

    new manual subscription tests

--- cfme/tests/configure/test_config_manual.py
+++ cfme/tests/configure/test_config_manual.py
@@ -100,3 +100,110 @@ def test_configuration_dropdown_roles_by_server():
         1455283
     """
     pass
+
+
+@pytest.mark.manual
+@pytest.mark.tier(3)
+def test_replication_subscription_crud():
+    """
+    Add/Edit/Remove replication subscription
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Configuration
+        caseimportance: medium
+        initialEstimate: 1/4h
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.tier(3)
+def test_add_duplicate_subscription():
+    """
+    Try adding duplicate record
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Configuration
+        caseimportance: low
+        initialEstimate: 1/6h
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.tier(3)
+def test_add_bad_subscription():
+    """
+    Try adding wrong subscriptions like
+      1. remote appliance does have remote replication type set
+      2. remote appliance isn't available and etc
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Configuration
+        caseimportance: low
+        initialEstimate: 1/4h
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.tier(3)
+def test_edit_bad_subscription():
+    """
+    Try changing subscriptions from good to bad or vise versa
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Configuration
+        caseimportance: low
+        initialEstimate: 1/4h
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.tier(3)
+def test_cancel_subscription():
+    """
+    Try canceling adding/changing/removing subscriptions
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Configuration
+        caseimportance: low
+        initialEstimate: 1/4h
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.tier(3)
+def test_change_subscription_type():
+    """
+    Try setting/removing global subscription
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Configuration
+        caseimportance: low
+        initialEstimate: 1/4h
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.tier(3)
+def test_subscription_disruption():
+    """
+    Test restoring subscription after temporary disruptions
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Configuration
+        caseimportance: low
+        initialEstimate: 1/4h
+    """
+    pass
