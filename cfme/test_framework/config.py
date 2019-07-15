baa1bbe13c1d2a62ac15640709e845ec1a0e2681
Mike Shriver <mshriver@redhat.com>
2019-07-15 18:14:49 -0400

    Refactor yaycl use for cfme.utils.conf imports
    Remove DeprecatedConfigWrapper

--- cfme/test_framework/config.py
+++ cfme/test_framework/config.py
@@ -2,39 +2,37 @@
 classes to manage the cfme test framework configuration
 """
 import os
-import warnings
 
 import attr
 import yaycl
 
 
+@attr.s
 class Configuration(object):
     """
     holds the current configuration
-    """
-    def __init__(self):
-        self.yaycl_config = None
 
-    def configure(self, config_dir, crypt_key_file=None):
+    Supports cfme.utils.conf module impersonation and lazy loading
+    """
+    yaycl_config = attr.ib(default=None)
+
+    def configure(self, config_dir, key_file=None):
         """
         do the defered initial loading of the configuration
 
-        :param config_dir: path to the folder with configuration files
-        :param crypt_key_file: optional name of a file holding the key for encrypted
-            configuration files
+        Args:
+            config_dir (str): path to the folder with configuration files
+            key_file (str): optional name of file holding the key for encrypted configuration files
 
         :raises: AssertionError if called more than once
 
         if the `utils.conf` api is removed, the loading can be transformed to eager loading
         """
-
-        assert self.yaycl_config is None
-        if crypt_key_file and os.path.exists(crypt_key_file):
-            self.yaycl_config = yaycl.Config(
-                config_dir=config_dir,
-                crypt_key_file=crypt_key_file)
-        else:
-            self.yaycl_config = yaycl.Config(config_dir=config_dir)
+        self.yaycl_config = yaycl.Config(
+            config_dir=config_dir,
+            crypt_key_file=key_file if key_file and os.path.exists(key_file) else None,
+        )
+        return self.yaycl_config
 
     def get_config(self, name):
         """returns a yaycl config object
@@ -45,46 +43,3 @@ class Configuration(object):
         if self.yaycl_config is None:
             raise RuntimeError('cfme configuration was not initialized')
         return getattr(self.yaycl_config, name)
-
-
-@attr.s
-class DeprecatedConfigWrapper(object):
-    """
-    a wrapper that provides the old :code:``utils.conf`` api
-    """
-    configuration = attr.ib()
-    _warn = attr.ib(default=False)
-
-    def __getattr__(self, key):
-        if self._warn:
-            warnings.warn(
-                'the configuration module {} will be deprecated'.format(key),
-                category=DeprecationWarning,
-                stacklevel=2,
-            )
-        return self.configuration.get_config(key)
-
-    @property
-    def runtime(self):
-        return self.configuration.runtime
-
-    def __getitem__(self, key):
-        if self._warn:
-            warnings.warn(
-                'the configuration module {} will be deprecated'.format(key),
-                category=DeprecationWarning,
-                stacklevel=2,
-            )
-        return self.configuration.get_config(key)
-
-    def __delitem__(self, key):
-        # used in bad logging
-        if self._warn:
-            warnings.warn('clearing configuration is bad', stacklevel=2)
-
-        del self.configuration.yaycl_config[key]
-
-
-# for the initial usage we keep a global object
-# later on we want to replace it
-global_configuration = Configuration()
--- cfme/utils/conf.py
+++ cfme/utils/conf.py
@@ -1,12 +1,13 @@
 import sys
 
-from cfme.test_framework.config import DeprecatedConfigWrapper
-from cfme.test_framework.config import global_configuration
+from cfme.test_framework.config import Configuration
 from cfme.utils import path
 
-global_configuration.configure(
-    config_dir=path.conf_path.strpath,
-    crypt_key_file=path.project_path.join('.yaml_key').strpath,
-)
+# in case this needs to be imported globally for Configuration.get_config() use
+global_configuration = Configuration()
 
-sys.modules[__name__] = DeprecatedConfigWrapper(global_configuration)
+# Module impersonation to support from cfme.utils.conf import <name>
+sys.modules[__name__] = global_configuration.configure(
+    config_dir=path.conf_path.strpath,
+    key_file=path.project_path.join('.yaml_key').strpath,
+)
