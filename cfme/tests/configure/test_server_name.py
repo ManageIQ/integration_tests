167ebbdb618279755d2663ae9ec63aa288580c38
Mike Shriver <mshriver@redhat.com>
2019-07-16 13:35:25 -0400

    Add BZ marker and update test_server_name patterns for filling

--- cfme/tests/configure/test_server_name.py
+++ cfme/tests/configure/test_server_name.py
@@ -9,7 +9,8 @@ from cfme.utils.appliance.implementations.ui import navigate_to
 pytestmark = [test_requirements.general_ui]
 
 
-@pytest.mark.tier(3)
+@pytest.mark.tier(1)
+@pytest.mark.meta(automates=[1635178])
 @pytest.mark.sauce
 def test_server_name(request, appliance):
     """Tests that changing the server name updates the about page
@@ -23,27 +24,29 @@ def test_server_name(request, appliance):
 
     view = navigate_to(appliance.server, 'Details')
     old_server_name = view.server.basic_information.appliance_name.read()
+    assert old_server_name != ''  # name should at least be set
 
     @request.addfinalizer
     def _ensure_name_reset():
-        appliance.server.settings.update_basic_information({'appliance_name': old_server_name})
+        appliance.rename(old_server_name)
 
-    new_server_name = "{}-TEST".format(old_server_name)
-    appliance.server.settings.update_basic_information({'appliance_name': new_server_name})
-    flash_message = (
-        'Configuration settings saved for {} Server "{} [{}]" in Zone "{}"'.format(
-            appliance.product_name,
-            appliance.server.name,
-            appliance.server.sid,
-            appliance.server.zone.name))
+    new_server_name = "RENAME-TEST"
+    assert view.server.basic_information.appliance_name.fill('updated-evm-name')
+    assert view.server.save.is_enabled
+    view.server.save.click()  # no boolean return
+    assert appliance.server.name == new_server_name
 
-    view.flash.assert_message(flash_message)
+    view.flash.assert_success_message(
+        'Configuration settings saved for {} Server "{} [{}]" in Zone "{}"'
+        .format(appliance.product_name,
+                appliance.server.name,
+                appliance.server.sid,
+                appliance.server.zone.name)
+    )
 
     # CFME updates about box only after any navigation BZ(1408681) - closed wontfix
     navigate_to(appliance.server, 'Dashboard')
 
     # opens and closes about modal
-    current_server_name = about.get_detail(about.SERVER, server=appliance.server)
-
-    assert new_server_name == current_server_name, (
+    assert new_server_name == about.get_detail(about.SERVER, server=appliance.server), (
         "Server name in About section does not match the new name")
