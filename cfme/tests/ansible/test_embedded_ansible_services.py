6d23180fe70e3a1bce7b53d69bf97c5e1e15622d
Satyajit Bulage <sbulage@redhat.com>
2019-07-25 20:17:35 +0530

    Added Ansible test for "play_against" Azure provider.

--- cfme/tests/ansible/test_embedded_ansible_services.py
+++ cfme/tests/ansible/test_embedded_ansible_services.py
@@ -6,6 +6,7 @@ import pytest
 from widgetastic_patternfly import BootstrapSelect
 
 from cfme import test_requirements
+from cfme.cloud.provider.azure import AzureProvider
 from cfme.cloud.provider.ec2 import EC2Provider
 from cfme.control.explorer.policies import VMControlPolicy
 from cfme.infrastructure.provider.rhevm import RHEVMProvider
@@ -41,6 +42,7 @@ CREDENTIALS = [
     ("Amazon", "", "list_ec2_instances.yml"),
     ("VMware", "vcenter_host", "gather_all_vms_from_vmware.yml"),
     ("Red Hat Virtualization", "host", "connect_to_rhv.yml"),
+    ("Azure", "", "get_resourcegroup_facts_azure.yml"),
 ]
 
 
@@ -633,7 +635,7 @@ def test_ansible_group_id_in_payload(
     "credential", CREDENTIALS, ids=[cred[0] for cred in CREDENTIALS]
 )
 @pytest.mark.provider(
-    [RHEVMProvider, EC2Provider, VMwareProvider], selector=ONE_PER_TYPE
+    [RHEVMProvider, EC2Provider, VMwareProvider, AzureProvider], selector=ONE_PER_TYPE
 )
 @pytest.mark.uncollectif(
     lambda credential, provider: not (
@@ -642,6 +644,7 @@ def test_ansible_group_id_in_payload(
         or (
             credential[0] == "Red Hat Virtualization" and provider.one_of(RHEVMProvider)
         )
+        or (credential[0] == "Azure" and provider.one_of(AzureProvider))
     )
 )
 def test_embed_tower_exec_play_against(
