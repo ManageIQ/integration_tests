91710f8682b5c4b2b6959b719d954dd10a72c4b0
PJ Richardson <prichard@redhat.com>
2019-07-30 10:22:39 -0400

    Added global manual marker and fixed formatting per PR comments

--- cfme/tests/infrastructure/test_infra_manual.py
+++ cfme/tests/infrastructure/test_infra_manual.py
@@ -4,8 +4,9 @@ import pytest
 
 from cfme import test_requirements
 
+pytestmark = [pytest.mark.manual]
 
-@pytest.mark.manual
+
 @test_requirements.discovery
 @pytest.mark.tier(1)
 def test_infrastructure_providers_rhevm_edit_provider_no_default_port():
@@ -29,7 +30,6 @@ def test_infrastructure_providers_rhevm_edit_provider_no_default_port():
     pass
 
 
-@pytest.mark.manual
 @pytest.mark.tier(1)
 @test_requirements.discovery
 def test_add_infra_provider_screen():
@@ -59,7 +59,6 @@ def test_add_infra_provider_screen():
 
 
 @test_requirements.general_ui
-@pytest.mark.manual
 @pytest.mark.tier(1)
 @pytest.mark.meta(coverage=[1532404])
 def test_provider_summary_topology():
@@ -84,7 +83,6 @@ def test_provider_summary_topology():
     pass
 
 
-@pytest.mark.manual
 @test_requirements.infra_hosts
 def test_host_setting_default_filter():
     """
@@ -98,22 +96,20 @@ def test_host_setting_default_filter():
         testSteps:
             1. Navigate to the Compute > Infrastructure > Hosts view.
             2. Select a filter from the accordion that does not have "(Default)" appended to the
-            name. Note items
-            displayed.
+                name. Note items displayed.
             3. Click "Select Default" button.
             4. Logout and log back in to CloudForms and navigate back to the Hosts view.
         expectedResults:
             1. Hosts view is displayed with hosts filtered via the Default filter (denoted by
             "(Default)" next to the filter name in the dropdown).
             2. Items displayed in the hosts panel will be filtered based upon the filter
-            selected.
+                  selected.
             3. "(Default)" will be displayed beside the filter name in the dropdown.
             4. Hosts view will be displayed filtered via the new Default filter.
     """
     pass
 
 
-@pytest.mark.manual
 @test_requirements.infra_hosts
 def test_host_comparison_properties():
     """
@@ -131,11 +127,11 @@ def test_host_comparison_properties():
             4. Click on "Host Properties(X)" in the Compare Host view.
             5. Click on "Host Properties(X)" again, in the Compare Host view.
             6. Click to expand the "Properties" comparison section, select "Hardware", and click
-             "Apply".
+                  "Apply".
             7. Click on "Hardware(X)" in the Compare Host view.
             8. Click on "Hardware(X)" again, in the Compare Host view.
             9. Click to expand the "Properties" comparison section, select "Network Adapters", and
-            click "Apply".
+                  click "Apply".
             10. Click on "Network Adapters(X)" in the Compare Host view.
             11. Click on the "#X" items that apply to the network adapters.
             12. Click on the "#X" items again that apply to the network adapters.
@@ -151,32 +147,30 @@ def test_host_comparison_properties():
             - "% Matched" text or graphs are displayed
             - when 3 or more hosts are displayed, remove icons exist for all non-base hosts.
             4. The row should be expanded to display all of the properties compared. Items that do
-            not match the base host should be in blue. There should be X properties displayed.
-            Properties for non-base hosts should be in purple/dark blue.
+                  not match the base host should be in blue. There should be X properties
+                  displayed. Properties for non-base hosts should be in purple/dark blue.
             5. The Host properties should collapse to one row again.
             6. A hardware row should be added to the view for all hosts with % matching graphs
-            displayed for non-base hosts.
+                  displayed for non-base hosts.
             7. The row should be expanded and displayed with same requirements as in step 4.
             8. The hardware metrics should collapse to one row again.
             9. A network adapters row should be added to the view for all hosts with % matching
-            graphs displayed for non-base hosts.
+                  graphs displayed for non-base hosts.
             10. The row should be expanded and displayed with same requirements as in step 4.
             11. The "#X" individual network adapter rows should be expanded and displayed with
-            same requirements as in step 4.
+                   same requirements as in step 4.
             12. The "#X" individual network adapter rows should collapse to one row again.
             13. The Network Adapters rows should collapse to one row again.
     """
     pass
 
 
-@pytest.mark.manual
 @test_requirements.infra_hosts
 def test_host_comparison_security():
     """
     Verify host comparisons view functionality for security section.
     This is going to be a similar test as test_host_comparison_properties, but for the Security
     Section.
-
 
     Polarion:
         assignee: prichard
@@ -189,13 +183,14 @@ def test_host_comparison_security():
             3. Click "Compare Selected Items" under the "Configuration" dropdown.
             4. Click to select/check the "Security" checkbox in "Comparison Sections".
             5. Click "Apply"
-            *If the displayed rows do not have any items (item count is '(0)') the next steps do
-            not apply.
+                  *If the displayed rows do not have any items (item count is '(0)') the next
+                  steps do not apply.
             6. If the security rows/sections have sub-items, click on one of the security row items
-             (Users, Groups, ...).
+                  (Users, Groups, ...).
             7. Click on the same security item again.
             8. Repeat steps 6 and 7 for all security sub-items.
-            *Steps 6 and 7 apply recursively to any sub items of parent sections that may exist.
+                  *Steps 6 and 7 apply recursively to any sub items of parent sections that may
+                  exist.
         expectedResults:
             1. Hosts view is displayed with hosts filtered via the Default filter (denoted by
             "(Default)" next to the filter name in the dropdown).
@@ -208,22 +203,22 @@ def test_host_comparison_security():
             - when 3 or more hosts are displayed, remove icons exist for all non-base hosts.
             4. "Security" in comparison sections should turn blue.
             5. New rows should be displayed in the compare host/node view for all security items
-            (Users, Groups, Firewall Rules). The new rows should have the following:
-            - "% Matched" text or graphs are displayed
-            - sub-items that are not empty should have the ">" displayed and be able to be
+                  (Users, Groups, Firewall Rules). The new rows should have the following:
+                  - "% Matched" text or graphs are displayed
+                  - sub-items that are not empty should have the ">" displayed and be able to be
             expanded.
             6. The security row/section should be expanded, displaying all sub-items in the
-            section.
-            - Items that do not match the base host should be in blue.
-            - The number of properties displayed should match the number denoted on the parent row.
-            - Properties for non-base hosts should be in purple/dark blue.
+                  section.
+                  - Items that do not match the base host should be in blue.
+                  - The number of properties displayed should match the number denoted on the
+                       parent row.
+                 - Properties for non-base hosts should be in purple/dark blue.
             7. The section should collapse again to only display the parent row.
             8. Same expected results as for step 6 and 7.
     """
     pass
 
 
-@pytest.mark.manual
 @test_requirements.infra_hosts
 def test_host_comparison_configuration():
     """
@@ -239,20 +234,21 @@ def test_host_comparison_configuration():
         testSteps:
             1. Navigate to the Compute > Infrastructure > Hosts view.
             2. Select at least 2 hosts (three if possible) by checking the box in upper left of
-            host quadicons.
+                  host quadicons.
             3. Click "Compare Selected Items" under the "Configuration" dropdown.
             4. Click to select/check the "Configuration" checkbox in "Comparison Sections".
             5. Click "Apply"
-            *If the displayed rows do not have any items (item count is '(0)') the next steps do
-            not apply.
+                  *If the displayed rows do not have any items (item count is '(0)') the next
+                  steps do not apply.
             6. If the configuration rows/sections have sub-items, click on one of the configuration
-             row items (OS, Guest Applications, ...).
+                  row items (OS, Guest Applications, ...).
             7. Click on the same configuration item again.
             8. Repeat steps 6 and 7 for all configuration sub-items.
-            *Steps 6 and 7 apply recursively to any sub items of parent sections that may exist.
+                  *Steps 6 and 7 apply recursively to any sub items of parent sections that may
+                  exist.
         expectedResults:
             1. Hosts view is displayed with hosts filtered via the Default filter (denoted by
-            "(Default)" next to the filter name in the dropdown).
+                  "(Default)" next to the filter name in the dropdown).
             2. The selected hosts should be displayed with a blue border and checked checkbox.
             3. The "Compare Host / Node" view should be displayed.
             - icons are displayed for all and only selected hosts with hostname displayed
@@ -262,10 +258,10 @@ def test_host_comparison_configuration():
             - when 3 or more hosts are displayed, remove icons exist for all non-base hosts.
             4. "Configuration" in comparison sections should turn blue.
             5. New rows should be displayed in the compare host/node view for all configuration
-            items (OS, Guest Applications, ...). The new rows should have the following:
-            - "% Matched" text or graphs are displayed
-            - sub-items that are not empty should have the ">" displayed and be able to be
-            expanded.
+                  items (OS, Guest Applications, ...). The new rows should have the following:
+                  - "% Matched" text or graphs are displayed
+                  - sub-items that are not empty should have the ">" displayed and be able to be
+                       expanded.
             6. The configuration row/section should be expanded, displaying all sub-items in the
             section.
             - Items that do not match the base host should be in blue.
@@ -277,13 +273,12 @@ def test_host_comparison_configuration():
     pass
 
 
-@pytest.mark.manual
 @test_requirements.infra_hosts
 def test_host_comparison_my_company_tags():
     """
     Verify host comparisons view functionality for 'My Company Tags' section.
     This is going to be the same test as test_host_comparison_properties, but for the "My Company
-     Tags" section.
+    Tags" section.
 
     Polarion:
         assignee: prichard
@@ -293,17 +288,18 @@ def test_host_comparison_my_company_tags():
         testSteps:
             1. Navigate to the Compute > Infrastructure > Hosts view.
             2. Select at least 2 hosts (three if possible) by checking the box in upper left of
-            host quadicons.
+                  host quadicons.
             3. Click "Compare Selected Items" under the "Configuration" dropdown.
             4. Click to select/check the "My Company Tags" checkbox in "Comparison Sections".
             5. Click "Apply"
-            *If the displayed rows do not have any items (item count is '(0)') the next steps do
-            not apply.
+                  *If the displayed rows do not have any items (item count is '(0)') the next
+                  steps do not apply.
             6. If the "My Company Tags" rows/sections have sub-items, click on one of the "My
-            Company Tags" row items (Department, Customer, ...).
+                  Company Tags" row items (Department, Customer, ...).
             7. Click on the same "My Company Tags" item again.
             8. Repeat steps 6 and 7 for all "My Company Tags" sub-items.
-            *Steps 6 and 7 apply recursively to any sub items of parent sections that may exist.
+                  *Steps 6 and 7 apply recursively to any sub items of parent sections that may
+                  exist.
         expectedResults:
             1. Hosts view is displayed.
             2. The selected hosts should be displayed with a blue border and checked checkbox.
@@ -315,12 +311,12 @@ def test_host_comparison_my_company_tags():
             - when 3 or more hosts are displayed, remove icons exist for all non-base hosts.
             4. "My Company Tags" in comparison sections should turn blue.
             5. New rows should be displayed in the compare host/node view for all "My Company Tags"
-             items (Department, Customer, ...). The new rows should have the following:
+                  items (Department, Customer, ...). The new rows should have the following:
             - "% Matched" text or graphs are displayed
             - sub-items that are not empty should have the ">" displayed and be able to be
-            expanded.
+                 expanded.
             6. The "My Company Tags" row/section should be expanded, displaying all sub-items in
-            the section.
+                  the section.
             - Items that do not match the base host should be in blue.
             - The number of properties displayed should match the number denoted on the parent row.
             - Properties for non-base hosts should be in purple/dark blue.
