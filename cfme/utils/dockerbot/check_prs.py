74dcb5fc87b85cd8edfe79c5422b7aef9556eb06
Eugene Zapolsky <izapolsk@redhat.com>
2019-08-01 00:11:47 +0300

    [RFR] a few fixes for py3 Prt (#9139)
    * a few fixes for py3 Prt
    
    * reverting Pr 6576 which breaks PRT
    
    * fix verify_commit
    
    * Fix order of imports -juwatts

--- cfme/utils/dockerbot/check_prs.py
+++ cfme/utils/dockerbot/check_prs.py
@@ -4,13 +4,14 @@ import logging
 import traceback
 from datetime import datetime
 
+import fauxfactory
 import pika
 import requests
 from slumber.exceptions import HttpClientError
 
-from . import dockerbot
 from cfme.utils.appliance import Appliance
 from cfme.utils.conf import docker as docker_conf
+from cfme.utils.dockerbot import dockerbot
 from cfme.utils.log import setup_logger
 from cfme.utils.trackerbot import api
 
@@ -111,11 +112,7 @@ def create_run(db_pr, pr):
         stream = group['name']
         logger.info('  Adding task stream {}...'.format(stream))
         task_data = dict(output="",
-            tid="pr{pr}-r{runs}-{stream}-ts{time:%Y-%m-%d-%H:%M:%S}".format(
-                pr=db_pr['number'],
-                runs=len(db_pr.get('runs', [])),
-                stream=stream,
-                time=datetime.now()),
+            tid=fauxfactory.gen_alphanumeric(8),
             result="pending",
             stream=stream,
             datestamp=str(datetime.now()),
