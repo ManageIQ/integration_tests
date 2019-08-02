2f46fd73d25e044cffd8a671cc791db0b4b1e533
nachandr <nachandr@redhat.com>
2019-08-02 14:09:28 -0500

    Add tests for automating Tower workflow jobs

--- cfme/tests/services/test_ansible_workflow_servicecatalogs.py
+++ cfme/tests/services/test_ansible_workflow_servicecatalogs.py
@@ -5,6 +5,7 @@ from cfme import test_requirements
 from cfme.services.myservice import MyService
 from cfme.services.service_catalogs import ServiceCatalogs
 from cfme.utils import testgen
+from cfme.utils.blockers import BZ
 from cfme.utils.blockers import GH
 from cfme.utils.log import logger
 
@@ -12,7 +13,8 @@ from cfme.utils.log import logger
 pytestmark = [
     test_requirements.service,
     pytest.mark.tier(2),
-    pytest.mark.ignore_stream('upstream')]
+    pytest.mark.ignore_stream('upstream')
+]
 
 
 def pytest_generate_tests(metafunc):
@@ -33,20 +35,18 @@ def pytest_generate_tests(metafunc):
 
 
 @pytest.fixture(scope="module")
-def config_manager(config_manager_obj):
-    """ Fixture that provides a random config manager and sets it up"""
+def tower_manager(config_manager_obj):
+    """ Fixture that sets up Ansible Tower"""
     if config_manager_obj.type == "Ansible Tower":
         config_manager_obj.create(validate=True)
-    else:
-        config_manager_obj.create()
     yield config_manager_obj
     config_manager_obj.delete()
 
 
 @pytest.fixture(scope="function")
-def catalog_item(appliance, request, config_manager, dialog, catalog):
+def ansible_workflow_catitem(appliance, request, tower_manager, dialog, catalog):
     job_type = 'workflow'
-    config_manager_obj = config_manager
+    config_manager_obj = tower_manager
     provider_name = config_manager_obj.yaml_data.get('name')
     template = config_manager_obj.yaml_data['provisioning_data'][job_type]
     catalog_item = appliance.collections.catalog_items.create(
@@ -58,11 +58,12 @@ def catalog_item(appliance, request, config_manager, dialog, catalog):
         dialog=dialog,
         provider='{} Automation Manager'.format(provider_name),
         config_template=template)
-    request.addfinalizer(catalog_item.delete)
-    return catalog_item
+    yield catalog_item
+    catalog_item.delete_if_exists()
 
 
-def test_tower_workflow_item(appliance, config_manager, catalog_item, request):
+@pytest.mark.meta(automates=[BZ(1719051)])
+def test_tower_workflow_item(appliance, tower_manager, ansible_workflow_catitem, request):
     """Tests ordering of catalog items for Ansible Workflow templates
     Metadata:
         test_flag: provision
@@ -73,20 +74,23 @@ def test_tower_workflow_item(appliance, config_manager, catalog_item, request):
         casecomponent: Services
         caseimportance: high
     """
-    service_catalogs = ServiceCatalogs(appliance, catalog_item.catalog, catalog_item.name)
+    service_catalogs = ServiceCatalogs(appliance, ansible_workflow_catitem.catalog,
+        ansible_workflow_catitem.name)
     service_catalogs.order()
-    logger.info('Waiting for cfme provision request for service %s', catalog_item.name)
-    cells = {'Description': catalog_item.name}
+    logger.info('Waiting for cfme provision request for service %s', ansible_workflow_catitem.name)
+    cells = {'Description': ansible_workflow_catitem.name}
     order_request = appliance.collections.requests.instantiate(cells=cells, partial_check=True)
     order_request.wait_for_request(method='ui')
     msg = 'Request failed with the message {}'.format(order_request.row.last_message.text)
     assert order_request.is_succeeded(method='ui'), msg
-    appliance.user.my_settings.default_views.set_default_view('Configuration Management Providers',
-                                                              'List View')
+    appliance.user.my_settings.default_views.set_default_view(
+        'Configuration Management Providers',
+        'List View'
+    )
 
 
 @pytest.mark.meta(blockers=[GH('ManageIQ/integration_tests:8610')])
-def test_retire_ansible_service(appliance, catalog_item, request):
+def test_retire_ansible_workflow(appliance, ansible_workflow_catalog_item, request):
     """Tests retiring of catalog items for Ansible Workflow templates
     Metadata:
         test_flag: provision
@@ -97,13 +101,14 @@ def test_retire_ansible_service(appliance, catalog_item, request):
         caseimportance: medium
         initialEstimate: 1/4h
     """
-    service_catalogs = ServiceCatalogs(appliance, catalog_item.catalog, catalog_item.name)
+    service_catalogs = ServiceCatalogs(appliance, ansible_workflow_catitem.name.catalog,
+        ansible_workflow_catitem.name)
     service_catalogs.order()
-    logger.info('Waiting for cfme provision request for service %s', catalog_item.name)
-    cells = {'Description': catalog_item.name}
+    logger.info('Waiting for cfme provision request for service %s', ansible_workflow_catitem.name)
+    cells = {'Description': ansible_workflow_catitem.name}
     order_request = appliance.collections.requests.instantiate(cells=cells, partial_check=True)
     order_request.wait_for_request(method='ui')
     msg = "Request failed with the message {}".format(order_request.row.last_message.text)
     assert order_request.is_succeeded(method='ui'), msg
-    myservice = MyService(appliance, catalog_item.name)
+    myservice = MyService(appliance, ansible_workflow_catitem.name)
     myservice.retire()
