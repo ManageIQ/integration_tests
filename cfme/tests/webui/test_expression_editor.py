203e8159f7b305e61e7aa266befd842b0144b53f
Pavel Simovec <psimovec@redhat.com>
2019-08-07 17:57:27 +0200

    parametrized

--- cfme/tests/webui/test_expression_editor.py
+++ cfme/tests/webui/test_expression_editor.py
@@ -4,61 +4,33 @@ import pytest
 from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.utils.blockers import BZ
 
+filter_value = 'beautifulpotato'
+filter_option = "Cloud Volume : Name"
 
-class TestDeleteExpressions(object):
-    filter_value = 'beautifulpotato'
-    filt = "Cloud Volume : Name"
-    collection = "volumes"  # any collection with advanced search
-    destination = "All"
 
-    def _delete_operator(self, appliance, operator):
-        view = navigate_to(
-            getattr(
-                appliance.collections,
-                self.collection),
-            self.destination)
-        name = fauxfactory.gen_alphanumeric()
-        view.entities.search.save_filter(
-            f'fill_field({self.filt}, =, {self.filter_value});'
-            f'select_expression_text;'
-            f'click_{operator};'
-            f'{f"fill_field({self.filt}, =, {self.filter_value});" if operator != "not" else ""}'
-            f'select_expression_text;'
-            f'click_remove;',
-            name)
-        editor = view.search.advanced_search_form.search_exp_editor
-        assert editor.expression_text == f'{self.filt} = "{self.filter_value}"'
-        view.entities.search.delete_filter()
-        view.entities.search.close_advanced_search()
-
-    @pytest.mark.meta(automates=[BZ(1720216)])
-    @pytest.mark.ignore_stream("5.10")
-    def test_delete_not(self, appliance):
-        """
-        Polarion:
-            assignee: anikifor
-            casecomponent: WebUI
-            caseimportance: medium
-            initialEstimate: 1/30h
-        """
-        self._delete_operator(appliance, "not")
-
-    def test_delete_and(self, appliance):
-        """
-        Polarion:
-            assignee: anikifor
-            casecomponent: WebUI
-            caseimportance: medium
-            initialEstimate: 1/30h
-        """
-        self._delete_operator(appliance, "and")
-
-    def test_delete_or(self, appliance):
-        """
-        Polarion:
-            assignee: anikifor
-            casecomponent: WebUI
-            caseimportance: medium
-            initialEstimate: 1/30h
-        """
-        self._delete_operator(appliance, "or")
+@pytest.mark.meta(automates=[BZ(1720216)])
+@pytest.mark.parametrize("operator", ["not", "and", "or"])
+# remove NOT is 5.11 feature, see BZ(1720216)
+@pytest.mark.uncollectif(lambda operator, appliance:
+                         operator == "not" and appliance.version < "5.11")
+def test_exp_editor_delete_operator(appliance, operator):
+    """
+    Polarion:
+        assignee: anikifor
+        casecomponent: WebUI
+        caseimportance: medium
+        initialEstimate: 1/30h
+    """
+    view = navigate_to(appliance.collections.volumes, "All")  # any collection with advanced search
+    name = fauxfactory.gen_alphanumeric()
+    view.entities.search.save_filter(f'''
+        fill_field({filter_option}, =, {filter_value});
+        select_expression_text;
+        click_{operator};
+        {f"fill_field({filter_option}, =, {filter_value});" if operator != "not" else ""}
+        select_expression_text;
+        click_remove;''', name)
+    editor = view.search.advanced_search_form.search_exp_editor
+    assert editor.expression_text == f'{filter_option} = "{filter_value}"'
+    view.entities.search.delete_filter()
+    view.entities.search.close_advanced_search()
