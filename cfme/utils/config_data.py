7637f71110cb83132dd21751e996842521abb8b4
Kedar Vijay Kulkarni <kkulkarn@redhat.com>
2019-08-28 11:16:44 -0400

    Adding way to load cfme_data from dynaconf

--- cfme/utils/config_data.py
+++ cfme/utils/config_data.py
@@ -4,6 +4,21 @@ from yaycl import AttrDict
 
 
 @attr.s
+class ConfigDataWrapper(object):
+    """A wrapper for credentials"""
+    config = attr.ib(default=AttrDict())
+    env = attr.ib(default='cfme-qe-test-envs')
+
+    def __getitem__(self, key):
+        return self.__getattr__(key)
+
+    def __getattr__(self, key):
+        with settings.using_env(self.env):
+            self.config = AttrDict(settings.as_dict()[key.upper()])
+            return self.config
+
+
+@attr.s
 class CredsWrapper(object):
     """A wrapper for credentials"""
     credential = attr.ib(default=AttrDict())
@@ -17,4 +32,5 @@ class CredsWrapper(object):
             return self.credential
 
 
+cfme_data = ConfigDataWrapper()
 credentials = CredsWrapper()
