9cf302144b89eec2d85e82aaff5fc6d68f08dbd7
Ievgen Zapolskyi <izapolsk@redhat.com>
2019-09-05 12:22:48 +0300

    updated test cases related to Multi-Region FA

---
+++ cfme/tests/cloud/test_instance_reconfigure.py
@@ -0,0 +1,40 @@
+import pytest
+
+from cfme import test_requirements
+from cfme.cloud.provider.openstack import OpenStackProvider
+from cfme.markers.env_markers.provider import ONE_PER_TYPE
+from cfme.utils.appliance import ViaREST
+from cfme.utils.appliance import ViaUI
+
+
+pytestmark = [
+    pytest.mark.usefixtures('setup_provider'),
+    pytest.mark.long_running,
+]
+
+
+@pytest.mark.manual
+@pytest.mark.tier(2)
+@pytest.mark.parametrize('context', [ViaREST, ViaUI])
+@pytest.mark.provider([OpenStackProvider], required_fields=['templates'],
+                      selector=ONE_PER_TYPE, override=True)
+@test_requirements.multi_region
+@test_requirements.reconfigure
+def test_vm_reconfigure_from_global_region(context):
+    """
+    reconfigure a VM via CA
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Infra
+        initialEstimate: 1/3h
+        testSteps:
+            1. Have a VM created in the provider in the Remote region which is
+               subscribed to Global.
+            2. Reconfigure the VM using the Global appliance.
+        expectedResults:
+            1.
+            2.
+            3. VM reconfigured, no errors.
+    """
+    pass
--- cfme/tests/configure/test_config_manual.py
+++ cfme/tests/configure/test_config_manual.py
@@ -58,6 +58,8 @@ def test_configure_icons_roles_by_server():
 
 
 @pytest.mark.manual
+@test_requirements.settings
+@test_requirements.multi_region
 @pytest.mark.tier(3)
 def test_replication_subscription_crud():
     """
@@ -92,6 +94,8 @@ def test_replication_subscription_crud():
 
 
 @pytest.mark.manual
+@test_requirements.settings
+@test_requirements.multi_region
 @pytest.mark.tier(3)
 def test_add_duplicate_subscription():
     """
@@ -122,6 +126,8 @@ def test_add_duplicate_subscription():
 
 
 @pytest.mark.manual
+@test_requirements.settings
+@test_requirements.multi_region
 @pytest.mark.tier(3)
 def test_add_bad_subscription():
     """
@@ -140,20 +146,20 @@ def test_add_bad_subscription():
             2. Add a provider to second appliance
             3. Set replication subscription type to Remote in second appliance
             4. Set replication subscription type to Global in first appliance
-            5. Add subscription to second appliance in first appliance
-            6. Try adding second subscription to second appliance in first appliance
+            5. Try adding subscription to second appliance in first appliance with wrong values
         expectedResults:
             1.
             2.
             3.
             4.
-            5.
-            6. Second subscription hasn't been added. Warning message has appeared
+            5. Subscription hasn't been added. Add subscription task has failed
     """
     pass
 
 
 @pytest.mark.manual
+@test_requirements.settings
+@test_requirements.multi_region
 @pytest.mark.tier(3)
 def test_edit_bad_subscription():
     """
@@ -170,19 +176,21 @@ def test_edit_bad_subscription():
             2. Add a provider to second appliance
             3. Set replication subscription type to Remote in second appliance
             4. Set replication subscription type to Global in first appliance
-            5. Try adding subscription to second appliance in first appliance with wrong values
+            5. Try changing existing subscription values to wrong ones. f.e wrong password
         expectedResults:
             1.
             2.
             3.
             4.
-            5. Subscription shouldn't be added or it should be added with a warning
-            that subscription couldn't be established
+            5. Subscription shouldn't be changed if connection couldn't be established.
+            Subscription update task should fail
     """
     pass
 
 
 @pytest.mark.manual
+@test_requirements.settings
+@test_requirements.multi_region
 @pytest.mark.tier(3)
 def test_cancel_subscription():
     """
@@ -211,6 +219,8 @@ def test_cancel_subscription():
 
 
 @pytest.mark.manual
+@test_requirements.settings
+@test_requirements.multi_region
 @pytest.mark.tier(3)
 def test_change_subscription_type():
     """
@@ -250,6 +260,8 @@ def test_change_subscription_type():
 
 
 @pytest.mark.manual
+@test_requirements.settings
+@test_requirements.multi_region
 @pytest.mark.tier(3)
 def test_subscription_disruption():
     """
@@ -284,5 +296,41 @@ def test_subscription_disruption():
             8.
             9. subscription was restored. Vm was provisioned. Changes made in remote appliance
             appeared in global appliance
+    """
+    pass
+
+
+@pytest.mark.manual
+@test_requirements.settings
+@test_requirements.multi_region
+@pytest.mark.meta(coverage=[1741240])
+@pytest.mark.tier(3)
+def test_subscription_region_unavailable():
+    """
+    Tests that Replication tab is open w/o issues and 502 error when
+    remote region has become unavailable
+
+    Polarion:
+        assignee: izapolsk
+        casecomponent: Configuration
+        caseimportance: medium
+        initialEstimate: 1/4h
+        testSteps:
+            1. Set up two appliances where first appliance resides in global region (99) and
+            second one resides in remote region (10). Those should use the same security key
+            2. Add a provider to second appliance
+            3. Set replication subscription type to Remote in second appliance
+            4. Set replication subscription type to Global in first appliance
+            5. Add subscription to second appliance in first appliance
+            6. Stop postgresql service in remote appliance
+            7. Go to Configuration->Settings-><Current Region>->Replication tab
+        expectedResults:
+            1.
+            2.
+            3.
+            4.
+            5.
+            6.
+            7. Replication tab is being opened for long time and finally displays 502 error alert
     """
     pass
