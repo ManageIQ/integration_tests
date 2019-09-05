bdd32c394aec5d2dab9ca25d87fdb19e89c442ab
john-dupuy <jdupuy@redhat.com>
2019-09-05 09:50:11 -0400

    Add unblock statement

--- cfme/tests/control/test_actions.py
+++ cfme/tests/control/test_actions.py
@@ -595,8 +595,10 @@ def test_action_create_snapshot_and_delete_last(appliance, request, vm, vm_on, p
     )
 
 
-@pytest.mark.meta(blockers=[BZ(1748410, forced_streams=["5.10"])], automates=[1748410])
 @pytest.mark.provider([VMwareProvider, RHEVMProvider], scope="module")
+@pytest.mark.meta(blockers=[BZ(1748410, forced_streams=["5.10"])],
+                  automates=[1748410],
+                  unblock=lambda provider: provider.one_of(VMwareProvider))
 def test_action_create_snapshots_and_delete_them(request, appliance, vm, vm_on, policy_for_testing):
     """ This test tests actions 'Create a Snapshot' (custom) and 'Delete all Snapshots'.
 
