e97ac1e9cb72750e2e8eadd59b848fa5d9c755d5
john-dupuy <44065123+john-dupuy@users.noreply.github.com>
2019-09-20 11:01:51 -0400

    [1LP][RFR] Add manual cases and cleanup Generic Object tests (#9381)
    * Move generic_objects tests under the automate directory
    
    * Add new manual cases for generic objects

---
+++ cfme/tests/automate/generic_objects/buttons/test_generic_class_custom_button.py
@@ -0,0 +1,367 @@
+import fauxfactory
+import pytest
+
+from cfme import test_requirements
+from cfme.base.login import BaseLoggedInPage
+from cfme.generic_objects.definition.definition_views import GenericObjectDefinitionDetailsView
+from cfme.utils.appliance import ViaUI
+from cfme.utils.appliance.implementations.ui import navigate_to
+from cfme.utils.blockers import BZ
+from cfme.utils.update import update
+
+
+pytestmark = [
+    pytest.mark.tier(2),
+    test_requirements.custom_button,
+    test_requirements.generic_objects
+]
+
+
+@pytest.fixture(scope="module")
+def generic_object_button(appliance, generic_object_button_group, generic_definition):
+    def _generic_object_button(button_group):
+        with appliance.context.use(ViaUI):
+            button_parent = (
+                generic_object_button_group(button_group) if button_group else generic_definition
+            )
+            button_name = 'button_{}'.format(fauxfactory.gen_alphanumeric())
+            button_desc = 'Button_description_{}'.format(fauxfactory.gen_alphanumeric())
+            generic_object_button = button_parent.collections.generic_object_buttons.create(
+                name=button_name,
+                description=button_desc,
+                image='fa-home',
+                request=fauxfactory.gen_alphanumeric()
+            )
+            view = appliance.browser.create_view(BaseLoggedInPage)
+            view.flash.assert_no_error()
+        return generic_object_button
+    return _generic_object_button
+
+
+@pytest.fixture(scope="module")
+def generic_object_button_group(appliance, generic_definition):
+    def _generic_object_button_group(create_action=True):
+        if create_action:
+            with appliance.context.use(ViaUI):
+                group_name = "button_group_{}".format(fauxfactory.gen_alphanumeric())
+                group_desc = "Group_button_description_{}".format(fauxfactory.gen_alphanumeric())
+                groups_buttons = generic_definition.collections.generic_object_groups_buttons
+                generic_object_button_group = groups_buttons.create(
+                    name=group_name, description=group_desc, image="fa-user"
+                )
+                view = appliance.browser.create_view(BaseLoggedInPage)
+                view.flash.assert_no_error()
+            return generic_object_button_group
+
+    return _generic_object_button_group
+
+
+@pytest.fixture(scope="module")
+def button_group(appliance, generic_definition):
+    with appliance.context.use(ViaUI):
+        group = generic_definition.collections.generic_object_groups_buttons.create(
+            name=fauxfactory.gen_numeric_string(13, start="btn_group", separator="-"),
+            description=fauxfactory.gen_alphanumeric(start="disc", separator="-"),
+            image="fa-user",
+        )
+        yield group
+        group.delete_if_exists()
+
+
+@pytest.mark.meta(automates=[1744478, 1753289])
+def test_custom_group_on_generic_class_crud(appliance, generic_definition):
+    """ Test custom button group crud operation on generic class definition
+
+    Bugzilla:
+        1744478
+        1753289
+
+    Polarion:
+        assignee: ndhandre
+        initialEstimate: 1/8h
+        caseimportance: critical
+        startsin: 5.10
+        casecomponent: CustomButton
+        testSteps:
+            1. Create custom button group on generic class
+            2. Update custom button group by editing
+            3. Delete custom button group
+    """
+
+    with appliance.context.use(ViaUI):
+        # create group
+        group = generic_definition.collections.generic_object_groups_buttons.create(
+            name=fauxfactory.gen_numeric_string(13, start="btn_group", separator="-"),
+            description=fauxfactory.gen_alphanumeric(start="disc", separator="-"),
+            image="fa-user",
+        )
+        view = appliance.browser.create_view(GenericObjectDefinitionDetailsView)
+        view.flash.assert_success_message(
+            f'Custom Button Group "{group.name}" has been successfully added.'
+        )
+        assert group.exists
+
+        # update group
+        with update(group):
+            group.name = fauxfactory.gen_numeric_string(13, start="btn_group", separator="-")
+            group.description = fauxfactory.gen_alphanumeric(start="disc", separator="-")
+        view.flash.assert_success_message(
+            f'Custom Button Group "{group.name}" has been successfully saved.'
+        )
+        assert group.exists
+
+        # delete group
+        group.delete()
+        # TODO(ndhandre): For now, we can not guess exact flash message.
+        #  Change flash as per BZ-1744478.
+        view.flash.assert_success_message('Button Group:"undefined" was successfully deleted')
+        assert not group.exists
+
+
+@pytest.mark.meta(automates=[1534539, 1744478, 1753289])
+@pytest.mark.parametrize("is_undefined", [True, False], ids=["undefined", "with_group"])
+def test_custom_button_on_generic_class_crud(appliance, button_group, is_undefined):
+    """Test custom button crud operation on generic class definition
+
+    Bugzilla:
+        1534539
+        1744478
+        1753289
+
+    Polarion:
+        assignee: ndhandre
+        initialEstimate: 1/4h
+        caseimportance: critical
+        startsin: 5.10
+        casecomponent: CustomButton
+        testSteps:
+            1. Create custom button on generic class (with group and undefined)
+            2. Update custom button by editing
+            3. Delete custom button
+    """
+    parent = button_group.parent.parent if is_undefined else button_group
+
+    with appliance.context.use(ViaUI):
+        # create button
+        button = parent.collections.generic_object_buttons.create(
+            name=fauxfactory.gen_numeric_string(start="btn", separator="-"),
+            description=fauxfactory.gen_numeric_string(start="disc", separator="-"),
+            image="fa-home",
+            request="InspectMe",
+        )
+        view = appliance.browser.create_view(GenericObjectDefinitionDetailsView)
+
+        if is_undefined:
+            msg = f'Custom Button "{button.name}" has been successfully added.'
+        else:
+            msg = (
+                f'Custom Button "{button.name}" has been successfully added '
+                f"under the selected button group."
+            )
+
+        view.flash.assert_success_message(msg)
+        assert button.exists
+
+        # update button
+        with update(button):
+            button.name = fauxfactory.gen_numeric_string(start="btn", separator="-")
+            button.description = fauxfactory.gen_alphanumeric(start="disc", separator="-")
+        view.flash.assert_success_message(
+            f'Custom Button "{button.name}" has been successfully saved.'
+        )
+        assert button.exists
+
+        # delete button
+        button.delete()
+        # TODO(ndhandre): For now, we can not guess exact flash message.
+        #  Change flash as per BZ-1744478.
+        view.flash.assert_success_message('Button:"undefined" was successfully deleted')
+        assert not button.exists
+
+
+@pytest.mark.parametrize('button_group', [True, False],
+                         ids=['button_group_with_button', 'single_button'])
+def test_generic_objects_with_buttons_ui(appliance, add_generic_object_to_service,
+                                         button_group, generic_object_button):
+    """
+        Tests buttons ui visibility assigned to generic object
+
+        Metadata:
+            test_flag: ui
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/4h
+        casecomponent: GenericObjects
+    """
+    instance = add_generic_object_to_service
+    generic_button = generic_object_button(button_group)
+    generic_button_group = generic_button.parent.parent
+
+    with appliance.context.use(ViaUI):
+        view = navigate_to(instance, 'MyServiceDetails')
+        if button_group:
+            assert view.toolbar.group(generic_button_group.name).custom_button.has_item(
+                generic_button.name)
+        else:
+            assert view.toolbar.button(generic_button.name).custom_button.is_displayed
+
+
+@pytest.mark.manual
+@pytest.mark.meta(coverage=[1650559])
+def test_ansible_playbook_generic_object_button():
+    """
+    Bugzilla:
+        1650559
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/4h
+        casecomponent: GenericObjects
+        testSteps:
+            1. Go to automation->Automate->Generic Objects
+            2. Create Generic Objects Class
+            3. Go Add New button
+            4. Change button type to Ansible Playbook
+        expectedResults:
+            1.
+            2.
+            3.
+            4. Ansible playbook fields should be displayed
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.meta(blockers=[BZ(1753237)], coverage=[1753237])
+def test_generic_object_button_edit_multiple_times():
+    """
+    Bugzilla:
+        1753237
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/6h
+        casecomponent: GenericObjects
+        testSteps:
+            1. Create a "Generic Object Class"
+            2. Create a custom button that has the "Request" as "ca"
+            3. Edit the custom button request to "call_instance"
+            4. Click to edit the custom button again
+        expectedResults:
+            1.
+            2.
+            3.
+            4. The request field should say "call_instance"
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.meta(blockers=[BZ(1753289), BZ(1744478)], coverage=[1753289, 1744478])
+def test_generic_object_button_delete_flash():
+    """
+    Bugzilla:
+        1744478
+        1753289
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/6h
+        casecomponent: GenericObjects
+        testSteps:
+            1. Add a generic object class
+            2. Create a button under the class
+            3. Delete the button
+        expectedResults:
+            1.
+            2.
+            3. Assert that the button name is in the flash message
+    """
+    pass
+
+
+@pytest.mark.manual
+@pytest.mark.meta(blockers=[BZ(1753281), BZ(1753388)], coverage=[1753281, 1753388])
+def test_generic_object_button_delete_multiple():
+    """
+    Bugzilla:
+        1753281
+        1753388
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/30h
+        casecomponent: GenericObjects
+        testSteps:
+            1. Create a generic object class
+            2. Create a custom button under this class
+            3. Create a second button
+            4. Delete the second button
+        expectedResults:
+            1.
+            2.
+            3.
+            4. The second button should be deleted, not both, no 404 error
+    """
+    pass
+
+
+@pytest.mark.manual
+@test_requirements.ansible
+@pytest.mark.meta(blockers=[BZ(1753338)], coverage=[1753338])
+def test_generic_object_button_execute_playbook():
+    """
+    Bugzilla:
+        1753338
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/30h
+        casecomponent: GenericObjects
+        testSteps:
+            1. Enable embedded ansible role
+            2. Add a repo with some playbooks
+                (e.g. https://github.com/ManageIQ/integration_tests_playbooks.git)
+            3. Add some Ansible credentials for a VMware provider
+            4. Create a catalog named "Catalog VMware"
+            5. Create a catalog item "VMware Catalog Item" under this catalog that executes
+                a playbook, if you're using the above repo you can use
+                e.g. "gather_all_vms_from_vmware.yml"
+            6. Order the service catalog and ensure that the playbook runs successfully
+            7. Now create a separate catalog "Test Custom Button"
+            8. Create a "generic" catalog item under this catalog "test custom button"
+            9. Order this service and ensure that the service appears under My Services
+            10. Create a generic object class "Test class"
+            11. Create a custom button of type "Ansible Playbook" that uses the catalog item
+                "VMware Catalog Item", with "Request" -> "Order_Ansible_Playbook"
+            12. Create a generic object instance under "Test class"
+            13. Associate the generic object instance with the service "Test Custom Button"
+                This can be done by REST action "add_resource" or by the rails console
+            14. Navigate to Services > My services > Services > Active Services > Test Custom Button
+            15. In the generic objects summary table, there should be one instance
+                associated with the service, click this instance
+            16. Click the generic object quadicon
+            17. The custom button you created in step 11 should be displayed, click that button.
+            18. Hit submit
+        expectedResults:
+            1.
+            2.
+            3.
+            4.
+            5.
+            6.
+            7.
+            8.
+            9.
+            10.
+            11.
+            12.
+            13.
+            14.
+            15.
+            16.
+            17.
+            18. The playbook should execute successfully
+    """
+    pass
--- cfme/tests/automate/generic_objects/test_instances.py
+++ cfme/tests/automate/generic_objects/test_instances.py
@@ -1,39 +1,22 @@
 # -*- coding: utf-8 -*-
 import fauxfactory
 import pytest
-import yaml
 
 import cfme.rest.gen_data as rest_gen_data
 from cfme import test_requirements
-from cfme.base.login import BaseLoggedInPage
 from cfme.generic_objects.instance.ui import MyServiceGenericObjectInstanceView
 from cfme.services.myservice import MyService
 from cfme.tests.automate.custom_button import TextInputDialogView
 from cfme.utils.appliance import ViaREST
 from cfme.utils.appliance import ViaUI
 from cfme.utils.appliance.implementations.ui import navigate_to
+from cfme.utils.blockers import BZ
 from cfme.utils.update import update
 from cfme.utils.wait import TimedOutError
 from cfme.utils.wait import wait_for
 
 
 pytestmark = [test_requirements.generic_objects]
-
-
-GEN_OBJ_DIRECTORY = "/var/www/miq/vmdb/tmp/generic_object_definitions"
-
-
-@pytest.fixture
-def gen_obj_def_import_export(appliance):
-    with appliance.context.use(ViaREST):
-        definition = appliance.collections.generic_object_definitions.create(
-            name="rest_gen_class_imp_exp{}".format(fauxfactory.gen_alphanumeric()),
-            description="Generic Object Definition",
-            attributes={'addr01': 'string'},
-            methods=['add_vm', 'remove_vm']
-        )
-        yield definition
-        definition.delete_if_exists()
 
 
 @pytest.fixture(scope="module")
@@ -61,45 +44,6 @@ def button_with_dialog(appliance, generic_object, dialog):
         yield button
 
         button.delete_if_exists()
-
-
-@pytest.fixture(scope="module")
-def generic_object_button_group(appliance, generic_definition):
-    def _generic_object_button_group(create_action=True):
-        if create_action:
-            with appliance.context.use(ViaUI):
-                group_name = "button_group_{}".format(fauxfactory.gen_alphanumeric())
-                group_desc = "Group_button_description_{}".format(fauxfactory.gen_alphanumeric())
-                groups_buttons = generic_definition.collections.generic_object_groups_buttons
-                generic_object_button_group = groups_buttons.create(
-                    name=group_name, description=group_desc, image="fa-user"
-                )
-                view = appliance.browser.create_view(BaseLoggedInPage)
-                view.flash.assert_no_error()
-            return generic_object_button_group
-
-    return _generic_object_button_group
-
-
-@pytest.fixture(scope="module")
-def generic_object_button(appliance, generic_object_button_group, generic_definition):
-    def _generic_object_button(button_group):
-        with appliance.context.use(ViaUI):
-            button_parent = (
-                generic_object_button_group(button_group) if button_group else generic_definition
-            )
-            button_name = 'button_{}'.format(fauxfactory.gen_alphanumeric())
-            button_desc = 'Button_description_{}'.format(fauxfactory.gen_alphanumeric())
-            generic_object_button = button_parent.collections.generic_object_buttons.create(
-                name=button_name,
-                description=button_desc,
-                image='fa-home',
-                request=fauxfactory.gen_alphanumeric()
-            )
-            view = appliance.browser.create_view(BaseLoggedInPage)
-            view.flash.assert_no_error()
-        return generic_object_button
-    return _generic_object_button
 
 
 @pytest.mark.sauce
@@ -161,34 +105,6 @@ def test_generic_objects_crud(appliance, context, request):
         assert not instance.exists
 
 
-@pytest.mark.parametrize('button_group', [True, False],
-                         ids=['button_group_with_button', 'single_button'])
-def test_generic_objects_with_buttons_ui(appliance, request, add_generic_object_to_service,
-                                         button_group, generic_object_button):
-    """
-        Tests buttons ui visibility assigned to generic object
-
-        Metadata:
-            test_flag: ui
-
-    Polarion:
-        assignee: jdupuy
-        initialEstimate: 1/4h
-        casecomponent: GenericObjects
-    """
-    instance = add_generic_object_to_service
-    generic_button = generic_object_button(button_group)
-    generic_button_group = generic_button.parent.parent
-
-    with appliance.context.use(ViaUI):
-        view = navigate_to(instance, 'MyServiceDetails')
-        if button_group:
-            assert view.toolbar.group(generic_button_group.name).custom_button.has_item(
-                generic_button.name)
-        else:
-            assert view.toolbar.button(generic_button.name).custom_button.is_displayed
-
-
 @test_requirements.tag
 @pytest.mark.parametrize('tag_place', [True, False], ids=['details', 'collection'])
 def test_generic_objects_tag_ui(appliance, generic_object, tag_place):
@@ -239,74 +155,6 @@ def test_generic_objects_tag_rest(appliance, generic_object, tags):
         assert tag.id not in [t.id for t in tag_available]
 
 
-@pytest.mark.ignore_stream("5.10")
-def test_import_export_generic_object_definition(request, appliance, gen_obj_def_import_export):
-    """
-    Bugzilla:
-        1595259
-
-    Polarion:
-        assignee: jdupuy
-        initialEstimate: 1/6h
-        caseimportance: high
-        caseposneg: positive
-        testtype: functional
-        startsin: 5.11
-        casecomponent: GenericObjects
-        testSteps:
-            1. Create generic object definition via Rest
-            2. Export the generic object definition
-            3. Delete the generic object definition
-            4. Import the generic object definition
-        expectedResults:
-            1. The generic object definition should be present in CFME
-            2. Yaml file should be present on the appliance with the generic object details
-            3. Generic object definition is deleted
-            4. Generic object definition once again exists on the appliance
-    """
-    # Create the generic object directory
-    assert appliance.ssh_client.run_command("mkdir {}".format(GEN_OBJ_DIRECTORY)).success
-
-    @request.addfinalizer
-    def cleanup():
-        assert appliance.ssh_client.run_command("rm -rf {}".format(GEN_OBJ_DIRECTORY)).success
-
-    # Export the user defined generic object definitions
-    assert appliance.ssh_client.run_rake_command(
-        "evm:export:generic_object_definitions -- --directory {}".format(GEN_OBJ_DIRECTORY)
-    ).success
-    # Verify the file's information
-    try:
-        with appliance.ssh_client.open_sftp().open(
-                "{}/{}.yaml".format(GEN_OBJ_DIRECTORY, gen_obj_def_import_export.name)
-        ) as f:
-            data = yaml.safe_load(f)[0]["GenericObjectDefinition"]
-
-    except IOError:
-        pytest.fail(
-            "IOError: {}/{}.yaml not found on the appliance, "
-            "exporting the generic object definition failed".format(
-                GEN_OBJ_DIRECTORY, gen_obj_def_import_export.name
-            )
-        )
-
-    assert data.get("description") == gen_obj_def_import_export.description
-    assert data.get("name") == gen_obj_def_import_export.name
-
-    # Delete the generic object definition via the UI
-    gen_obj_def_import_export.delete_if_exists()
-
-    # Import the generic object yaml by running the rake command
-    assert appliance.ssh_client.run_rake_command(
-        "evm:import:generic_object_definitions -- --source {}/{}.yaml".format(
-            GEN_OBJ_DIRECTORY,
-            gen_obj_def_import_export.name,
-        )
-    ).success
-
-    assert gen_obj_def_import_export.exists
-
-
 @test_requirements.customer_stories
 @pytest.mark.meta(automates=[1729341, 1743266])
 def test_generic_object_with_service_button(appliance, generic_object, button_with_dialog):
@@ -341,3 +189,32 @@ def test_generic_object_with_service_button(appliance, generic_object, button_wi
         generic_object.create_view(MyServiceGenericObjectInstanceView, wait=10)
     except TimedOutError:
         pytest.fail("Could not wait for service's generic object view to displayed.")
+
+
+@pytest.mark.manual
+@pytest.mark.meta(blockers=[BZ(1741050)], coverage=[1741050])
+def test_generic_object_on_service_breadcrumb():
+    """
+    Bugzilla:
+        1741050
+
+    Polarion:
+        assignee: jdupuy
+        initialEstimate: 1/6h
+        casecomponent: GenericObjects
+        testSteps:
+            1. Generate a service viewable under My Services
+            2. Create Generic Object Class & Instance
+            3. Assign the generic object instance to the service
+            4. Navigate to the service
+            5. Click on the generic object instances
+            6. Check the breadcrumb link
+        expectedResults:
+            1.
+            2.
+            3.
+            4.
+            5.
+            6. Breadcrumb should work properly
+    """
+    pass
