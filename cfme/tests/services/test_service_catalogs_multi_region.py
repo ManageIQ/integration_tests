56f51ddac0798ee17d3c8027b15d58c770efc9b4
Ievgen Zapolskyi <izapolsk@redhat.com>
2019-09-20 21:04:16 +0300

    added steps

--- cfme/tests/services/test_service_catalogs_multi_region.py
+++ cfme/tests/services/test_service_catalogs_multi_region.py
@@ -30,5 +30,26 @@ def test_service_provision_retire_from_global_region(item_type, catalog_location
         caseimportance: high
         casecomponent: Services
         initialEstimate: 1/3h
+        testSteps:
+            1. Take two or more appliances
+            2. Configure DB manually
+            3. Make one appliance as Global region and second are Remote
+            4. Add appropriate provider to remote region appliance
+            5. Create Dialog
+            6. Create Catalog
+            7. Create Catalog Item of above provider type in remote appliance
+            8. Order appearing service catalog in global appliance
+            9. Retire provisioned service in global appliance
+
+        expectedResults:
+            1.
+            2.
+            3.
+            4.
+            5.
+            6.
+            7.
+            8. service catalog has been successfully provisioned
+            9. service has been successfully retired
     """
     pass
