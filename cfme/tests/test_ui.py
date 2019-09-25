95c04731dae531f77b027553d976cd4c195f8cbb
Parthvi Vala <pvala@redhat.com>
2019-09-25 17:23:57 +0530

    Fix test: test_each_page

--- cfme/tests/test_ui.py
+++ cfme/tests/test_ui.py
@@ -1,3 +1,5 @@
+import pytest
+
 from cfme import test_requirements
 from cfme.fixtures.soft_assert import soft_assert
 from cfme.utils.appliance.implementations.ui import navigate_to
@@ -21,17 +23,23 @@ def traverse(dic, paths, path=None):
     return paths
 
 
+@pytest.mark.meta(coverage=[1648338])
 def test_each_page(appliance):
     """
+    Bugzilla:
+        1648338
+
     Polarion:
         assignee: pvala
         initialEstimate: 1/4h
         casecomponent: WebUI
     """
     view = navigate_to(appliance.server, 'Dashboard')
-    # test meta is here and CFME will be displayed correctly in IE11
-    edge_header = view.browser.element('//meta[@content="IE=edge"]')
-    soft_assert(edge_header.get_attribute('http-equiv') == 'X-UA-Compatible')
+    # no meta is present for 5.11
+    if appliance.version < "5.11":
+        # test meta is here and CFME will be displayed correctly in IE11
+        edge_header = view.browser.element('//meta[@content="IE=edge"]')
+        soft_assert(edge_header.get_attribute('http-equiv') == 'X-UA-Compatible')
     tree = view.navigation.nav_item_tree()
     paths = []
     traverse(tree, paths, path=None)
