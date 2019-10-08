edd81bfccfceb553acd6f4bf23efe52a0b454446
john-dupuy <jdupuy@redhat.com>
2019-10-08 13:55:23 -0400

    Move manual login cases to test_auth_manual.py

--- cfme/tests/test_login.py
+++ cfme/tests/test_login.py
@@ -74,157 +74,6 @@ def test_bad_password(context, request, appliance):
         assert view.password.read() == '' and view.username.read() == ''
 
 
-@pytest.mark.parametrize('context', [ViaUI])
-@pytest.mark.meta(blockers=[BZ(1632718)])
-def test_update_password(context, request, appliance):
-    """ Test updating password from the login screen.
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Infra
-        initialEstimate: 1/6h
-    """
-
-    # First, create a temporary new user
-    username = 'user_temp_{}'.format(fauxfactory.gen_alphanumeric(4).lower())
-    new_creds = Credential(principal=username, secret='redhat')
-    user_group = appliance.collections.groups.instantiate(description="EvmGroup-vm_user")
-    user = appliance.collections.users.create(
-        name=username,
-        credential=new_creds,
-        groups=user_group
-    )
-    error_message = "Login failed: Unauthorized"
-
-    # Try to login as the new user to verify it has been created
-    logged_in_page = appliance.server.login(user)
-    assert logged_in_page.is_displayed
-    logged_in_page.logout()
-
-    # Now try to login while changing the password for the user
-    changed_pass_page = appliance.server.update_password(new_password='changeme', user=user)
-    assert changed_pass_page.is_displayed
-    changed_pass_page.logout()
-
-    # Try to login with the user with old password
-    with pytest.raises(Exception, match=error_message):
-        appliance.server.login(user)
-
-    # Now try changing password with invalid default password
-    new_cred = Credential(principal=username, secret="made_up_invalid_pass")
-    user2 = appliance.collections.users.instantiate(credential=new_cred, name=username)
-    with pytest.raises(Exception, match=error_message):
-        appliance.server.update_password(new_password='changeme', user=user2)
-
-    # Workaround for emptying the password fields.
-    # If this wasn't here, we would change the password for admin user while
-    # deleting our user. Which is bad.
-    appliance.server.browser.refresh()
-
-    # Delete the user we created
-    user.delete()
-
-
-@pytest.mark.manual
-@test_requirements.auth
-@pytest.mark.tier(2)
-def test_pwd_trailing_whitespace():
-    """
-    Test changing password to one with trailing whitespace.
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Auth
-        caseimportance: medium
-        initialEstimate: 1/8h
-    """
-    pass
-
-
-@pytest.mark.manual
-@test_requirements.auth
-@pytest.mark.tier(2)
-def test_pwd_special_chars():
-    """
-    Test password with special characters.
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Auth
-        caseimportance: medium
-        initialEstimate: 1/8h
-    """
-    pass
-
-
-@pytest.mark.manual
-@test_requirements.auth
-@pytest.mark.tier(2)
-def test_pwd_blank():
-    """
-    Test changing password to a blank one.
-    Test creating user with a blank password.
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Auth
-        caseimportance: medium
-        initialEstimate: 1/8h
-    """
-    pass
-
-
-@pytest.mark.manual
-@test_requirements.auth
-@pytest.mark.tier(2)
-def test_pwd_16_chars():
-    """
-    Password > 16 char
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Auth
-        caseimportance: medium
-        initialEstimate: 1/8h
-    """
-    pass
-
-
-@pytest.mark.manual
-@test_requirements.auth
-@pytest.mark.tier(2)
-def test_login_invalid_user():
-    """
-    Login with invalid user
-    Authentication expected to fail, check audit.log and evm.log for
-    correct log messages.
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Auth
-        caseimportance: low
-        caseposneg: negative
-        initialEstimate: 1/4h
-    """
-    pass
-
-
-@pytest.mark.manual
-@test_requirements.auth
-@pytest.mark.tier(2)
-def test_pwd_leading_whitespace():
-    """
-    Password with leading whitespace
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Auth
-        caseimportance: medium
-        initialEstimate: 1/8h
-    """
-    pass
-
-
 @pytest.mark.manual
 @test_requirements.rbac
 @pytest.mark.tier(2)
@@ -277,3 +126,56 @@ def test_multiregion_displayed_on_login(context, setup_multi_region_cluster, mul
         login_view = navigate_to(rapp.server, 'LoginScreen')
         assert login_view.is_displayed
         assert 'Remote' in login_view.details.region.text
+
+
+@pytest.mark.parametrize('context', [ViaUI])
+@pytest.mark.meta(automates=[BZ(1632718)])
+@pytest.mark.browser_isolation
+@test_requirements.auth
+def test_update_password(context, request, appliance):
+    """ Test updating password from the login screen.
+
+    Polarion:
+        assignee: jdupuy
+        casecomponent: Infra
+        initialEstimate: 1/6h
+    """
+
+    # First, create a temporary new user
+    username = 'user_temp_{}'.format(fauxfactory.gen_alphanumeric(4).lower())
+    new_creds = Credential(principal=username, secret='redhat')
+    user_group = appliance.collections.groups.instantiate(description="EvmGroup-vm_user")
+    user = appliance.collections.users.create(
+        name=username,
+        credential=new_creds,
+        groups=user_group
+    )
+    error_message = "Login failed: Unauthorized"
+
+    # Try to login as the new user to verify it has been created
+    logged_in_page = appliance.server.login(user)
+    assert logged_in_page.is_displayed
+    logged_in_page.logout()
+
+    # Now try to login while changing the password for the user
+    changed_pass_page = appliance.server.update_password(new_password='changeme', user=user)
+    assert changed_pass_page.is_displayed
+    changed_pass_page.logout()
+
+    # Try to login with the user with old password
+    with pytest.raises(Exception, match=error_message):
+        appliance.server.login(user)
+
+    # Now try changing password with invalid default password
+    new_cred = Credential(principal=username, secret="made_up_invalid_pass")
+    user2 = appliance.collections.users.instantiate(credential=new_cred, name=username)
+    with pytest.raises(Exception, match=error_message):
+        appliance.server.update_password(new_password='changeme', user=user2)
+
+    # Workaround for emptying the password fields.
+    # If this wasn't here, we would change the password for admin user while
+    # deleting our user. Which is bad.
+    appliance.server.browser.refresh()
+
+    # Delete the user we created
+    user.delete()
