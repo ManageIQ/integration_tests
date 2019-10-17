b7095b26bf6f887e69f07dff285295dcf34b3261
john-dupuy <jdupuy@redhat.com>
2019-10-17 11:45:03 -0400

    Automate test-pwd tests, delete manual cases

---
+++ cfme/tests/integration/test_db_auth.py
@@ -0,0 +1,71 @@
+import fauxfactory
+import pytest
+
+from cfme import test_requirements
+from cfme.base.credential import Credential
+from cfme.utils.appliance.implementations.ui import navigate_to
+from cfme.utils.update import update
+
+# Tests concerning database authentication
+
+pytestmark = [test_requirements.auth]
+
+
+TEST_PASSWORDS = [
+    f"{fauxfactory.gen_alpha()} ",  # trailing whitespace
+    f" {fauxfactory.gen_alpha()}",  # leading whitespace
+    f"$#!{fauxfactory.gen_alpha()}",  # spec char
+    f"{fauxfactory.gen_alpha(17)}",  # pw > 16 char
+    "",  # blank
+]
+
+
+@pytest.fixture(scope="module")
+def user(appliance):
+    name = f"test-user-{fauxfactory.gen_alpha()}"
+    creds = Credential(principal=name, secret=fauxfactory.gen_alpha())
+    user_group = appliance.collections.groups.instantiate(description="EvmGroup-vm_user")
+    user = appliance.collections.users.create(
+        name=name,
+        credential=creds,
+        groups=user_group,
+    )
+    yield user
+    user.delete_if_exists()
+
+
+@pytest.mark.parametrize(
+    "pwd",
+    TEST_PASSWORDS,
+    ids=["trailing_whitspace", "leading_whitespace", "spec_char", "gt_16char", "blank"]
+)
+def test_db_user_pwd(appliance, user, pwd, soft_assert):
+    """
+    Polarion:
+        assignee: jdupuy
+        casecomponent: Appliance
+        initialEstimate: 1/6h
+    """
+    new_credential = Credential(principal=user.credential.principal, secret=pwd)
+    if pwd:
+        with update(user):
+            user.credential = new_credential
+        with user:
+            # now make sure the user can login
+            view = navigate_to(appliance.server, "LoggedIn")
+            soft_assert(view.current_fullname == user.name,
+                        'user full name "{}" did not match UI display name "{}"'
+                        .format(user.name, view.current_fullname))
+            soft_assert(user.groups[0].description in view.group_names,
+                        'local group "{}" not displayed in UI groups list "{}"'
+                        .format(user.groups[0].description, view.group_names))
+    else:
+        # blank pwd doesn't allow you to click save
+        view = navigate_to(user, 'Edit')
+        user.change_stored_password()
+        view.fill({
+            'password_txt': new_credential.secret,
+            'password_verify_txt': new_credential.verify_secret
+        })
+        assert view.save_button.disabled
+        view.cancel_button.click()
