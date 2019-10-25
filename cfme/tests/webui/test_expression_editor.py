d258712126baf1d3b63bcc42c73e720419f27184
Angelina Nikiforova <anikifor@redhat.com>
2019-10-25 13:25:48 +0200

    automated BZ 1741243

--- cfme/tests/webui/test_expression_editor.py
+++ cfme/tests/webui/test_expression_editor.py
@@ -2,8 +2,10 @@ import fauxfactory
 import pytest
 
 from cfme import test_requirements
+from cfme.services.myservice import MyService
 from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.utils.blockers import BZ
+from cfme.utils.log_validator import LogValidator
 
 pytestmark = [test_requirements.filtering]
 filter_value = 'beautifulpotato'
@@ -36,3 +38,27 @@ def test_exp_editor_delete_operator(appliance, operator):
     assert editor.expression_text == f'{filter_option} = "{filter_value}"'
     view.entities.search.delete_filter()
     view.entities.search.close_advanced_search()
+
+
+@pytest.mark.meta(automates=[1741243, 1761525], blockers=[BZ(1741243)])
+def test_apply_after_save():
+    """
+    Bugzilla:
+        1741243
+        1761525
+
+    There are a few ways to reproduce this BZ but this is the most reliable
+
+    Polarion:
+        assignee: anikifor
+        casecomponent: WebUI
+        caseimportance: high
+        initialEstimate: 1/10h
+    """
+    filter_name = fauxfactory.gen_alphanumeric()
+    view = navigate_to(MyService, 'All')
+    view.entities.search.save_filter("fill_field(Service : Custom 1, =, value)", filter_name)
+    with LogValidator("/var/www/miq/vmdb/log/production.log",
+                      failure_patterns=[".*FATAL.*"]).waiting(timeout=60):
+        assert view.entities.search.apply_filter()
+        assert view.wait_displayed("10s")  # in case of error it's not displayed properly
