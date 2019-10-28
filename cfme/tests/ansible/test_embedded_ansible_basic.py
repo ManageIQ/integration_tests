eee5106cfd904eb30dff529838cbe291998d2d95
Satyajit Bulage <sbulage@redhat.com>
2019-10-28 13:16:49 +0530

    Ansible Automation for Private Repository CRUD.

--- cfme/tests/ansible/test_embedded_ansible_basic.py
+++ cfme/tests/ansible/test_embedded_ansible_basic.py
@@ -3,6 +3,7 @@ import fauxfactory
 import pytest
 
 from cfme import test_requirements
+from cfme.utils import conf
 from cfme.utils.appliance.implementations.ui import navigate_to
 from cfme.utils.conf import cfme_data
 from cfme.utils.log import logger
@@ -211,6 +212,56 @@ def test_embedded_ansible_repository_invalid_url_crud(request, appliance, wait_f
         else "error"
     )
 
+    repository.delete_if_exists()
+
+
+@pytest.mark.rhel_testing
+@pytest.mark.tier(1)
+def test_embedded_ansible_add_private_repository_crud(
+    request, credentials_collection, appliance, wait_for_ansible
+):
+    """
+    Add Private Repository by using SCM credentials. Check repository gets added successfully.
+
+    Polarion:
+        assignee: sbulage
+        casecomponent: Ansible
+        caseimportance: critical
+        initialEstimate: 1/6h
+        tags: ansible_embed
+    """
+    # Create SCM credentials.
+    creds = {
+        "username": conf.credentials["scm_creds"]["username"],
+        "password": conf.credentials["scm_creds"]["password"],
+    }
+
+    scm_credential = credentials_collection.create(
+        "Scm_credential",
+        "Scm",
+        **creds
+    )
+
+    assert scm_credential.exists
+
+    # Create private repository using SCM credentials.
+    repositories = appliance.collections.ansible_repositories
+    repository = repositories.create(
+        name=fauxfactory.gen_alpha(),
+        url="https://gitlab.com/cfme/ansible_private_repo.git",
+        description=fauxfactory.gen_alpha(),
+        scm_credentials=scm_credential.name,
+    )
+    view = navigate_to(repository, "Details")
+    wait_for(
+        lambda: repository.status == "successful",
+        num_sec=120,
+        delay=5,
+        fail_func=view.toolbar.refresh.click
+    )
+    assert repository.exists
+
+    scm_credential.delete_if_exists()
     repository.delete_if_exists()
 
 
