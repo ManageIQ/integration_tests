7ef59833d384440841d05648f02aaabbb1218850
Mike Shriver <mshriver@redhat.com>
2019-11-19 16:44:45 -0500

    Update authentication for bz util, add clone coverage
    handle api_key authentication to bugzilla
    remove contextmanager for login, the underlying library should authenticate when URL is passed
    Add scan for clone BZs to the method used for BZ coverage reports

--- cfme/scripting/bz.py
+++ cfme/scripting/bz.py
@@ -51,6 +51,7 @@ def get_report(directory):
         "--long-running",
         "--use-template-cache",
         "--collect-only",
+        "--dummy-appliance",
         "-q",
         "--generate-bz-report", directory
     ])
