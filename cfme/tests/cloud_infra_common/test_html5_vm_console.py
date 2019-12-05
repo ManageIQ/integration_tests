b053ff156d481ffbb91c970d90ed70ab26f0a096
Kedar Vijay Kulkarni <kkulkarn@redhat.com>
2019-12-05 13:40:54 -0500

    Moving ssui console manual tests to new module as well

--- cfme/tests/cloud_infra_common/test_html5_vm_console.py
+++ cfme/tests/cloud_infra_common/test_html5_vm_console.py
@@ -130,6 +130,7 @@ def test_html5_vm_console(appliance, setup_provider, provider, configure_websock
 @pytest.mark.tier(2)
 @pytest.mark.parametrize('browser', ['chrome_latest', 'firefox_latest'])
 @pytest.mark.parametrize('operating_system', ['fedora_latest', 'rhel8.x', 'rhel7.x', 'rhel6.x'])
+@pytest.mark.provider([CloudProvider, InfraProvider], required_flags=['html5_console'])
 def test_html5_console_linux(browser, operating_system, provider):
     """
     This testcase is here to reflect testing matrix for html5 consoles. Combinations listed
@@ -170,6 +171,7 @@ def test_html5_console_linux(browser, operating_system, provider):
 @pytest.mark.parametrize('browser', ['edge', 'internet_explorer'])
 @pytest.mark.parametrize('operating_system', ['windows7', 'windows10',
  'windows_server2012', 'windows_server2016'])
+@pytest.mark.provider([CloudProvider, InfraProvider], required_flags=['html5_console'])
 def test_html5_console_windows(browser, operating_system, provider):
     """This testcase is here to reflect testing matrix for html5 consoles. Combinations listed
     are being tested manually. Originally, there was one testcase for every combination, this
@@ -197,83 +199,6 @@ def test_html5_console_windows(browser, operating_system, provider):
         expectedResults:
             1. VM Details displayed
             2. Console launched
-            3. Console accepts characters
-            4. Characters not garbled; no visual defect
-    """
-    pass
-
-
-@pytest.mark.manual('manualonly')
-@test_requirements.html5
-@pytest.mark.tier(2)
-@pytest.mark.parametrize('browser', ['chrome_latest', 'firefox_latest'])
-@pytest.mark.parametrize('operating_system', ['fedora_latest', 'rhel8.x', 'rhel7.x', 'rhel6.x'])
-def test_html5_ssui_console_linux(browser, operating_system, provider):
-    """
-    This testcase is here to reflect testing matrix for html5 consoles going via ssui.
-    Combinations listed are being tested manually. Originally, there was one testcase for every
-    combination, this approach reduces number of needed testcases.
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Appliance
-        initialEstimate: 2/3h
-        setup:
-            1. Login to CFME Appliance as admin.
-            2. On top right click Administrator|EVM -> Configuration.
-            3. Under VMware Console Support section and click on Dropdown in front
-               of "Use" and select "VNC".
-            4. Click save at the bottom of the page.
-               This will setup your appliance for using HTML5 VNC Console and not to
-               use VMRC Plug in which is Default when you setup appliance.
-            6. Create a service dialog and catalog that provisions a VM
-        testSteps:
-            1. Via ssui, order the catalog and wait for VM provision
-            2. Via ssui, navigate to service details and click on
-                Access-> VM Console for testing VM
-            3. Make sure the console accepts commands
-            4. Make sure the characters are visible
-        expectedResults:
-            1. Catalog ordered; VM provisioned
-            3. Console accepts characters
-            4. Characters not garbled; no visual defect
-    """
-    pass
-
-
-@pytest.mark.manual('manualonly')
-@test_requirements.html5
-@pytest.mark.tier(2)
-@pytest.mark.parametrize('browser', ['edge', 'internet_explorer'])
-@pytest.mark.parametrize('operating_system', ['windows7', 'windows10',
-    'windows_server2012', 'windows_server2016'])
-def test_html5_ssui_console_windows(browser, operating_system, provider):
-    """
-    This testcase is here to reflect testing matrix for html5 consoles going via ssui.
-    Combinations listed are being tested manually. Originally, there was one testcase for every
-    combination, this approach reduces number of needed testcases.
-
-    Polarion:
-        assignee: apagac
-        casecomponent: Appliance
-        initialEstimate: 2/3h
-        setup:
-            1. Login to CFME Appliance as admin.
-            2. On top right click Administrator|EVM -> Configuration.
-            3. Under VMware Console Support section and click on Dropdown in front
-               of "Use" and select "VNC".
-            4. Click save at the bottom of the page.
-               This will setup your appliance for using HTML5 VNC Console and not to
-               use VMRC Plug in which is Default when you setup appliance.
-            6. Create a service dialog and catalog that provisions a VM
-        testSteps:
-            1. Via ssui, order the catalog and wait for VM provision
-            2. Via ssui, navigate to service details and click on
-                Access-> VM Console for testing VM
-            3. Make sure the console accepts commands
-            4. Make sure the characters are visible
-        expectedResults:
-            1. Catalog ordered; VM provisioned
             3. Console accepts characters
             4. Characters not garbled; no visual defect
     """
