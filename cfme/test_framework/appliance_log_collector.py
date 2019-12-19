51eef713de697701ac275dc44efeddf9a4781322
Jaroslav Henner <jhenner@redhat.com>
2019-12-19 15:51:46 +0100

    Make appliance_log_collector able to fetch logs non-default appliances.
    Register a pytest_collect_logs hook that when called, the logs will get
    collected when --collect-log is specified.
    
    Future work: remove the numerous appliance fixtures by creating
    appliance_factory fixture:
    https://docs.pytest.org/en/latest/fixture.html#factories-as-fixtures

--- cfme/test_framework/appliance_log_collector.py
+++ cfme/test_framework/appliance_log_collector.py
@@ -32,50 +32,75 @@ def pytest_addoption(parser):
                            'shutdown.  Configured via log_collector in env.yaml'))
 
 
+def pytest_addhooks(pluginmanager):
+    """ This example assumes the hooks are grouped in the 'hooks' module. """
+    pluginmanager.add_hookspecs(CollectLogsHookSpecs)
+
+
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_unconfigure(config):
     yield  # since hookwrapper, let hookimpl run
-    if config.getoption('--collect-logs'):
-        logger.info('Starting log collection on appliances')
-        log_files = DEFAULT_FILES
-        local_dir = DEFAULT_LOCAL
-        try:
-            log_files = env.log_collector.log_files
-        except (AttributeError, KeyError):
-            logger.info('No log_collector.log_files in env, use default files: %s', log_files)
-            pass
-        try:
-            local_dir = log_path.join(env.log_collector.local_dir)
-        except (AttributeError, KeyError):
-            logger.info('No log_collector.local_dir in env, use default local_dir: %s', local_dir)
-            pass
+    from cfme.test_framework.appliance import PLUGIN_KEY
+    holder = config.pluginmanager.get_plugin(PLUGIN_KEY)
+    if holder is None:
+        # No appliances to fetch logs from
+        logger.warning('No logs to collect, appliance holder is empty')
+        return
 
-        # Handle local dir existing
-        local_dir.ensure(dir=True)
-        from cfme.test_framework.appliance import PLUGIN_KEY
-        holder = config.pluginmanager.get_plugin(PLUGIN_KEY)
-        if holder is None:
-            # No appliances to fetch logs from
-            logger.warning('No logs collected, appliance holder is empty')
-            return
+    collect_logs = config.pluginmanager.hook.pytest_collect_logs
+    collect_logs(config=config, appliances=holder.appliances)
 
-        written_files = []
-        for app in holder.appliances:
-            with app.ssh_client as ssh_client:
-                tar_file = 'log-collector-{}.tar.gz'.format(
-                    app.hostname)
-                logger.debug('Creating tar file on app %s:%s with log files %s',
-                             app, tar_file, ' '.join(log_files))
-                # wrap the files in ls, redirecting stderr, to ignore files that don't exist
-                tar_result = ssh_client.run_command(
-                    'tar -czvf {tar} $(ls {files} 2>/dev/null)'
-                    .format(tar=tar_file, files=' '.join(log_files)))
-                try:
-                    assert tar_result.success
-                except AssertionError:
-                    logger.exception('Tar command non-zero RC when collecting logs on %s: %s',
-                                     app, tar_result.output)
-                    continue
-                ssh_client.get_file(tar_file, local_dir.strpath)
-            written_files.append(tar_file)
-        logger.info('Wrote the following files to local log path: %s', written_files)
+
+@pytest.hookimpl
+def pytest_collect_logs(config, appliances):
+    if not config.getoption('--collect-logs'):
+        return
+
+    for app in appliances:
+        try:
+            collect_logs(app)
+        except Exception as exc:
+            logger.error(f"Failed to collect logs: {exc}")
+
+
+def collect_logs(app):
+    log_files = DEFAULT_FILES
+    local_dir = DEFAULT_LOCAL
+    try:
+        log_files = env.log_collector.log_files
+    except (AttributeError, KeyError):
+        logger.info('No log_collector.log_files in env, use default files: %s', log_files)
+        pass
+    try:
+        local_dir = log_path.join(env.log_collector.local_dir)
+    except (AttributeError, KeyError):
+        logger.info('No log_collector.local_dir in env, use default local_dir: %s', local_dir)
+        pass
+
+    # Handle local dir existing
+    local_dir.ensure(dir=True)
+
+    with app.ssh_client as ssh_client:
+        logger.info(f'Starting log collection on appliance {app.hostname}')
+        tar_file = 'log-collector-{}.tar.gz'.format(
+            app.hostname)
+        logger.debug('Creating tar file on app %s:%s with log files %s',
+                    app, tar_file, ' '.join(log_files))
+        # wrap the files in ls, redirecting stderr, to ignore files that don't exist
+        tar_result = ssh_client.run_command(
+            'tar -czvf {tar} $(ls {files} 2>/dev/null)'
+            .format(tar=tar_file, files=' '.join(log_files)))
+        try:
+            assert tar_result.success
+        except AssertionError:
+            raise RuntimeError(
+                f'Tar command non-zero RC when collecting logs on {app}: {tar_result.output}')
+        ssh_client.get_file(tar_file, local_dir.strpath)
+        logger.info('Wrote the following file to %s: %s', local_dir.strpath, tar_file)
+
+
+class CollectLogsHookSpecs:
+    @pytest.hookspec
+    def pytest_collect_logs(config, appliances):
+        logger.warning('Executing the hookspec')
+        pass
