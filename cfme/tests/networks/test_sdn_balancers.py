7ba401065d5b1dc79e20e7955d0148bfde824194
Angelina Nikiforova <anikifor@redhat.com>
2020-01-10 09:56:01 +0100

    test_sdn_balancers_tagvis to run only on one azure provider

--- cfme/tests/networks/test_sdn_balancers.py
+++ cfme/tests/networks/test_sdn_balancers.py
@@ -7,6 +7,7 @@ from cfme.cloud.provider.azure import AzureProvider
 from cfme.cloud.provider.ec2 import EC2Provider
 from cfme.cloud.provider.gce import GCEProvider
 from cfme.exceptions import DestinationNotFound
+from cfme.markers.env_markers.provider import ONE
 from cfme.utils.appliance.implementations.ui import navigate_to
 
 pytestmark = [
@@ -72,7 +73,7 @@ def test_sdn_balancers_detail(provider, network_prov_with_load_balancers):
 
 
 # only one provider is needed for that test, used Azure as it has balancers
-@pytest.mark.provider([AzureProvider], scope='module')
+@pytest.mark.provider([AzureProvider], scope='module', selector=ONE)
 @test_requirements.tag
 @pytest.mark.parametrize('visibility', [True, False], ids=['visible', 'notVisible'])
 def test_sdn_balancers_tagvis(check_item_visibility, visibility, network_prov_with_load_balancers):
