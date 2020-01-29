1a5067d35919f320801e536afa51e83c103f8ab4
Mayuri Lahane <mlahane@redhat.com>
2020-01-29 19:14:54 +0530

    Updated with requested changes

--- cfme/tests/cloud_infra_common/test_relationships.py
+++ cfme/tests/cloud_infra_common/test_relationships.py
@@ -407,12 +407,13 @@ def test_change_network_security_groups_per_page_items(setup_provider, appliance
     """
     view = navigate_to(provider, "NetworkSecurityGroup")
     view.toolbar.view_selector.select('List View')
-    view.paginator.set_items_per_page(1000)
-    total_count = len(view.entities.get_all())
+    total_count = len(view.entities.get_all(surf_pages=True))
     for item_count in [5, 10, 20, 50, 100, 200, 500, 1000]:
         if item_count <= total_count:
             view.paginator.set_items_per_page(item_count)
             assert len(view.entities.get_all()) <= view.paginator.items_per_page
+        else:
+            break
 
 
 @pytest.fixture(scope="function")
