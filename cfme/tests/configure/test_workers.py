0336fe9293c2b9af063d85771b1c027df88a7e16
Pavel Šimovec <52415835+psimovec@users.noreply.github.com>
2020-02-18 18:30:31 +0100

    [1LP][RFR] Automate remaining test_set_memory_threshold test cases & check in advanced settings (#9897)
    * Automate test_memory_thresholds_workers_tab - BZ 1658377
    
    * automate test_set_memory_threshold in advanced settings
    
    * 5.10
    
    * removed _view fixture optimization

--- cfme/tests/configure/test_workers.py
+++ cfme/tests/configure/test_workers.py
@@ -2,24 +2,42 @@ from collections import namedtuple
 
 import pytest
 
+from cfme import test_requirements
 from cfme.utils.appliance.implementations.ui import navigate_to
+from cfme.utils.blockers import BZ
 from cfme.utils.wait import wait_for
 
 pytestmark = [pytest.mark.rhel_testing]
 
-Dropdown = namedtuple('Dropdown', 'dropdown id')
+# paths in advanced settings yaml where to put their memory_threshold
+# starting in worker_base and '*' means worker itself
+BASE_PATH = ['*']
+QUEUE_WORKER_PATH = ['queue_worker_base', '*']
+QUEUE_WORKER_DEFAULTS_PATH = QUEUE_WORKER_PATH + ['defaults']
 
-DROPDOWNS = [
-    Dropdown('generic_worker_threshold', 'generic'),
-    Dropdown('cu_data_collector_worker_threshold', 'cu_data_coll'),
-    Dropdown('event_monitor_worker_threshold', 'event_monitor'),
-    Dropdown('connection_broker_worker_threshold', 'conn_broker'),
-    Dropdown('reporting_worker_threshold', 'reporting'),
-    Dropdown('web_service_worker_threshold', 'web_service'),
-    Dropdown('priority_worker_threshold', 'priority'),
-    Dropdown('cu_data_processor_worker_threshold', 'cu_data_proc'),
-    Dropdown('refresh_worker_threshold', 'refresh'),
-    Dropdown('vm_analysis_collectors_worker_threshold', 'vm_analysis')
+Worker = namedtuple('Worker', 'dropdown id advanced path')
+
+WORKERS = [
+    Worker('generic_worker_threshold', 'generic',
+           'generic_worker', QUEUE_WORKER_PATH),
+    Worker('cu_data_collector_worker_threshold', 'cu_data_coll',
+           'ems_metrics_collector_worker', QUEUE_WORKER_DEFAULTS_PATH),
+    Worker('event_monitor_worker_threshold', 'event_monitor',
+           'event_catcher', BASE_PATH),
+    Worker('connection_broker_worker_threshold', 'conn_broker',
+           'vim_broker_worker', BASE_PATH),
+    Worker('reporting_worker_threshold', 'reporting',
+           'reporting_worker', QUEUE_WORKER_PATH),
+    Worker('web_service_worker_threshold', 'web_service',
+           'web_service_worker', BASE_PATH),
+    Worker('priority_worker_threshold', 'priority',
+           'priority_worker', QUEUE_WORKER_PATH),
+    Worker('cu_data_processor_worker_threshold', 'cu_data_proc',
+           'ems_metrics_processor_worker', QUEUE_WORKER_PATH),
+    Worker('refresh_worker_threshold', 'refresh',
+           'ems_refresh_worker', QUEUE_WORKER_DEFAULTS_PATH),
+    Worker('vm_analysis_collectors_worker_threshold',
+           'vm_analysis', 'smart_proxy_worker', QUEUE_WORKER_PATH)
 ]
 
 
@@ -45,9 +63,60 @@ def test_restart_workers(appliance):
              message="Wait for all original workers are back online")
 
 
+v510 = {
+    "1.1.gigabytes": 1178599424,
+    1178599424: "1.1.gigabytes",
+    "1.2.gigabytes": 1283457024,
+    1283457024: "1.2.gigabytes"
+}
+
+
+def set_memory_threshold_in_ui(appliance, worker, new_threshold):
+    view = navigate_to(appliance.server, 'Workers')
+    view.browser.refresh()
+    mem_threshold = getattr(view.workers, worker.dropdown)
+    if appliance.version < '5.11' and new_threshold in v510:
+        new_threshold = v510[new_threshold]
+    mem_threshold.select_by_visible_text(new_threshold.replace('.gigabytes', ' GB'))
+    view.workers.save.click()
+    return new_threshold
+
+
+def get_memory_threshold_in_advanced_settings(appliance, worker):
+    worker_base = appliance.server.advanced_settings
+    loc = worker_base
+    steps = (['workers', 'worker_base'] +
+             [step if step != '*' else worker.advanced for step in worker.path])
+    for step in steps:
+        worker_base = loc
+        loc = loc.get(step)
+    return loc.get('memory_threshold', worker_base['defaults']['memory_threshold'])
+
+
+def set_memory_threshold_in_advanced_settings(appliance, worker, new_threshold):
+    steps = (['workers', 'worker_base'] +
+             [step if step != '*' else worker.advanced for step in worker.path])
+    patch = {'memory_threshold': new_threshold}
+    for step in steps[::-1]:
+        patch = {step: patch}
+    appliance.server.update_advanced_settings(patch)
+    if appliance.version < '5.11' and new_threshold in v510:
+        new_threshold = v510[new_threshold]
+    return new_threshold
+
+
+@test_requirements.settings
 @pytest.mark.tier(2)
-@pytest.mark.parametrize("dropdown", [x.dropdown for x in DROPDOWNS], ids=[x.id for x in DROPDOWNS])
-def test_set_memory_threshold_in_ui(appliance, dropdown):
+@pytest.mark.meta(blockers=[BZ(1787350),
+                            BZ(1799443, unblock=lambda worker, set_memory_threshold:
+                            worker.path != QUEUE_WORKER_DEFAULTS_PATH or
+                            set_memory_threshold != set_memory_threshold_in_ui)],
+                  automates=[1658373, 1715633])
+@pytest.mark.parametrize("set_memory_threshold",
+                         [set_memory_threshold_in_ui, set_memory_threshold_in_advanced_settings],
+                         ids=["in_UI", "in_advanced_setting"])
+@pytest.mark.parametrize("worker", WORKERS, ids=[x.id for x in WORKERS])
+def test_set_memory_threshold(appliance, worker, request, set_memory_threshold):
     """
     Bugzilla:
         1656873
@@ -57,30 +126,37 @@ def test_set_memory_threshold_in_ui(appliance, dropdown):
         casecomponent: WebUI
         caseimportance: medium
         initialEstimate: 1/6h
-        testSteps:
-            1. Navigate to Configuration
-            2. Select the workers tab
-            3. Set memory threshold of any dropdown to a value above 1 GB
-            4. Hit save
-        expectedResults:
-            1.
-            2.
-            3.
-            4. the change should be reflected in the dropdown
     """
     view = navigate_to(appliance.server, 'Workers')
-    mem_threshold = getattr(view.workers, dropdown)
-    before = mem_threshold.selected_option
-    change = "1.1 GB"
-    if before == change:
-        change = "1.2 GB"
-    mem_threshold.select_by_visible_text(change)
-    view.workers.save.click()
-    view.wait_displayed()
-    after = mem_threshold.selected_option
+    before = get_memory_threshold_in_advanced_settings(appliance, worker)
+    threshold_change = "1.1.gigabytes"
+    other_change = "1.2.gigabytes"
+    if set_memory_threshold == set_memory_threshold_in_advanced_settings:
+        threshold_change, other_change = other_change, threshold_change
+    if threshold_change in [before, v510.get(before)]:
+        threshold_change = other_change
+    if appliance.version < "5.11":
+        threshold_change = v510[threshold_change]
+    change = set_memory_threshold(appliance, worker, threshold_change)
+    request.addfinalizer(
+        lambda: set_memory_threshold_in_advanced_settings(appliance, worker, before)
+    )
 
-    assert before != after
-    assert after == change
+    def _ui_check():
+        view.browser.refresh()
+        mem_threshold = getattr(view.workers, worker.dropdown)
+        after = mem_threshold.selected_option
+        return after.startswith(change.replace(".gigabytes", " GB"))
+    wait_for(_ui_check, delay=0, timeout=45)
 
-    # reset the mem threshold after the test
-    view.workers.reset.click()
+    # check advanced settings
+    change_val = float(change.replace('.gigabytes', ''))
+    mem_threshold_real = get_memory_threshold_in_advanced_settings(appliance, worker)
+    MESSAGE = "memory threshold have changed incorrectly in advanced settings"
+    if appliance.version >= "5.11":
+        assert mem_threshold_real == f"{change_val}.gigabytes", MESSAGE
+    else:
+        GB = 2 ** 30
+        expected_value = change_val * GB
+        # this tests if memory threshold has changed and is approximately correct
+        assert abs(mem_threshold_real - expected_value) < expected_value * 0.01, MESSAGE
