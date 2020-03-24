421dee7188983b35e346504f9d9b51ffbcc6c981
nachandr <nachandr@redhat.com>
2020-03-24 16:36:33 -0500

    Parametrize correct fixture on transformation_method for v2v

--- cfme/tests/v2v/test_v2v_smoke.py
+++ cfme/tests/v2v/test_v2v_smoke.py
@@ -31,7 +31,7 @@ pytestmark = [
 ]
 
 
-@pytest.mark.parametrize("mapping_data_vm_obj_single_datastore",
+@pytest.mark.parametrize("v2v_provider_setup",
                          ['SSH', 'VDDK'], indirect=True)
 @pytest.mark.parametrize(
     "source_type, dest_type, template_type",
