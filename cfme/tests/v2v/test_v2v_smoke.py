6bd2eeec0e94fa70e828c57a6da5ed149cc8d147
nachandr <nachandr@redhat.com>
2020-03-25 15:16:48 -0500

    Parametrize correct fixture on transformation_method for v2v

--- cfme/tests/v2v/test_v2v_smoke.py
+++ cfme/tests/v2v/test_v2v_smoke.py
@@ -39,12 +39,13 @@ pytestmark = [
         ["nfs", "nfs", Templates.RHEL7_MINIMAL]
     ]
 )
+@pytest.mark.meta(automates=[1815046])
 def test_single_vm_migration_with_ssh_and_vddk(request, appliance, provider,
                                                source_type, dest_type, template_type,
                                                mapping_data_vm_obj_single_datastore):
     """
     Polarion:
-        assignee: sshveta
+        assignee: nachandr
         caseimportance: high
         caseposneg: positive
         testtype: functional
