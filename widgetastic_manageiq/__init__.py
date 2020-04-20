647ab2b1d95a4265591e7d17c535c4fdcb0d81f9
nachandr <nachandr@redhat.com>
2020-04-20 17:42:21 -0500

    Updating set_conversion_host_api() for 5.11.5

--- widgetastic_manageiq/__init__.py
+++ widgetastic_manageiq/__init__.py
@@ -4754,14 +4754,7 @@ class MigrationPlansList(Widget):
     """Represents the list of Migration Plans."""
 
     ROOT = ParametrizedLocator(".//div[contains(@class,{@list_class|quote})]")
-    ITEM_LOCATOR = VersionPick(
-        {
-            Version.lowest(): ParametrizedLocator(
-                './/div[contains(@class,"{@list_class}__list-item")]'
-            ),
-            "5.11": ParametrizedLocator('.//tr[contains(@class,"{@list_class}__list-item")]'),
-        }
-    )
+    ITEM_LOCATOR = ParametrizedLocator('.//*[contains(@class,"{@list_class}__list-item")]')
     # ITEM_TEXT_LOCATOR helps locate name of the Migration plan. ITEM_LOCATOR.text does not suffice.
     # Also, ITEM_LOCATOR does not yield element which can be clicked to navigate to details page.
     ITEM_TEXT_LOCATOR = './/div[contains(@class,"list-group-item-heading")]'
