e05ee3f2539caf30f24e888a76c1db8017b79dce
mnadeem92 <mnadeem@redhat.com>
2020-04-27 11:19:15 +0530

    Adding test coverage for bugzilla
    Signed-off-by: mnadeem92 <mnadeem@redhat.com>

--- cfme/tests/v2v/test_v2v_manual.py
+++ cfme/tests/v2v/test_v2v_manual.py
@@ -194,3 +194,130 @@ def test_osp_migration_request_details_page_shows_vms_for_not_started_plans():
             3. Details page shows VM
     """
     pass
+
+
+@pytest.mark.ignore_stream('5.10')
+@pytest.mark.meta(coverage=[1810091])
+@pytest.mark.tier(2)
+def test_detection_non_uci_vm():
+    """
+    title: Test the non uci vm should not detect while configuring conversion host
+
+    Bugzilla:
+        1810091
+    Polarion:
+        assignee: mnadeem
+        casecomponent: V2V
+        initialEstimate: 1/8h
+        startsin: 5.11
+        testSteps:
+            1. Open Tab for configure UCI vm via GUI
+        expectedResults:
+            1. Check VM in power-off state should not visible
+    """
+    pass
+
+
+@pytest.mark.ignore_stream('5.10')
+@pytest.mark.meta(coverage=[1804263])
+@pytest.mark.tier(2)
+def test_mapping_non_admin_project():
+    """
+    title: Test mapping creation by selecting non-admin project
+
+    Bugzilla:
+        1804263
+    Polarion:
+        assignee: mnadeem
+        casecomponent: V2V
+        initialEstimate: 1/8h
+        startsin: 5.11
+        testSteps:
+            1. Click mapping creation widget
+            2. Select non-admin project
+        expectedResults:
+            1.
+            2. Mapping should create successfully
+    """
+    pass
+
+
+@pytest.mark.ignore_stream('5.10')
+@pytest.mark.meta(coverage=[1719266])
+@pytest.mark.tier(2)
+def test_vmware_host_wrong_cred():
+    """
+    title: Test UI throws proper error message for wrong vmware credentials
+
+    Bugzilla:
+        1719266
+    Polarion:
+        assignee: mnadeem
+        casecomponent: V2V
+        initialEstimate: 1/8h
+        startsin: 5.11
+        testSteps:
+            1. Add VMware provider with wrong host credential
+            2. Start migration
+        expectedResults:
+            1.
+            2. The GUI throws proper error message for wrong host credential
+    """
+    pass
+
+
+@pytest.mark.ignore_stream('5.10')
+@pytest.mark.meta(coverage=[1760040])
+@pytest.mark.tier(2)
+def test_vm_validation_warm_migration():
+    """
+    title: Test vm validation for warm migration
+
+    Bugzilla:
+        1760040
+    Polarion:
+        assignee: mnadeem
+        casecomponent: V2V
+        initialEstimate: 1/8h
+        startsin: 5.11
+            testSteps:
+            1. Open migration widget
+            2. Select vm with snapshots
+        expectedResults:
+            1.
+            2. The GUI will shows the validation message.
+    """
+    pass
+
+
+@pytest.mark.ignore_stream('5.10')
+@pytest.mark.meta(coverage=[1809035])
+@pytest.mark.tier(2)
+def test_cancel_migration_state_file_missing():
+    """
+    title:  Test migration should not stuck and properly gets cancelled if
+    virt-v2v-wrapper state file doesn't exist
+
+    Bugzilla:
+        1809035
+    Polarion:
+        assignee: mnadeem
+        casecomponent: V2V
+        initialEstimate: 1/8h
+        startsin: 5.11
+        testSteps:
+            1. Create a CloudForms appliance with region 0, say cf1.example.com
+            2. Configuration the conversion host in cf1.example.com
+            3. Run a migration on cf1.example.com, the pod will be called conversion-1
+            4. Create another appliance with region 0, say cf.example.com
+            5. Configuration the conversion host in cf2.example.com
+            6. Run a migration on cf2.example.com
+        expectedResults:
+            1.
+            2.
+            3.
+            4.
+            5.
+            6. The pod can't be created as conversion-1 name already exist
+    """
+    pass
