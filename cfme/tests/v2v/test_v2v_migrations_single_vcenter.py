a390648ac40f0f1661f36f8e671f71586e88152b
nachandr <nachandr@redhat.com>
2020-05-07 12:52:42 -0500

    Fix test_multi_host_multi_vm_migration

--- cfme/tests/v2v/test_v2v_migrations_single_vcenter.py
+++ cfme/tests/v2v/test_v2v_migrations_single_vcenter.py
@@ -100,9 +100,9 @@ def test_single_vm_migration_power_state_tags_retirement(appliance, provider,
 @pytest.mark.parametrize('source_type, dest_type, template_type',
                          [
                              ['nfs', 'nfs', [Templates.RHEL7_MINIMAL,
-                                             Templates.UBUNTU16_TEMPLATE,
-                                             Templates.RHEL69_TEMPLATE,
-                                             Templates.WIN7_TEMPLATE]
+                                             Templates.RHEL7_MINIMAL,
+                                             Templates.RHEL7_MINIMAL,
+                                             Templates.RHEL7_MINIMAL]
                               ]
                          ])
 def test_multi_host_multi_vm_migration(request, appliance, provider,
@@ -137,14 +137,15 @@ def test_multi_host_multi_vm_migration(request, appliance, provider,
     vms = request_details_list.read()
     # testing multi-host utilization
 
-    def _is_migration_started():
-        for vm in vms:
-            if 'Migrating' not in request_details_list.get_message_text(vm):
-                return False
+    def _is_migration_started(vm):
+        if 'Converting' not in request_details_list.get_message_text(vm):
+            return False
         return True
 
-    wait_for(func=_is_migration_started, message="migration is not started for all VMs, "
-             "be patient please", delay=5, num_sec=5400)
+    for vm in vms:
+        wait_for(func=_is_migration_started, func_args=[vm],
+            message="migration has not started for all VMs", delay=5, num_sec=300)
+
     host_creds = provider.hosts.all()
     hosts_dict = {key.name: [] for key in host_creds}
     for vm in vms:
