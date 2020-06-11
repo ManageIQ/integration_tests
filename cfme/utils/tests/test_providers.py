42e18909e744229cf41881a984bdd3231405f290
Tasos Papaioannou <tpapaioa@redhat.com>
2020-06-11 14:12:43 -0400

    Fix provider parametrization for 'all' selector.

--- cfme/utils/tests/test_providers.py
+++ cfme/utils/tests/test_providers.py
@@ -2,6 +2,7 @@ import pytest
 
 from cfme.cloud.provider.azure import AzureProvider
 from cfme.common.provider import BaseProvider
+from cfme.markers.env_markers.provider import ALL
 from cfme.markers.env_markers.provider import ONE_PER_VERSION
 
 
@@ -17,3 +18,11 @@ def test_provider_fixtures(provider, setup_provider):
     assert provider.exists, f"Provider {provider.name} not found on appliance."
     BaseProvider.clear_providers()
     assert not provider.exists, f"Provider {provider.name} not deleted from appliance."
+
+
+@pytest.mark.provider([BaseProvider], selector=ALL)
+def test_provider_all_selector(request, provider):
+    """Verify that the 'all' selector works, and that the provider matches the parametrization."""
+    expected_id = f"test_provider_all_selector[{provider.type}-{provider.version}-{provider.key}]"
+    test_id = request.node.name
+    assert test_id == expected_id, f"Provider parametrization failed."
