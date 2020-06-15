37216d97287493aa33e6c4186d6b25a7578fa394
PJ Richardson <prichard@redhat.com>
2020-06-15 10:22:44 -0400

    Remove manual test and add markets to automated version

--- cfme/tests/infrastructure/test_snapshot.py
+++ cfme/tests/infrastructure/test_snapshot.py
@@ -463,16 +463,19 @@ def test_snapshot_history_btn(create_vm, provider):
     assert snapshot_view.is_displayed
 
 
+@pytest.mark.tier(1)
+@pytest.mark.meta(automates=[1395116])
 def test_snapshot_link_after_delete(create_vm, provider):
     """Tests snapshot link and history button after delete
     Metadata:
         test_flag: snapshot
-
     Polarion:
         assignee: prichard
         casecomponent: Infra
         caseimportance: medium
         initialEstimate: 1/6h
+    Bugzilla:
+        1395116
     """
     has_name = not provider.one_of(RHEVMProvider)
     snapshot1 = new_snapshot(create_vm, has_name=has_name)
@@ -480,7 +483,7 @@ def test_snapshot_link_after_delete(create_vm, provider):
     snapshot2 = new_snapshot(create_vm, has_name=has_name)
     snapshot2.create()
     snapshot1.delete()
-    # snap 1 will be the active so we'd be testing a negative case for rhv(rhv should not allow
+    # snap 2 will be the active so we'd be testing a negative case for rhv(rhv should not allow
     # deletion of active snap).
     snapshot_view = navigate_to(create_vm, 'SnapshotsAll')
     back_to_vm_item = f'VM and Instance "{create_vm.name}"'
@@ -715,44 +718,6 @@ def test_snapshot_tree_view_functionality():
                snapshot is the last one created
     Bugzilla:
         1398239
-    """
-    pass
-
-
-@pytest.mark.manual
-@pytest.mark.provider([VMwareProvider])
-@pytest.mark.tier(1)
-@pytest.mark.meta(coverage=[1395116])
-def test_snapshot_link_after_deleting_snapshot():
-    """
-    test snapshot link in vm summary page after deleting snapshot
-    Have a vm, create couple of snapshots. Delete one snapshot. From the
-    vm summary page use the history button and try to go back to
-    snapshots. Go to the vm summary page again and try to click snapshots
-    link, it should work.
-
-    Polarion:
-        assignee: prichard
-        casecomponent: Infra
-        caseimportance: medium
-        initialEstimate: 1/6h
-        setup:
-            1. Add vmware provider
-            2. Create testing VM
-        testSteps:
-            1. Create two snapshots
-            2. Delete one snapshot
-            3. Use history button to navigate back to VM summary page
-            4. From the vm summary page use the history button and try to go back to snapshots.
-            5. From vm summary page click the snapshot link
-        expectedResults:
-            1. Snapshots successfully created
-            2. Snapshot successfully deleted
-            3. VM summary page displayed
-            4. Snapshots page displayed
-            5. Snapshots page displayed
-    Bugzilla:
-        1395116
     """
     pass
 
