c08899566b9f5205920912e07217d2687fce2b78
dgaikwad <dgaikwad@redhat.com>
2020-07-21 11:34:59 +0530

    Revert "test fixing"
    This reverts commit 44a6e44902abda9de9ac7cc30f75be74813c01ad.

--- cfme/tests/test_login.py
+++ cfme/tests/test_login.py
@@ -49,6 +49,8 @@ def test_login(context, method, appliance):
 @pytest.mark.tier(2)
 @pytest.mark.sauce
 @pytest.mark.parametrize('context', [ViaUI])
+# BZ 1632718 is only relevant for Chrome browser
+@pytest.mark.meta(blockers=[BZ(1632718)])
 def test_bad_password(context, request, appliance):
     """ Tests logging in with a bad password.
 
@@ -65,9 +67,7 @@ def test_bad_password(context, request, appliance):
     user = appliance.collections.users.instantiate(credential=cred, name='Administrator')
 
     with appliance.context.use(context):
-        with pytest.raises(
-            Exception, match="Sorry, the username or password you entered is incorrect."
-        ):
+        with pytest.raises(Exception, match="Login failed: Unauthorized"):
             appliance.server.login(user)
         view = appliance.browser.create_view(LoginPage)
         assert view.password.read() == '' and view.username.read() == ''
