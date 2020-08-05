f520e1c5bf7ac20d055d77215b60f4b958d50d98
Ansari Niyaz Akhtar <nansari@redhat.com>
2020-08-05 20:14:36 +0530

    Automting user view service request details

--- cfme/tests/services/test_service_catalog_dialog_manual.py
+++ cfme/tests/services/test_service_catalog_dialog_manual.py
@@ -27,23 +27,6 @@ def test_notification_banner_vm_provisioning_notification_and_service_request_sh
 
 @pytest.mark.manual
 @pytest.mark.tier(3)
-def test_user_should_be_able_to_see_requests_irrespective_of_tags_assigned():
-    """ User should be able to see requests irrespective of tags assigned
-    Polarion:
-        assignee: nansari
-        casecomponent: Services
-        initialEstimate: 1/4h
-        testtype: functional
-        startsin: 5.9
-        tags: service
-    Bugzilla:
-        1641012
-    """
-    pass
-
-
-@pytest.mark.manual
-@pytest.mark.tier(3)
 def test_changing_action_order_in_catalog_bundle_should_not_removes_resource():
     """
     Polarion:
