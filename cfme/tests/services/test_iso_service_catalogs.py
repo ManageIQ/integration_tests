a937fade6522892721d8de40cb6798b910b56a47
Jaroslav Henner <jhenner@redhat.com>
2020-08-10 12:30:32 +0200

    Do the (un)blocking bazed on forced_streams + unblock

--- cfme/tests/services/test_iso_service_catalogs.py
+++ cfme/tests/services/test_iso_service_catalogs.py
@@ -7,7 +7,7 @@ from cfme.infrastructure.provider import InfraProvider
 from cfme.infrastructure.pxe import get_template_from_config
 from cfme.infrastructure.pxe import ISODatastore
 from cfme.services.service_catalogs import ServiceCatalogs
-from cfme.utils.blockers import Blocker
+from cfme.utils.blockers import BZ
 from cfme.utils.generators import random_vm_name
 from cfme.utils.log import logger
 from cfme.utils.version import Version
@@ -96,28 +96,9 @@ def catalog_item(appliance, provider, dialog, catalog, provisioning):
     )
 
 
-# There is a Libvirt bug BZ(1783355) on our RHV 4.4. This seem to prevent some tests to run with it.
-# I was about to use this blocker: JIRA('RHCFQE-14575'), but this didn't work because we seem to
-# lack credentials to read Jira tickets. There seemed to be no nicer way how to mark them other than
-# creating EternalBlocker and use the `unblock` kwarg that is handled in special way in the
-# `blockers` metaplugin to block only the tests executed against the RHV 4.4.
-
-
-class EternalBlocker(Blocker):
-    def blocks(self):
-        return True
-
-    # This needs to be defined, otherwise we are getting some exception.
-    def url(self):
-        return None
-
-
 @test_requirements.rhev
-@pytest.mark.meta(
-    blockers=[
-        EternalBlocker(unblock=lambda provider: provider.version < Version("4.4"))
-    ]
-)
+@pytest.mark.meta(blockers=[BZ(1783355, forced_streams=["5.11", "5.10"],
+                               unblock=lambda provider: provider.version != Version("4.4"))])
 def test_rhev_iso_servicecatalog(appliance, provider, setup_provider, setup_iso_datastore,
                                  catalog_item, request):
     """Tests RHEV ISO service catalog
