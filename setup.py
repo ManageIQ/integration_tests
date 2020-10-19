25323c5e5c0528f86c164d10d778072d8aed7b87
Mike Shriver <mshriver@redhat.com>
2020-10-19 14:52:08 -0400

    Enforce py3.7 as minimum

--- setup.py
+++ setup.py
@@ -14,4 +14,4 @@ elif ALLOWED_COMMANDS.intersection(sys.argv):
 else:
     sys.exit("this is a hack, use pip install -e")
 
-setup(setup_requires=["setuptools_scm>=3.0.0"], use_scm_version=True)
+setup(setup_requires=["setuptools_scm>=3.0.0"], use_scm_version=True, python_requires="~=3.7")
